1a1e17dba3560d2bf7b39fbde38a3e523583e3b8
widal001 <williamdaly422@gmail.com>
2023-05-30 13:01:38 -0400

    feat: Adds pydantic schemas for milestones
    These schemas will be used to parse the yaml file which contains the
    milestone summary as well as to generate the diagram and markdown files

---
+++ milestone-cli/milestone_cli/schemas.py
@@ -0,0 +1,69 @@
+from pydantic import BaseModel
+
+
+class MilestoneBase(BaseModel):
+    """Contains fields shared by Milestone and Section"""
+
+    heading: str
+    diagram_name: str
+    description: str
+    key: str | None = None
+
+
+class Milestone(MilestoneBase):
+    """Contains a summary of details about a project milestone"""
+
+    status: str
+    section: str | None = None
+    dependencies: list[str] | None = None
+
+
+class MilestoneSection(MilestoneBase):
+    """Represents a group of project milestones"""
+
+    milestones: dict[str, Milestone]
+
+
+class Dependency(BaseModel):
+    """Documents a (downstream) milestone's dependency on an upstream milestone"""
+
+    upstream: Milestone
+    downstream: Milestone
+
+
+class MilestoneSummary(BaseModel):
+    """Stores the list of milestones and the sections they belong to"""
+
+    version: str
+    sections: dict[str, MilestoneSection]
+
+    def map_dependencies(self, milestone: Milestone) -> list[Dependency] | None:
+        """Split milestone dependencies into a list of Dependency objects"""
+        if not milestone.dependencies:
+            return None
+        deps = []
+        for parent in milestone.dependencies:
+            upstream = self.milestones.get(parent)
+            if not upstream:
+                raise KeyError  # TODO: Change to custom error type
+            deps.append(Dependency(upstream=upstream, downstream=milestone))
+        return deps
+
+    @property
+    def dependencies(self) -> list[Dependency]:
+        """Returns a list of Dependency objects across all milestones"""
+        results = []
+        for milestone in self.milestones.values():
+            dependencies = self.map_dependencies(milestone)
+            if dependencies:
+                results.extend(dependencies)
+        return results
+
+    @property
+    def milestones(self) -> dict[str, Milestone]:
+        """Returns a combined list of Milestone objects"""
+        return {
+            milestone: details
+            for section in self.sections.values()
+            for milestone, details in section.milestones.items()
+        }
---
+++ milestone-cli/tests/test_schemas.py
@@ -0,0 +1,113 @@
+from copy import deepcopy
+import pytest
+
+from milestone_cli.schemas import (
+    Dependency,
+    Milestone,
+    MilestoneSection,
+    MilestoneSummary,
+)
+
+
+@pytest.fixture(scope="module", name="summary")
+def summary_fixture():
+    """Creates a sample MilestoneSummary fixture for tests"""
+    sections = {
+        "project-goals": MilestoneSection(
+            heading="Project Goals",
+            diagram_name="Project Goals",
+            description="Project goals description",
+            milestones={
+                "define-goals": Milestone(
+                    heading="Define Goals",
+                    diagram_name="Define-Goals",
+                    description="Goals description",
+                    status="executing",
+                ),
+                "measurement-strategy": Milestone(
+                    key="measurement-strategy",
+                    heading="Measurement Strategy",
+                    diagram_name="Measurement-Strategy",
+                    description="Measurement description",
+                    status="executing",
+                    dependencies=["define-goals"],
+                ),
+            },
+        ),
+        "communications-tooling": MilestoneSection(
+            heading="Communications tooling",
+            diagram_name="Communications Tooling",
+            description="Project goals description",
+            milestones={
+                "comms-platform": Milestone(
+                    heading="Communications Platform",
+                    diagram_name="Comms-Platform",
+                    description="Comms platform description",
+                    status="planning",
+                    section="communications-tooling",
+                    dependencies=["define-goals", "measurement-strategy"],
+                ),
+            },
+        ),
+    }
+    return MilestoneSummary(version="0.1.0", sections=sections)
+
+
+class TestDependencies:
+    """Tests the MilestoneSummary.dependencies hybrid property"""
+
+    def test_returns_list_of_dependency_objects(
+        self,
+        summary: MilestoneSummary,
+    ) -> None:
+        """Should return a list of Depdency objects"""
+        # execution
+        output = summary.dependencies
+        # validation
+        assert isinstance(output[0], Dependency)
+
+    def test_returns_correct_dependencies(
+        self,
+        summary: MilestoneSummary,
+    ) -> None:
+        """Should return a separate Dependency object for each dependency"""
+        # setup
+        goals = summary.milestones.get("define-goals")
+        measure = summary.milestones.get("measurement-strategy")
+        comms = summary.milestones.get("comms-platform")
+        expected = [
+            Dependency(upstream=goals, downstream=measure),
+            Dependency(upstream=goals, downstream=comms),
+            Dependency(upstream=measure, downstream=comms),
+        ]
+        # execution
+        output = summary.dependencies
+        # validation
+        assert len(output) == len(expected)
+        for dependency in expected:
+            assert dependency in output
+
+    def test_return_empty_list_if_no_deps(
+        self,
+        summary: MilestoneSummary,
+    ) -> None:
+        """Should return None if none of the milestones have dependencies"""
+        # setup - Set all dependencies to None
+        new_summary = deepcopy(summary)
+        for milestone in new_summary.milestones.values():
+            milestone.dependencies = None
+        # execution
+        output = new_summary.dependencies
+        # validation
+        assert output == []
+
+
+class TestGetMilestonesBySection:
+    """Tests the MilestoneSummary.get_milestones_by_section() method"""
+
+    def test_returns_list_of_milestones(
+        self,
+        summary: MilestoneSummary,
+    ) -> None:
+        """Should return a list of Milestone objects"""
+        assert 1
