b8922fd33bc0b40122527d71416ff51932507b91
widal001 <billy.daly@agile6.com>
2023-07-21 11:09:02 -0400

    refactor: Renames metrics to analytics
    Changes the name of the metrics sub-directory and package to analytics

--- analytics/tests/test_version.py
+++ analytics/tests/test_version.py
@@ -1,6 +1,6 @@
-import metrics
+import analytics
 
 
 def test_package_import_and_name():
     """The package should be imported and named correctly"""
-    assert metrics.__name__ == "metrics"
+    assert analytics.__name__ == "analytics"
