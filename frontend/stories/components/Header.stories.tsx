b71e4e5db32e15c4860f2f6b2e0b7816aa07ab9f
Daphne Gold <daphnegold@navapbc.com>
2023-07-26 16:22:55 -0700

    Fill out stories

--- frontend/stories/components/Header.stories.tsx
+++ frontend/stories/components/Header.stories.tsx
@@ -8,17 +8,22 @@ const meta: Meta<typeof Header> = {
 };
 export default meta;
 
-/**
- * Below is an example of using Storybook's `args` feature to render
- * different versions of the same component.
- * @see https://storybook.js.org/docs/react/writing-stories/args
- */
-export const Preview1 = {
-  args: {
-  },
+export const Default = {
 };
 
-export const Preview2 = {
+export const WithProps = {
   args: {
-  },
+    logoPath: '/img/logo.svg',
+    primaryLinks: [
+      {
+        i18nKey: "nav_link_home",
+        href: "/",
+      },
+      {
+        i18nKey: "nav_link_health",
+        href: "/health",
+      },
+    ],
+    showMenu: true
+  }
 };
