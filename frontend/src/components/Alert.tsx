4695231ba49045c8110d15b15736eb056a468b60
Sammy <sammysteiner@navapbc.com>
2023-08-01 16:30:54 -0400

    [Issue 360] Static Site Alert Component (#366)
    ---------
    Co-authored-by: Daphne Gold <daphnegold@navapbc.com>
    Co-authored-by: acouch <acinternets@gmail.com>

---
+++ frontend/src/components/Alert.tsx
@@ -0,0 +1,16 @@
+import { Alert as USWDSAlert } from "@trussworks/react-uswds";
+
+type Props = {
+  type: "success" | "warning" | "error" | "info";
+  children: React.ReactNode;
+};
+
+const Alert = ({ type, children }: Props) => {
+  return (
+    <USWDSAlert type={type} headingLevel="h4" slim>
+      {children}
+    </USWDSAlert>
+  );
+};
+
+export default Alert;
