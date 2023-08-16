5e0e25e2417a437898a8620d0188ddce0462558e
Daphne Gold <daphnegold@navapbc.com>
2023-08-16 15:39:46 -0700

    Fix format

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -7,12 +7,18 @@ const Hero = () => {
   });
 
   return (
-    <div data-testid="hero" className="bg-primary desktop:padding-y-15 tablet:padding-y-2 padding-y-1">
+    <div
+      data-testid="hero"
+      className="bg-primary desktop:padding-y-15 tablet:padding-y-2 padding-y-1"
+    >
       <GridContainer>
-          <h1 className="text-white desktop:font-sans-3xl tablet:font-sans-2xl font-sans-xl">{t("title")}</h1>
-          <div className="desktop:font-sans-xl tablet:font-sans-lg font-sans-md">
-            <span className="text-yellow text-bold">{t("beta")}</span>&nbsp;<span className="text-white">{t("content")}</span>
-          </div>
+        <h1 className="text-white desktop:font-sans-3xl tablet:font-sans-2xl font-sans-xl">
+          {t("title")}
+        </h1>
+        <div className="desktop:font-sans-xl tablet:font-sans-lg font-sans-md">
+          <span className="text-yellow text-bold">{t("beta")}</span>&nbsp;
+          <span className="text-white">{t("content")}</span>
+        </div>
       </GridContainer>
     </div>
   );
