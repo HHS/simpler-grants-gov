1198af75de3a6dcfecff84d163816f7a937bdd9e
Daphne Gold <daphnegold@navapbc.com>
2023-08-16 15:03:05 -0700

    Remove trailing comma

--- frontend/tests/components/Hero.test.tsx
+++ frontend/tests/components/Hero.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import Hero from "src/components/Hero";
 
-describe("Alert", () => {
+describe("Hero", () => {
   it("Renders without errors", () => {
     render(<Hero />);
     const hero = screen.getByTestId("hero");
