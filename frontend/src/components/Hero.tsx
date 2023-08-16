2846369afedab96cb804ad4501d886cf69857912
Daphne Gold <daphnegold@navapbc.com>
2023-08-16 10:23:47 -0700

    Hero files, strings

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "next-i18next";
+import {
+  Grid,
+  GridContainer,
+} from "@trussworks/react-uswds";
+
+const Footer = () => {
+  const { t } = useTranslation("common", {
+    keyPrefix: "Footer",
+  });
+
+  return (
+    'applesauce'
+  );
+};
+
+export default Footer;
