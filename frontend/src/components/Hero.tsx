2561ecccaafd65ac7de984a454a55b878f94b7fc
Daphne Gold <daphnegold@navapbc.com>
2023-08-17 11:15:12 -0700

    Change to <p> tag and add usa-intro cause prettier!

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -15,10 +15,10 @@ const Hero = () => {
         <h1 className="text-white desktop:font-sans-3xl tablet:font-sans-2xl font-sans-xl">
           {t("title")}
         </h1>
-        <div className="desktop:font-sans-xl tablet:font-sans-lg font-sans-md">
+        <p className="usa-intro desktop:font-sans-xl tablet:font-sans-lg font-sans-md">
           <span className="text-yellow text-bold">{t("beta")}</span>&nbsp;
           <span className="text-white">{t("content")}</span>
-        </div>
+        </p>
       </GridContainer>
     </div>
   );
