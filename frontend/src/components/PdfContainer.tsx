27a97e8259078a76594366d718c5d807afd03fc6
Sammy Steiner <sammysteiner@navapbc.com>
2023-08-17 11:58:41 -0400

    responsive div only needed for next image fill

--- frontend/src/components/PdfContainer.tsx
+++ frontend/src/components/PdfContainer.tsx
@@ -8,20 +8,18 @@ const PdfContainer = ({ file, image, alt }: pdf) => {
   return (
     <Grid className="margin-bottom-2" tablet={{ col: 6 }} tabletLg={{ col: 3 }}>
       <Link className="padding-0" href={file} target="_blank">
-        <div style={{ position: "relative" }}>
-          <Image
-            alt={alt}
-            className="pdf-card"
-            src={image}
-            height={1290}
-            width={980}
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-            sizes="(max-width: 640px) 100vw, (max-width: 880px) 50vw, 25vw"
-          />
-        </div>
+        <Image
+          alt={alt}
+          className="pdf-card"
+          src={image}
+          height={1290}
+          width={980}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          sizes="(max-width: 640px) 100vw, (max-width: 880px) 50vw, 25vw"
+        />
       </Link>
     </Grid>
   );
