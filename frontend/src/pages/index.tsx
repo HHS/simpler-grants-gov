3280efe21e80efa0accaf6b5d1f06abf52506277
Andy Cochran <andycochran@users.noreply.github.com>
2023-08-18 15:02:41 -0400

    [ISSUE 382] tweak typography (#383)
    * tweak typography sizes and spacing
    
    * remove example index content 
    
    * remove hero from layout, put in index
    
    ---------
    
    Co-authored-by: Sammy Steiner <sammysteiner@navapbc.com>

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,24 +2,18 @@ import type { GetServerSideProps, NextPage } from "next";
 
 import { Trans, useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
 
-import GoalContent from "src/components/GoalContent";
 import FullWidthAlert from "../components/FullWidthAlert";
 import FundingContent from "../components/FundingContent";
+import GoalContent from "../components/GoalContent";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   const { t } = useTranslation("common", { keyPrefix: "Index" });
 
   return (
     <>
-      <Head>
-        <title>{t("title")}</title>
-      </Head>
-      {/* Demonstration of responsive utility classes: */}
-      <h1 className="font-sans-2xl tablet:font-sans-3xl margin-y-3 tablet:margin-top-6">
-        {t("title")}
-      </h1>
+      <Hero />
       <FullWidthAlert type="info">
         <Trans
           t={t}
@@ -31,27 +25,6 @@ const Home: NextPage = () => {
       </FullWidthAlert>
       <GoalContent />
       <FundingContent />
-
-      {/* Demonstration of more complex translated strings, with safe-listed links HTML elements */}
-      <p className="usa-intro">
-        <Trans
-          t={t}
-          i18nKey="intro"
-          components={{
-            LinkToNextJs: <a href="https://nextjs.org/docs" />,
-          }}
-        />
-      </p>
-      <div className="measure-6">
-        <Trans
-          t={t}
-          i18nKey="body"
-          components={{
-            ul: <ul className="usa-list" />,
-            li: <li />,
-          }}
-        />
-      </div>
     </>
   );
 };
