f7c873d4fc06f8ba43b6dcdef5883c5828b1755f
Andy Cochran <andrewthomascochran@gmail.com>
2023-08-18 14:54:54 -0400

    clean up index imports

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,12 +3,10 @@ import type { GetServerSideProps, NextPage } from "next";
 import { Trans, useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import GoalContent from "src/components/GoalContent";
-// import Head from "next/head";
-
-import Hero from "src/components/Hero";
 import FullWidthAlert from "../components/FullWidthAlert";
 import FundingContent from "../components/FundingContent";
+import GoalContent from "../components/GoalContent";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   const { t } = useTranslation("common", { keyPrefix: "Index" });
