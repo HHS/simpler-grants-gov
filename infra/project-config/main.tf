68beff9c3be2b05f17d52c4bb353634b1cd168ce
Daphne Gold <daphnegold@navapbc.com>
2023-08-18 08:49:55 -0700

    Set up project config

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,18 +1,18 @@
 locals {
   # Machine readable project name (lower case letters, dashes, and underscores)
   # This will be used in names of AWS resources
-  project_name = "<PROJECT_NAME>"
+  project_name = "grants-equity"
 
   # Project owner (e.g. navapbc). Used for tagging infra resources.
-  owner = "<OWNER>"
+  owner = "navapbc"
 
   # URL of project source code repository
-  code_repository_url = "<REPO_URL>"
+  code_repository_url = "https://github.com/HHS/grants-equity"
 
   # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1).
   # This is dependent on where your project is located (if regional)
   # otherwise us-east-1 is a good default
-  default_region = "<DEFAULT_REGION>"
+  default_region = "us-east-1"
 
   github_actions_role_name = "${local.project_name}-github-actions"
 }
