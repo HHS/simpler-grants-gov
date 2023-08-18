472b0aa96de90ec57bb218b0b8d6b8b29b96ebc7
Sammy Steiner <sammysteiner@navapbc.com>
2023-08-18 15:30:49 -0400

    format index after merge

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,11 +3,11 @@ import type { GetServerSideProps, NextPage } from "next";
 import { Trans, useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import WtGIContent from "src/components/WtGIContent";
 import FullWidthAlert from "../components/FullWidthAlert";
 import FundingContent from "../components/FundingContent";
 import GoalContent from "../components/GoalContent";
 import Hero from "../components/Hero";
-import WtGIContent from "src/components/WtGIContent";
 
 const Home: NextPage = () => {
   const { t } = useTranslation("common", { keyPrefix: "Index" });
