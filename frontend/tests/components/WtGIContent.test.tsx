662e5f0b6b01d74806340e1f7f9a7210d400634c
Sammy Steiner <sammysteiner@navapbc.com>
2023-08-22 12:30:42 -0400

    test typo

--- frontend/tests/components/WtGIContent.test.tsx
+++ frontend/tests/components/WtGIContent.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import WtGIContent from "src/components/WtGIContent";
 
-describe("Hero", () => {
+describe("Ways to get involved", () => {
   it("Renders without errors", () => {
     render(<WtGIContent />);
     const wtgi = screen.getByTestId("wtgi-content");
