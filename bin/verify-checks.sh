13c8ed07bdaeaab3ed731c2dd1a0d10835e48371
Daphne Gold <daphnegold@navapbc.com>
2023-09-08 20:07:01 -0700

    Back to check-suite but with a call to get check-suite-id from run

--- bin/verify-checks.sh
+++ bin/verify-checks.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-# Required environment variables
-CURRENT_GITHUB_RUN_ID=${CURRENT_GITHUB_RUN_ID:-""}
-SHA=${SHA:-""}
-REPO=${REPO:-""}
+if [ -z "$REPO" ] || [ -z "$SHA" ] || [ -z "$CURRENT_GITHUB_RUN_ID" ]; then
+  echo "Error: Required environment variables are not set."
+  exit 1
+fi
 
-TIMEOUT_MINUTES=6
+TIMEOUT_MINUTES=5
 SLEEP_SECONDS=15
 MAX_ITERATIONS=$(( ($TIMEOUT_MINUTES * 60) / $SLEEP_SECONDS ))
 
 iteration=0
 
-echo "Starting check run status polling for SHA: $SHA."
+echo "Starting check suite status polling for SHA: $SHA."
 echo "Maximum iterations: $MAX_ITERATIONS. Time per iteration: $SLEEP_SECONDS seconds."
+
+# Fetch the current check_suite_id
+CURRENT_CHECK_SUITE_ID=$(gh api repos/$REPO/actions/runs/$CURRENT_GITHUB_RUN_ID --jq '.check_suite_id')
 
 while :; do
   iteration=$(( $iteration + 1 ))
@@ -24,42 +27,40 @@ while :; do
     exit 1
   fi
 
-  echo "Fetching check run details... :)"
-  CHECK_RUN_DETAILS=$(gh api repos/$REPO/commits/$SHA/check-runs)
-  total_count=$(echo "${CHECK_RUN_DETAILS}" | jq '.total_count')
-  echo "Total number of check runs: $total_count"
+  echo "Fetching check suite details... <3"
+  CHECK_SUITE_DETAILS=$(gh api repos/$REPO/commits/$SHA/check-suites)
+  total_count=$(echo "${CHECK_SUITE_DETAILS}" | jq '.total_count')
+  all_suites_passed=true
 
-  all_runs_passed=true
+  echo "Total number of check suites: $total_count"
 
   for (( i=0; i<$total_count; i++ )); do
-    run=$(echo "${CHECK_RUN_DETAILS}" | jq -c ".check_runs[$i]")
-    CHECK_RUN_ID=$(echo "$run" | jq -r '.id')
-    CHECK_RUN_STATUS=$(echo "$run" | jq -r '.status')
-    CHECK_RUN_CONCLUSION=$(echo "$run" | jq -r '.conclusion')
-
-    echo "Debug: CHECK_RUN_ID=$CHECK_RUN_ID, CURRENT_GITHUB_RUN_ID=$CURRENT_GITHUB_RUN_ID"
+    suite=$(echo "${CHECK_SUITE_DETAILS}" | jq -c ".check_suites[$i]")
+    CHECK_SUITE_ID=$(echo "$suite" | jq -r '.id')
+    CHECK_SUITE_STATUS=$(echo "$suite" | jq -r '.status')
+    CHECK_SUITE_CONCLUSION=$(echo "$suite" | jq -r '.conclusion')
 
     # Skip the current GitHub Actions run to prevent the script from waiting on itself to complete.
-    if [[ "$CHECK_RUN_ID" == "$CURRENT_GITHUB_RUN_ID" ]]; then
-      echo "Skipping current run ID: $CURRENT_GITHUB_RUN_ID."
+    if [[ "$CHECK_SUITE_ID" == "$CURRENT_CHECK_SUITE_ID" ]]; then
+      echo "Skipping current check suite ID: $CURRENT_CHECK_SUITE_ID."
       continue
     fi
 
-    echo "Processing check run ID $CHECK_RUN_ID."
+    echo "Processing check suite $(echo "$suite" | jq -r '.id')."
 
-    if [[ "$CHECK_RUN_STATUS" != "completed" ]]; then
-      echo "Check run ID $CHECK_RUN_ID has not yet completed. Status: $CHECK_RUN_STATUS."
-      all_runs_passed=false
+    if [[ "$CHECK_SUITE_STATUS" != "completed" ]]; then
+      echo "Check suite with ID $(echo "$suite" | jq -r '.id') has not yet completed. Status: $CHECK_SUITE_STATUS."
+      all_suites_passed=false
       break
-    elif [[ "$CHECK_RUN_CONCLUSION" != "success" ]]; then
-      all_runs_passed=false
+    elif [[ "$CHECK_SUITE_CONCLUSION" != "success" ]]; then
+      all_suites_passed=false
 
-      case $CHECK_RUN_CONCLUSION in
+      case $CHECK_SUITE_CONCLUSION in
         failure|cancelled|timed_out|action_required|stale|neutral)
-          echo "Check run ID $CHECK_RUN_ID concluded with status $CHECK_RUN_CONCLUSION."
+          echo "Check suite with ID $(echo "$suite" | jq -r '.id') concluded with status $CHECK_SUITE_CONCLUSION."
           ;;
         *)
-          echo "Check run ID $CHECK_RUN_ID concluded with an unknown status."
+          echo "Check suite with ID $(echo "$suite" | jq -r '.id') concluded with an unknown status."
           ;;
       esac
 
@@ -67,8 +68,8 @@ while :; do
     fi
   done
 
-  if [ "$all_runs_passed" = true ]; then
-    echo "All check runs have passed."
+  if [ "$all_suites_passed" = true ]; then
+    echo "All check suites have passed."
     break
   fi
 
