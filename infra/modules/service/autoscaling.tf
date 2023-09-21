371d87febfbbc907c064bbd68814bf1bc8c96796
Daphne Gold <daphnegold@navapbc.com>
2023-09-21 16:14:57 -0700

    Switch strategy to target away from step so we aren't dealing with annoying alarms to scale down

--- infra/modules/service/autoscaling.tf
+++ infra/modules/service/autoscaling.tf
@@ -1,21 +1,3 @@
-resource "aws_cloudwatch_metric_alarm" "cpu_high" {
-  count               = var.enable_autoscaling ? 1 : 0
-  alarm_name          = "cpu-high-${var.service_name}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "30"
-  statistic           = "Average"
-  threshold           = "75"
-  alarm_description   = "Alarm when CPU exceeds 75%"
-  alarm_actions       = [aws_appautoscaling_policy.scale_up[0].arn]
-  dimensions = {
-    ClusterName = aws_ecs_cluster.cluster.name
-    ServiceName = var.service_name
-  }
-}
-
 resource "aws_appautoscaling_target" "ecs_target" {
   count = var.enable_autoscaling ? 1 : 0
 
@@ -26,23 +8,21 @@ resource "aws_appautoscaling_target" "ecs_target" {
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "scale_up" {
+resource "aws_appautoscaling_policy" "ecs_scale_policy_cpu" {
   count = var.enable_autoscaling ? 1 : 0
 
-  name               = "scale-up-${var.service_name}"
-  policy_type        = "StepScaling"
+  name               = "${var.service_name}-ecs-scale-policy-cpu"
+  policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs_target[0].resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target[0].scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs_target[0].service_namespace
 
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = 30
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      metric_interval_lower_bound = 0
-      scaling_adjustment          = 1
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 30
+    target_value       = 75
   }
 }
