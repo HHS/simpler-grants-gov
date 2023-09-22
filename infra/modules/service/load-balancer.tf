dd6c37130b8e9a1b8c6c57675fd87c2c0ebdef44
Sammy Steiner <sammysteiner@navapbc.com>
2023-09-22 15:05:37 -0400

    ssl policy

--- infra/modules/service/load-balancer.tf
+++ infra/modules/service/load-balancer.tf
@@ -100,6 +100,7 @@ resource "aws_lb_listener" "alb_listener_https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.cert_arn
 
   default_action {
