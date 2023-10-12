1183eb9fb9697a8f2728ef018c3552cf8d5d4b3c
Sammy Steiner <sammysteiner@navapbc.com>
2023-10-12 16:17:43 -0400

    curl and healthcheck support

--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -80,8 +80,14 @@ resource "aws_ecs_task_definition" "app" {
         interval = 30,
         retries  = 3,
         timeout  = 5,
-        command = ["CMD-SHELL",
-          "wget --no-verbose --tries=1 --spider http://localhost:${var.container_port}/health || exit 1"
+        command = ["CMD",
+           "curl",
+            "--fail",
+            "-sLo",
+            "/dev/null",
+            "-w",
+            "%%{http_code}",
+          "http://localhost:8000/health"
         ]
       },
       environment = local.environment_variables,
