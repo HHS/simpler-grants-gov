63ef19a60c5d17539b64581b139fd9ca4c33addc
Daphne Gold <daphnegold@navapbc.com>
2023-10-23 16:13:27 -0700

    Delete new api test dir since just using old one with strategy that is working well

--- infra/api-abort/app-config/dev.tf
+++
@@ -1,8 +0,0 @@
-module "dev_config" {
-  source                          = "./env-config"
-  app_name                        = local.app_name
-  default_region                  = module.project_config.default_region
-  environment                     = "dev"
-  has_database                    = local.has_database
-  has_incident_management_service = local.has_incident_management_service
-}
--- infra/api-abort/database/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "environment_name" {
-  type        = string
-  description = "name of the application environment"
-}
