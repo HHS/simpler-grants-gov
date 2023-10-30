cb28ca1b8e3e3e0c67a34fe2de0413d0fb029ed6
Michael Chouinard <chouinar6@gmail.com>
2023-10-30 11:11:41 -0400

    [Issue-617] Rename app and db in docker-compose

--- api/bin/wait-for-local-db.sh
+++ api/bin/wait-for-local-db.sh
@@ -13,7 +13,7 @@ WAIT_TIME=0
 # Use pg_isready to wait for the DB to be ready to accept connections
 # We check every 3 seconds and consider it failed if it gets to 30+
 # https://www.postgresql.org/docs/current/app-pg-isready.html
-until pg_isready -h localhost -d main-db -q;
+until pg_isready -h localhost -d grants-db -q;
 do
   echo "waiting on Postgres DB to initialize..."
   sleep 3
@@ -21,8 +21,8 @@ do
   WAIT_TIME=$(($WAIT_TIME+3))
   if [ $WAIT_TIME -gt $MAX_WAIT_TIME ]
   then
-    echo -e "${RED}ERROR: Database appears to not be starting up, running \"docker logs main-db\" to troubleshoot.${NO_COLOR}"
-    docker logs main-db
+    echo -e "${RED}ERROR: Database appears to not be starting up, running \"docker logs grants-db\" to troubleshoot.${NO_COLOR}"
+    docker logs grants-db
     exit 1
   fi
 done
