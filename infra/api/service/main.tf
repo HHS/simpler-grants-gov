0fc0675076f257d9de25db6a53fa09f7004cd76c
Daphne Gold <daphnegold@navapbc.com>
2023-11-01 15:58:46 -0700

    Infra format

--- infra/api/service/main.tf
+++ infra/api/service/main.tf
@@ -91,8 +91,8 @@ module "service" {
   image_tag             = local.image_tag
   vpc_id                = data.aws_vpc.default.id
   subnet_ids            = data.aws_subnets.default.ids
-  cpu = 512
-  memory = 1024
+  cpu                   = 512
+  memory                = 1024
 
   db_vars = module.app_config.has_database ? {
     security_group_ids         = data.aws_rds_cluster.db_cluster[0].vpc_security_group_ids
