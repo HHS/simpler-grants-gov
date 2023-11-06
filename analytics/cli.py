692431377aa1c41be7154bf0169eb4625538ba4a
widal001 <billy.daly@agile6.com>
2023-11-06 17:13:48 -0500

    refactor: Updates CLI for exporting GitHub data
    Removes export_github_data and changes how the functions to export
    project and issue data are called from cli.py

--- analytics/cli.py
+++ analytics/cli.py
@@ -1,4 +1,32 @@
-from analytics.etl.github import export_github_data
+import argparse
+
+from analytics.etl import github
+
+ISSUE_FILE = "data/issue-data.json"
+PROJECT_FILE = "data/project-data.json"
 
 if __name__ == "__main__":
-    export_github_data()
+    # parse arguments passed to the command line
+    parser = argparse.ArgumentParser(
+        prog="Analytics CLI tool",
+        description="Run analytics pipelines from the command line",
+    )
+    parser.add_argument("--owner")
+    parser.add_argument("--repo")
+    parser.add_argument("--project")
+    # check that the arguments were passed correctly
+    args = parser.parse_args()
+    for arg in ["owner", "repo", "project"]:
+        if not getattr(args, arg):
+            raise KeyError(f"{arg} not passed correctly")
+    # export the issue and project data
+    github.export_issue_data(
+        owner=args.owner,
+        repo=args.repo,
+        output_file=ISSUE_FILE,
+    )
+    github.export_project_data(
+        owner=args.owner,
+        project=args.project,
+        output_file=PROJECT_FILE,
+    )
