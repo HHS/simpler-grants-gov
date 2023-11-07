cfadb48350b9dbd0847cb48de709baba9beb9374
widal001 <billy.daly@agile6.com>
2023-11-07 01:11:40 -0500

    refactor: Reorganizes SprintBoard class methods

--- analytics/src/analytics/datasets/github_projects.py
+++ analytics/src/analytics/datasets/github_projects.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import pandas as pd
 from numpy import datetime64
@@ -9,12 +10,38 @@ from analytics.datasets.base import BaseDataset
 class SprintBoard(BaseDataset):
     """Stores the GitHub project data for the Sprint Planning Board"""
 
+    ISSUE_DATE_COLS = ["created_date", "closed_date"]
+    ISSUE_COLUMN_MAP = {
+        "number": "issue_number",
+        "createdAt": "created_date",
+        "closedAt": "closed_date",
+    }
+    SPRINT_DATE_COLS = ["sprint_start_date", "milestone_due_date"]
+    SPRINT_COLUMN_MAP = {
+        "content.number": "issue_number",
+        "title": "issue_title",
+        "content.type": "type",
+        "content.body": "issue_body",
+        "status": "status",
+        "assignees": "assignees",
+        "labels": "labels",
+        "content.url": "url",
+        "story Points": "story_points",
+        "milestone.title": "milestone",
+        "milestone.dueOn": "milestone_due_date",
+        "milestone.description": "milestone_description",
+        "sprint.title": "sprint",
+        "sprint.startDate": "sprint_start_date",
+        "sprint.duration": "sprint_duration",
+    }
+    SPRINT_COLUMN_MAP
+
     def __init__(
         self,
-        project_file: str = "data/sprint-data.json",
+        sprint_file: str = "data/sprint-data.json",
         issue_file: str = "data/issue-data.json",
     ) -> None:
-        """"""
+        """Intializes the sprint board dataset"""
         # set named columns
         self.opened_col = "created_date"
         self.closed_col = "closed_date"
@@ -22,7 +49,7 @@ class SprintBoard(BaseDataset):
         self.sprint_start_col = "sprint_start_date"
         self.sprint_end_col = "sprint_end_date"
         # load the input data
-        self.df = self.load_data(project_file, issue_file)
+        self.df = self._load_data(sprint_file, issue_file)
         # initialize the parent class
         super().__init__()
 
@@ -40,67 +67,73 @@ class SprintBoard(BaseDataset):
         sprint_end = sprint_end.tz_localize("UTC")
         return sprint_end
 
-    def load_data(self, project_file: str, issue_file: str) -> pd.DataFrame:
+    def _load_data(self, sprint_file: str, issue_file: str) -> pd.DataFrame:
         """Load the input datasets and generate the final dataframe"""
         # load and merge input datasets
-        df_sprints = load_project_data(project_file)
-        df_issues = load_issue_data(issue_file)
-        return df_sprints.merge(df_issues, on="issue_number")
+        df_sprints = load_json_data_as_df(
+            file_path=sprint_file,
+            column_map=self.SPRINT_COLUMN_MAP,
+            date_cols=self.SPRINT_DATE_COLS,
+            key_for_nested_items="items",
+        )
+        df_issues = load_json_data_as_df(
+            file_path=issue_file,
+            column_map=self.ISSUE_COLUMN_MAP,
+            date_cols=self.ISSUE_DATE_COLS,
+        )
+        df = df_sprints.merge(df_issues, on="issue_number")
+        return self._apply_transformations(df)
+
+    def _apply_transformations(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Apply column specific data transformations"""
+        # calculate sprint end date
+        df["sprint_duration"] = pd.to_timedelta(df["sprint_duration"], unit="day")
+        df["sprint_end_date"] = df["sprint_start_date"] + df["sprint_duration"]
+        return df
 
 
-def load_issue_data(file_path: str) -> pd.DataFrame:
-    issue_cols = {
-        "number": "issue_number",
-        "createdAt": "created_date",
-        "closedAt": "closed_date",
-    }
-    # load issue data from the local file
+def load_json_data_as_df(
+    file_path: str,
+    column_map: dict,
+    date_cols: list[str],
+    key_for_nested_items: Optional[str] = None,
+) -> pd.DataFrame:
+    """Load a file that contains JSON data and format is as a DataFrame
+
+    Parameters
+    ----------
+    file_path: str
+        Path to the JSON file with the exported issue data
+    column_map: dict
+        Dictionary mapping of existing JSON keys to their new column names
+    date_cols: list[str]
+        List of columns that need to be converted to date types
+    key_for_items: str
+        Name of the
+
+    Returns
+    -------
+    pd.DataFrame
+        Pandas dataframe with columns renamed to match the values of the column map
+
+    Notes
+    -----
+    TODO: @widal001 2023-11-06 - Consider replacing column_map and date_cols with a
+        pydantic schema which would also allow us to do type validation and conversions
+    """
+    # load json data from the local file
     with open(file_path) as f:
-        issue_data = json.loads(f.read())
+        json_data = json.loads(f.read())
+    # if the items we want to convert are nested under a key extract them
+    if key_for_nested_items:
+        json_data = json_data[key_for_nested_items]
     # flatten the nested json into a dataframe
-    df = pd.json_normalize(issue_data)
+    df = pd.json_normalize(json_data)
     # reorder and rename the columns
-    df = df[issue_cols.keys()]
-    df = df.rename(columns=issue_cols)
+    df = df[column_map.keys()]
+    df = df.rename(columns=column_map)
     # convert datetime columns to date
-    date_cols = ["created_date", "closed_date"]
     for col in date_cols:
         # strip off the timestamp portion of the date
         df[col] = pd.to_datetime(df[col]).dt.floor("d")
-    return df
-
-
-def load_project_data(file_path: str) -> pd.DataFrame:
-    # load sprint data
-    project_cols = {
-        "content.number": "issue_number",
-        "title": "issue_title",
-        "content.type": "type",
-        "content.body": "issue_body",
-        "status": "status",
-        "assignees": "assignees",
-        "labels": "labels",
-        "content.url": "url",
-        "story Points": "story_points",
-        "milestone.title": "milestone",
-        "milestone.dueOn": "milestone_due_date",
-        "sprint.title": "sprint",
-        "sprint.startDate": "sprint_start_date",
-        "sprint.duration": "sprint_duration",
-    }
-    with open(file_path) as f:
-        sprint_data = json.loads(f.read())["items"]
-    # flatten the nested json into a dataframe
-    df = pd.json_normalize(sprint_data)
-    # reorder and rename the columns
-    df = df[project_cols.keys()]
-    df = df.rename(columns=project_cols)
-    # convert date columns
-    date_cols = ["sprint_start_date", "milestone_due_date"]
-    for col in date_cols:
-        # strip off the timestamp portion of the date
-        df[col] = pd.to_datetime(df[col]).dt.floor("d")
-    # calculate sprint end
-    df["sprint_duration"] = pd.to_timedelta(df["sprint_duration"], unit="day")
-    df["sprint_end_date"] = df["sprint_start_date"] + df["sprint_duration"]
     return df
