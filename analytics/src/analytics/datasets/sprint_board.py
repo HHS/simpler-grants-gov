ffecba1d670913e893c6f80f83b0551d341f636d
widal001 <billy.daly@agile6.com>
2023-11-09 10:27:13 -0500

    refactor: Moves shared function into separate file
    - Moves functions to generate test data into conftest.py
    - Moves function to load data from json file to utils.py

--- analytics/src/analytics/datasets/sprint_board.py
+++ analytics/src/analytics/datasets/sprint_board.py
@@ -1,10 +1,8 @@
-import json
-from typing import Optional
-
 import pandas as pd
 from numpy import datetime64
 
 from analytics.datasets.base import BaseDataset
+from analytics.datasets.utils import load_json_data_as_df
 
 
 class SprintBoard(BaseDataset):
@@ -26,7 +24,7 @@ class SprintBoard(BaseDataset):
         "assignees": "assignees",
         "labels": "labels",
         "content.url": "url",
-        "story Points": "story_points",
+        "story Points": "points",
         "milestone.title": "milestone",
         "milestone.dueOn": "milestone_due_date",
         "milestone.description": "milestone_description",
@@ -65,7 +63,7 @@ class SprintBoard(BaseDataset):
         cls,
         sprint_file: str = "data/sprint-data.json",
         issue_file: str = "data/issue-data.json",
-    ) -> pd.DataFrame:
+    ) -> None:
         """Load the input datasets and generate the final dataframe"""
         # load and merge input datasets
         df_sprints = load_json_data_as_df(
@@ -84,7 +82,7 @@ class SprintBoard(BaseDataset):
         return cls(df)
 
     @classmethod
-    def _apply_transformations(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _apply_transformations(cls, df: pd.DataFrame) -> pd.DataFrame:
         """Apply column specific data transformations"""
         # calculate sprint end date
         df["sprint_duration"] = pd.to_timedelta(df["sprint_duration"], unit="day")
@@ -97,50 +95,3 @@ class SprintBoard(BaseDataset):
             .astype("Int64")
         )
         return df
-
-
-def load_json_data_as_df(
-    file_path: str,
-    column_map: dict,
-    date_cols: list[str],
-    key_for_nested_items: Optional[str] = None,
-) -> pd.DataFrame:
-    """Load a file that contains JSON data and format is as a DataFrame
-
-    Parameters
-    ----------
-    file_path: str
-        Path to the JSON file with the exported issue data
-    column_map: dict
-        Dictionary mapping of existing JSON keys to their new column names
-    date_cols: list[str]
-        List of columns that need to be converted to date types
-    key_for_items: str
-        Name of the
-
-    Returns
-    -------
-    pd.DataFrame
-        Pandas dataframe with columns renamed to match the values of the column map
-
-    Notes
-    -----
-    TODO: @widal001 2023-11-06 - Consider replacing column_map and date_cols with a
-        pydantic schema which would also allow us to do type validation and conversions
-    """
-    # load json data from the local file
-    with open(file_path) as f:
-        json_data = json.loads(f.read())
-    # if the items we want to convert are nested under a key extract them
-    if key_for_nested_items:
-        json_data = json_data[key_for_nested_items]
-    # flatten the nested json into a dataframe
-    df = pd.json_normalize(json_data)
-    # reorder and rename the columns
-    df = df[column_map.keys()]
-    df = df.rename(columns=column_map)
-    # convert datetime columns to date
-    for col in date_cols:
-        # strip off the timestamp portion of the date
-        df[col] = pd.to_datetime(df[col]).dt.floor("d")
-    return df
--- analytics/tests/datasets/test_sprint_board.py
+++ analytics/tests/datasets/test_sprint_board.py
@@ -1,63 +1,12 @@
-import json
-
 import pandas as pd
 import pytest
 
 from analytics.datasets.sprint_board import SprintBoard
-
-
-def json_issue_row(
-    issue: int,
-    created_at: str = "2023-11-01T00:00:00Z",
-    closed_at: str = "2023-11-01T00:00:00Z",
-) -> dict:
-    """Generate a row of JSON issue data for testing"""
-    return {
-        "closedAt": closed_at,
-        "createdAt": created_at,
-        "number": issue,
-    }
-
-
-def json_sprint_row(
-    issue: int,
-    parent_number: int = -99,
-    sprint_name: str = "Sprint 1",
-    sprint_date: str = "2023-11-01",
-    status: str = "Done",
-    story_points: int = 5,
-) -> dict:
-    """Generate a row of JSON sprint data for testing"""
-    return {
-        "assignees": ["mickeymouse"],
-        "content": {
-            "type": "Issue",
-            "body": f"Description of test issue {issue}",
-            "title": f"Test issue {issue}",
-            "number": issue,
-            "repository": "HHS/simpler-grants-gov",
-            "url": f"https://github.com/HHS/simpler-grants-gov/issues/{issue}",
-        },
-        "id": "PVTI_lADOABZxns4ASDf3zgJhmCk",
-        "labels": ["topic: infra", "project: grants.gov"],
-        "linked pull requests": [],
-        "milestone": {
-            "title": "Sample milestone",
-            "description": f"30k ft deliverable: #{parent_number}",
-            "dueOn": "2023-10-20T00:00:00Z",
-        },
-        "repository": "https://github.com/HHS/simpler-grants-gov",
-        "sprint": {"title": sprint_name, "startDate": sprint_date, "duration": 14},
-        "status": status,
-        "story Points": story_points,
-        "title": "Test issue 1",
-    }
-
-
-def write_test_data_to_file(data: dict, output_file: str):
-    """"""
-    with open(output_file, "w") as f:
-        f.write(json.dumps(data))
+from tests.conftest import (
+    json_issue_row,
+    json_sprint_row,
+    write_test_data_to_file,
+)
 
 
 class TestSprintBoard:
