c9503dd41a0b620b9d91b86605adc099e1b75364
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2023-11-09 13:04:16 -0500

    [Issue 644] Add a GET /opportunity/:opportunity-id endpoint (#660)
    [Issue 644] Add a GET /opportunity/:opportunity-id endpoint
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

---
+++ api/src/services/opportunities/get_opportunities.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+import src.adapters.db as db
+from src.api.route_utils import raise_flask_error
+from src.db.models.opportunity_models import Opportunity
+
+
+def get_opportunity(db_session: db.Session, opportunity_id: int) -> Opportunity:
+    # TODO - issue-642 - figure out if we want to filter by is_draft here as well
+
+    opportunity: Opportunity | None = db_session.execute(
+        select(Opportunity).where(Opportunity.opportunity_id == opportunity_id)
+    ).scalar_one_or_none()
+
+    if opportunity is None:
+        raise_flask_error(404, message=f"Could not find Opportunity with ID {opportunity_id}")
+
+    return opportunity
