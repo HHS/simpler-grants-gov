fcab8e5fcb3a3ba05d50c5e4c1f390d9d7b27f5f
widal001 <billy.daly@agile6.com>
2023-11-08 20:07:19 -0500

    refactor: Moves _load_data() to a class method
    The SprintBoard._load_data() method used to be called during __init__()
    This commit changes it to a class method so that dataset can be loaded
    through other methods like from_dict() or from_csv()

--- analytics/src/analytics/datasets/base.py
+++ analytics/src/analytics/datasets/base.py
@@ -5,13 +5,24 @@ import pandas as pd
 class BaseDataset:
     """Base class for all datasets"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, df: pd.DataFrame) -> None:
+        """Instantiate the dataset"""
+        self.df = df
+
+    @classmethod
+    def from_csv(cls, file_path: str) -> None:
+        """Load and instantiate the dataset from a csv file"""
+        return cls(df=pd.read_csv(file_path))
+
+    @classmethod
+    def from_dict(cls, data: list[dict]):
+        """Load the dataset from a list of python dictionaries representing records"""
+        return cls(df=pd.DataFrame(data))
 
     def to_csv(self) -> None:
         """Export the dataset to a csv"""
-        raise NotImplementedError
+        return self.df.to_csv()
 
-    def to_df(self) -> pd.DataFrame:
-        """Export the dataset to a dataframe"""
-        raise NotImplementedError
+    def to_dict(self) -> pd.DataFrame:
+        """Export the dataset to a list of python dictionaries representing records"""
+        return self.df.to_dict(orient="records")
