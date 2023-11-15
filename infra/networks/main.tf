8c887df3dc791f33909d7249fb0f04b1116ea258
Daphne Gold <daphnegold@navapbc.com>
2023-11-15 13:22:33 -0800

    [ISSUE 505] Setup VPC Peering for DMS (#655)

--- infra/networks/main.tf
+++ infra/networks/main.tf
@@ -93,3 +93,32 @@ resource "aws_vpc_endpoint" "aws_service" {
   subnet_ids          = data.aws_subnets.default.ids
   private_dns_enabled = true
 }
+
+# VPC Configuration for DMS
+# ----------------------------------------
+
+data "aws_ssm_parameter" "dms_peer_owner_id" {
+  name = "/network/dms/peer-owner-id"
+}
+
+data "aws_ssm_parameter" "dms_peer_vpc_id" {
+  name = "/network/dms/peer-vpc-id"
+}
+
+resource "aws_vpc_peering_connection" "dms" {
+  peer_owner_id = data.aws_ssm_parameter.dms_peer_owner_id.value
+  peer_vpc_id   = data.aws_ssm_parameter.dms_peer_vpc_id.value
+  vpc_id        = data.aws_vpc.default.id
+  peer_region   = "us-east-2"
+
+  tags = {
+    Name = "DMS VPC Peering"
+  }
+}
+
+resource "aws_route" "dms" {
+  route_table_id = data.aws_vpc.default.main_route_table_id
+  # MicroHealth VPC CIDR block
+  destination_cidr_block    = "10.220.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.dms.id
+}
