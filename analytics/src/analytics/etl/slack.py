bb5b8f4405010e67a1682114f85e9dd94dc8df38
widal001 <billy.daly@agile6.com>
2023-11-15 16:35:02 -0500

    refactor: Wraps slack functions in a SlackBot class Moving slack functions into a class allows us to inject that class as a dependency in an export function

--- analytics/src/analytics/etl/slack.py
+++ analytics/src/analytics/etl/slack.py
@@ -1,5 +1,7 @@
 """A series of ETL functions that allows us to read and write data to slack."""
+import functools
 from dataclasses import dataclass
+from typing import Callable, Any
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -10,36 +12,51 @@ from slack_sdk.web.slack_response import SlackResponse
 class FileMapping:
     """A dataclass that maps file path to file name."""
 
-    file_path: str
-    file_name: str
+    path: str
+    name: str
 
 
-def fetch_slack_channel_info(
-    client: WebClient,
-    channel_id: str,
-) -> SlackResponse | None:
-    """Get info about a slack channel."""
-    try:
+def slack_api_error_handler(slackbot_api_call: Callable) -> Callable:
+    """Wrap SlackBot methods with a try-except block."""
+
+    @functools.wraps(slackbot_api_call)
+    def try_to_make_slackbot_api_call_and_catch_error(
+        *args: Any,
+        **kwargs: Any,
+    ) -> SlackResponse | None:
+        """Try to make a slack API call, and print the error if it fails."""
+        try:
+            return slackbot_api_call(*args, **kwargs)
+        except SlackApiError as e:
+            print(e)
+            return None
+
+    return try_to_make_slackbot_api_call_and_catch_error
+
+
+class SlackBot:
+    """Interact with slack using a slack bot."""
+
+    def __init__(self, client: WebClient) -> None:
+        """Instantiate the SlackBot class."""
+        self.client = client
+
+    @slack_api_error_handler
+    def fetch_slack_channel_info(self, channel_id: str) -> SlackResponse | None:
+        """Get info about a slack channel."""
         # Call the conversations.info method using the WebClient
-        return client.conversations_info(channel=channel_id)
-    except SlackApiError as e:
-        print(f"Error fetching conversations: {e}")
-    return None
+        return self.client.conversations_info(channel=channel_id)
 
-
-def upload_file_to_slack_channel(
-    client: WebClient,
-    channel_id: str,
-    files: list[FileMapping],
-    message: str,
-) -> SlackResponse | None:
-    """Post to a slack channel."""
-    try:
-        return client.files_upload_v2(
-            file_uploads=[{"file": f.file_path, "title": f.file_name} for f in files],
+    @slack_api_error_handler
+    def upload_file_to_slack_channel(
+        self,
+        channel_id: str,
+        files: list[FileMapping],
+        message: str,
+    ) -> SlackResponse | None:
+        """Upload files to a slack channel with a message."""
+        return self.client.files_upload_v2(
+            file_uploads=[{"file": f.path, "title": f.name} for f in files],
             channel=channel_id,
             initial_comment=message,
         )
-    except SlackApiError as e:
-        print(f"Error fetching conversations: {e}")
-    return None
--- analytics/tests/integrations/test_slack.py
+++ analytics/tests/integrations/test_slack.py
@@ -1,43 +1,45 @@
 """Test the code in analytics.etl.slack."""
-from slack_sdk import WebClient  # noqa: I001
+from pathlib import Path  # noqa: I001
 
-from analytics.etl.slack import (
-    FileMapping,
-    fetch_slack_channel_info,
-    upload_file_to_slack_channel,
-)
+import pytest
+from slack_sdk import WebClient
+
+from analytics.etl.slack import FileMapping, SlackBot
 from config import settings
 
 client = WebClient(token=settings.slack_bot_token)
 
 
-def test_fetch_slack_channels():
+@pytest.fixture(name="slackbot")
+def mock_slackbot() -> SlackBot:
+    """Create a SlackBot instance for testing."""
+    return SlackBot(client=client)
+
+
+def test_fetch_slack_channels(slackbot: SlackBot):
     """The fetch_slack_channels() function should execute correctly."""
-    result = fetch_slack_channel_info(
-        client=client,
-        channel_id=settings.reporting_channel_id,
-    )
+    result = slackbot.fetch_slack_channel_info(channel_id=settings.reporting_channel_id)
     assert result["ok"] is True
-    assert result["channel"]["name"] == "z_bot-sprint-reporting"
+    assert result["channel"]["name"] == "z_bot-sprint-reporting-test"
 
 
-def test_upload_files_to_slack_channel():
+def test_upload_files_to_slack_channel(slackbot: SlackBot):
     """The upload_files_to_slack_channel() function should execute correctly."""
     # setup - create test files to upload
     files = [
-        FileMapping(file_path="data/test1.txt", file_name="test1.txt"),
-        FileMapping(file_path="data/test2.txt", file_name="test2.txt"),
+        FileMapping(path="data/test1.txt", name="test1.txt"),
+        FileMapping(path="data/test2.txt", name="test2.txt"),
     ]
     for _file in files:
-        with open(_file.file_path, "w", encoding="utf-8") as f:
-            f.write(_file.file_name)
+        test_dir = Path(_file.path).parent
+        test_dir.mkdir(exist_ok=True, parents=True)
+        with open(_file.path, "w", encoding="utf-8") as f:
+            f.write(_file.name)
     # execution - run the upload
-    result = upload_file_to_slack_channel(
-        client=client,
+    result = slackbot.upload_file_to_slack_channel(
         files=files,
         channel_id=settings.reporting_channel_id,
         message="This is a test upload",
     )
-    print(result["files"])
     assert result["ok"] is True
     assert result["files"] is not None
