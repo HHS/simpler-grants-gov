1950d539cbe8b8b8318e11caef67cbbe89f826f9
widal001 <billy.daly@agile6.com>
2023-11-24 17:15:23 -0500

    feat: Add SprintBoard.current_sprint property This property returns the name of the current sprint, which depends on another new method: get_sprint_name_from_date()

--- analytics/tests/datasets/test_sprint_board.py
+++ analytics/tests/datasets/test_sprint_board.py
@@ -1,13 +1,60 @@
 """Tests for analytics/datasets/sprint_board.py."""
-import pandas as pd  # noqa: I001
-import pytest
 
+import pandas as pd
+import pytest
 from analytics.datasets.sprint_board import SprintBoard
+
 from tests.conftest import (
     json_issue_row,
     json_sprint_row,
     write_test_data_to_file,
 )
+
+DAY_1 = "2023-11-01"
+DAY_2 = "2023-11-02"
+DAY_3 = "2023-11-03"
+DAY_4 = "2023-11-04"
+DAY_5 = "2023-11-05"
+
+
+def sprint_board_row(
+    issue: int,
+    created: str = DAY_1,
+    closed: str | None = None,
+    status: str = "In Progress",
+    points: int = 1,
+    sprint: int = 1,
+    sprint_start: str = DAY_1,
+    sprint_length: int = 2,
+) -> dict:
+    """Create a sample row of the SprintBoard dataset."""
+    # create timestamp and time delta fields
+    sprint_start_ts = pd.Timestamp(sprint_start)
+    sprint_duration = pd.Timedelta(days=sprint_length)
+    sprint_end_ts = sprint_start_ts + sprint_duration
+    created_date = pd.Timestamp(created, tz="UTC")
+    closed_date = pd.Timestamp(closed, tz="UTC") if closed else None
+    # return the sample record
+    return {
+        "issue_number": issue,
+        "issue_title": f"Issue {issue}",
+        "type": "issue",
+        "issue_body": f"Description of issue {issue}",
+        "status": "Done" if closed else status,
+        "assignees": "mickeymouse",
+        "labels": [],
+        "url": f"https://github.com/HHS/simpler-grants-gov/issues/{issue}",
+        "points": points,
+        "milestone": "Milestone 1",
+        "milestone_due_date": sprint_end_ts,
+        "milestone_description": "Milestone 1 description",
+        "sprint": f"Sprint {sprint}",
+        "sprint_start_date": sprint_start_ts,
+        "sprint_end_date": sprint_end_ts,
+        "sprint_duration": sprint_duration,
+        "created_date": created_date,
+        "closed_date": closed_date,
+    }
 
 
 class TestSprintBoard:
@@ -94,3 +141,43 @@ class TestSprintBoard:
         df = df.set_index("issue_number")
         # validation -- check that issue 111's parent_issue_number is 222
         assert df.loc[111]["parent_issue_number"] == parent_number
+
+
+class TestGetSprintNameFromDate:
+    """Test the SprintBoard.get_sprint_name_from_date() method."""
+
+    @pytest.mark.parametrize(
+        ("date", "expected"),
+        [
+            (DAY_1, "Sprint 1"),
+            (DAY_2, "Sprint 1"),
+            (DAY_4, "Sprint 2"),
+            (DAY_5, "Sprint 2"),
+        ],
+    )
+    def test_return_name_if_matching_sprint_exists(self, date: str, expected: str):
+        """Test that correct sprint is returned if date exists in a sprint."""
+        # setup - create sample dataset
+        board_data = [
+            sprint_board_row(issue=1, sprint=1, sprint_start=DAY_1),
+            sprint_board_row(issue=2, sprint=1, sprint_start=DAY_1),
+            sprint_board_row(issue=3, sprint=2, sprint_start=DAY_4),
+        ]
+        board = SprintBoard.from_dict(board_data)
+        # validation
+        sprint_date = pd.Timestamp(date)
+        sprint_name = board.get_sprint_name_from_date(sprint_date)
+        assert sprint_name == expected
+
+    def test_return_none_if_no_matching_sprint(self):
+        """The method should return None if no sprint contains the date."""
+        # setup - create sample dataset
+        board_data = [
+            sprint_board_row(issue=1, sprint=1, sprint_start=DAY_1),
+            sprint_board_row(issue=2, sprint=2, sprint_start=DAY_4),
+        ]
+        board = SprintBoard.from_dict(board_data)
+        # validation
+        bad_date = pd.Timestamp("1900-01-01")
+        sprint_name = board.get_sprint_name_from_date(bad_date)
+        assert sprint_name is None
