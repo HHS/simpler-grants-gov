94b038b83047cfcd59cb0dfe3127f57c1effc67c
Sammy Steiner <sammysteiner@navapbc.com>
2023-11-29 09:40:19 -0500

    process, research, 404 pages

---
+++ frontend/src/pages/process.tsx
@@ -0,0 +1,42 @@
+import type { GetStaticProps, NextPage } from "next";
+import { ExternalRoutes } from "src/constants/routes";
+
+import { Trans, useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import PageSEO from "src/components/PageSEO";
+import FullWidthAlert from "../components/FullWidthAlert";
+
+const Process: NextPage = () => {
+  const { t } = useTranslation("common", { keyPrefix: "Process" });
+
+  return (
+    <>
+      <PageSEO title={t("page_title")} description={t("meta_description")} />
+      <FullWidthAlert type="info" heading={t("alert_title")}>
+        <Trans
+          t={t}
+          i18nKey="alert"
+          components={{
+            LinkToGrants: (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={ExternalRoutes.GRANTS_HOME}
+              />
+            ),
+          }}
+        />
+      </FullWidthAlert>
+      Process Placeholder
+    </>
+  );
+};
+
+// Change this to GetServerSideProps if you're using server-side rendering
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(locale ?? "en");
+  return { props: { ...translations } };
+};
+
+export default Process;
