c92f6d727e2403964b54daa45acfaf1d686434bd
Sammy Steiner <sammysteiner@navapbc.com>
2023-12-01 11:08:28 -0500

    icons

--- frontend/src/pages/content/IndexResearchContent.tsx
+++ frontend/src/pages/content/IndexResearchContent.tsx
@@ -2,6 +2,12 @@ import { useTranslation, Trans } from "next-i18next";
 import { Button, Grid, Icon } from "@trussworks/react-uswds";
 import ContentLayout from "src/components/ContentLayout";
 import Link from "next/link";
+import Image from "next/image";
+
+import embarrassed from "../../../public/img/noun-embarrassed.svg"
+import goal from "../../../public/img/noun-goal.svg"
+import hiring from "../../../public/img/noun-hiring.svg"
+import leadership from "../../../public/img/noun-leadership.svg"
 
 const IndexResearchContent = () => {
   const { t } = useTranslation("common", { keyPrefix: "Index" });
@@ -18,10 +24,22 @@ const IndexResearchContent = () => {
         <h3 className="tablet-lg:font-sans-lg tablet-lg:margin-bottom-05">
           {t("research.title_2")}
         </h3>
-        <p className="font-sans-md line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_2" /></p>
-        <p className="font-sans-md line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_3" /></p>
-        <p className="font-sans-md line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_4" /></p>
-        <p className="font-sans-md line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_5" /></p>
+        <Grid row gap={2}>
+            <Grid col={2}><Image style={{ filter: 'invert(33%)' }} src={embarrassed} alt="embarrased"/></Grid>
+            <Grid col={10}><p className="font-sans-md margin-top-05 line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_2" /></p></Grid>
+        </Grid>
+        <Grid row gap={2}>
+            <Grid col={2}><Image style={{ filter: 'invert(33%)' }} src={leadership} alt="leadership"/></Grid>
+            <Grid col={10}><p className="font-sans-md margin-top-05 line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_3" /></p></Grid>
+        </Grid>
+        <Grid row gap={2}>
+            <Grid col={2}><Image style={{ filter: 'invert(33%)' }} src={goal} alt="goal"/></Grid>
+            <Grid col={10}><p className="font-sans-md margin-top-05 line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_4" /></p></Grid>
+        </Grid>
+        <Grid row gap={2}>
+            <Grid col={2}><Image style={{ filter: 'invert(33%)' }} src={hiring} alt="hiring"/></Grid>
+            <Grid col={10}><p className="font-sans-md margin-top-05 line-height-sans-4 desktop-lg:line-height-sans-6"><Trans t={t} i18nKey="research.paragraph_5" /></p></Grid>
+        </Grid>
       </Grid>
     </ContentLayout>  
   )
