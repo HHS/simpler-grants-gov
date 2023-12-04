a28cd3397c1569a8aceb7549741937fb947f40ef
Andy Cochran <andrewthomascochran@gmail.com>
2023-12-04 13:17:53 -0500

    fix z-index 0f sticky alert

--- frontend/src/components/FullWidthAlert.tsx
+++ frontend/src/components/FullWidthAlert.tsx
@@ -25,11 +25,7 @@ const getBGColor = (type: Props["type"]) => {
 
 const FullWidthAlert = ({ type, heading, children }: Props) => {
   return (
-    <div
-      className={`${getBGColor(
-        type
-      )} tablet:position-sticky top-0 desktop:z-top`}
-    >
+    <div className={`${getBGColor(type)} tablet:position-sticky top-0 z-200`}>
       <GridContainer className="padding-y-1 tablet-lg:padding-y-2">
         <Grid>
           <USWDSAlert
