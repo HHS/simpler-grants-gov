9703db8b67e8fe975a1a0c64ec20d3cdba80213b
Sammy <sammysteiner@navapbc.com>
2023-12-11 09:04:42 -0500

    [Issues 718,829, 830, 831] Process Page and Get Involved Section

--- frontend/src/pages/content/ProcessMilestones.tsx
+++ frontend/src/pages/content/ProcessMilestones.tsx
@@ -2,25 +2,85 @@ import { ExternalRoutes } from "src/constants/routes";
 
 import { Trans, useTranslation } from "next-i18next";
 import Link from "next/link";
-import { Button, Grid, Icon } from "@trussworks/react-uswds";
+import {
+  Button,
+  Grid,
+  Icon,
+  IconList,
+  IconListContent,
+  IconListIcon,
+  IconListItem,
+  IconListTitle,
+} from "@trussworks/react-uswds";
 
 import ContentLayout from "src/components/ContentLayout";
 
-const ProcessMethodology = () => {
+type Boxes = {
+  title: string;
+  content: string;
+};
+
+const ProcessMilestones = () => {
   const { t } = useTranslation("common", { keyPrefix: "Process" });
+
+  const iconList: Boxes[] = t("milestones.icon_list", { returnObjects: true });
+
+  const getIcon = (iconIndex: number) => {
+    switch (iconIndex) {
+      case 0:
+        return <Icon.Search aria-hidden={true} className="text-middle" />;
+      case 1:
+        return <Icon.Assessment aria-hidden={true} className="text-middle" />;
+      case 2:
+        return <Icon.ContentCopy aria-hidden={true} className="text-middle" />;
+      default:
+        return <></>;
+    }
+  };
 
   return (
     <>
-      <Grid className="grid-container padding-top-1 tablet:padding-top-3 desktop-lg:padding-top-6 padding-bottom-0 tablet:padding-bottom-0 desktop-lg:padding-bottom-0">
-        <strong className="tablet-lg:font-sans-lg tablet-lg:margin-bottom-05">
-          {t("milestones.tag")}
-        </strong>
-      </Grid>
+      <ContentLayout
+        title={t("milestones.tag")}
+        data-test-id="process-high-level-content"
+        titleSize="m"
+        bottomBorder="none"
+      >
+        {!Array.isArray(iconList)
+          ? ""
+          : iconList.map((box, index) => {
+              return (
+                <Grid key={box.title + "-key"} tabletLg={{ col: 4 }}>
+                  <IconList className="usa-icon-list--size-lg">
+                    <IconListItem className="margin-top-4">
+                      <IconListIcon>{getIcon(index)}</IconListIcon>
+                      <IconListContent>
+                        <IconListTitle className="margin-bottom-2" type="h3">
+                          {box.title}
+                        </IconListTitle>
+                        <Trans
+                          t={t}
+                          i18nKey={box.content}
+                          components={{
+                            p: (
+                              <p className="font-sans-md line-height-sans-4 desktop-lg:line-height-sans-6"></p>
+                            ),
+                          }}
+                        />
+                      </IconListContent>
+                    </IconListItem>
+                  </IconList>
+                </Grid>
+              );
+            })}
+      </ContentLayout>
+      <div className="padding-2 tablet:padding-1" />
       <ContentLayout
         title={t("milestones.title_1")}
         data-testid="process-methodology-content"
         titleSize="m"
         bottomBorder="none"
+        paddingTop={false}
       >
         <Grid tabletLg={{ col: 6 }}>
           <p className="usa-intro">{t("milestones.paragraph_1")}</p>
@@ -104,4 +164,4 @@ const ProcessMethodology = () => {
   );
 };
 
-export default ProcessMethodology;
+export default ProcessMilestones;
