39d0d3e964d089cf1d307b93df73701e0c16e3a0
widal001 <billy.daly@agile6.com>
2023-12-12 14:40:03 -0500

    test: Tests required implementation of abstract methods

---
+++ analytics/tests/metrics/test_base.py
@@ -0,0 +1,52 @@
+"""Test the BaseMetric class."""
+# pylint: disable=abstract-method
+import pandas as pd  # noqa: I001
+import pytest
+
+from analytics.metrics.base import BaseMetric, Statistic
+
+
+class MetricWithoutStats(BaseMetric):
+    """Create a mock metric for testing without get_stats() method."""
+
+    def calculate(self) -> pd.DataFrame:
+        """Implement calculate method."""
+        return pd.DataFrame()
+
+
+class MetricWithoutPlotResults(BaseMetric):
+    """Create a mock metric for testing without get_stats() method."""
+
+    def calculate(self) -> pd.DataFrame:
+        """Implement calculate method."""
+        return pd.DataFrame()
+
+    def get_stats(self) -> dict[str, Statistic]:
+        """Implement get_stats method."""
+        return {}
+
+
+class TestRequiredImplementations:
+    """Check that NotImplementedError is raised for abstract methods."""
+
+    def test_raise_not_implemented_on_init_due_to_calculate(self):
+        """Error should be raised for __init__() method without calculate()."""
+        with pytest.raises(NotImplementedError):
+            BaseMetric()
+
+    def test_raise_not_implemented_on_init_due_to_get_stats(self):
+        """Error should be raised for __init__() method without get_stats()."""
+        with pytest.raises(NotImplementedError):
+            MetricWithoutStats()
+
+    def test_raise_not_implemented_for_plot_results(self):
+        """NotImplementedError should be raised for plot_results()."""
+        mock_metric = MetricWithoutPlotResults()
+        with pytest.raises(NotImplementedError):
+            mock_metric.plot_results()
+
+    def test_raise_not_implemented_for_format_slack_message(self):
+        """NotImplementedError should be raised for format_slack_message()."""
+        mock_metric = MetricWithoutPlotResults()
+        with pytest.raises(NotImplementedError):
+            mock_metric.format_slack_message()
