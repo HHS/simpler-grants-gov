e47b06ba0575dfddf1e2513ac8e64f10fca5e54a
Andy Cochran <andrewthomascochran@gmail.com>
2023-12-15 17:31:49 -0500

    update milestone layout

--- frontend/src/pages/content/ProcessMilestones.tsx
+++ frontend/src/pages/content/ProcessMilestones.tsx
@@ -76,7 +76,20 @@ const ProcessMilestones = () => {
       </ContentLayout>
       <div className="padding-2 tablet:padding-1" />
       <ContentLayout
-        title={t("milestones.title_1")}
+        title={
+          <>
+            <small className="display-block font-sans-lg margin-bottom-105">
+              {t("milestones.roadmap_1")}
+              <Icon.NavigateNext
+                className="text-middle text-base-light"
+                size={4}
+                aria-label="launch"
+              />
+              {t("milestones.title_1")}
+            </small>
+            {t("milestones.name_1")}
+          </>
+        }
         data-testid="process-methodology-content"
         titleSize="m"
         bottomBorder="none"
@@ -111,7 +124,20 @@ const ProcessMilestones = () => {
         </Grid>
       </ContentLayout>
       <ContentLayout
-        title={t("milestones.title_2")}
+        title={
+          <>
+            <small className="display-block font-sans-lg margin-bottom-105">
+              {t("milestones.roadmap_2")}
+              <Icon.NavigateNext
+                className="text-middle text-base-light"
+                size={4}
+                aria-label="launch"
+              />
+              {t("milestones.title_2")}
+            </small>
+            {t("milestones.name_2")}
+          </>
+        }
         data-testid="process-methodology-content"
         paddingTop={false}
         titleSize="m"
