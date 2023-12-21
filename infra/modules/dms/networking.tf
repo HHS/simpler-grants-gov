9b2ec492f12181bf954870ca336e4afbf65545cc
Alsia Plybeah <alsia@navapbc.com>
2023-12-21 12:29:16 -0500

    add peering connection ref

--- infra/modules/dms/networking.tf
+++ infra/modules/dms/networking.tf
@@ -1,31 +1,10 @@
 # Add Security Groups for VPC -> DMS here
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
-
-resource "aws_vpc_security_group_egress_rule" "vpc_egress_from_dms" {
-  security_group_id = aws_security_group.vpc.id
-  description       = "VPC Endpoint security group egress rules for DMS"
-
-  from_port                    = 443
-  to_port                      = 443
-  ip_protocol                  = "tcp" # may need to change protocol too
-  referenced_security_group_id = aws_security_group.vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "vpc_ingress_from_dms" {
-  security_group_id = aws_security_group.vpc.id
-  description       = "VPC Endpoint security group inress rules for DMS"
-
-  from_port                    = 443
-  to_port                      = 443
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.vpc.id
-}
-
-# This will also need to be attached to our DMS instance when created
-resource "aws_security_group" "vpc" {
-  # checkov:skip= CKV2_AWS_5:DMS instance not created yet
-  name_prefix = "${var.name}-vpc"
-  description = "Security group to manage the connection between DMS and the VPC"
-  vpc_id      = var.vpc_id
-
+# This connection was created in the console, the provided arguments act more as a query than a reference
+data "aws_vpc_peering_connection" "dms" {
+  owner_id        = data.aws_caller_identity.current.account_id
+  cidr_block      = "172.31.0.0/16" # our cidr block
+  peer_cidr_block = "10.220.0.0/16" # their cidr blcok
 }
