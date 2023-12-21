ebbd789cfc0b0706922af408078649eb0e0900f4
Alsia Plybeah <alsia@navapbc.com>
2023-12-21 13:58:51 -0500

    add more descriptive comments

--- infra/modules/dms/networking.tf
+++ infra/modules/dms/networking.tf
@@ -5,6 +5,6 @@ data "aws_region" "current" {}
 # This connection was created in the console, the provided arguments act more as a query than a reference
 data "aws_vpc_peering_connection" "dms" {
   owner_id        = data.aws_caller_identity.current.account_id
-  cidr_block      = "172.31.0.0/16" # our cidr block
-  peer_cidr_block = "10.220.0.0/16" # their cidr blcok
+  cidr_block      = "172.31.0.0/16" # our cidr block, where the target database for the DMS is located
+  peer_cidr_block = "10.220.0.0/16" # their cidr block, where the origin database for the DMS is located
 }
