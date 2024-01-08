b484138091618744efb8ce93f44c3bbd2a996f42
Sammy <sammysteiner@navapbc.com>
2024-01-08 15:39:42 -0500

    [Issue 957] Search Index and Google Tag per Environment (#977)
    * add next third party tools and update gtm
    * noindex and env conditional for robots and gtm

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,9 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import { PUBLIC_ENV } from "src/constants/environments";
 
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 
 import Layout from "../components/Layout";
 
@@ -16,25 +16,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="icon" href={assetPath("/img/favicon.ico")} sizes="any" />
+        {process.env.NEXT_PUBLIC_ENVIRONMENT !== "prod" && (
+          <meta name="robots" content="noindex,nofollow" />
+        )}
       </Head>
-      <Script id="google-tag-manager">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${PUBLIC_ENV.GOOGLE_TAG_ID}');
-      `}
-      </Script>
-
+      {process.env.NEXT_PUBLIC_ENVIRONMENT === "prod" && (
+        <GoogleTagManager gtmId={PUBLIC_ENV.GOOGLE_TAG_ID} />
+      )}
       <Layout>
-        <noscript
-          id="gtm-iframe"
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${PUBLIC_ENV.GOOGLE_TAG_ID}"
-            height="0" width="0" style="display:none;visibility:hidden" />`,
-          }}
-        />
         <Component {...pageProps} />
       </Layout>
     </>
