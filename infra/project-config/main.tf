1d23042e12832dbdabefeb866e1e43702fc19e5b
Kai Siren <kai@Kais-MacBook-Pro.local>
2024-01-10 13:07:55 -0800

    checkpoint

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,21 +1,20 @@
 locals {
   # Machine readable project name (lower case letters, dashes, and underscores)
   # This will be used in names of AWS resources
-  project_name = "<PROJECT_NAME>"
+  project_name = "simpler-grants-gov"
 
   # Project owner (e.g. navapbc). Used for tagging infra resources.
-  owner = "<OWNER>"
+  owner = "navapbc"
 
   # URL of project source code repository
-  code_repository_url = "<REPO_URL>"
+  code_repository_url = "https://github.com/HHS/simpler-grants-gov"
 
   # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1).
   # This is dependent on where your project is located (if regional)
   # otherwise us-east-1 is a good default
-  default_region = "<DEFAULT_REGION>"
+  default_region = "us-east-1"
 
-  github_actions_role_name = "${local.project_name}-github-actions"
-
+  github_actions_role_name                = "${local.project_name}-github-actions"
   aws_services_security_group_name_prefix = "aws-service-vpc-endpoints"
 
   network_configs = {
