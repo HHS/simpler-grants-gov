ebcb1a950604e97ba2958923f4ec05b56d7095a2
Alsia Plybeah <alsia@navapbc.com>
2024-01-23 13:57:07 -0500

    add suggested fixes; modify policy name

--- infra/modules/dms/main.tf
+++ infra/modules/dms/main.tf
@@ -1,13 +1,16 @@
 # Put IAM Roles here
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 resource "aws_iam_policy" "dms_access" {
-  name   = "DMS Access"
+  name   = "dms-access"
   policy = data.aws_iam_policy_document.dms_access.json
 }
 resource "aws_iam_role" "dms_access" {
   name               = "dms-access-role"
   assume_role_policy = data.aws_iam_policy_document.dms-assume-role-policy.json
 }
+
 data "aws_iam_policy_document" "dms-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -31,12 +34,13 @@ data "aws_iam_policy_document" "dms_access" {
   statement {
     sid       = "AllowDMSAccess"
     effect    = "Allow"
-    actions   = ["dms:*"] # TODO: try to narrow this down from dms:*
-    resources = [""]      # arn for the actual dms service goes here
+    actions   = ["dms:*"]
+    resources = [""] # arn for the actual dms service goes here
   }
 
   statement {
-    # Allows DMS to create the roles it needs if not created beforehand 
+    # Allows DMS to create the roles it needs if not created beforehand
+    # Actions List: https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsidentityandaccessmanagementiam.html
     sid    = "AllowCreateIAM"
     effect = "Allow"
     actions = [
@@ -45,10 +49,11 @@ data "aws_iam_policy_document" "dms_access" {
       "iam:CreateRole",
       "iam:AttachRolePolicy"
     ]
-    resources = [""] # DMS arn here
+    resources = ["arn:aws:iam:*:${data.aws_caller_identity.current.account_id}:*"]
   }
   statement {
     # Allow DMS to configure the network it needs
+    # Actions List: https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonec2.html
     sid    = "EC2Access"
     effect = "Allow"
     actions = [
@@ -61,14 +66,14 @@ data "aws_iam_policy_document" "dms_access" {
       "ec2:CreateNetworkInterface",
       "ec2:DeleteNetworkInterface"
     ]
-    resources = [""] # DMS or EC2 arn?
+    resources = ["arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:*"]
   }
   statement {
     # View replication metrics
     sid       = "AllowCloudwatchMetrics"
     effect    = "Allow"
     actions   = ["cloudwatch:Get*", "cloudwatch:List*"]
-    resources = [""]
+    resources = ["*"]
   }
   statement {
     # View replication logs
@@ -80,6 +85,6 @@ data "aws_iam_policy_document" "dms_access" {
       "logs:ilterLogEvents",
       "logs:GetLogEvents"
     ]
-    resources = [""] # DMS arn
+    resources = ["*"] # don't have log group yet
   }
 }
