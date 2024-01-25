efd36eae6e7ed5b121d90779621fadccd7bcde7b
kai [they] <coilysiren@gmail.com>
2024-01-25 12:16:53 -0800

    [Issue 1051] Fix a security group name (#1095)
    ## Summary
    
    Rolls up to #1051
    
    ### Time to review: __1 mins__
    
    ## Context for reviewers
    
    I misnamed a security group, and that was blocking our staging deploy

--- infra/modules/network/vpc-endpoints.tf
+++ infra/modules/network/vpc-endpoints.tf
@@ -72,6 +72,9 @@ resource "aws_security_group" "aws_services" {
   name_prefix = var.aws_services_security_group_name_prefix
   description = "VPC endpoints to access AWS services from the VPCs private subnets"
   vpc_id      = module.aws_vpc.vpc_id
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpc_endpoints_ingress_from_vpc_cidr" {
