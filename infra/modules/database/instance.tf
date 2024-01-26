5eb195226b047cc71a8a5612b7c200d6be31cde4
Alsia Plybeah <alsia@navapbc.com>
2024-01-26 14:07:21 -0500

    add endpoints

--- infra/modules/database/instance.tf
+++ infra/modules/database/instance.tf
@@ -19,4 +19,22 @@ resource "aws_dms_replication_instance" "simpler_db" {
 
 }
 
+# TODO: find way to pull credentials
+resource "aws_dms_endpoint" "target_endpoint" {
+  certificate_arn                 = "arn:aws:dms:us-east-1:315341936575:cert:GWOIQRTIVQVRBL5ERMCKTUPHMM33MMDGIP57J4I"
+  database_name                   = "app"
+  endpoint_id                     = "api-dev-primary"
+  endpoint_type                   = "source"
+  engine_name                     = "aurora-postgresql"
+  secrets_manager_access_role_arn = "" # username, pw, server name and port should be pulled using this role
+  ssl_mode                        = "verify-ca"
+}
 
+resource "aws_dms_endpoint" "source_endpoint" {
+  database_name                   = "tstgrnts"
+  endpoint_id                     = "hhs-source"
+  endpoint_type                   = "source"
+  engine_name                     = "oracle"
+  ssl_mode                        = "none"
+  secrets_manager_access_role_arn = "" # username, pw, server name and port should be pulled using this role
+}
