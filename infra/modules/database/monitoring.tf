f1917053e0ff157bf5a395c8d890c10997801d9f
James Bursa <james@navapbc.com>
2024-01-31 10:27:05 -0500

    [Issue 1086] Bring in updates to `infra/modules/database` (#1116)
    ## Summary
    Part of #1086
    
    ### Time to review: __5 mins__
    
    ## Changes proposed
    - Update `infra/modules/database` based on latest template
    https://github.com/navapbc/template-infra/tree/c803535438afb08efccc36952cfad65d695cbce6/infra/modules/database
    
    ## Context for reviewers
    This was manually imported by comparing the directories and bringing in
    most changes.
    
    ## Additional information
    Successfully applied in `dev` and confirmed that the role_manager lambda
    runs again.
    
    ```python
    $ make infra-update-app-database-roles APP_NAME=api ENVIRONMENT=dev
    ...
    [INFO]	2024-01-31T15:07:01.197Z	30379894-a781-448a-947f-e21c9f2998ea	Connecting to database: user=root... host=api-dev.cluster-....us-east-1.rds.amazonaws.com port=5432 database=app
    ...
    [INFO]	2024-01-31T15:07:02.219Z	30379894-a781-448a-947f-e21c9f2998ea	Configuring roles
    [INFO]	2024-01-31T15:07:02.220Z	30379894-a781-448a-947f-e21c9f2998ea	Configuring role: username=migrator
    [INFO]	2024-01-31T15:07:02.313Z	30379894-a781-448a-947f-e21c9f2998ea	Configuring role: username=app
    [INFO]	2024-01-31T15:07:02.401Z	30379894-a781-448a-947f-e21c9f2998ea	Configuring schema
    [INFO]	2024-01-31T15:07:02.402Z	30379894-a781-448a-947f-e21c9f2998ea	Creating schema: schema_name=api
    [INFO]	2024-01-31T15:07:02.406Z	30379894-a781-448a-947f-e21c9f2998ea	Changing schema owner: schema_name=api owner=migrator
    [INFO]	2024-01-31T15:07:02.411Z	30379894-a781-448a-947f-e21c9f2998ea	Granting schema usage privileges: schema_name=api role=app
    ...
    ```
    
    Reviewers: @coilysiren

--- infra/modules/database/monitoring.tf
+++ infra/modules/database/monitoring.tf
@@ -3,7 +3,7 @@
 #----------------------------------#
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
-  name_prefix        = "${var.name}-enhanced-monitoring-"
+  name_prefix        = "${var.name}-db-monitor-"
   assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
 }
 
