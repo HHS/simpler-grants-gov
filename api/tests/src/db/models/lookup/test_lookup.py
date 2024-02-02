50df07a76dbfef1f4510ef59de067fc998ab17ba
Michael Chouinard <chouinar6@gmail.com>
2024-02-02 14:48:54 -0500

    Fix location of test files

--- api/tests/src/db/models/lookup/test_lookup.py
+++ api/tests/src/db/models/lookup/test_lookup.py
@@ -2,6 +2,8 @@ from enum import StrEnum
 
 import pytest
 
+from src.adapters.db.type_decorators.postgres_type_decorators import LookupColumn
+from src.db.models import metadata
 from src.db.models.lookup import LookupConfig, LookupStr
 
 
@@ -85,3 +87,32 @@ def test_lookup_config_zero():
         match="Only positive lookup_val values are allowed",
     ):
         LookupConfig([LookupStr(EnumX.A, 0)])
+
+
+def test_lookup_columns_named_in_db_correctly():
+    """
+    If you are seeing this fail, here's an example of a valid one (context below)::
+
+    pay_type: Mapped[PayType] = mapped_column(
+        "pay_type_id",  # <<< Make sure to specifically define the column name as the first parameter
+        LookupColumn(LkAdditionalPayType),
+        ForeignKey(LkAdditionalPayType.additional_pay_type_id),
+        nullable=False,
+    )
+
+    We want our lookup columns to always be named `<lookup_type>_id` in the DB
+    so that it's clear they are an ID pointing to another table and not the actual value.
+
+    It's fine if our in-code value doesn't have ID, as we aren't working with an ID,
+    we are instead working with the enum directly.
+
+    """
+
+    # This will validate the behavior of all columns in every table derived
+    # from the PostgresBase class which is everything we'll ever generate migrations for
+    for table in metadata.tables.values():
+        for column in table.columns:
+            if isinstance(column.type, LookupColumn):
+                assert column.name.endswith(
+                    "_id"
+                ), f"Lookup column {table.name}.{column.name} must be named with '_id' suffix"
