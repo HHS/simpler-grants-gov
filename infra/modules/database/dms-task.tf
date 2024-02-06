4a369210dd16837a11863bd4fcbd3d5142e50a34
Kai Siren <kaisiren@navapbc.com>
2024-02-06 10:17:12 -0800

    update rules

--- infra/modules/database/dms-task.tf
+++ infra/modules/database/dms-task.tf
@@ -40,98 +40,30 @@ resource "aws_dms_replication_task" "task" {
         {
           "rule-type" : "transformation",
           "rule-id" : "3",
-          "rule-name" : "Rename table TOPPORTUNITY to opportunity",
+          "rule-name" : "Rename table TOPPORTUNITY to transfer_topportunity",
           "rule-action" : "rename",
           "rule-target" : "table",
           "object-locator" : {
             "schema-name" : "EGRANTSADMIN",
             "table-name" : "TOPPORTUNITY"
           },
-          "value" : "opportunity"
+          "value" : "transfer_topportunity"
         },
         {
           "rule-type" : "transformation",
           "rule-id" : "4",
-          "rule-name" : "Rename OPPNUMBER to opportunity_number",
-          "rule-action" : "rename",
+          "rule-name" : "Lowercase column names",
+          "rule-action" : "convert-lowercase",
           "rule-target" : "column",
           "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "OPPNUMBER"
-          },
-          "value" : "opportunity_number"
+            "schema-name" : "%",
+            "table-name" : "%",
+            "column-name" : "%",
+          }
         },
         {
           "rule-type" : "transformation",
           "rule-id" : "5",
-          "rule-name" : "Rename OPPTITLE to opportunity_title",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "OPPTITLE"
-          },
-          "value" : "opportunity_title"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "6",
-          "rule-name" : "Rename OWNINGAGENCY to agency",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "OWNINGAGENCY"
-          },
-          "value" : "agency"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "7",
-          "rule-name" : "Rename OPPCATEGORY to category",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "OPPCATEGORY"
-          },
-          "value" : "category"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "8",
-          "rule-name" : "Rename+convert boolean IS_DRAFT to is_draft",
-          "rule-action" : "add-column",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY"
-          },
-          "value" : "is_draft",
-          "expression" : "CASE WHEN $IS_DRAFT 'Y' THEN 1 WHEN $IS_DRAFT 'N' THEN 0 ELSE null END",
-          "data-type" : {
-            "type" : "boolean"
-          }
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "9",
-          "rule-name" : "Remove renamed column IS_DRAFT",
-          "rule-action" : "remove-column",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "IS_DRAFT"
-          }
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "10",
           "rule-name" : "Exclude column TOPPORTUNITY.LISTED",
           "rule-action" : "remove-column",
           "rule-target" : "column",
@@ -143,7 +75,7 @@ resource "aws_dms_replication_task" "task" {
         },
         {
           "rule-type" : "transformation",
-          "rule-id" : "11",
+          "rule-id" : "6",
           "rule-name" : "Exclude column TOPPORTUNITY.INITIAL_OPPORTUNITY_ID",
           "rule-action" : "remove-column",
           "rule-target" : "column",
@@ -155,7 +87,7 @@ resource "aws_dms_replication_task" "task" {
         },
         {
           "rule-type" : "transformation",
-          "rule-id" : "12",
+          "rule-id" : "7",
           "rule-name" : "Exclude column TOPPORTUNITY.FLAG_2006",
           "rule-action" : "remove-column",
           "rule-target" : "column",
@@ -165,136 +97,7 @@ resource "aws_dms_replication_task" "task" {
             "column-name" : "FLAG_2006"
           }
         },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "13",
-          "rule-name" : "Rename LAST_UPD_DATE to updated_at",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "LAST_UPD_DATE"
-          },
-          "value" : "updated_at"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "14",
-          "rule-name" : "Rename CREATED_DATE to created_at",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "CREATED_DATE"
-          },
-          "value" : "created_at"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "15",
-          "rule-name" : "Rename REVISION_NUMBER to revision_number",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "REVISION_NUMBER"
-          },
-          "value" : "revision_number"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "16",
-          "rule-name" : "Rename PUBLISHER_PROFILE_ID to publisher_profile_id",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "PUBLISHER_PROFILE_ID"
-          },
-          "value" : "publisher_profile_id"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "17",
-          "rule-name" : "Rename PUBLISHERUID to publisheruid",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "PUBLISHERUID"
-          },
-          "value" : "publisheruid"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "18",
-          "rule-name" : "Rename OPPORTUNITY_ID to opportunity_id",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "OPPORTUNITY_ID"
-          },
-          "value" : "opportunity_id"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "19",
-          "rule-name" : "Rename MODIFIED_COMMENTS to modified_comments",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "MODIFIED_COMMENTS"
-          },
-          "value" : "modified_comments"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "20",
-          "rule-name" : "Rename LAST_UPD_ID to last_upd_id",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "LAST_UPD_ID"
-          },
-          "value" : "last_upd_id"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "21",
-          "rule-name" : "Rename CREATOR_ID to creator_id",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "CREATOR_ID"
-          },
-          "value" : "creator_id"
-        },
-        {
-          "rule-type" : "transformation",
-          "rule-id" : "22",
-          "rule-name" : "Rename CATEGORY_EXPLANATION to category_explanation",
-          "rule-action" : "rename",
-          "rule-target" : "column",
-          "object-locator" : {
-            "schema-name" : "EGRANTSADMIN",
-            "table-name" : "TOPPORTUNITY",
-            "column-name" : "CATEGORY_EXPLANATION"
-          },
-          "value" : "category_explanation"
-        }
+
       ]
     }
   )
