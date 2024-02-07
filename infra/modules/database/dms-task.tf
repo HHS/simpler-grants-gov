0b53b206b4422876895fa3b09db11353249822d5
Kai Siren <kaisiren@navapbc.com>
2024-02-07 10:23:06 -0800

    uncomment

--- infra/modules/database/dms-task.tf
+++ infra/modules/database/dms-task.tf
@@ -110,77 +110,77 @@ resource "aws_dms_replication_task" "task" {
           },
           "rule-action" : "explicit"
         },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "2",
-        #   "rule-name" : "Rename schema",
-        #   "rule-action" : "rename",
-        #   "rule-target" : "schema",
-        #   "object-locator" : {
-        #     "schema-name" : "EGRANTSADMIN"
-        #   },
-        #   "value" : "public"
-        # },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "3",
-        #   "rule-name" : "Rename table TOPPORTUNITY to transfer_topportunity",
-        #   "rule-action" : "rename",
-        #   "rule-target" : "table",
-        #   "object-locator" : {
-        #     "schema-name" : "EGRANTSADMIN",
-        #     "table-name" : "TOPPORTUNITY"
-        #   },
-        #   "value" : "transfer_topportunity"
-        # },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "4",
-        #   "rule-name" : "Lowercase column names",
-        #   "rule-action" : "convert-lowercase",
-        #   "rule-target" : "column",
-        #   "object-locator" : {
-        #     "schema-name" : "%",
-        #     "table-name" : "%",
-        #     "column-name" : "%",
-        #   }
-        # },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "5",
-        #   "rule-name" : "Exclude column TOPPORTUNITY.LISTED",
-        #   "rule-action" : "remove-column",
-        #   "rule-target" : "column",
-        #   "object-locator" : {
-        #     "schema-name" : "EGRANTSADMIN",
-        #     "table-name" : "TOPPORTUNITY",
-        #     "column-name" : "LISTED"
-        #   }
-        # },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "6",
-        #   "rule-name" : "Exclude column TOPPORTUNITY.INITIAL_OPPORTUNITY_ID",
-        #   "rule-action" : "remove-column",
-        #   "rule-target" : "column",
-        #   "object-locator" : {
-        #     "schema-name" : "EGRANTSADMIN",
-        #     "table-name" : "TOPPORTUNITY",
-        #     "column-name" : "INITIAL_OPPORTUNITY_ID"
-        #   }
-        # },
-        # {
-        #   "rule-type" : "transformation",
-        #   "rule-id" : "7",
-        #   "rule-name" : "Exclude column TOPPORTUNITY.FLAG_2006",
-        #   "rule-action" : "remove-column",
-        #   "rule-target" : "column",
-        #   "object-locator" : {
-        #     "schema-name" : "EGRANTSADMIN",
-        #     "table-name" : "TOPPORTUNITY",
-        #     "column-name" : "FLAG_2006"
-        #   }
-        # },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "2",
+          "rule-name" : "Rename schema",
+          "rule-action" : "rename",
+          "rule-target" : "schema",
+          "object-locator" : {
+            "schema-name" : "EGRANTSADMIN"
+          },
+          "value" : "public"
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "3",
+          "rule-name" : "Rename table TOPPORTUNITY to transfer_topportunity",
+          "rule-action" : "rename",
+          "rule-target" : "table",
+          "object-locator" : {
+            "schema-name" : "EGRANTSADMIN",
+            "table-name" : "TOPPORTUNITY"
+          },
+          "value" : "transfer_topportunity"
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "4",
+          "rule-name" : "Lowercase column names",
+          "rule-action" : "convert-lowercase",
+          "rule-target" : "column",
+          "object-locator" : {
+            "schema-name" : "%",
+            "table-name" : "%",
+            "column-name" : "%",
+          }
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "5",
+          "rule-name" : "Exclude column TOPPORTUNITY.LISTED",
+          "rule-action" : "remove-column",
+          "rule-target" : "column",
+          "object-locator" : {
+            "schema-name" : "EGRANTSADMIN",
+            "table-name" : "TOPPORTUNITY",
+            "column-name" : "LISTED"
+          }
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "6",
+          "rule-name" : "Exclude column TOPPORTUNITY.INITIAL_OPPORTUNITY_ID",
+          "rule-action" : "remove-column",
+          "rule-target" : "column",
+          "object-locator" : {
+            "schema-name" : "EGRANTSADMIN",
+            "table-name" : "TOPPORTUNITY",
+            "column-name" : "INITIAL_OPPORTUNITY_ID"
+          }
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "7",
+          "rule-name" : "Exclude column TOPPORTUNITY.FLAG_2006",
+          "rule-action" : "remove-column",
+          "rule-target" : "column",
+          "object-locator" : {
+            "schema-name" : "EGRANTSADMIN",
+            "table-name" : "TOPPORTUNITY",
+            "column-name" : "FLAG_2006"
+          }
+        },
       ]
     }
   )
