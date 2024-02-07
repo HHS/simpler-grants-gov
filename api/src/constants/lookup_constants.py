66c7459d1092fafd6a06a69ba284d35cc24be640
Michael Chouinard <chouinar6@gmail.com>
2024-02-07 12:42:45 -0500

    Adding a comment

--- api/src/constants/lookup_constants.py
+++ api/src/constants/lookup_constants.py
@@ -22,6 +22,8 @@ OPPORTUNITY_STATUS_CONFIG = LookupConfig(
 
 class OpportunityCategory(StrEnum):
     # TODO - change these to full text once we build the next version of the API
+    # will do this when constructing that API as I'll need to make a second version of this
+    # to avoid breaking the existing API endpoint
     DISCRETIONARY = "D"
     MANDATORY = "M"
     CONTINUATION = "C"
