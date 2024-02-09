196ad2f64c688d5205f652fda7a4e2a44a180790
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2024-02-09 12:25:59 -0500

    [Issue 1166] Create tables for expanded opportunity data model (#1187)
    ## Summary
    Fixes #1166
    
    This requires https://github.com/HHS/simpler-grants-gov/pull/1136 to be
    merged first as it builds ontop of that work
    
    ### Time to review: __5 mins__
    
    ## Changes proposed
    Created the opportunity tables for the expanded data model
    
    https://app.gitbook.com/o/cFcvhi6d0nlLyH2VzVgn/s/v1V0jIH7mb7Yb3jlNrgk/engineering/learnings/opportunity-endpoint-data-model#overview
    
    Setup factories to generate data in a reasonable way for these tables
    
    ## Context for reviewers
    This sets up all of the tables for our expanded opportunity data model -
    tables we'll copy to for DMS that will later be transformed to these
    tables will be a separate PR.
    
    The factories try to make somewhat realistic data with some variability.
    
    
    Note that I did adjust a few things from the tech spec, which I just
    haven't changed in that document yet (can't merge the changes myself)
    - The `other_value` fields in the link tables actually make sense to
    stay in the summary table, I found examples of them working different
    than I had assumed, so it makes more sense to keep them in a general
    place.
    - The `opportunity_agency` table idea I've dropped - if we want to
    adjust how agency data is stored, will deal with it later when we deal
    with the rest of the agency data
    - Didn't adjust the primary keys - just keeping everything on
    `opportunity_id` - already was going to keep several that way - don't
    see a reason to have several different primary/foreign key setups.
    - Miscellaneous small renames to more closely match the design of the
    front-end
    
    ## Additional information
    Running `make db-seed-local` after the migrations shows the data in
    various tables in what I would expect:
    ![Screenshot 2024-02-07 at 12 55 48
    PM](https://github.com/HHS/simpler-grants-gov/assets/46358556/1432481d-0563-45d2-9660-66721c16c4ba)
    ![Screenshot 2024-02-07 at 12 56 14
    PM](https://github.com/HHS/simpler-grants-gov/assets/46358556/5d9a63fb-4339-42cc-b445-f204f9a7842c)
    ![Screenshot 2024-02-07 at 12 56 21
    PM](https://github.com/HHS/simpler-grants-gov/assets/46358556/d61999ed-f2c9-4bbe-9c09-8337fdf38914)
    ![Screenshot 2024-02-07 at 12 56 26
    PM](https://github.com/HHS/simpler-grants-gov/assets/46358556/478002bb-6c41-4429-ae6b-d088e3f8297f)
    ![Screenshot 2024-02-07 at 12 56 44
    PM](https://github.com/HHS/simpler-grants-gov/assets/46358556/2211c4cb-6f99-4655-b814-09db66b4aeb9)
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

--- api/src/db/models/lookup_models.py
+++ api/src/db/models/lookup_models.py
@@ -17,3 +17,59 @@ class LkOpportunityCategory(LookupTable, TimestampMixin):
         return LkOpportunityCategory(
             opportunity_category_id=lookup.lookup_val, description=lookup.get_description()
         )
+
+
+@LookupRegistry.register_lookup(lookup_constants.APPLICANT_TYPE_CONFIG)
+class LkApplicantType(LookupTable, TimestampMixin):
+    __tablename__ = "lk_applicant_type"
+
+    applicant_type_id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str]
+
+    @classmethod
+    def from_lookup(cls, lookup: Lookup) -> "LkApplicantType":
+        return LkApplicantType(
+            applicant_type_id=lookup.lookup_val, description=lookup.get_description()
+        )
+
+
+@LookupRegistry.register_lookup(lookup_constants.FUNDING_CATEGORY_CONFIG)
+class LkFundingCategory(LookupTable, TimestampMixin):
+    __tablename__ = "lk_funding_category"
+
+    funding_category_id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str]
+
+    @classmethod
+    def from_lookup(cls, lookup: Lookup) -> "LkFundingCategory":
+        return LkFundingCategory(
+            funding_category_id=lookup.lookup_val, description=lookup.get_description()
+        )
+
+
+@LookupRegistry.register_lookup(lookup_constants.FUNDING_INSTRUMENT_CONFIG)
+class LkFundingInstrument(LookupTable, TimestampMixin):
+    __tablename__ = "lk_funding_instrument"
+
+    funding_instrument_id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str]
+
+    @classmethod
+    def from_lookup(cls, lookup: Lookup) -> "LkFundingInstrument":
+        return LkFundingInstrument(
+            funding_instrument_id=lookup.lookup_val, description=lookup.get_description()
+        )
+
+
+@LookupRegistry.register_lookup(lookup_constants.OPPORTUNITY_STATUS_CONFIG)
+class LkOpportunityStatus(LookupTable, TimestampMixin):
+    __tablename__ = "lk_opportunity_status"
+
+    opportunity_status_id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str]
+
+    @classmethod
+    def from_lookup(cls, lookup: Lookup) -> "LkOpportunityStatus":
+        return LkOpportunityStatus(
+            opportunity_status_id=lookup.lookup_val, description=lookup.get_description()
+        )
