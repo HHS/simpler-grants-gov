0368b9dca729fa30cc3b90868ce4bc5d7930a560
Kai Siren <kaisiren@navapbc.com>
2024-02-14 16:33:18 -0800

    move folder

--- infra/api/service/copy_oracle_data.tf
+++ infra/api/service/copy_oracle_data.tf
@@ -1,9 +1,8 @@
 # docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine
 resource "aws_sfn_state_machine" "copy_oracle_data" {
-  count = var.schedule_copy_oracle_data ? 1 : 0
 
-  name     = "${var.service_name}-copy-oracle-data"
-  role_arn = aws_iam_role.task_executor.arn
+  name     = "${local.service_name}-copy-oracle-data"
+  role_arn = module.service.task_role_arn
 
   definition = jsonencode({
     "StartAt" : "ExecuteECSTask",
@@ -13,19 +12,19 @@ resource "aws_sfn_state_machine" "copy_oracle_data" {
         # docs: https://docs.aws.amazon.com/step-functions/latest/dg/connect-ecs.html
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
         "Parameters" : {
-          "Cluster" : aws_ecs_cluster.cluster.arn,
-          "TaskDefinition" : aws_ecs_task_definition.app.arn,
+          "Cluster" : module.service.cluster_arn,
+          "TaskDefinition" : module.service.task_definition_arn,
           "LaunchType" : "FARGATE",
           "NetworkConfiguration" : {
             "AwsvpcConfiguration" : {
-              "Subnets" : var.private_subnet_ids,
-              "SecurityGroups" : [aws_security_group.app.id],
+              "Subnets" : data.aws_subnets.private.ids,
+              "SecurityGroups" : [module.service.app_security_group_id],
             }
           },
           "Overrides" : {
             "ContainerOverrides" : [
               {
-                "Name" : var.service_name,
+                "Name" : local.service_name,
                 "Environment" : [
                   {
                     "Name" : "FLASK_APP",
@@ -49,7 +48,7 @@ resource "aws_sfn_state_machine" "copy_oracle_data" {
   })
 
   logging_configuration {
-    log_destination        = "${aws_cloudwatch_log_group.service_logs.arn}:*"
+    log_destination        = "${module.service.service_logs_arn}:*"
     include_execution_data = true
     level                  = "ERROR"
   }
@@ -61,18 +60,16 @@ resource "aws_sfn_state_machine" "copy_oracle_data" {
 
 # docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/scheduler_schedule_group
 resource "aws_scheduler_schedule_group" "copy_oracle_data" {
-  count = var.schedule_copy_oracle_data ? 1 : 0
-  name  = "${var.service_name}-copy-oracle-data"
+  name = "${local.service_name}-copy-oracle-data"
 }
 
 # docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/scheduler_schedule
 resource "aws_scheduler_schedule" "copy_oracle_data" {
   # checkov:skip=CKV_AWS_297:Ignore the managed customer KMS key requirement for now
 
-  count                        = var.schedule_copy_oracle_data ? 1 : 0
-  name                         = "${var.service_name}-copy-oracle-data"
+  name                         = "${local.service_name}-copy-oracle-data"
   state                        = "ENABLED"
-  group_name                   = aws_scheduler_schedule_group.copy_oracle_data[0].id
+  group_name                   = aws_scheduler_schedule_group.copy_oracle_data.id
   schedule_expression          = "rate(2 minutes)"
   schedule_expression_timezone = "US/Eastern"
 
@@ -82,8 +79,8 @@ resource "aws_scheduler_schedule" "copy_oracle_data" {
 
   # target is the state machine
   target {
-    arn      = aws_sfn_state_machine.copy_oracle_data[0].arn
-    role_arn = aws_iam_role.task_executor.arn
+    arn      = aws_sfn_state_machine.copy_oracle_data.arn
+    role_arn = module.service.task_role_arn
 
     retry_policy {
       maximum_retry_attempts = 0 # dont retry, just wait for the next execution
