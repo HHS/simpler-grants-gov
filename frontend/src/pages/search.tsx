c1c16b4b0d59d5cc086072bba16410251348bb90
Sammy Steiner <sammysteiner@navapbc.com>
2024-02-15 13:25:29 -0500

    use showSearchV0 flag

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,19 +1,24 @@
 import type { GetStaticProps, NextPage } from "next";
+import { useFeatureFlags } from "src/hooks/useFeatureFlags";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import PageNotFound from "./404";
+
 const Search: NextPage = () => {
+  const { featureFlagsManager, mounted } = useFeatureFlags();
+
+  if (!mounted) return null;
+  if (!featureFlagsManager.isFeatureEnabled("showSearchV0")) {
+    return <PageNotFound />;
+  }
+
   return <>Search Boilerplate</>;
 };
 
 // Change this to GetServerSideProps if you're using server-side rendering
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const translations = await serverSideTranslations(locale ?? "en");
-
-  // TODO: update the statement below with a feature flag check
-  const hideSearchPage = true;
-  // redirects to default 404 page
-  if (hideSearchPage) return { notFound: true };
 
   return { props: { ...translations } };
 };
