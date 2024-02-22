f3b2d25f909805ad8b10fddd92da89acf8cc4588
Ryan Lewis <ryanlewis@navapbc.com>
2024-02-21 16:20:08 -0800

    fix test ts errors

--- frontend/tests/api/SearchOpportunityApi.test.ts
+++ frontend/tests/api/SearchOpportunityApi.test.ts
@@ -1,7 +1,7 @@
 import SearchOpportunityAPI from "../../src/api/SearchOpportunityAPI";
 
 const mockFetch = ({
-  response = { data: [], errors: [], warnings: [] },
+  response = { data: { opportunities: [] }, errors: [], warnings: [] },
   ok = true,
   status = 200,
 }) => {
@@ -12,13 +12,31 @@ const mockFetch = ({
   });
 };
 
+const originalEnv = process.env;
+
 describe("SearchOpportunityAPI", () => {
   let searchApi: SearchOpportunityAPI;
   const baseRequestHeaders = {
     "Content-Type": "application/json",
   };
 
+  beforeAll(() => {
+    process.env = {
+      NODE_ENV: "development",
+      apiUrl: "http://localhost",
+    };
+  });
+
+  afterAll(() => {
+    delete process.env.apiUrl;
+  });
+
   beforeEach(() => {
+    jest.resetModules();
+    process.env = {
+      ...originalEnv,
+    };
+
     jest.resetAllMocks();
 
     searchApi = new SearchOpportunityAPI();
@@ -29,28 +47,26 @@ describe("SearchOpportunityAPI", () => {
       global.fetch = mockFetch({
         response: {
           data: { opportunities: [] },
+          errors: [],
+          warnings: [],
         },
       });
     });
 
     it("sends GET request to search opportunities endpoint with query parameters", async () => {
       const queryParams = { keyword: "science" };
-      await searchApi.getSearchOpportunities(queryParams);
+      const response = await searchApi.getSearchOpportunities(queryParams);
 
       expect(fetch).toHaveBeenCalledWith(
-        `${process.env.apiUrl}/search/opportunities?keyword=science`,
+        `${process.env.apiUrl as string}/search/opportunities?keyword=science`,
         {
           method: "GET",
           headers: baseRequestHeaders,
+          body: null,
         }
       );
-    });
-
-    it("resolves with search response data", async () => {
-      const queryParams = { keyword: "science" };
-      const response = await searchApi.getSearchOpportunities(queryParams);
-
       expect(response.data).toEqual({ opportunities: [] });
+      expect(1).toBe(1);
     });
   });
 });
