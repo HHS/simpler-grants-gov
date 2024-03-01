259642a940d4421e8f8c09f304bda290edaf685c
Ryan Lewis <ryanlewis@navapbc.com>
2024-02-29 20:18:30 -0800

    format

--- frontend/src/services/searchfetcher/MockSearchFetcher.ts
+++ frontend/src/services/searchfetcher/MockSearchFetcher.ts
@@ -1,11 +1,11 @@
 import { Opportunity } from "../../types/searchTypes";
 import { SearchFetcher } from "./SearchFetcher";
-import mockData from '../../api/mock/APIMockResponse.json';
+import mockData from "../../api/mock/APIMockResponse.json";
 
 export class MockSearchFetcher extends SearchFetcher {
   async fetchOpportunities(): Promise<Opportunity[]> {
     // simulate delay
-    await new Promise(resolve => setTimeout(resolve, 750));
+    await new Promise((resolve) => setTimeout(resolve, 750));
     return mockData.data;
   }
 }
