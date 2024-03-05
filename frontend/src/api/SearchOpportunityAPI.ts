1abd0c39a73a6ad876d573e52f3ee89339dc644b
Ryan Lewis <93001277+rylew1@users.noreply.github.com>
2024-03-04 16:34:37 -0800

    [Issue #1294]: API setup part 2 (#1386)
    ## Summary
    Fixes #1294 
    
    - Working API calls local-to-local
    - MockSearchFetcher fetches json file
    - Add CORS to API
    - Hardcode public auth token 
    - Fix tests

--- frontend/src/api/SearchOpportunityAPI.ts
+++ frontend/src/api/SearchOpportunityAPI.ts
@@ -1,29 +1,42 @@
 import BaseApi, { JSONRequestBody } from "./BaseApi";
 
-export interface SearchResponseData {
-  opportunities: unknown[];
-}
+import { Opportunity } from "../types/searchTypes";
+
+export type SearchResponseData = Opportunity[];
 
 export default class SearchOpportunityAPI extends BaseApi {
   get basePath(): string {
-    return "search/opportunities";
+    return process.env.NEXT_PUBLIC_API_URL || "";
   }
 
   get namespace(): string {
-    return "searchOpportunities";
+    return "opportunities";
   }
 
   get headers() {
-    return {};
+    const baseHeaders = super.headers;
+    const searchHeaders = {};
+    return { ...baseHeaders, ...searchHeaders };
   }
 
-  async getSearchOpportunities(queryParams?: JSONRequestBody) {
-    const subPath = "";
+  async searchOpportunities(queryParams?: JSONRequestBody) {
+    const requestBody = {
+      pagination: {
+        order_by: "opportunity_id",
+        page_offset: 1,
+        page_size: 25,
+        sort_direction: "ascending",
+      },
+      ...queryParams,
+    };
 
+    const subPath = "search";
     const response = await this.request<SearchResponseData>(
-      "GET",
+      "POST",
+      this.basePath,
+      this.namespace,
       subPath,
-      queryParams,
+      requestBody,
     );
 
     return response;
--- frontend/src/services/searchfetcher/SearchFetcher.ts
+++ frontend/src/services/searchfetcher/SearchFetcher.ts
@@ -1,12 +1,12 @@
-import { Opportunity } from "../../types/searchTypes";
+import { SearchResponseData } from "../../api/SearchOpportunityAPI";
 
 export abstract class SearchFetcher {
-  abstract fetchOpportunities(): Promise<Opportunity[]>;
+  abstract fetchOpportunities(): Promise<SearchResponseData>;
 }
 
 export async function fetchSearchOpportunities(
   searchFetcher: SearchFetcher,
-): Promise<Opportunity[]> {
+): Promise<SearchResponseData> {
   try {
     return await searchFetcher.fetchOpportunities();
   } catch (error) {
