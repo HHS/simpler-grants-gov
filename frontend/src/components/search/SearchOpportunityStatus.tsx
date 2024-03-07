f00bfd3daa4452221d1bd79315eca7463bc76325
Ryan Lewis <ryanlewis@navapbc.com>
2024-03-06 22:37:27 -0800

    add search opportunity checkboxes

--- frontend/src/components/search/SearchOpportunityStatus.tsx
+++ frontend/src/components/search/SearchOpportunityStatus.tsx
@@ -2,56 +2,27 @@ import React from "react";
 
 export default function SearchOpportunityStatus() {
   return (
-    <fieldset className="usa-fieldset">
-      <legend className="usa-sr-only">Opportunity status</legend>
-      <div className="usa-checkbox">
-        <input
-          className="usa-checkbox__input"
-          id="checkbox-forecasted"
-          type="checkbox"
-          name="status"
-          value="forecasted"
-        />
-        <label className="usa-checkbox__label" htmlFor="checkbox-forecasted">
-          Forecasted
-        </label>
-      </div>
-      <div className="usa-checkbox">
-        <input
-          className="usa-checkbox__input"
-          id="checkbox-posted"
-          type="checkbox"
-          name="status"
-          value="posted"
-        />
-        <label className="usa-checkbox__label" htmlFor="checkbox-posted">
-          Posted
-        </label>
-      </div>
-      <div className="usa-checkbox">
-        <input
-          className="usa-checkbox__input"
-          id="checkbox-closed"
-          type="checkbox"
-          name="status"
-          value="closed"
-        />
-        <label className="usa-checkbox__label" htmlFor="checkbox-closed">
-          Closed
-        </label>
-      </div>
-      <div className="usa-checkbox">
-        <input
-          className="usa-checkbox__input"
-          id="checkbox-archived"
-          type="checkbox"
-          name="status"
-          value="archived"
-        />
-        <label className="usa-checkbox__label" htmlFor="checkbox-archived">
-          Archived
-        </label>
-      </div>
-    </fieldset>
+    <div className="checkbox-quadrant">
+      <label className="checkbox-container">
+        Forecasted
+        <input type="checkbox" name="forecased" />
+        <span className="checkmark"></span>
+      </label>
+      <label className="checkbox-container">
+        Posted
+        <input type="checkbox" name="posted" />
+        <span className="checkmark"></span>
+      </label>
+      <label className="checkbox-container">
+        Closed
+        <input type="checkbox" name="closed" />
+        <span className="checkmark"></span>
+      </label>
+      <label className="checkbox-container">
+        Archived
+        <input type="checkbox" name="archived" />
+        <span className="checkmark"></span>
+      </label>
+    </div>
   );
 }
