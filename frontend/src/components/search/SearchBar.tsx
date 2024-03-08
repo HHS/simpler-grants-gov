3a0cc90ed931478997a0d5fb3ef9cd837547a8a0
Andy Cochran <andycochran@users.noreply.github.com>
2024-03-08 12:27:44 -0500

    [Issue #1223] add layout to search results (#1453)
    - Add basic layout and styling to the search results list 
    - Nix unnecessary search CSS
    - Use Truss components where possible instead of rolling our own, or use USWDS markup
    - Clean up quite a lot of layout and markup for the new search components
    - Leave TODO comments in places that need more work

--- frontend/src/components/search/SearchBar.tsx
+++ frontend/src/components/search/SearchBar.tsx
@@ -2,17 +2,24 @@ import React from "react";
 
 export default function SearchBar() {
   return (
-    <>
-      <input
-        className="usa-input"
-        id="search-input-text"
-        name="search-input"
-        type="search"
-        placeholder="Search a keyword"
-      />
-      <button className="usa-button search-submit-button" type="submit">
+    <form className="usa-search usa-search--big" role="search">
+      <label className="usa-sr-only" htmlFor="search-field">
         Search
+      </label>
+      <input
+        className="usa-input maxw-none"
+        id="search-field"
+        type="search"
+        name="search"
+      />
+      <button className="usa-button" type="submit">
+        <span className="usa-search__submit-text">Search </span>
+        {/* <img
+          src="/assets/img/usa-icons-bg/search--white.svg"
+          className="usa-search__submit-icon"
+          alt="Search"
+        /> */}
       </button>
-    </>
+    </form>
   );
 }
