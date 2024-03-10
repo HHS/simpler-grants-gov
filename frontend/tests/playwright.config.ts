6ce4bd7b9540b7385eb9d55b9e8fcddc8fd3e34e
kim <7771178+kniedermaier@users.noreply.github.com>
2024-03-10 14:07:05 -0700

    Update frontend/tests/playwright.config.ts
    Co-authored-by: Aaron Couch <acinternets@gmail.com>

--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -51,21 +51,6 @@ export default defineConfig({
     {
       name: "Mobile Chrome",
       use: { ...devices["Pixel 7"] },
-    },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 14"] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
