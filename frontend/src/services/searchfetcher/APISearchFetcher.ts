d055b39d68c31c65c0d9c6efdc3d590a449305c6
Ryan Lewis <ryanlewis@navapbc.com>
2024-03-11 09:34:29 -0700

    add back in simulated delay on APISearchFetcher (but commented out)

--- frontend/src/services/searchfetcher/APISearchFetcher.ts
+++ frontend/src/services/searchfetcher/APISearchFetcher.ts
@@ -16,6 +16,10 @@ export class APISearchFetcher extends SearchFetcher {
 
   async fetchOpportunities(): Promise<SearchResponseData> {
     try {
+
+      // Keep commented in case we need to simulate a delay to test loaders
+      //  await new Promise((resolve) => setTimeout(resolve, 1250));
+
       const response = await this.searchApi.searchOpportunities();
       if (!response.data) {
         throw new Error(`No data returned from API`);
