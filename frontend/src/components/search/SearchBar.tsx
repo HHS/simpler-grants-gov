38822e3058b782ab51b57672b49037c2faef866b
Ryan Lewis <ryanlewis@navapbc.com>
2024-03-11 09:33:58 -0700

    change to `search-text-input` name

--- frontend/src/components/search/SearchBar.tsx
+++ frontend/src/components/search/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
         className="usa-input maxw-none"
         id="search-field"
         type="search"
-        name="search-keyword"
+        name="search-text-input"
       />
       <button className="usa-button" type="submit">
         <span className="usa-search__submit-text">Search </span>
