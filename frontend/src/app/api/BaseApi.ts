99d4144ab345aefd87bbd0e065b8a8b370c689fb
Ryan Lewis <ryanlewis@navapbc.com>
2024-03-14 14:11:41 -0700

    turn off camelcase to allow api values

--- frontend/src/app/api/BaseApi.ts
+++ frontend/src/app/api/BaseApi.ts
@@ -103,7 +103,6 @@ export default abstract class BaseApi {
       // throw fetchErrorToNetworkError(error);
     }
 
-    /* eslint-disable camelcase */
 
     const { data, message, pagination_info, status_code, errors, warnings } =
       responseBody;
