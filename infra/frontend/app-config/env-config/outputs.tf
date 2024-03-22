e126ebd72fd0a7b79eff055b7f7e60fa82c0a52e
Kai Siren <kaisiren@navapbc.com>
2024-03-21 22:19:05 -0700

    add to frontend as well

--- infra/frontend/app-config/env-config/outputs.tf
+++ infra/frontend/app-config/env-config/outputs.tf
@@ -14,6 +14,12 @@ output "database_config" {
 output "service_config" {
   value = {
     region = var.default_region
+    extra_environment_variables = merge(
+      local.default_extra_environment_variables,
+      var.service_override_extra_environment_variables
+    )
+
+    secrets = toset(local.secrets)
   }
 }
 
