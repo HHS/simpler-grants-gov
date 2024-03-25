9b740a2a07210220f15fbbead4fb200a6c2f0095
widal001 <billy.daly@agile6.com>
2024-03-25 14:27:59 -0400

    refactor: Parses args passed to linter

--- linters/scripts/close-issues-in-done-col.sh
+++ linters/scripts/close-issues-in-done-col.sh
@@ -2,13 +2,54 @@
 # Usage: ./scripts/close-issues-in-done-col.sh --dry-run
 # Closes issues that are in the "Done" column on a project but still open in the repo
 
-# set the variables
+# parse command line args with format `--option arg`
+# see this stack overflow for more details:
+# https://stackoverflow.com/a/14203146/7338319
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --dry-run)
+      echo "Running in dry run mode"
+      DRY_RUN=YES
+      shift # past argument
+      ;;
+    --batch)
+      BATCH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --org)
+      ORG="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --project)
+      PROJECT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --status)
+      STATUS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+# set script-specific variables
 mkdir -p tmp
 to_close_file="./tmp/open-issues-that-are-done.txt"
 query=$(cat ./queries/get-project-items.graphql)
 
 # print the parsed variables for debugging
-echo "Finding open issues in the ${STATUS} column of GitHub project: ${ORG}/${PROJECT}"
+echo "Finding open issues in the '${STATUS}' column of GitHub project: ${ORG}/${PROJECT}"
 
 # get all tickets from the project with their
 # URL, open/closed state in the repo, and status on the project
@@ -31,7 +72,12 @@ gh api graphql \
 # iterate through the list of URLs written to the to_close_file
 # and close them with a comment indicating the reason for closing
 while read URL; do
-  echo "Would close issue with URL: ${URL}"
-#   gh issue close $URL \
-#   --comment "Closing because issue was marked as '${STATUS}' in https://github.com/orgs/${ORG}/projects/${PROJECT}"
+  if [[ $DRY_RUN == "YES" ]];
+  then
+    echo "Would close issue with URL: ${URL}"
+  else
+    echo "Closing issue with URL: ${URL}"
+    gh issue close $URL \
+    --comment "Closing because issue was marked as '${STATUS}' in https://github.com/orgs/${ORG}/projects/${PROJECT}"
+  fi
 done < $to_close_file
