982746b7ee7436c8c17fd12cb0830fd9a79b7ee4
Kai Siren <kaisiren@navapbc.com>
2024-03-25 12:28:52 -0700

    checkpoint-kai-1711394932

---
+++ infra/analytics/app-config/outputs.tf
@@ -0,0 +1,19 @@
+output "app_name" {
+  value = local.app_name
+}
+
+output "account_names_by_environment" {
+  value = local.account_names_by_environment
+}
+
+output "image_repository_name" {
+  value = local.image_repository_name
+}
+
+output "build_repository_config" {
+  value = local.build_repository_config
+}
+
+output "environment_configs" {
+  value = local.environment_configs
+}
--- infra/analytics/database/main.tf
+++ infra/analytics/database/main.tf
@@ -18,7 +18,13 @@ data "aws_subnets" "database" {
   }
 }
 
+
 locals {
+  # The prefix key/value pair is used for Terraform Workspaces, which is useful for projects with multiple infrastructure developers.
+  # By default, Terraform creates a workspace named “default.” If a non-default workspace is not created this prefix will equal “default”,
+  # if you choose not to use workspaces set this value to "dev"
+  prefix = terraform.workspace == "default" ? "" : "${terraform.workspace}-"
+
   # Add environment specific tags
   tags = merge(module.project_config.default_tags, {
     environment = var.environment_name
@@ -69,4 +75,29 @@ data "aws_security_groups" "aws_services" {
     name   = "vpc-id"
     values = [data.aws_vpc.network.id]
   }
+}
+
+module "database" {
+  source = "../../modules/database"
+
+  name                        = "${local.prefix}${local.database_config.cluster_name}"
+  access_policy_name          = "${local.prefix}${local.database_config.access_policy_name}"
+  app_access_policy_name      = "${local.prefix}${local.database_config.app_access_policy_name}"
+  migrator_access_policy_name = "${local.prefix}${local.database_config.migrator_access_policy_name}"
+
+  # The following are not AWS infra resources and therefore do not need to be
+  # isolated via the terraform workspace prefix
+  app_username      = local.database_config.app_username
+  migrator_username = local.database_config.migrator_username
+  schema_name       = local.database_config.schema_name
+  instance_count    = local.database_config.instance_count
+
+  enable_http_endpoint = true
+
+  vpc_id                         = data.aws_vpc.network.id
+  private_subnet_ids             = data.aws_subnets.database.ids
+  aws_services_security_group_id = data.aws_security_groups.aws_services.ids[0]
+  db_subnet_group_name           = var.environment_name
+  environment_name               = var.environment_name
+  grants_gov_oracle_cidr_block   = module.project_config.network_configs[var.environment_name].grants_gov_oracle_cidr_block
 }
