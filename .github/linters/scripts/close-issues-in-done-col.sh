8bea3cbf69490d4a6bb6421950a94e1fb1058f7c
Billy Daly <billy.daly@agile6.com>
2024-03-26 15:36:48 -0400

    [Issue #1400] Close issues in done column (#1533)
    * feat: Adds a `.github/linters/` sub-directory 
    Stores custom linters for the project
    * feat: Adds a `.github/linters/scripts/`
    Stores custom scripts used by the linters
    * Creates a `.github/linters/queries/` sub-directory
    Stores graphql queries used by the linters
    * feat: Adds `linters/scripts/close-issues-in-done-col.sh`
    Linter to close issues in the done column that are still open
    * ci: Adds `.github/workflows/ci-project-linters.yml` 
    Checks all linters by doing a dry run
    * ci: Adds `.github/workflows/lint-close-done-issues.yml` 
    Runs the close done issues linter each night at midnight

---
+++ .github/linters/scripts/close-issues-in-done-col.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+# Usage: ./scripts/close-issues-in-done-col.sh --dry-run
+# Closes issues that are in the "Done" column on a project but still open in the repo
+
+# parse command line args with format `--option arg`
+# see this stack overflow for more details:
+# https://stackoverflow.com/a/14203146/7338319
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --dry-run)
+      echo "Running in dry run mode"
+      DRY_RUN=YES
+      shift # past argument
+      ;;
+    --batch)
+      BATCH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --org)
+      ORG="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --project)
+      PROJECT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --status)
+      STATUS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+# set script-specific variables
+mkdir -p tmp
+to_close_file="./tmp/open-issues-that-are-done.txt"
+query=$(cat ./queries/get-project-items.graphql)
+
+# print the parsed variables for debugging
+echo "Finding open issues in the '${STATUS}' column of GitHub project: ${ORG}/${PROJECT}"
+
+# get all tickets from the project with their
+# URL, open/closed state in the repo, and status on the project
+gh api graphql \
+ --paginate \
+ --field login="${ORG}" \
+ --field project="${PROJECT}" \
+ --field batch="${BATCH}" \
+ -f query="${query}" \
+ --jq ".data.organization.projectV2.items.nodes" |\
+ # combine results into a single array
+ jq --slurp 'add' |\
+ # isolate the URLs of the issues that are marked "Done" in the project
+ # but still open in the repo, and use --raw-ouput flag to remove quotes
+ jq --raw-output "
+ .[]
+ | select((.status.name == \"${STATUS}\") and (.issue.state == \"OPEN\"))
+ | .issue.url" > $to_close_file  # write output to a file
+
+# iterate through the list of URLs written to the to_close_file
+# and close them with a comment indicating the reason for closing
+while read URL; do
+  if [[ $DRY_RUN == "YES" ]];
+  then
+    echo "Would close issue with URL: ${URL}"
+  else
+    echo "Closing issue with URL: ${URL}"
+    gh issue close $URL \
+    --comment "Closing because issue was marked as '${STATUS}' in https://github.com/orgs/${ORG}/projects/${PROJECT}"
+  fi
+done < $to_close_file
