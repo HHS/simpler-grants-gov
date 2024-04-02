4ee41df84d7a4504494b755ff900edcd41918e8b
Kai Siren <kaisiren@navapbc.com>
2024-04-02 10:55:10 -0700

    sprint reports sfn

---
+++ infra/analytics/service/sfn_sprint_reports.tf
@@ -0,0 +1,81 @@
+# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine
+resource "aws_sfn_state_machine" "sprint_reports" {
+
+  name     = "${local.service_name}-sprint-reports"
+  role_arn = module.service.task_role_arn
+
+  definition = jsonencode({
+    "StartAt" : "ExecuteECSTask",
+    "States" : {
+      "ExecuteECSTask" : {
+        "Type" : "Task",
+        # docs: https://docs.aws.amazon.com/step-functions/latest/dg/connect-ecs.html
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "Parameters" : {
+          "Cluster" : module.service.cluster_arn,
+          "TaskDefinition" : module.service.task_definition_arn,
+          "LaunchType" : "FARGATE",
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : data.aws_subnets.private.ids,
+              "SecurityGroups" : [module.service.app_security_group_id],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : local.service_name,
+                "Command" : [
+                  "make",
+                  "gh-data-export",
+                  "sprint-reports"
+                ]
+              }
+            ]
+          }
+        },
+        "End" : true
+      }
+    }
+  })
+
+  logging_configuration {
+    log_destination        = "${module.service.service_logs_arn}:*"
+    include_execution_data = true
+    level                  = "ERROR"
+  }
+
+  tracing_configuration {
+    enabled = true
+  }
+}
+
+# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/scheduler_schedule_group
+resource "aws_scheduler_schedule_group" "sprint_reports" {
+  name = "${local.service_name}-sprint-reports"
+}
+
+# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/scheduler_schedule
+resource "aws_scheduler_schedule" "sprint_reports" {
+  # checkov:skip=CKV_AWS_297:Ignore the managed customer KMS key requirement for now
+
+  name                         = "${local.service_name}-sprint-reports"
+  state                        = "ENABLED"
+  group_name                   = aws_scheduler_schedule_group.sprint_reports.id
+  schedule_expression          = "rate(1 days)"
+  schedule_expression_timezone = "US/Eastern"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  # target is the state machine
+  target {
+    arn      = aws_sfn_state_machine.sprint_reports.arn
+    role_arn = module.service.task_role_arn
+
+    retry_policy {
+      maximum_retry_attempts = 0 # dont retry, just wait for the next execution
+    }
+  }
+}
