3661f0287d478c3dead8899e0e60490b9acbd671
Kai Siren <kaisiren@navapbc.com>
2024-04-05 12:42:19 -0700

    Add action configuration

--- infra/analytics/app-config/dev.tf
+++ infra/analytics/app-config/dev.tf
@@ -3,4 +3,8 @@ module "dev_config" {
   app_name       = local.app_name
   default_region = module.project_config.default_region
   environment    = "dev"
+  service_override_extra_environment_variables = {
+    # In dev, only show the results in the AWS console
+    ACTION = "show-results"
+  }
 }
--- infra/analytics/app-config/staging.tf
+++ infra/analytics/app-config/staging.tf
@@ -3,4 +3,8 @@ module "staging_config" {
   app_name       = local.app_name
   default_region = module.project_config.default_region
   environment    = "staging"
+  service_override_extra_environment_variables = {
+    # In staging, post results to the #z_bot-analytics-ci-test channel in slack
+    ACTION = "post-results"
+  }
 }
