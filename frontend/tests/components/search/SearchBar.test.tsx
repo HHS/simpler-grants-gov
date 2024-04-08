5a36cc039eacb6c60cea42eced28179aea237cb6
Ryan Lewis <93001277+rylew1@users.noreply.github.com>
2024-04-08 15:08:32 -0700

    [Issue #1471]: Tests for search components (#1617)
    ## Summary
    Fixes #1471
    
    ## Changes 
    - Tests for most of the search components

---
+++ frontend/tests/components/search/SearchBar.test.tsx
@@ -0,0 +1,49 @@
+import "@testing-library/jest-dom";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import React from "react";
+import SearchBar from "../../../src/components/search/SearchBar";
+import { axe } from "jest-axe";
+
+// Mock the hook since it's used in the component
+const mockUpdateQueryParams = jest.fn();
+
+jest.mock("../../../src/hooks/useSearchParamUpdater", () => ({
+  useSearchParamUpdater: () => ({
+    updateQueryParams: mockUpdateQueryParams,
+  }),
+}));
+
+describe("SearchBar", () => {
+  const initialQueryParams = "initial query";
+
+  it("should not have basic accessibility issues", async () => {
+    const { container } = render(
+      <SearchBar initialQueryParams={initialQueryParams} />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it("updates the input value when typing in the search field", () => {
+    render(<SearchBar initialQueryParams={initialQueryParams} />);
+
+    const input = screen.getByRole("searchbox");
+    fireEvent.change(input, { target: { value: "new query" } });
+
+    expect(input).toHaveValue("new query");
+  });
+
+  it("calls updateQueryParams with the correct argument when submitting the form", () => {
+    render(<SearchBar initialQueryParams={initialQueryParams} />);
+
+    const input = screen.getByRole("searchbox");
+    fireEvent.change(input, { target: { value: "new query" } });
+
+    const searchButton = screen.getByRole("button", { name: /search/i });
+    fireEvent.click(searchButton);
+
+    expect(mockUpdateQueryParams).toHaveBeenCalledWith("new query", "query");
+  });
+});
