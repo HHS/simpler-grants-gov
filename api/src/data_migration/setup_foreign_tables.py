43da4b8ab10a8e896f2fc4b37c6fb81fabf4f586
James Bursa <james@navapbc.com>
2024-04-18 10:33:55 -0400

    Depend on SQLAlchemy to do more of the generating for us

--- api/src/data_migration/setup_foreign_tables.py
+++ api/src/data_migration/setup_foreign_tables.py
@@ -1,13 +1,12 @@
 import logging
-import re
 
 import sqlalchemy
 from pydantic import Field
-from sqlalchemy import text
 
 import src.adapters.db as db
 import src.adapters.db.flask_db as flask_db
 import src.db.foreign
+import src.db.foreign.dialect
 from src.constants.schema import Schemas
 from src.data_migration.data_migration_blueprint import data_migration_blueprint
 from src.util.env_config import PydanticBaseEnvConfig
@@ -40,50 +39,31 @@ def build_sql(table: sqlalchemy.schema.Table, is_local: bool, schema_name: str) 
     Build the SQL for creating a possibly foreign data table. If running
     with is_local, it instead creates a regular table.
 
-    Assume you have a table with two columns, an "ID" primary key column, and a "description" text column,
-    you would call this as::
-
-        build_sql("EXAMPLE_TABLE", [Column("ID", "integer", is_nullable=False, is_primary_key=True), Column("DESCRIPTION", "text")], is_local)
-
-    Depending on whether the is_local bool is true or false would give two different outputs.
-
     is_local is True::
 
-        CREATE TABLE IF NOT EXISTS foreign_example_table (ID integer CONSTRAINT EXAMPLE_TABLE_pkey PRIMARY KEY NOT NULL,DESCRIPTION text)
+        CREATE TABLE IF NOT EXISTS foreign_example_table
+        (ID integer CONSTRAINT EXAMPLE_TABLE_pkey PRIMARY KEY NOT NULL,DESCRIPTION text)
 
     is_local is False::
 
-        CREATE FOREIGN TABLE IF NOT EXISTS foreign_example_table (ID integer OPTIONS (key 'true') NOT NULL,DESCRIPTION text) SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'EXAMPLE_TABLE')
+        CREATE FOREIGN TABLE IF NOT EXISTS foreign_example_table
+        (ID integer OPTIONS (key 'true') NOT NULL,DESCRIPTION text)
+        SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'EXAMPLE_TABLE')
     """
 
-    column_sql_parts = []
-    for column in table.columns:
-        column_sql = str(sqlalchemy.schema.CreateColumn(column))
-
-        # Primary keys are defined as constraints in a regular table
-        # and as options in a foreign data table
-        if column.primary_key and is_local:
-            column_sql += f" CONSTRAINT {table.name}_pkey PRIMARY KEY"
-        elif column.primary_key and not is_local:
-            column_sql = re.sub(r"^(.*?)( NOT NULL)?$", r"\1 OPTIONS (key 'true')\2", column_sql)
-
-        column_sql_parts.append(column_sql)
-
-    create_table_command = "CREATE FOREIGN TABLE IF NOT EXISTS"
+    create_table = sqlalchemy.schema.CreateTable(table, if_not_exists=True)
     if is_local:
-        # Don't make a foreign table if running locally
-        create_table_command = "CREATE TABLE IF NOT EXISTS"
-
-    create_command_suffix = f" SERVER grants OPTIONS (schema 'EGRANTSADMIN', table '{table.name.upper()}')"  # noqa: B907
-    if is_local:
-        # We don't want the config at the end if we're running locally so unset it
-        create_command_suffix = ""
-
-    return f"{create_table_command} {schema_name}.{table.name} ({','.join(column_sql_parts)}){create_command_suffix}"
+        compiler = create_table.compile(schema_translate_map={"grants": schema_name})
+    else:
+        compiler = create_table.compile(
+            dialect=src.db.foreign.dialect.ForeignTableDialect(),
+            schema_translate_map={"grants": schema_name},
+        )
+    return str(compiler).strip()
 
 
 def _run_create_table_commands(db_session: db.Session, config: ForeignTableConfig) -> None:
     for table in src.db.foreign.metadata.tables.values():
         sql = build_sql(table, config.is_local_foreign_table, config.schema_name)
         logger.info("create table", extra={"table": table.name, "sql": sql})
-        db_session.execute(text(sql))
+        db_session.execute(sqlalchemy.text(sql))
--- api/tests/src/data_migration/test_setup_foreign_tables.py
+++ api/tests/src/data_migration/test_setup_foreign_tables.py
@@ -1,53 +1,57 @@
+import re
+
 import pytest
 import sqlalchemy
 
 import src.db.foreign
 from src.data_migration.setup_foreign_tables import build_sql
 
-EXPECTED_LOCAL_OPPORTUNITY_SQL = (
-    "CREATE TABLE IF NOT EXISTS {}.topportunity "
-    "(opportunity_id NUMERIC(20) NOT NULL CONSTRAINT topportunity_pkey PRIMARY KEY,"
-    "oppnumber VARCHAR(40),"
-    "revision_number NUMERIC(20),"
-    "opptitle VARCHAR(255),"
-    "owningagency VARCHAR(255),"
-    "publisheruid VARCHAR(255),"
-    "listed CHAR(1),"
-    "oppcategory CHAR(1),"
-    "initial_opportunity_id NUMERIC(20),"
-    "modified_comments VARCHAR(2000),"
-    "created_date DATE,"
-    "last_upd_date DATE,"
-    "creator_id VARCHAR(50),"
-    "last_upd_id VARCHAR(50),"
-    "flag_2006 CHAR(1),"
-    "category_explanation VARCHAR(255),"
-    "publisher_profile_id NUMERIC(20),"
-    "is_draft VARCHAR(1))"
-)
+EXPECTED_LOCAL_OPPORTUNITY_SQL = [
+    "CREATE TABLE IF NOT EXISTS __[SCHEMA_grants].topportunity (",
+    "opportunity_id NUMERIC(20) NOT NULL,",
+    "oppnumber VARCHAR(40),",
+    "revision_number NUMERIC(20),",
+    "opptitle VARCHAR(255),",
+    "owningagency VARCHAR(255),",
+    "publisheruid VARCHAR(255),",
+    "listed CHAR(1),",
+    "oppcategory CHAR(1),",
+    "initial_opportunity_id NUMERIC(20),",
+    "modified_comments VARCHAR(2000),",
+    "created_date DATE,",
+    "last_upd_date DATE,",
+    "creator_id VARCHAR(50),",
+    "last_upd_id VARCHAR(50),",
+    "flag_2006 CHAR(1),",
+    "category_explanation VARCHAR(255),",
+    "publisher_profile_id NUMERIC(20),",
+    "is_draft VARCHAR(1),",
+    "PRIMARY KEY (opportunity_id)",
+    ")",
+]
 
-EXPECTED_NONLOCAL_OPPORTUNITY_SQL = (
-    "CREATE FOREIGN TABLE IF NOT EXISTS {}.topportunity "
-    "(opportunity_id NUMERIC(20) OPTIONS (key 'true') NOT NULL,"
-    "oppnumber VARCHAR(40),"
-    "revision_number NUMERIC(20),"
-    "opptitle VARCHAR(255),"
-    "owningagency VARCHAR(255),"
-    "publisheruid VARCHAR(255),"
-    "listed CHAR(1),"
-    "oppcategory CHAR(1),"
-    "initial_opportunity_id NUMERIC(20),"
-    "modified_comments VARCHAR(2000),"
-    "created_date DATE,"
-    "last_upd_date DATE,"
-    "creator_id VARCHAR(50),"
-    "last_upd_id VARCHAR(50),"
-    "flag_2006 CHAR(1),"
-    "category_explanation VARCHAR(255),"
-    "publisher_profile_id NUMERIC(20),"
-    "is_draft VARCHAR(1))"
-    " SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'TOPPORTUNITY')"
-)
+EXPECTED_NONLOCAL_OPPORTUNITY_SQL = [
+    "CREATE FOREIGN TABLE IF NOT EXISTS __[SCHEMA_grants].topportunity (",
+    "opportunity_id NUMERIC(20) OPTIONS (key 'true') NOT NULL,",
+    "oppnumber VARCHAR(40),",
+    "revision_number NUMERIC(20),",
+    "opptitle VARCHAR(255),",
+    "owningagency VARCHAR(255),",
+    "publisheruid VARCHAR(255),",
+    "listed CHAR(1),",
+    "oppcategory CHAR(1),",
+    "initial_opportunity_id NUMERIC(20),",
+    "modified_comments VARCHAR(2000),",
+    "created_date DATE,",
+    "last_upd_date DATE,",
+    "creator_id VARCHAR(50),",
+    "last_upd_id VARCHAR(50),",
+    "flag_2006 CHAR(1),",
+    "category_explanation VARCHAR(255),",
+    "publisher_profile_id NUMERIC(20),",
+    "is_draft VARCHAR(1)",
+    ") SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'TOPPORTUNITY')",
+]
 
 
 TEST_METADATA = sqlalchemy.MetaData()
@@ -56,18 +60,21 @@ TEST_TABLE = sqlalchemy.Table(
     TEST_METADATA,
     sqlalchemy.Column("id", sqlalchemy.Integer, nullable=False, primary_key=True),
     sqlalchemy.Column("description", sqlalchemy.Text),
+    schema="schema1",
 )
-EXPECTED_LOCAL_TEST_SQL = (
-    "CREATE TABLE IF NOT EXISTS {}.test_table "
-    "(id INTEGER NOT NULL CONSTRAINT test_table_pkey PRIMARY KEY,"
-    "description TEXT)"
-)
-EXPECTED_NONLOCAL_TEST_SQL = (
-    "CREATE FOREIGN TABLE IF NOT EXISTS {}.test_table "
-    "(id INTEGER OPTIONS (key 'true') NOT NULL,"
-    "description TEXT)"
-    " SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'TEST_TABLE')"
-)
+EXPECTED_LOCAL_TEST_SQL = [
+    "CREATE TABLE IF NOT EXISTS __[SCHEMA_schema1].test_table (",
+    "id INTEGER NOT NULL,",
+    "description TEXT,",
+    "PRIMARY KEY (id)",
+    ")",
+]
+EXPECTED_NONLOCAL_TEST_SQL = [
+    "CREATE FOREIGN TABLE IF NOT EXISTS __[SCHEMA_schema1].test_table (",
+    "id INTEGER OPTIONS (key 'true') NOT NULL,",
+    "description TEXT",
+    ") SERVER grants OPTIONS (schema 'EGRANTSADMIN', table 'TEST_TABLE')",
+]
 
 
 @pytest.mark.parametrize(
@@ -90,4 +97,4 @@ EXPECTED_NONLOCAL_TEST_SQL = (
 def test_build_sql(table, is_local, expected_sql, test_foreign_schema):
     sql = build_sql(table, is_local, test_foreign_schema)
 
-    assert sql == expected_sql.format(test_foreign_schema)
+    assert re.split(r"\s*\n\s*", sql) == expected_sql
