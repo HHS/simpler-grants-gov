9e99a0a287c6b18f94d42e988c870d083b754338
Kai Siren <kaisiren@navapbc.com>
2024-04-24 15:45:20 -0700

    yet more

--- infra/api/app-config/dev.tf
+++ infra/api/app-config/dev.tf
@@ -6,7 +6,7 @@ module "dev_config" {
   has_database                    = local.has_database
   database_enable_http_endpoint   = true
   has_incident_management_service = local.has_incident_management_service
-  database_max_capacity           = 8
+  database_max_capacity           = 16
   database_min_capacity           = 2
 
   service_override_extra_environment_variables = {
