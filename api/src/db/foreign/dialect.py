15e92eca332e1f212a5b3272fcdc4c8d9ccda760
James Bursa <james@navapbc.com>
2024-04-24 17:09:34 -0400

    Add type annotations

--- api/src/db/foreign/dialect.py
+++ api/src/db/foreign/dialect.py
@@ -3,28 +3,32 @@
 #
 
 import re
+from typing import Any
 
 import sqlalchemy
+import sqlalchemy.dialects.postgresql
 
 
 class ForeignTableDDLCompiler(sqlalchemy.sql.compiler.DDLCompiler):
     """SQLAlchemy compiler for creating foreign tables."""
 
-    def create_table_constraints(self, _table, _include_foreign_key_constraints=None, **kw):
+    def create_table_constraints(
+        self, _table: Any, _include_foreign_key_constraints: Any = None, **kw: Any
+    ) -> str:
         return ""  # Don't generate any constraints.
 
-    def visit_create_table(self, create, **kw):
+    def visit_create_table(self, create: Any, **kw: Any) -> str:
         table = create.element
         table._prefixes = ("FOREIGN",)  # Add "FOREIGN" before "TABLE".
         sql = super().visit_create_table(create, **kw)
         table._prefixes = ()
         return sql
 
-    def post_create_table(self, table):
+    def post_create_table(self, table: Any) -> str:
         # Add foreign options at the end.
         return f" SERVER grants OPTIONS (schema 'EGRANTSADMIN', table '{table.name.upper()}')"
 
-    def visit_create_column(self, create, first_pk=False, **kw):
+    def visit_create_column(self, create: Any, first_pk: bool = False, **kw: Any) -> str:
         column = create.element
         sql = super().visit_create_column(create, first_pk, **kw)
         if sql and column.primary_key:
