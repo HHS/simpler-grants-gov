81ebfb4176a9e01af3ba6fdb43cf2b266dd96a22
James Bursa <james@navapbc.com>
2024-04-26 12:39:31 -0400

    [Issue #1686] load data from legacy (Oracle) tables to staging tables

---
+++ api/src/data_migration/load/load_oracle_data_task.py
@@ -0,0 +1,29 @@
+#
+# Load data from legacy (Oracle) tables to staging tables.
+#
+
+import logging
+
+import src.task.task
+from src.adapters.db import PostgresDBClient
+
+logger = logging.getLogger(__name__)
+
+
+class LoadOracleDataTask(src.task.task.Task):
+    def run_task(self) -> None:
+        with self.db_session.begin():
+            self.load_data()
+
+    def load_data(self) -> None:
+        pass
+
+
+def main():
+    import src.logging
+
+    with src.logging.init(__name__):
+        db_client = PostgresDBClient()
+
+        with db_client.get_session() as db_session:
+            LoadOracleDataTask(db_session).run()
