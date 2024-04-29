660ff52a815ee7bf0b589eb42f38f465b2dc688b
Aaron Couch <acinternets@gmail.com>
2024-04-29 10:52:09 -0400

    Update server.ts for ts format error

--- frontend/src/i18n/server.ts
+++ frontend/src/i18n/server.ts
@@ -8,6 +8,8 @@ import { getMessagesWithFallbacks } from "./getMessagesWithFallbacks";
  * This method is used behind the scenes by `next-intl/plugin`, which is setup in next.config.js.
  * @see https://next-intl-docs.vercel.app/docs/usage/configuration#nextconfigjs
  */
+
+// @ts-expect-error TS2345: Argument of type error is expected behavior by next-intl maintainer: https://github.com/amannn/next-intl/issues/991#issuecomment-2050087509
 export default getRequestConfig(async ({ locale }) => {
   return {
     formats,
