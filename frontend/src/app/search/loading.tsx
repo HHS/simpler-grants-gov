bf35150139187a5bb3e30c31f4d76d05f2a65b1e
Ryan Lewis <ryanlewis@navapbc.com>
2024-05-02 16:45:07 -0700

    initial commit

--- frontend/src/app/search/loading.tsx
+++ frontend/src/app/search/loading.tsx
@@ -1,27 +1,11 @@
 import React from "react";
+import Spinner from "../../components/Spinner";
 
 export default function Loading() {
-  const listStyle: React.CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    height: "50vh",
-    listStyleType: "none",
-  };
-
-  const skeletonStyle = {
-    backgroundColor: "#eee",
-    borderRadius: "4px",
-    height: "20px",
-    margin: "10px 0",
-    width: "50%",
-  };
-
   return (
-    <ul style={listStyle}>
-      {Array.from({ length: 10 }).map((_, index) => (
-        <li key={index} style={skeletonStyle} />
-      ))}
-    </ul>
+    <div className="usa-flex usa-align-center">
+      <Spinner />
+      <span className="margin-left-3 bold">Loading results...</span>
+    </div>
   );
 }
