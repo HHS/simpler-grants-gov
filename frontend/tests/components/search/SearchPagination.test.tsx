945942811e62f1c52f4c3a06dfdc398a970fe172
Ryan Lewis <ryanlewis@navapbc.com>
2024-05-06 15:43:36 -0700

    uncomment tests

--- frontend/tests/components/search/SearchPagination.test.tsx
+++ frontend/tests/components/search/SearchPagination.test.tsx
@@ -3,15 +3,11 @@ import "@testing-library/jest-dom";
 import SearchPagination, {
   PaginationPosition,
 } from "../../../src/components/search/SearchPagination";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import React from "react";
+import { axe } from "jest-axe";
 import { jest } from "@jest/globals";
-
-jest.mock("react-dom", () => ({
-  __esModule: true,
-  useFormStatus: jest.fn(() => ({ pending: false })),
-}));
 
 describe("SearchPagination", () => {
   const mockHandlePageChange = jest.fn();
@@ -21,25 +17,25 @@ describe("SearchPagination", () => {
     jest.clearAllMocks();
   });
 
-  //   it("should not have basic accessibility issues", async () => {
-  //     const { container } = render(
-  //       <SearchPagination
-  //         showHiddenInput={true}
-  //         totalPages={totalPages}
-  //         page={page}
-  //         handlePageChange={mockHandlePageChange}
-  //         position={PaginationType.Top}
-  //       />,
-  //     );
-  //     const results = await axe(container, {
-  //       rules: {
-  //         // Disable specific rules that are known to fail due to third-party components
-  //         list: { enabled: false },
-  //         "svg-img-alt": { enabled: false },
-  //       },
-  //     });
-  //     expect(results).toHaveNoViolations();
-  //   });
+  it("should not have basic accessibility issues", async () => {
+    const { container } = render(
+      <SearchPagination
+        showHiddenInput={true}
+        totalPages={totalPages}
+        page={page}
+        handlePageChange={mockHandlePageChange}
+        position={PaginationPosition.Top}
+      />,
+    );
+    const results = await axe(container, {
+      rules: {
+        // Disable specific rules that are known to fail due to third-party components
+        list: { enabled: false },
+        "svg-img-alt": { enabled: false },
+      },
+    });
+    expect(results).toHaveNoViolations();
+  });
 
   it("renders hidden input when showHiddenInput is true", () => {
     render(
@@ -56,44 +52,44 @@ describe("SearchPagination", () => {
     expect(hiddenInput).toHaveValue("1");
   });
 
-  //   it("does not render hidden input when showHiddenInput is false", () => {
-  //     render(
-  //       <SearchPagination
-  //         showHiddenInput={false}
-  //         totalPages={totalPages}
-  //         page={page}
-  //         handlePageChange={mockHandlePageChange}
-  //         position={PaginationType.Top}
-  //       />,
-  //     );
-  //     expect(screen.queryByTestId("hiddenCurrentPage")).not.toBeInTheDocument();
-  //   });
+  it("does not render hidden input when showHiddenInput is false", () => {
+    render(
+      <SearchPagination
+        showHiddenInput={false}
+        totalPages={totalPages}
+        page={page}
+        handlePageChange={mockHandlePageChange}
+        position={PaginationPosition.Top}
+      />,
+    );
+    expect(screen.queryByTestId("hiddenCurrentPage")).not.toBeInTheDocument();
+  });
 
-  //   it("calls handlePageChange with next page on next button click", () => {
-  //     render(
-  //       <SearchPagination
-  //         showHiddenInput={true}
-  //         totalPages={totalPages}
-  //         page={page}
-  //         handlePageChange={mockHandlePageChange}
-  //         position={PaginationType.Top}
-  //       />,
-  //     );
-  //     fireEvent.click(screen.getByLabelText("Next page"));
-  //     expect(mockHandlePageChange).toHaveBeenCalledWith(page + 1);
-  //   });
+  it("calls handlePageChange with next page on next button click", () => {
+    render(
+      <SearchPagination
+        showHiddenInput={true}
+        totalPages={totalPages}
+        page={page}
+        handlePageChange={mockHandlePageChange}
+        position={PaginationPosition.Top}
+      />,
+    );
+    fireEvent.click(screen.getByLabelText("Next page"));
+    expect(mockHandlePageChange).toHaveBeenCalledWith(page + 1);
+  });
 
-  //   it("calls handlePageChange with previous page on previous button click", () => {
-  //     render(
-  //       <SearchPagination
-  //         showHiddenInput={true}
-  //         totalPages={totalPages}
-  //         page={2} // Set to second page to test going back to first page
-  //         handlePageChange={mockHandlePageChange}
-  //         position={PaginationType.Top}
-  //       />,
-  //     );
-  //     fireEvent.click(screen.getByLabelText("Previous page"));
-  //     expect(mockHandlePageChange).toHaveBeenCalledWith(1);
-  //   });
+  it("calls handlePageChange with previous page on previous button click", () => {
+    render(
+      <SearchPagination
+        showHiddenInput={true}
+        totalPages={totalPages}
+        page={2} // Set to second page to test going back to first page
+        handlePageChange={mockHandlePageChange}
+        position={PaginationPosition.Top}
+      />,
+    );
+    fireEvent.click(screen.getByLabelText("Previous page"));
+    expect(mockHandlePageChange).toHaveBeenCalledWith(1);
+  });
 });
