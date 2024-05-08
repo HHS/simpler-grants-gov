183723252e487e4644679fa73881808808baae4e
James Bursa <james@navapbc.com>
2024-05-08 11:44:55 -0400

    Add options to enable/disable each task

--- api/src/data_migration/command/load_transform.py
+++ api/src/data_migration/command/load_transform.py
@@ -4,6 +4,8 @@
 
 import logging
 
+import click
+
 import src.adapters.db as db
 import src.adapters.db.flask_db as flask_db
 import src.db.foreign
@@ -20,15 +22,23 @@ logger = logging.getLogger(__name__)
 @data_migration_blueprint.cli.command(
     "load-transform", help="Load and transform data from the legacy database into our database"
 )
+@click.option("--load/--no-load", default=True, help="run LoadOracleDataTask")
+@click.option("--transform/--no-transform", default=True, help="run TransformOracleDataTask")
+@click.option(
+    "--set-current/--no-set-current", default=True, help="run SetCurrentOpportunitiesTask"
+)
 @flask_db.with_db_session()
-def load_transform(db_session: db.Session) -> None:
+def load_transform(db_session: db.Session, load: bool, transform: bool, set_current: bool) -> None:
     logger.info("load and transform start")
 
     foreign_tables = {t.name: t for t in src.db.foreign.metadata.tables.values()}
     staging_tables = {t.name: t for t in src.db.models.staging.metadata.tables.values()}
 
-    LoadOracleDataTask(db_session, foreign_tables, staging_tables).run()
-    TransformOracleDataTask(db_session).run()
-    SetCurrentOpportunitiesTask(db_session).run()
+    if load:
+        LoadOracleDataTask(db_session, foreign_tables, staging_tables).run()
+    if transform:
+        TransformOracleDataTask(db_session).run()
+    if set_current:
+        SetCurrentOpportunitiesTask(db_session).run()
 
     logger.info("load and transform complete")
