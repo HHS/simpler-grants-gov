b49f46a0ef209de506dda9c056a204a884f6f4f9
James Bursa <james@navapbc.com>
2024-05-08 12:32:53 -0400

    Lint fixes

--- api/src/data_migration/__init__.py
+++ api/src/data_migration/__init__.py
@@ -1,6 +1,6 @@
 from src.data_migration.data_migration_blueprint import data_migration_blueprint
 
-from . import command
+from . import command  # noqa: F401
 
 # import any of the other files so they get initialized and attached to the blueprint
 import src.data_migration.copy_oracle_data  # noqa: F401 E402 isort:skip
--- api/src/data_migration/command/__init__.py
+++ api/src/data_migration/command/__init__.py
@@ -1,2 +1,2 @@
 # import all files so they get initialized and attached to the blueprint
-from . import load_transform
+from . import load_transform  # noqa: F401
