6758d3df7e8727d008e5c6f92c9509549d5315f1
James Bursa <james@navapbc.com>
2024-05-09 17:07:58 -0400

    Make the tool more robust & add --cluster flag

--- api/bin/execute_sql_rds.sh
+++ api/bin/execute_sql_rds.sh
@@ -4,7 +4,7 @@
 #
 # Examples:
 #   ./execute_sql_rds.sh <sql/table_list.sql
-#   ./execute_sql_rds.sh --multiple <sql/select_from_foreign_table.sql
+#   ./execute_sql_rds.sh --cluster=api-prod --multiple <sql/select_from_foreign_table.sql
 #
 # When using --multiple, provide one SQL statement per input line.
 #
@@ -17,19 +17,29 @@ CYAN='\033[96m'
 GREEN='\033[92m'
 END='\033[0m'
 
-RESOURCE_ARN='arn:aws:rds:...'
-SECRET_ARN='arn:aws:secretsmanager:...'
+CLUSTER=api-dev
+
+USAGE="Usage: $PROGRAM_NAME [OPTION]
+
+  --multiple         one SQL statement per input line (otherwise expects a single multi-line statement)
+  --cluster=CLUSTER  target RDS cluster (default $CLUSTER)
+"
 
 
 main() {
-  print_log "using database $RESOURCE_ARN"
+  cluster="$CLUSTER"
+  parse_arguments "$@"
+  print_log "using cluster $cluster"
+  read_cluster_arns
+  create_temporary_directory
 
-  if [ "$1" == "--multiple" ]
+  count=1
+  if [ $multiple ]
   then
-    print_log "multiple mode enabled (one statement per input line)"
     while read line
     do
       execute_statement "$line"
+      count=$((count + 1))
     done
   else
     execute_statement "$(cat)"
@@ -37,16 +47,57 @@ main() {
 }
 
 
+parse_arguments() {
+  for arg in "$@"
+  do
+    if [ "$arg" == "--multiple" ]; then
+      print_log "multiple mode enabled (one statement per input line)"
+      multiple=1
+    elif [[ "$arg" =~ ^--cluster=(.*)$ ]]; then
+      cluster="${BASH_REMATCH[1]}"
+    else
+      echo "$USAGE"
+      exit 1
+    fi
+  done
+}
+
+
+read_cluster_arns() {
+  resource_arn=$(aws rds describe-db-clusters --db-cluster-identifier="$cluster" \
+                     --query='DBClusters[0].DBClusterArn' --output=text)
+  secret_arn=$(aws rds describe-db-clusters --db-cluster-identifier="$cluster" \
+                   --query='DBClusters[0].MasterUserSecret.SecretArn' --output=text)
+  print_log "database resource $resource_arn"
+}
+
+
+create_temporary_directory() {
+  tmp_dir="/tmp/execute_sql_rds/execute_sql_rds.$(date +%s)"
+  mkdir -m "u=rwx,g=,o=" -p "$tmp_dir"
+  print_log "temporary directory $tmp_dir"
+}
+
+
 execute_statement() {
   print_log "$1"
+  result_path="$tmp_dir/result_$count.json"
+
   aws rds-data execute-statement \
-      --resource-arn "$RESOURCE_ARN" \
+      --resource-arn "$resource_arn" \
       --database "app" \
-      --secret-arn "$SECRET_ARN" \
+      --secret-arn "$secret_arn" \
       --sql "$1" \
+      --continue-after-timeout \
       --format-records-as JSON \
-      | jq -r .formattedRecords \
-      | jtbl --truncate
+      >"$result_path"
+
+  if grep formattedRecords "$result_path" >/dev/null
+  then
+    jq -r .formattedRecords "$result_path" | jtbl --truncate --markdown
+  else
+    cat "$result_path"
+  fi
 }
 
 
@@ -56,4 +107,4 @@ print_log() {
 }
 
 # Entry point
-main "$1"
+main "$@"
--- api/bin/sql/table_list.sql
+++ api/bin/sql/table_list.sql
@@ -1,7 +1,10 @@
 SELECT n.nspname as "Schema",
   c.relname as "Name",
   CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 't' THEN 'TOAST table' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'partitioned table' WHEN 'I' THEN 'partitioned index' END as "Type",
-  pg_catalog.pg_get_userbyid(c.relowner) as "Owner"
+  pg_catalog.pg_get_userbyid(c.relowner) as "Owner",
+  pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) as "Size",
+  pg_stat_get_last_analyze_time(c.oid) AS last_analyze,
+  pg_stat_get_last_autoanalyze_time(c.oid) AS last_autoanalyze
 FROM pg_catalog.pg_class c
      LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
      LEFT JOIN pg_catalog.pg_am am ON am.oid = c.relam
