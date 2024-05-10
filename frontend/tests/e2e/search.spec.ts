e309eafde227c6cb757d12b02036e448aadd4113
Ryan Lewis <ryanlewis@navapbc.com>
2024-05-09 18:04:27 -0700

    cleanup + more tests

--- frontend/tests/e2e/search.spec.ts
+++ frontend/tests/e2e/search.spec.ts
@@ -1,32 +1,28 @@
-import { expect, test } from "@playwright/test";
+import { BrowserContext, Locator, Page, expect, test } from "@playwright/test";
 
 test("should navigate from index to search page", async ({ page }) => {
   // Start from the index page with feature flag set
   await page.goto("/?_ff=showSearchV0:true");
 
   // Mobile chrome must first click the menu button
-  const menuButton = page.locator("button >> text=MENU");
-  if (await menuButton.isVisible()) {
-    await menuButton.click();
+  if (await hasMobileMenu(page)) {
+    const menuButton = getMobileMenuButton(page);
+    await clickMobileNavMenu(menuButton);
   }
-  // Locate the 'Search' link in the navigation bar and click on it
-  await page.click("nav >> text=Search");
+
+  await clickSearchNavLink(page);
 
   // Verify that the new URL is correct
-  await expect(page).toHaveURL("/search?status=forecasted,posted");
+  expectURLContainsQueryParam(page, "status", "forecasted,posted");
 
   // Verify the presence of "Search" content on the page
   await expect(page.locator("h1")).toContainText(
     "Search funding opportunities",
   );
 
-  // Verify that the 'forecasted' checkbox is checked
-  const forecastedCheckbox = page.locator("#status-forecasted");
-  await expect(forecastedCheckbox).toBeChecked();
-
-  // Verify that correct status checkboxes are checked
-  const postedCheckbox = page.locator("#status-posted");
-  await expect(postedCheckbox).toBeChecked();
+  // Verify that the 'forecasted' and 'posted' are checked
+  await expectIDIsChecked(page, "#status-forecasted");
+  await expectIDIsChecked(page, "#status-posted");
 });
 
 test.describe("Search page tests", () => {
@@ -39,15 +35,10 @@ test.describe("Search page tests", () => {
     page,
   }) => {
     const searchTerm = "0resultearch";
-    // Locate the search input field
-    const searchInput = page.locator("#query");
-    await searchInput.fill(searchTerm);
 
-    // Click on the 'Search' button
-    await page.click(".usa-search >> button[type='submit']");
+    await fillSearchInputAndSubmit(searchTerm, page);
 
-    const currentURL = page.url();
-    expect(currentURL).toContain(`query=${searchTerm}`);
+    expectURLContainsQueryParam(page, "query", searchTerm);
 
     const resultsHeading = page.getByRole("heading", {
       name: /0 Opportunities/i,
@@ -59,43 +50,130 @@ test.describe("Search page tests", () => {
     );
   });
 
-  test("should show and hide 'Loading results...' when clicking the Forecasted checkbox", async ({
-    page,
-  }) => {
-    // Locators
+  test("should show and hide loading state", async ({ page }) => {
+    const searchTerm = "advanced";
+    await fillSearchInputAndSubmit(searchTerm, page);
+
     const loadingIndicator = page.locator("text='Loading results...'");
+    await expect(loadingIndicator).toBeVisible();
+    await expect(loadingIndicator).toBeHidden();
 
-    // Scroll explicitly to the checkbox
-    await page.locator("#status-forecasted").scrollIntoViewIfNeeded();
+    const searchTerm2 = "agency";
+    await fillSearchInputAndSubmit(searchTerm2, page);
+    await expect(loadingIndicator).toBeVisible();
+    await expect(loadingIndicator).toBeHidden();
+  });
 
-    // Ensure the checkbox is enabled before interacting
-    await expect(page.locator("#status-forecasted")).toBeEnabled();
+  test("should retain filters in a new tab", async ({ page, context }) => {
+    const searchTerm = "education";
+    const checkboxIDs = ["#status-forecasted", "#status-posted"];
 
-    // console.log(
-    //   "Forecasted Checkbox Visible:",
-    //   await forecastedCheckbox.isVisible(),
-    // );
-    // console.log(
-    //   "Forecasted Checkbox Enabled:",
-    //   await forecastedCheckbox.isEnabled(),
-    // );
-    // console.log(
-    //   "Forecasted Checkbox Bounding Box:",
-    //   await forecastedCheckbox.boundingBox(),
-    // );
+    // await waitForSearchResultsLoaded(page);
 
-    // // Click the checkbox (force-click to ensure interaction)
-    // await forecastedCheckbox.click({ force: true });
+    await fillSearchInputAndSubmit(searchTerm, page);
 
-    // // Verify 'Loading results...' appears
-    // await expect(loadingIndicator).toBeVisible();
-    // // Verify 'Loading results...' disappears after data loads
-    // await expect(loadingIndicator).toBeHidden();
+    for (const checkboxId of checkboxIDs) {
+      await toggleCheckbox(page, checkboxId, true);
+    }
+    // Ensure URL contains all query parameters
+    expectURLContainsQueryParam(page, "query", searchTerm);
+    expectURLContainsQueryParam(page, "status", checkboxIDs.join(","));
 
-    // // Scroll explicitly to the checkbox again
-    // await forecastedCheckbox.scrollIntoViewIfNeeded();
+    const newPage = await goToNewPageUsingCurrentURL(page, context);
 
-    // // Click the checkbox again (force-click)
-    // await forecastedCheckbox.click({ force: true });
+    // Ensure search term and checkboxes are retained in the new tab
+    const searchInput = getSearchInput(page);
+    await expect(searchInput).toHaveValue(searchTerm);
+
+    for (const checkboxID of checkboxIDs) {
+      await expectIDIsChecked(newPage, checkboxID);
+    }
+
+    // Close the new tab
+    await newPage.close();
   });
 });
+
+// =========================
+// Test Helper Functions
+// =========================
+
+function getSearchInput(page: Page) {
+  return page.locator("#query");
+}
+
+async function fillSearchInputAndSubmit(term: string, page: Page) {
+  const searchInput = getSearchInput(page);
+  await searchInput.fill(term);
+  await page.click(".usa-search >> button[type='submit']");
+}
+
+function expectURLContainsQueryParam(
+  page: Page,
+  queryParamName: string,
+  queryParamValue: string,
+) {
+  const currentURL = page.url();
+  expect(currentURL).toContain(`${queryParamName}=${queryParamValue}`);
+}
+
+async function clickSearchNavLink(page: Page) {
+  await page.click("nav >> text=Search");
+}
+
+function getMobileMenuButton(page: Page) {
+  return page.locator("button >> text=MENU");
+}
+
+async function hasMobileMenu(page: Page) {
+  const menuButton = getMobileMenuButton(page);
+  return await menuButton.isVisible();
+}
+
+async function clickMobileNavMenu(menuButton: Locator) {
+  await menuButton.click();
+}
+
+async function expectIDIsChecked(page: Page, idWithHash: string) {
+  const checkbox: Locator = page.locator(idWithHash);
+  await expect(checkbox).toBeChecked();
+}
+
+async function toggleCheckbox(
+  page: Page,
+  idWithHash: string,
+  shouldBeChecked: boolean,
+) {
+  const checkbox: Locator = page.locator(idWithHash);
+  // Wait for the checkbox to be enabled and then scroll into view
+  await checkbox.isEnabled();
+  await checkbox.scrollIntoViewIfNeeded();
+
+  const isChecked = await checkbox.isChecked();
+  if (isChecked !== shouldBeChecked) {
+    await checkbox.click({ timeout: 5000 });
+    await checkbox.dispatchEvent("click");
+  }
+}
+
+async function goToNewPageUsingCurrentURL(page: Page, context: BrowserContext) {
+  // Copy the current URL
+  const currentURL = page.url();
+
+  // Open a new tab with the copied URL
+  const newPage = await context.newPage();
+  await newPage.goto(currentURL);
+
+  return newPage;
+}
+
+// async function waitForSearchResultsLoaded(page: Page) {
+//   // Wait specifically for the heading indicating the number of opportunities
+//   //   await page
+//   //     .locator("div.usa-prose h2:has-text('Opportunities')")
+//   //     .waitFor({ state: "visible" });
+//   await page
+//     .locator("ul.usa-list--unstyled")
+//     .first()
+//     .waitFor({ state: "visible", timeout: 60000 });
+// }
