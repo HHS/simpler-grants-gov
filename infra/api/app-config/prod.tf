fdcad6d518643fc0db8e8adaa523f474c1451b52
Michael Chouinard <chouinar6@gmail.com>
2024-05-13 15:41:05 -0400

    [Issue #1688] Remove feature flag for v0.1 endpoints

--- infra/api/app-config/prod.tf
+++ infra/api/app-config/prod.tf
@@ -12,7 +12,5 @@ module "prod_config" {
   database_min_capacity           = 2
 
   service_override_extra_environment_variables = {
-    # determines whether the v0.1 endpoints are available in the API
-    ENABLE_V_0_1_ENDPOINTS = "false"
   }
 }
