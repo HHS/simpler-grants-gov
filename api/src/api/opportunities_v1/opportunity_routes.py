b40344d25f3e6ae65ed11012c0b24410f60e03e4
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2024-05-22 14:05:43 -0400

    [Issue #12] Setup the opportunity v1 endpoint which will be backed by the index (#44)
    ## Summary
    Fixes #12
    
    ### Time to review: __5 mins__
    
    ## Changes proposed
    Made a new set of v1 endpoints that are basically copy-pastes of the
    v0.1 opportunity endpoints
    
    ## Context for reviewers
    Some changes I want to make to the schemas wouldn't make sense without
    the search index (eg. adding the filter counts to the response). As we
    have no idea what the actual launch of the v0.1 endpoint is going to
    look like, I don't want to mess with any of that code or try to make a
    weird hacky approach that needs to account for both the DB
    implementation and the search index one.
    
    Also, I think we've heard that with the launch of the search index,
    we'll be "officially" launched, so might as well call in v1 at the same
    time.
    
    Other than adjusting the names of a few schemas in v0.1, I left that
    implementation alone and just copied the boilerplate that I'll fill out
    in subsequent tickets.
    
    ## Additional information
    The endpoint appears locally:
    ![Screenshot 2024-05-20 at 12 18 32
    PM](https://github.com/navapbc/simpler-grants-gov/assets/46358556/86231ec1-417a-41c6-ad88-3d06bb6214e5)
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

--- api/src/api/opportunities_v1/opportunity_routes.py
+++ api/src/api/opportunities_v1/opportunity_routes.py
@@ -2,13 +2,13 @@ import logging
 
 import src.adapters.db as db
 import src.adapters.db.flask_db as flask_db
-import src.api.opportunities_v0_1.opportunity_schemas as opportunity_schemas
+import src.api.opportunities_v1.opportunity_schemas as opportunity_schemas
 import src.api.response as response
-from src.api.opportunities_v0_1.opportunity_blueprint import opportunity_blueprint
+from src.api.opportunities_v1.opportunity_blueprint import opportunity_blueprint
 from src.auth.api_key_auth import api_key_auth
 from src.logging.flask_logger import add_extra_data_to_current_request_logs
-from src.services.opportunities_v0_1.get_opportunity import get_opportunity
-from src.services.opportunities_v0_1.search_opportunities import search_opportunities
+from src.services.opportunities_v1.get_opportunity import get_opportunity
+from src.services.opportunities_v1.search_opportunities import search_opportunities
 from src.util.dict_util import flatten_dict
 
 logger = logging.getLogger(__name__)
@@ -24,59 +24,20 @@ Features in this endpoint are still under heavy development, and subject to chan
 See [Release Phases](https://github.com/github/roadmap?tab=readme-ov-file#release-phases) for further details.
 """
 
-examples = {
-    "example1": {
-        "summary": "No filters",
-        "value": {
-            "pagination": {
-                "order_by": "opportunity_id",
-                "page_offset": 1,
-                "page_size": 25,
-                "sort_direction": "ascending",
-            },
-        },
-    },
-    "example2": {
-        "summary": "All filters",
-        "value": {
-            "query": "research",
-            "filters": {
-                "agency": {"one_of": ["US-ABC", "HHS"]},
-                "applicant_type": {
-                    "one_of": ["state_governments", "county_governments", "individuals"]
-                },
-                "funding_category": {"one_of": ["recovery_act", "arts", "natural_resources"]},
-                "funding_instrument": {"one_of": ["cooperative_agreement", "grant"]},
-                "opportunity_status": {"one_of": ["forecasted", "posted"]},
-            },
-            "pagination": {
-                "order_by": "opportunity_id",
-                "page_offset": 1,
-                "page_size": 25,
-                "sort_direction": "descending",
-            },
-        },
-    },
-}
-
 
 @opportunity_blueprint.post("/opportunities/search")
 @opportunity_blueprint.input(
-    opportunity_schemas.OpportunitySearchRequestV01Schema,
-    arg_name="search_params",
-    examples=examples,
+    opportunity_schemas.OpportunitySearchRequestV1Schema, arg_name="search_params"
 )
 # many=True allows us to return a list of opportunity objects
-@opportunity_blueprint.output(opportunity_schemas.OpportunityV01Schema(many=True))
+@opportunity_blueprint.output(opportunity_schemas.OpportunityV1Schema(many=True))
 @opportunity_blueprint.auth_required(api_key_auth)
 @opportunity_blueprint.doc(description=SHARED_ALPHA_DESCRIPTION)
-@flask_db.with_db_session()
-def opportunity_search(db_session: db.Session, search_params: dict) -> response.ApiResponse:
+def opportunity_search(search_params: dict) -> response.ApiResponse:
     add_extra_data_to_current_request_logs(flatten_dict(search_params, prefix="request.body"))
-    logger.info("POST /v0.1/opportunities/search")
+    logger.info("POST /v1/opportunities/search")
 
-    with db_session.begin():
-        opportunities, pagination_info = search_opportunities(db_session, search_params)
+    opportunities, pagination_info = search_opportunities(search_params)
 
     add_extra_data_to_current_request_logs(
         {
@@ -92,13 +53,13 @@ def opportunity_search(db_session: db.Session, search_params: dict) -> response.
 
 
 @opportunity_blueprint.get("/opportunities/<int:opportunity_id>")
-@opportunity_blueprint.output(opportunity_schemas.OpportunityV01Schema)
+@opportunity_blueprint.output(opportunity_schemas.OpportunityV1Schema)
 @opportunity_blueprint.auth_required(api_key_auth)
 @opportunity_blueprint.doc(description=SHARED_ALPHA_DESCRIPTION)
 @flask_db.with_db_session()
 def opportunity_get(db_session: db.Session, opportunity_id: int) -> response.ApiResponse:
     add_extra_data_to_current_request_logs({"opportunity.opportunity_id": opportunity_id})
-    logger.info("GET /v0.1/opportunities/:opportunity_id")
+    logger.info("GET /v1/opportunities/:opportunity_id")
     with db_session.begin():
         opportunity = get_opportunity(db_session, opportunity_id)
 
