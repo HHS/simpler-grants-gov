daddf579fa12e18ae5a761759870955b94e08342
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2024-05-22 13:58:12 -0400

    [Issue #2089] Setup opensearch locally (navapbc/simpler-grants-gov#39)
    Fixes #2089
    
    Setup a search index to run locally via Docker
    
    Updated makefile to automatically initialize the index + added a script
    to wait for the index to start up before proceeding.
    
    Setup a very basic client for connecting to the search index (will be
    expanded more in subsequent PRs)
    
    Basic test / test utils to verify it is working (also will be expanded)
    
    This is the first step in getting the search index working locally. This
    actually gets it running, and the client works, we just aren't doing
    anything meaningful with it yet besides tests.
    
    This doesn't yet create an index that we can use, except in the test.
    However, if you want to test out a search index, you can go to
    http://localhost:5601/app/dev_tools#/console (after running `make init`)
    to run some queries against the (one node) cluster.
    https://opensearch.org/docs/latest/getting-started/communicate/#sending-requests-in-dev-tools
    provides some examples of how to create + use indexes that you can
    follow.

---
+++ api/src/adapters/search/opensearch_config.py
@@ -0,0 +1,33 @@
+import logging
+
+from pydantic import Field
+from pydantic_settings import SettingsConfigDict
+
+from src.util.env_config import PydanticBaseEnvConfig
+
+logger = logging.getLogger(__name__)
+
+
+class OpensearchConfig(PydanticBaseEnvConfig):
+    model_config = SettingsConfigDict(env_prefix="OPENSEARCH_")
+
+    host: str  # OPENSEARCH_HOST
+    port: int  # OPENSEARCH_PORT
+    use_ssl: bool = Field(default=True)  # OPENSEARCH_USE_SSL
+    verify_certs: bool = Field(default=True)  # OPENSEARCH_VERIFY_CERTS
+
+
+def get_opensearch_config() -> OpensearchConfig:
+    opensearch_config = OpensearchConfig()
+
+    logger.info(
+        "Constructed opensearch configuration",
+        extra={
+            "host": opensearch_config.host,
+            "port": opensearch_config.port,
+            "use_ssl": opensearch_config.use_ssl,
+            "verify_certs": opensearch_config.verify_certs,
+        },
+    )
+
+    return opensearch_config
