493fb4e65096a86facdd5e2e2f8593db5f463d5e
Ryan Lewis <93001277+rylew1@users.noreply.github.com>
2024-05-22 10:39:57 -0700

    [Issue #37]: finish e2e tests (#38)
    ## Summary
    Fixes #37 
    
    ## Changes proposed
    - add some of the relevant tests from bug bash

--- frontend/tests/e2e/search/searchSpecUtil.ts
+++ frontend/tests/e2e/search/searchSpecUtil.ts
@@ -19,9 +19,16 @@ export function expectURLContainsQueryParam(
   page: Page,
   queryParamName: string,
   queryParamValue: string,
+  shouldContain = true,
 ) {
   const currentURL = page.url();
-  expect(currentURL).toContain(`${queryParamName}=${queryParamValue}`);
+  const queryParam = `${queryParamName}=${queryParamValue}`;
+
+  if (shouldContain) {
+    expect(currentURL).toContain(queryParam);
+  } else {
+    expect(currentURL).not.toContain(queryParam);
+  }
 }
 
 export async function waitForURLContainsQueryParam(
@@ -116,7 +123,7 @@ export async function expectSortBy(page: Page, value: string) {
   expect(selectedValue).toBe(value);
 }
 
-export async function waitForSearchResultsLoaded(page: Page) {
+export async function waitForSearchResultsInitialLoad(page: Page) {
   // Wait for number of opportunities to show
   const resultsHeading = page.locator('h2:has-text("Opportunities")');
   await resultsHeading.waitFor({ state: "visible", timeout: 60000 });
@@ -129,4 +136,72 @@ export async function clickAccordionWithTitle(
   await page
     .locator(`button.usa-accordion__button:has-text("${accordionTitle}")`)
     .click();
+}
+
+export async function clickPaginationPageNumber(
+  page: Page,
+  pageNumber: number,
+) {
+  const paginationButton = page.locator(
+    `button[data-testid="pagination-page-number"][aria-label="Page ${pageNumber}"]`,
+  );
+  await paginationButton.first().click();
+}
+
+export async function clickLastPaginationPage(page: Page) {
+  const paginationButtons = page.locator("li > button");
+  const count = await paginationButtons.count();
+
+  // must be more than 1 page
+  if (count > 2) {
+    await paginationButtons.nth(count - 2).click();
+  }
+}
+
+export async function getFirstSearchResultTitle(page: Page) {
+  const firstResultSelector = page.locator(
+    ".usa-list--unstyled > li:first-child h2 a",
+  );
+  return await firstResultSelector.textContent();
+}
+
+export async function getLastSearchResultTitle(page: Page) {
+  const lastResultSelector = page.locator(
+    ".usa-list--unstyled > li:last-child h2 a",
+  );
+  return await lastResultSelector.textContent();
+}
+
+// If descending, select the ascending variant
+export async function selectOppositeSortOption(page: Page) {
+  const sortByDropdown = page.locator("#search-sort-by-select");
+  const currentValue = await sortByDropdown.inputValue();
+  let oppositeValue;
+
+  if (currentValue.includes("Asc")) {
+    oppositeValue = currentValue.replace("Asc", "Desc");
+  } else if (currentValue.includes("Desc")) {
+    oppositeValue = currentValue.replace("Desc", "Asc");
+  } else {
+    throw new Error(`Unexpected sort value: ${currentValue}`);
+  }
+
+  await sortByDropdown.selectOption(oppositeValue);
+}
+
+export async function waitForLoaderToBeHidden(page: Page) {
+  await page.waitForSelector(
+    ".display-flex.flex-align-center.flex-justify-center.margin-bottom-15.margin-top-15",
+    { state: "hidden" },
+  );
+}
+
+export async function getNumberOfOpportunitySearchResults(page: Page) {
+  await waitForLoaderToBeHidden(page);
+  const opportunitiesText = await page
+    .locator("h2.tablet-lg\\:grid-col-fill")
+    .textContent();
+  return opportunitiesText
+    ? parseInt(opportunitiesText.replace(/\D/g, ""), 10)
+    : 0;
 }
