e5245b2489bfa34a914c0a6a3fcbf83a98754e5d
Alsia Plybeah <alsia@navapbc.com>
2024-07-26 16:05:54 -0400

    [Task] Update analytics db to use local.env and Remove Dynaconf from Analytics (navapbc/simpler-grants-gov#136)
    Fixes #107
    Fixes #115
    
    * removed the `*.toml` files related to dynaconf
    * removed references to Dynaconf (e.g. in Docstrings, gitignore)
    * use Pydantic for loading
    
    > After getting feedback for
    https://github.com/navapbc/simpler-grants-gov/issues/107, the consensus
    was to reevaluate the way the database loader works for more uniformity.
    Changes will need to be made primarily in db.py and cli.py
    >
    > With the PR https://github.com/navapbc/simpler-grants-gov/pull/84 ,
    the env settings for the db are stored in settings.toml. The config
    settings should be updated to use the existing local.env file
    
    > Screenshots, GIF demos, code examples or output to help show the
    changes working as expected.

--- analytics/src/analytics/integrations/db.py
+++ analytics/src/analytics/integrations/db.py
@@ -3,13 +3,12 @@
 
 from sqlalchemy import Engine, create_engine
 
-from config import settings
+from config import get_db_settings
+
+# The variables used in the connection url are pulled from local.env
+# and configured in the DBSettings class found in config.py
 
 
-# The variables used in the connection url are set in settings.toml and
-# .secrets.toml. These can be overridden with the custom prefix defined in config.py: "ANALYTICS".
-# e.g. `export ANALYTICS_POSTGRES_USER=new_usr`.
-# Docs: https://www.dynaconf.com/envvars/
 def get_db() -> Engine:
     """
     Get a connection to the database using a SQLAlchemy engine object.
@@ -22,8 +21,10 @@ def get_db() -> Engine:
     sqlalchemy.engine.Engine
     A SQLAlchemy engine object representing the connection to the database.
     """
+    db = get_db_settings()
+    print(f"postgresql+psycopg://{db.user}:{db.password}@{db.db_host}:{db.port}")
     return create_engine(
-        f"postgresql+psycopg://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}",
+        f"postgresql+psycopg://{db.user}:{db.password}@{db.db_host}:{db.port}",
         pool_pre_ping=True,
         hide_parameters=True,
     )
