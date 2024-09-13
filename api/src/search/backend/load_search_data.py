2854d435f50f23e06709eeb72ca488e3c4095e7f
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2024-09-13 13:10:28 -0400

    [Issue #179] Incrementally load search data (#180)
    ## Summary
    Fixes #179
    
    ### Time to review: __10 mins__
    
    ## Changes proposed
    Updated the load search data task to partially support incrementally
    loading + deleting records in the search index rather than just fully
    remaking it.
    
    Various changes to the search utilities to support this work
    
    ## Context for reviewers
    Technically this doesn't fully support a true incremental load as it
    updates every record rather than just the ones with changes. I think the
    logic necessary to detect changes both deserves its own ticket, and may
    evolve when we later support indexing files to OpenSearch, so I think it
    makes sense to hold off on that for now.

--- api/src/search/backend/load_search_data.py
+++ api/src/search/backend/load_search_data.py
@@ -1,3 +1,5 @@
+import click
+
 import src.adapters.db as db
 import src.adapters.search as search
 from src.adapters.db import flask_db
@@ -8,8 +10,13 @@ from src.search.backend.load_search_data_blueprint import load_search_data_bluep
 @load_search_data_blueprint.cli.command(
     "load-opportunity-data", help="Load opportunity data from our database to the search index"
 )
+@click.option(
+    "--full-refresh/--incremental",
+    default=True,
+    help="Whether to run a full refresh, or only incrementally update oppportunities",
+)
 @flask_db.with_db_session()
-def load_opportunity_data(db_session: db.Session) -> None:
+def load_opportunity_data(db_session: db.Session, full_refresh: bool) -> None:
     search_client = search.SearchClient()
 
-    LoadOpportunitiesToIndex(db_session, search_client).run()
+    LoadOpportunitiesToIndex(db_session, search_client, full_refresh).run()
--- api/tests/src/search/backend/test_load_opportunities_to_index.py
+++ api/tests/src/search/backend/test_load_opportunities_to_index.py
@@ -4,17 +4,18 @@ from src.search.backend.load_opportunities_to_index import (
     LoadOpportunitiesToIndex,
     LoadOpportunitiesToIndexConfig,
 )
+from src.util.datetime_util import get_now_us_eastern_datetime
 from tests.conftest import BaseTestClass
 from tests.src.db.models.factories import OpportunityFactory
 
 
-class TestLoadOpportunitiesToIndex(BaseTestClass):
+class TestLoadOpportunitiesToIndexFullRefresh(BaseTestClass):
     @pytest.fixture(scope="class")
     def load_opportunities_to_index(self, db_session, search_client, opportunity_index_alias):
         config = LoadOpportunitiesToIndexConfig(
             alias_name=opportunity_index_alias, index_prefix="test-load-opps"
         )
-        return LoadOpportunitiesToIndex(db_session, search_client, config)
+        return LoadOpportunitiesToIndex(db_session, search_client, True, config)
 
     def test_load_opportunities_to_index(
         self,
@@ -83,3 +84,70 @@ class TestLoadOpportunitiesToIndex(BaseTestClass):
         assert set([opp.opportunity_id for opp in opportunities]) == set(
             [record["opportunity_id"] for record in resp.records]
         )
+
+
+class TestLoadOpportunitiesToIndexPartialRefresh(BaseTestClass):
+    @pytest.fixture(scope="class")
+    def load_opportunities_to_index(self, db_session, search_client, opportunity_index_alias):
+        config = LoadOpportunitiesToIndexConfig(
+            alias_name=opportunity_index_alias, index_prefix="test-load-opps"
+        )
+        return LoadOpportunitiesToIndex(db_session, search_client, False, config)
+
+    def test_load_opportunities_to_index(
+        self,
+        truncate_opportunities,
+        enable_factory_create,
+        db_session,
+        search_client,
+        opportunity_index_alias,
+        load_opportunities_to_index,
+    ):
+        index_name = "partial-refresh-index-" + get_now_us_eastern_datetime().strftime(
+            "%Y-%m-%d_%H-%M-%S"
+        )
+        search_client.create_index(index_name)
+        search_client.swap_alias_index(
+            index_name, load_opportunities_to_index.config.alias_name, delete_prior_indexes=True
+        )
+
+        # Load a bunch of records into the DB
+        opportunities = []
+        opportunities.extend(OpportunityFactory.create_batch(size=6, is_posted_summary=True))
+        opportunities.extend(OpportunityFactory.create_batch(size=3, is_forecasted_summary=True))
+        opportunities.extend(OpportunityFactory.create_batch(size=2, is_closed_summary=True))
+        opportunities.extend(
+            OpportunityFactory.create_batch(size=8, is_archived_non_forecast_summary=True)
+        )
+        opportunities.extend(
+            OpportunityFactory.create_batch(size=6, is_archived_forecast_summary=True)
+        )
+
+        load_opportunities_to_index.run()
+
+        resp = search_client.search(opportunity_index_alias, {"size": 100})
+        assert resp.total_records == len(opportunities)
+
+        # Add a few more opportunities that will be created
+        opportunities.extend(OpportunityFactory.create_batch(size=3, is_posted_summary=True))
+
+        # Delete some opportunities
+        opportunities_to_delete = [opportunities.pop(), opportunities.pop(), opportunities.pop()]
+        for opportunity in opportunities_to_delete:
+            db_session.delete(opportunity)
+
+        load_opportunities_to_index.run()
+
+        resp = search_client.search(opportunity_index_alias, {"size": 100})
+        assert resp.total_records == len(opportunities)
+
+    def test_load_opportunities_to_index_index_does_not_exist(self, db_session, search_client):
+        config = LoadOpportunitiesToIndexConfig(
+            alias_name="fake-index-that-will-not-exist", index_prefix="test-load-opps"
+        )
+        load_opportunities_to_index = LoadOpportunitiesToIndex(
+            db_session, search_client, False, config
+        )
+
+        with pytest.raises(RuntimeError, match="please run the full refresh job"):
+            load_opportunities_to_index.run()
