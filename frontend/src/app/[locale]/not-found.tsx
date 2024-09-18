ab6465a8486e26dadd93d01b03143c814c3870cb
acouch <acinternets@gmail.com>
2024-09-18 15:20:42 -0400

    [Issue #2029] Prepare for Nava fork to HHS

---
+++ frontend/src/app/[locale]/not-found.tsx
@@ -0,0 +1,33 @@
+import BetaAlert from "src/components/BetaAlert";
+import { GridContainer } from "@trussworks/react-uswds";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { Metadata } from "next";
+
+export async function generateMetadata() {
+  const t = await getTranslations({ locale: "en" });
+  const meta: Metadata = {
+    title: t("ErrorPages.page_not_found.title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
+
+export default function NotFound() {
+  unstable_setRequestLocale("en");
+  const t = useTranslations("ErrorPages.page_not_found");
+
+  return (
+    <>
+      <BetaAlert />
+      <GridContainer className="padding-y-1 tablet:padding-y-3 desktop-lg:padding-y-15">
+        <h1 className="nj-h1">{t("title")}</h1>
+        <p className="margin-bottom-2">{t("message_content_1")}</p>
+        <Link className="usa-button" href="/" key="returnToHome">
+          {t("visit_homepage_button")}
+        </Link>
+      </GridContainer>
+    </>
+  );
+}
