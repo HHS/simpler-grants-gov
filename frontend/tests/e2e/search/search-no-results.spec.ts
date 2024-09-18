3ab81911ef21b60c5666824dc0bbcccb29f6488a
acouch <acinternets@gmail.com>
2024-09-18 15:20:42 -0400

    [Issue #2029] Prepare for Nava fork to HHS

--- frontend/tests/e2e/search/search-no-results.spec.ts
+++ frontend/tests/e2e/search/search-no-results.spec.ts
@@ -1,5 +1,8 @@
 import { Page, expect, test } from "@playwright/test";
-import { fillSearchInputAndSubmit } from "./searchSpecUtil";
+import {
+  expectURLContainsQueryParam,
+  fillSearchInputAndSubmit,
+} from "./searchSpecUtil";
 
 import { BrowserContextOptions } from "playwright-core";
 
@@ -15,7 +18,7 @@ test.describe("Search page tests", () => {
     await page.goto("/search?_ff=showSearchV0:true");
   });
 
-  test("should show and hide loading state", async ({
+  test("should return 0 results when searching for obscure term", async ({
     page,
     browserName,
   }: PageProps) => {
@@ -24,16 +27,21 @@ test.describe("Search page tests", () => {
       browserName === "webkit",
       "Skipping test for WebKit due to a query param issue.",
     );
-    const searchTerm = "advanced";
+
+    const searchTerm = "0resultearch";
+
     await fillSearchInputAndSubmit(searchTerm, page);
+    await new Promise((resolve) => setTimeout(resolve, 3250));
+    expectURLContainsQueryParam(page, "query", searchTerm);
 
-    const loadingIndicator = page.locator("text='Loading results...'");
-    await expect(loadingIndicator).toBeVisible();
-    await expect(loadingIndicator).toBeHidden();
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    const resultsHeading = page.getByRole("heading", {
+      name: /0 Opportunities/i,
+    });
+    await expect(resultsHeading).toBeVisible();
 
-    const searchTerm2 = "agency";
-    await fillSearchInputAndSubmit(searchTerm2, page);
-    await expect(loadingIndicator).toBeVisible();
-    await expect(loadingIndicator).toBeHidden();
+    await expect(page.locator("div.usa-prose h2")).toHaveText(
+      "Your search did not return any results.",
+    );
   });
 });
