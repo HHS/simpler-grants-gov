0244776f312ebfbfd6ec610b65ebcfd1804d8782
Doug Schrashun <dougschrashun@navapbc.com>
2024-09-24 17:27:01 -0400

    cleanup

--- frontend/src/utils/assetPath.ts
+++ frontend/src/utils/assetPath.ts
@@ -1,6 +1,5 @@
 import { environment } from "src/constants/environments";
 
 export function assetPath(relativePath: string) {
-  const basePath = environment.NEXT_PUBLIC_BASE_PATH ?? "";
-  return `${basePath}${relativePath}`;
+  return `${environment.NEXT_PUBLIC_BASE_PATH}${relativePath}`;
 }
