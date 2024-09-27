77431eca5e2137351f0464e7c11a3b6824fad1db
Doug Schrashun <dougschrashun@navapbc.com>
2024-09-27 10:40:35 -0400

    button styling good enough for now

--- frontend/src/components/search/SearchFilterToggle.tsx
+++ frontend/src/components/search/SearchFilterToggle.tsx
@@ -11,12 +11,19 @@ export default function SearchFilterToggle({
   showFilterOptions: boolean;
   buttonText: string;
 }) {
+  const iconName = showFilterOptions ? "arrow_drop_up" : "arrow_drop_down";
+  // todo: make the button part of this sharable
   return (
-    <div className="grants-search-filter-toggle">
-      <button onClick={(_event) => setShowFilterOptions(!showFilterOptions)}>
-        <USWDSIcon name={"Arrow Drop Down"} className="" />
+    <div className="grants-search-filter-toggle grants-toggle grid-row flex-wrap">
+      <div className="grid-col-4" />
+      <a
+        onClick={(_event) => setShowFilterOptions(!showFilterOptions)}
+        aria-pressed={showFilterOptions}
+        className="grid-col-4 usa-link text-bold grants-toggle-button"
+      >
+        <USWDSIcon name={iconName} className="usa-icon usa-icon--size-4" />
         <span>{buttonText}</span>
-      </button>
+      </a>
     </div>
   );
 }
