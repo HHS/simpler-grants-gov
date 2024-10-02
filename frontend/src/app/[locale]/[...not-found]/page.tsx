9b89e7413f0a3df50df3fa289a3038342839ba0f
Emily <emilycarlsen@navapbc.com>
2024-10-02 16:48:10 -0400

    add suggested page title change

--- frontend/src/app/[locale]/[...not-found]/page.tsx
+++ frontend/src/app/[locale]/[...not-found]/page.tsx
@@ -1,4 +1,18 @@
+import { Metadata } from "next";
+
 import { notFound } from "next/navigation";
+
+import { getTranslations } from "next-intl/server";
+
+
+export async function generateMetadata() {
+  const t = await getTranslations({ locale: "en" });
+  const meta: Metadata = {
+    title: t("ErrorPages.page_not_found.title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
 
 export default function NotFoundDummy() {
   notFound();
