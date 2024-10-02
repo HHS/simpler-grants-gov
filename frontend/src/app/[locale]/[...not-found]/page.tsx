e7b282f1998a27b68824553f9c198b78de3aa3f7
Emily <emilycarlsen@navapbc.com>
2024-10-02 16:49:52 -0400

    format again

--- frontend/src/app/[locale]/[...not-found]/page.tsx
+++ frontend/src/app/[locale]/[...not-found]/page.tsx
@@ -1,9 +1,7 @@
 import { Metadata } from "next";
 
-import { notFound } from "next/navigation";
-
 import { getTranslations } from "next-intl/server";
-
+import { notFound } from "next/navigation";
 
 export async function generateMetadata() {
   const t = await getTranslations({ locale: "en" });
