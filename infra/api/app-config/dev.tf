7db900a1634b37b500ee75157d179cb1c5cbfa19
Kai Siren <kaisiren@navapbc.com>
2024-10-07 10:54:23 -0700

    more docs

--- infra/api/app-config/dev.tf
+++ infra/api/app-config/dev.tf
@@ -10,11 +10,12 @@ module "dev_config" {
   database_max_capacity           = 16
   database_min_capacity           = 2
 
-  has_opensearch         = true
-  opensearch_volume_size = 20
+  has_opensearch = true
   # https://docs.aws.amazon.com/opensearch-service/latest/developerguide/supported-instance-types.html
   opensearch_dedicated_master_type = "m6g.large.search"
   opensearch_instance_type         = "or1.medium.search"
+  # 20 is the minimum volume size for the or1.medium.search instance type.
+  opensearch_volume_size = 20
   # https://docs.aws.amazon.com/opensearch-service/latest/developerguide/what-is.html#choosing-version
   opensearch_engine_version = "OpenSearch_2.15"
 
