f277a3f9a857119f5c705697d040e12274fe9cb4
Kai Siren <kaisiren@navapbc.com>
2024-10-15 14:50:43 -0700

    git restore

--- bin/publish-release.sh
+++ bin/publish-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euox pipefail
+set -euo pipefail
 
 APP_NAME=$1
 IMAGE_NAME=$2
@@ -15,12 +15,12 @@ echo "  IMAGE_NAME=$IMAGE_NAME"
 echo "  IMAGE_TAG=$IMAGE_TAG"
 
 # Need to init module when running in CD since GitHub actions does a fresh checkout of repo
-terraform -chdir="infra/$APP_NAME/app-config" init >/dev/null
-terraform -chdir="infra/$APP_NAME/app-config" apply -refresh-only -auto-approve >/dev/null
+terraform -chdir="infra/$APP_NAME/app-config" init > /dev/null
+terraform -chdir="infra/$APP_NAME/app-config" apply -refresh-only -auto-approve> /dev/null
 IMAGE_REPOSITORY_NAME=$(terraform -chdir="infra/$APP_NAME/app-config" output -raw image_repository_name)
 
 REGION=$(./bin/current-region.sh)
-read -r IMAGE_REGISTRY_ID IMAGE_REPOSITORY_URL <<<"$(aws ecr describe-repositories --repository-names "$IMAGE_REPOSITORY_NAME" --query "repositories[0].[registryId,repositoryUri]" --output text)"
+read -r IMAGE_REGISTRY_ID IMAGE_REPOSITORY_URL <<< "$(aws ecr describe-repositories --repository-names "$IMAGE_REPOSITORY_NAME" --query "repositories[0].[registryId,repositoryUri]" --output text)"
 IMAGE_REGISTRY="$IMAGE_REGISTRY_ID.dkr.ecr.$REGION.amazonaws.com"
 
 echo "Build repository info:"
@@ -30,13 +30,13 @@ echo "  IMAGE_REPOSITORY_NAME=$IMAGE_REPOSITORY_NAME"
 echo "  IMAGE_REPOSITORY_URL=$IMAGE_REPOSITORY_URL"
 echo
 echo "Authenticating Docker with ECR"
-aws ecr get-login-password --region "$REGION" |
-  docker login --username AWS --password-stdin "$IMAGE_REGISTRY"
+aws ecr get-login-password --region "$REGION" \
+  | docker login --username AWS --password-stdin "$IMAGE_REGISTRY"
 echo
 echo "Check if tag has already been published..."
 RESULT=""
-RESULT=$(aws ecr describe-images --repository-name "$IMAGE_REPOSITORY_NAME" --image-ids "imageTag=$IMAGE_TAG" --region "$REGION" 2>/dev/null) || true
-if [ -n "$RESULT" ]; then
+RESULT=$(aws ecr describe-images --repository-name "$IMAGE_REPOSITORY_NAME" --image-ids "imageTag=$IMAGE_TAG" --region "$REGION" 2> /dev/null ) || true
+if [ -n "$RESULT" ];then
   echo "Image with tag $IMAGE_TAG already published"
   exit 0
 fi
