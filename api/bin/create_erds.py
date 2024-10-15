9d234db81f631f71ed44b9955f6e775814aff998
Bruk Abebe <brukty@gmail.com>
2024-10-15 15:04:25 -0400

    clean up

--- api/bin/create_erds.py
+++ api/bin/create_erds.py
@@ -7,7 +7,6 @@ import pdb
 from typing import Any
 
 import pydot
-import sadisplay
 
 import src.db.models.staging.forecast as staging_forecast_models
 import src.db.models.staging.opportunity as staging_opportunity_models
@@ -16,13 +15,7 @@ import src.logging
 from src.db.models import agency_models, opportunity_models
 from src.db.models.transfer import topportunity_models
 
-from eralchemy import render_er
-
 from sqlalchemy_schemadisplay import create_uml_graph
-from sqlalchemy.orm import class_mapper
-from sqlalchemy import inspect
-
-
 
 logger = logging.getLogger(__name__)
 
@@ -50,9 +43,9 @@ ALL_MODULES = API_MODULES + STAGING_TABLE_MODULES + TRANSFER_TABLE_MODULES
 
 def create_erds(modules: Any, file_name: str) -> None:
     logger.info("Generating ERD diagrams for %s", file_name)
-    import pdb
+
     items = []
-    all_mappers =[]
+    all_mappers = []
 
     for module in modules:
         items.extend([getattr(module, attr) for attr in dir(module) if attr[0] != '_'])
@@ -60,40 +53,17 @@ def create_erds(modules: Any, file_name: str) -> None:
     for item in items: # get mapped classes
         try:
             all_mappers.extend([cls for cls in item.registry.mappers])
-        except Exception as e:
-            logger.error("sqlalchemy.exc.NoInspectionAvailable")
+        except:
+            #not a mapper
             pass
 
-
     graph = create_uml_graph(all_mappers,
-                             show_operations=False,  # not necessary in this case
-                             show_multiplicity_one=False  # some people like to see the ones, some don't
+                             show_operations=False,
+                             show_multiplicity_one=False
                              )
 
     png_file_path = ERD_FOLDER / f"{file_name}.png"
-    graph.write_png(png_file_path)  # write out the file
-
-    # render_er(
-    #     items[0],  # List your models here
-    #
-    #     png_file_path
-    # )
-
-    # dot_file_name = ERD_FOLDER / f"{file_name}.dot"
-    #
-    # # We create a temporary .dot file which we then convert to a png
-    # with codecs.open(str(dot_file_name), "w", encoding="utf8") as f:
-    #     f.write(sadisplay.dot(description))
-    #
-    # (graph,) = pydot.graph_from_dot_file(dot_file_name)
-    #
-    # png_file_path = ERD_FOLDER / f"{file_name}.png"
-    # logger.info("Creating ERD diagram at %s", png_file_path)
-    # graph.write_png(png_file_path)
-
-   # remove the temporary .dot file
-    # os.remove(dot_file_name)
-
+    graph.write_png(png_file_path)
 
 def main() -> None:
     with src.logging.init(__package__):
