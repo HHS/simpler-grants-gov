82faca189ce9e756bec24f94e8c7d8fb1811d7e2
Kai Siren <kaisiren@navapbc.com>
2024-10-15 14:50:18 -0700

    releases publishing

--- infra/ecs-terraform/app-config/prod.tf
+++ infra/ecs-terraform/app-config/prod.tf
@@ -1,4 +1,4 @@
-module "dev_config" {
+module "prod_config" {
   source                                       = "./env-config"
   app_name                                     = local.app_name
   default_region                               = module.project_config.default_region
