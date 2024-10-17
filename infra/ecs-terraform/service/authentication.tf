e31d0d2509d5510c62b06a70a4dad19c7e743c45
Kai Siren <kaisiren@navapbc.com>
2024-10-17 12:25:01 -0700

    adds permissions

--- infra/ecs-terraform/service/authentication.tf
+++ infra/ecs-terraform/service/authentication.tf
@@ -6,9 +6,36 @@ locals {
 
 data "aws_iam_policy_document" "search_deployment" {
   statement {
+    sid       = "get-ssm-parameters"
     effect    = "Allow"
-    actions   = ["ec2:Describe*"]
-    resources = ["*"]
+    actions   = ["ssm:GetParameters"]
+    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/search/api-${var.environment_name}/endpoint"]
+  }
+
+  statement {
+    sid       = "read-s3-objects"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::simpler-grants-gov-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-tf"]
+  }
+
+  statement {
+    sid       = "write-s3-objects"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["arn:aws:s3:::simpler-grants-gov-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-tf/infra/api/opensearch/${var.environment_name}.tfstate"]
+  }
+
+  statement {
+    sid    = "dynamodb-access"
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/simpler-grants-gov-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-tf-state-locks"]
   }
 }
 
--- infra/ecs-terraform/service/main.tf
+++ infra/ecs-terraform/service/main.tf
@@ -1,4 +1,7 @@
-# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 data "aws_vpc" "network" {
   filter {
     name   = "tag:Name"
@@ -6,7 +9,6 @@ data "aws_vpc" "network" {
   }
 }
 
-# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnet
 data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
@@ -18,7 +20,6 @@ data "aws_subnets" "private" {
   }
 }
 
-# docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnet
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
