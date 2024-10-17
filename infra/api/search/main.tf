2131c244d282ace603471b9232ac3bcc983f2d9e
Kai Siren <kaisiren@navapbc.com>
2024-10-17 15:54:25 -0700

    add an internal admin role for opensearch

--- infra/api/search/main.tf
+++ infra/api/search/main.tf
@@ -15,6 +15,8 @@ terraform {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 module "project_config" {
   source = "../../project-config"
 }
@@ -25,4 +27,29 @@ data "aws_ssm_parameter" "search_endpoint_arn" {
 
 provider "opensearch" {
   url = data.aws_ssm_parameter.search_endpoint_arn.value
+}
+
+resource "opensearch_role" "admin" {
+  role_name   = "admin"
+  description = "admin test"
+
+  cluster_permissions = ["*"]
+
+  index_permissions {
+    index_patterns  = ["*"]
+    allowed_actions = ["*"]
+  }
+
+  tenant_permissions {
+    tenant_patterns = ["*"]
+    allowed_actions = ["*"]
+  }
+}
+
+resource "opensearch_roles_mapping" "admin" {
+  role_name   = "logs_writer"
+  description = "Mapping AWS IAM roles to ES role"
+  backend_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/api-${var.environment_name}-app"
+  ]
 }
