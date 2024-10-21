a90897d3d7341a955541743a9b9240d18f30ac18
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2024-10-21 13:06:49 -0400

    [Issue 2448] use v1 search endpoint and frontend fetch pattern refactors (#2518)
    * opportunity search change to use the `v1` version of the endpoint and activate the use of
    Open Search on the backend
    * various refactors to the `API` fetch system for general improvement and some minor bug fixes
    * fix to loading spinner test that was acting flaky
    * update e2e script to include seeding open search

--- frontend/tests/e2e/search/search-loading.spec.ts
+++ frontend/tests/e2e/search/search-loading.spec.ts
@@ -1,29 +1,25 @@
-import { expect, Page, test } from "@playwright/test";
-import { BrowserContextOptions } from "playwright-core";
-
-import { fillSearchInputAndSubmit } from "./searchSpecUtil";
-
-interface PageProps {
-  page: Page;
-  browserName?: string;
-  contextOptions?: BrowserContextOptions;
-}
+import { expect, test } from "@playwright/test";
+import { chromium } from "playwright-core";
+import {
+  fillSearchInputAndSubmit,
+  generateRandomString,
+} from "tests/e2e/search/searchSpecUtil";
 
 test.describe("Search page tests", () => {
-  test.beforeEach(async ({ page }: PageProps) => {
-    // Navigate to the search page with the feature flag set
+  test("should show and hide loading state", async () => {
+    const searchTerm = generateRandomString([4, 5]);
+    const searchTerm2 = generateRandomString([8]);
+
+    const browser = await chromium.launch({ slowMo: 100 });
+
+    const page = await browser.newPage();
     await page.goto("/search?_ff=showSearchV0:true");
-  });
-
-  test("should show and hide loading state", async ({ page }: PageProps) => {
-    const searchTerm = "advanced";
-    await fillSearchInputAndSubmit(searchTerm, page);
-
     const loadingIndicator = page.getByTestId("loading-message");
+
+    await fillSearchInputAndSubmit(searchTerm, page);
     await expect(loadingIndicator).toBeVisible();
     await expect(loadingIndicator).toBeHidden();
 
-    const searchTerm2 = "agency";
     await fillSearchInputAndSubmit(searchTerm2, page);
     await expect(loadingIndicator).toBeVisible();
     await expect(loadingIndicator).toBeHidden();
