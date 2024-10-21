9b0f0dcdb83c17e2cb1f5670a81eb3c251ac9686
Doug Schrashun <dougschrashun@navapbc.com>
2024-10-21 11:51:22 -0400

    well that actually worked, let's clean it up

--- frontend/tests/e2e/search/searchSpecUtil.ts
+++ frontend/tests/e2e/search/searchSpecUtil.ts
@@ -34,17 +34,6 @@ export const generateRandomString = (desiredPattern: number[]) => {
   }, "");
 };
 
-//   let result = "";
-//   const charactersLength = characters.length;
-//   let counter = 0;
-//   while (counter < length) {
-//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     counter += 1;
-//   }
-//   return result;
-// };
-// (Math.random() + 1).toString(36).substring(7);
-
 export function expectURLContainsQueryParam(
   page: Page,
   queryParamName: string,
