ebfbc8b3a4b4f955dbc2331b4657c0c38805f8ad
widal001 <billy.daly@agile6.com>
2024-10-24 11:23:52 -0400

    feat: Adds new export functions for GitHub data - export_sprint_data() - export_roadmap_data()

--- analytics/src/analytics/integrations/github/main.py
+++ analytics/src/analytics/integrations/github/main.py
@@ -4,6 +4,7 @@ import shlex
 import subprocess
 from pathlib import Path
 
+PARENT_DIR = Path(__file__).resolve().parent
 # Set the max number of records to return with CLI commands to 10,000
 # NOTE: GitHub exports data in batches of 100 so exporting 10k issues could take over a minute
 # TODO(@widal001): 2023-11-29 - Switch to incremental export pattern
@@ -38,3 +39,134 @@ def export_issue_data(owner: str, repo: str, output_file: str) -> None:
         f"-R {owner}/{repo} -L {MAX_RECORDS} --state all"
     )
     pipe_command_output_to_file(command, output_file)
+
+
+def export_sprint_data(
+    owner: str,
+    project: int,
+    sprint_field: str,
+    points_field: str,
+    output_file: str,
+) -> None:
+    """Export the issue and project data from a Sprint Board."""
+    # Get the path script and the GraphQL query
+    script = PARENT_DIR / "make-graphql-query.sh"
+    query_path = PARENT_DIR / "getSprintData.graphql"
+    # Load the query
+    with open(query_path) as f:
+        query = f.read()
+    # Create the post-pagination transform jq
+    jq = """
+[
+    # iterate through each project item
+    .[] |
+    # reformat each item
+    {
+        issue_title: .content.title,
+        issue_url: .content.url,
+        issue_parent: .content.parent.url,
+        issue_type: .content.issueType.name,
+        issue_is_closed: .content.closed,
+        issue_opened_at: .content.createdAt,
+        issue_closed_at: .content.closedAt,
+        issue_points: .points.number,
+        sprint_id: .sprint.iterationId,
+        sprint_name: .sprint.title,
+        sprint_start: .sprint.startDate,
+        sprint_length: .sprint.duration,
+        sprint_end: (
+            if .sprint.startDate == null
+            then null
+            else (
+                (.sprint.startDate | strptime(\"%Y-%m-%d\") | mktime)
+                + (.sprint.duration * 86400) | strftime(\"%Y-%m-%d\")
+            )
+            end
+        ),
+    } |
+    # filter for task-level issues
+    select(.issue_type != \"Deliverable\")
+]
+"""
+    # Make the command
+    # fmt: off
+    command = [
+        script,
+        "--batch", "100",
+        "--field", f"login={owner}",
+        "--field", f"project={project}",
+        "--field", f"sprintField='{sprint_field}'",
+        "--field", f"pointsField='{points_field}'",
+        "--query", f"{query}",
+        "--paginate-jq", "'.data.organization.projectV2.items.nodes'",
+        "--transform-jq", jq,
+    ]
+    # fmt: on
+    # invoke the command via a subprocess and write the output to a file
+    with open(output_file, "w", encoding="utf-8") as f:
+        subprocess.call(command, stdout=f)  # noqa: S603
+
+
+def export_roadmap_data(
+    owner: str,
+    project: int,
+    quad_field: str,
+    pillar_field: str,
+    output_file: str,
+) -> None:
+    """Export the issue and project data from a Sprint Board."""
+    # Get the path script and the GraphQL query
+    script = PARENT_DIR / "make-graphql-query.sh"
+    query_path = PARENT_DIR / "getRoadmapData.graphql"
+    # Load the query
+    with open(query_path) as f:
+        query = f.read()
+    # Create the post-pagination transform jq
+    jq = """
+[
+    # iterate through each project item
+    .[] |
+    # reformat each item
+    {
+        issue_title: .content.title,
+        issue_url: .content.url,
+        issue_parent: .content.parent.url,
+        issue_type: .content.issueType.name,
+        issue_is_closed: .content.closed,
+        issue_opened_at: .content.createdAt,
+        issue_closed_at: .content.closedAt,
+        deliverable_pillar: .pillar.name,
+        quad_id: .quad.iterationId,
+        quad_name: .quad.title,
+        quad_start: .quad.startDate,
+        quad_length: .quad.duration,
+        quad_end: (
+            if .quad.startDate == null
+            then null
+            else (
+                (.quad.startDate | strptime(\"%Y-%m-%d\") | mktime)
+                + (.quad.duration * 86400) | strftime(\"%Y-%m-%d\")
+            )
+            end
+        ),
+    }
+
+]
+"""
+    # Make the command
+    # fmt: off
+    command = [
+        script,
+        "--batch", "100",
+        "--field", f"login={owner}",
+        "--field", f"project={project}",
+        "--field", f"quadField='{quad_field}'",
+        "--field", f"pillarField='{pillar_field}'",
+        "--query", f"{query}",
+        "--paginate-jq", "'.data.organization.projectV2.items.nodes'",
+        "--transform-jq", jq,
+    ]
+    # fmt: on
+    # invoke the command via a subprocess and write the output to a file
+    with open(output_file, "w", encoding="utf-8") as f:
+        subprocess.call(command, stdout=f)  # noqa: S603
