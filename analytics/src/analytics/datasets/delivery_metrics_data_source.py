b837ddf4f6d1abf872ea05ffa839bd2cc531dacc
David Dudas <david.dudas@intuitial.com>
2024-10-28 10:54:47 -0700

    created dataset for delivery metrics

---
+++ analytics/src/analytics/datasets/delivery_metrics_data_source.py
@@ -0,0 +1,170 @@
+"""
+Implements the DeliveryMetricsDataSource dataset.
+
+This is a sub-class of BaseDataset that models 
+quad, deliverable, epic, issue, and sprint data.
+"""
+from enum import Enum
+from typing import Self
+
+import pandas
+
+from analytics.datasets.base import BaseDataset
+from analytics.datasets.utils import load_json_data_as_df
+
+
+class DeliveryMetricsEntityType(Enum):
+    """ Entity types in the db schema """
+
+    DELIVERABLE = "deliverable"
+    EPIC = "epic"
+    ISSUE = "issue"
+    SPRINT = "sprint"
+    QUAD = "quad"
+
+
+class DeliveryMetricsDataSource(BaseDataset):
+    """ Models quad, deliverable, epic, issue, and sprint data exported from github """
+
+    COLUMN_MAP = {
+        "issue_url": "issue_ghid",
+        "issue_title": "issue_title",
+        "issue_parent": "issue_parent",
+        "issue_type": "issue_type",
+        "issue_is_closed": "issue_is_closed",
+        "issue_opened_at": "issue_opened_at",
+        "issue_closed_at": "issue_closed_at",
+        "issue_points": "issue_points",
+        "issue_status": "issue_status",
+        "sprint_id": "sprint_ghid",
+        "sprint_name": "sprint_name",
+        "sprint_start": "sprint_start",
+        "sprint_length": "sprint_length",
+        "sprint_end": "sprint_end",
+        "quad_id": "quad_ghid",
+        "quad_name": "quad_name",
+        "quad_start": "quad_start",
+        "quad_length": "quad_length",
+        "quad_end": "quad_end",
+        "deliverable_url": "deliverable_ghid",
+        "deliverable_title": "deliverable_title",
+        "deliverable_pillar": "deliverable_pillar",
+        "epic_url": "epic_ghid",
+        "epic_title": "epic_title"
+    }
+
+
+    @classmethod
+    def load_from_json_file(cls, file_path) -> Self:
+        """
+        Loads the input json file and instantiates an instance of DeliveryMetricsDataSource.
+
+        Parameters
+        ----------
+        file_path: str
+            Path to the local json file containing data exported from GitHub
+
+        Returns
+        -------
+        Self:
+            An instance of the DeliveryMetricsDataSource dataset class
+        """
+
+        # load input datasets
+        df = load_json_data_as_df(
+            file_path=file_path,
+            column_map=cls.COLUMN_MAP,
+            date_cols=None
+        )
+
+        # transform entity id columns
+        for col in ('deliverable_ghid', 'epic_ghid', 'issue_ghid', 'issue_parent'):
+            df[col] = df[col].apply(lambda x: DeliveryMetricsDataSource._remove_fqdn_prefix(x))
+
+        return cls(df)
+
+
+    @classmethod
+    def _remove_fqdn_prefix(cls, value: str) -> str:
+
+        """ Removes the fully qualified domain name prefix (if any) from a string """
+        prefix = 'https://github.com/'
+
+        if not isinstance(value, str) or not value.startswith(prefix):
+            return value
+
+        return value.replace(prefix, '')
+
+
+    # QUADS
+
+    def get_quad(self, quad_ghid: str) -> pandas.DataFrame:
+
+        """ Fetches data about a given quad """
+        query_string = "quad_ghid == '{}'".format(quad_ghid)
+        return self.df.query(query_string).iloc[0]
+
+
+    def get_quad_ghids(self) -> [str]:
+
+        """ Fetches an array of unique quad ghids """
+        return self.df.quad_ghid.unique()
+
+
+    # DELIVERABLES
+
+    def get_deliverable(self, deliverable_ghid: str) -> pandas.DataFrame:
+
+        """ Fetches data about a given deliverable """
+        query_string = "deliverable_ghid == '{}'".format(deliverable_ghid)
+        return self.df.query(query_string).iloc[0]
+
+
+    def get_deliverable_ghids(self) -> [str]:
+
+        """ Fetches an array of unique deliverable ghids """
+        return self.df.deliverable_ghid.unique()
+
+
+    # SPRINTS
+
+    def get_sprint(self, sprint_ghid: str) -> pandas.DataFrame:
+
+        """ Fetches data about a given sprint """
+        query_string = "sprint_ghid == '{}'".format(sprint_ghid)
+        return self.df.query(query_string).iloc[0]
+
+
+    def get_sprint_ghids(self) -> [str]:
+
+        """ Fetches an array of unique sprint ghids """
+        return self.df.sprint_ghid.unique()
+
+
+    # EPICS
+
+    def get_epic(self, epic_ghid: str) -> pandas.DataFrame:
+
+        """ Fetches data about a given epic """
+        query_string = "epic_ghid == '{}'".format(epic_ghid)
+        return self.df.query(query_string).iloc[0]
+
+
+    def get_epic_ghids(self) -> [str]:
+
+        """ Fetches an array of unique epic ghids """
+        return self.df.epic_ghid.unique()
+
+    # ISSUES
+
+    def get_issue(self, issue_ghid: str) -> pandas.DataFrame:
+
+        """ Fetches data about a given issue """
+        query_string = "issue_ghid == '{}'".format(issue_ghid)
+        return self.df.query(query_string).iloc[0]
+
+
+    def get_issue_ghids(self) -> [str]:
+
+        """ Fetches an array of unique issue ghids """
+        return self.df.issue_ghid.unique()
