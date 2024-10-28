565a5233c893485697c1dbd3967a2ad51596eea6
widal001 <billy.daly@agile6.com>
2024-10-28 15:20:43 -0400

    test: Expands tests for GitHubIssues

--- analytics/tests/datasets/test_issues.py
+++ analytics/tests/datasets/test_issues.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import pandas as pd
+import pytest
 from analytics.datasets.issues import (
     GitHubIssues,
     IssueMetadata,
@@ -11,7 +12,14 @@ from analytics.datasets.issues import (
 )
 from analytics.datasets.utils import dump_to_json
 
-from tests.conftest import DAY_0
+from tests.conftest import (
+    DAY_0,
+    DAY_1,
+    DAY_2,
+    DAY_3,
+    DAY_4,
+    DAY_5,
+)
 
 
 def issue(
@@ -30,12 +38,10 @@ def issue(
     # Create issue name
     name = f"{kind.value}{issue}"
     # create timestamp and time delta fields
-    sprint_name = f"Sprint{sprint}"
+    sprint_name = f"Sprint {sprint}"
     sprint_start_ts = pd.Timestamp(sprint_start)
     sprint_duration = pd.Timedelta(days=sprint_length)
     sprint_end_ts = sprint_start_ts + sprint_duration
-    sprint_end = sprint_end_ts.strftime("%Y-%m-%d")
-    print(f"Sprint End: {sprint_end}")
     return IssueMetadata(
         issue_title=name,
         issue_type=kind.value,
@@ -53,7 +59,7 @@ def issue(
         sprint_id=sprint_name,
         sprint_name=sprint_name,
         sprint_start=sprint_start,
-        sprint_end=sprint_end,
+        sprint_end=sprint_end_ts.strftime("%Y-%m-%d"),
     )
 
 
@@ -207,3 +213,64 @@ class TestGetParentWithType:
         )
         # Assert
         assert got == wanted
+
+
+class TestGetSprintNameFromDate:
+    """Test the GitHubIssues.get_sprint_name_from_date() method."""
+
+    @pytest.mark.parametrize(
+        ("date", "expected"),
+        [
+            (DAY_1, "Sprint 1"),
+            (DAY_2, "Sprint 1"),
+            (DAY_4, "Sprint 2"),
+            (DAY_5, "Sprint 2"),
+        ],
+    )
+    def test_return_name_if_matching_sprint_exists(self, date: str, expected: str):
+        """Test that correct sprint is returned if date exists in a sprint."""
+        # setup - create sample dataset
+        board_data = [
+            issue(issue=1, sprint=1, sprint_start=DAY_0, sprint_length=3),
+            issue(issue=2, sprint=1, sprint_start=DAY_0, sprint_length=3),
+            issue(issue=3, sprint=2, sprint_start=DAY_3, sprint_length=3),
+        ]
+        board_data = [i.__dict__ for i in board_data]
+        board = GitHubIssues.from_dict(board_data)
+        # validation
+        sprint_date = pd.Timestamp(date)
+        sprint_name = board.get_sprint_name_from_date(sprint_date)
+        assert sprint_name == expected
+
+    def test_return_none_if_no_matching_sprint(self):
+        """The method should return None if no sprint contains the date."""
+        # setup - create sample dataset
+        board_data = [
+            issue(issue=1, sprint=1, sprint_start=DAY_1),
+            issue(issue=2, sprint=2, sprint_start=DAY_4),
+        ]
+        board_data = [i.__dict__ for i in board_data]
+        board = GitHubIssues.from_dict(board_data)
+        # validation
+        bad_date = pd.Timestamp("1900-01-01")
+        sprint_name = board.get_sprint_name_from_date(bad_date)
+        assert sprint_name is None
+
+    def test_return_previous_sprint_if_date_is_start_of_next_sprint(self):
+        """
+        Test correct behavior for sprint end/start dates.
+
+        If date provided is both the the end of one sprint and the beginning of
+        another, then return the name of the sprint that just ended.
+        """
+        # setup - create sample dataset
+        board_data = [
+            issue(issue=1, sprint=1, sprint_start=DAY_1, sprint_length=2),
+            issue(issue=2, sprint=2, sprint_start=DAY_3, sprint_length=2),
+        ]
+        board_data = [i.__dict__ for i in board_data]
+        board = GitHubIssues.from_dict(board_data)
+        # execution
+        bad_date = pd.Timestamp(DAY_3)  # end of sprint 1 and start of sprint 2
+        sprint_name = board.get_sprint_name_from_date(bad_date)
+        assert sprint_name == "Sprint 1"
