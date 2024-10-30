5a9ec7557d6763923477143cd621891e2c2709b0
David Dudas <david.dudas@intuitial.com>
2024-10-29 20:43:00 -0700

    added class to encapsulate db connection

---
+++ analytics/src/analytics/integrations/etldb/etldb.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from sqlalchemy import Connection
+from analytics.integrations import db
+
+class EtlDb:
+    """An abstraction to encapsulate db connection."""
+
+    def __init__(self):
+        self._db_engine = db.get_db()
+        self.effective_date = None
+
+    def __del__(self):
+        self.disconnect()
+
+    def connection(self) -> Connection:
+        return self._db_engine.connect()
+    
+    def commit(self, connection) -> None:
+        connection.commit()
+
+    def disconnect(self) -> None:
+        self._db_engine.dispose()
+
+
+class EtlChangeType(Enum):
+    """An enum to describe ETL change types"""
+    NONE = 0
+    INSERT = 1
+    UPDATE = 2
