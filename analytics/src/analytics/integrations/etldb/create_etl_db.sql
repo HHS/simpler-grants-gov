e8c20ae5c6c4892d403b12d9b6f33e7db28958ab
David Dudas <david.dudas@intuitial.com>
2024-10-29 20:41:03 -0700

    update sql to use postgres syntax

--- analytics/src/analytics/integrations/etldb/create_etl_db.sql
+++ analytics/src/analytics/integrations/etldb/create_etl_db.sql
@@ -1,8 +1,6 @@
-# create tables
-
 CREATE TABLE IF NOT EXISTS gh_deliverable (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
+	id SERIAL PRIMARY KEY,
+	ghid TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
 	pillar TEXT, 
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -10,7 +8,7 @@ CREATE TABLE IF NOT EXISTS gh_deliverable (
 );
 
 CREATE TABLE IF NOT EXISTS gh_deliverable_quad_map (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id SERIAL PRIMARY KEY,
 	deliverable_id INTEGER NOT NULL,
 	quad_id INTEGER,
 	d_effective DATE NOT NULL,
@@ -20,15 +18,15 @@ CREATE TABLE IF NOT EXISTS gh_deliverable_quad_map (
 CREATE INDEX IF NOT EXISTS dqm_i1 on gh_deliverable_quad_map(quad_id, d_effective);
 
 CREATE TABLE IF NOT EXISTS gh_epic (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
+	id SERIAL PRIMARY KEY,
+	ghid TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	t_modified TIMESTAMP 
 );
 
 CREATE TABLE IF NOT EXISTS gh_epic_deliverable_map (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id SERIAL PRIMARY KEY,
 	epic_id INTEGER NOT NULL,
 	deliverable_id INTEGER,
 	d_effective DATE NOT NULL,
@@ -38,13 +36,13 @@ CREATE TABLE IF NOT EXISTS gh_epic_deliverable_map (
 CREATE INDEX IF NOT EXISTS edm_i1 on gh_epic_deliverable_map(deliverable_id, d_effective);
 
 CREATE TABLE IF NOT EXISTS gh_issue (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
+	id SERIAL PRIMARY KEY,
+	ghid TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
 	type TEXT NOT NULL,
 	opened_date DATE,
 	closed_date DATE,
-	parent_issue_guid TEXT,
+	parent_issue_ghid TEXT,
 	epic_id INTEGER,
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	t_modified TIMESTAMP 
@@ -52,7 +50,7 @@ CREATE TABLE IF NOT EXISTS gh_issue (
 CREATE INDEX IF NOT EXISTS issue_i1 on gh_issue(epic_id);
 
 CREATE TABLE IF NOT EXISTS gh_issue_history (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id SERIAL PRIMARY KEY,
 	issue_id INTEGER NOT NULL,
 	status TEXT,
 	is_closed INTEGER NOT NULL,
@@ -64,7 +62,7 @@ CREATE TABLE IF NOT EXISTS gh_issue_history (
 CREATE INDEX IF NOT EXISTS ih_i1 on gh_issue_history(issue_id, d_effective);
 
 CREATE TABLE IF NOT EXISTS gh_issue_sprint_map (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id SERIAL PRIMARY KEY,
 	issue_id INTEGER NOT NULL,
 	sprint_id INTEGER,
 	d_effective DATE NOT NULL,
@@ -73,8 +71,8 @@ CREATE TABLE IF NOT EXISTS gh_issue_sprint_map (
 );
 
 CREATE TABLE IF NOT EXISTS gh_sprint (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
+	id SERIAL PRIMARY KEY,
+	ghid TEXT UNIQUE NOT NULL,
 	name TEXT NOT NULL,
 	start_date DATE,
 	end_date DATE,
@@ -85,8 +83,8 @@ CREATE TABLE IF NOT EXISTS gh_sprint (
 );
 
 CREATE TABLE IF NOT EXISTS gh_quad (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
+	id SERIAL PRIMARY KEY,
+	ghid TEXT UNIQUE NOT NULL,
 	name TEXT NOT NULL,
 	start_date DATE,
 	end_date DATE,
