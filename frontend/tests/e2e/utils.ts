8e628ffd409a382ef1e17f33da393d784d1f0cd3
Doug Schrashun <dougschrashun@navapbc.com>
2024-11-04 11:39:14 -0500

    remove halfway done e2e test

--- frontend/tests/e2e/utils.ts
+++
@@ -1,8 +0,0 @@
-import { expect, Page, test } from "@playwright/test";
-import { BrowserContextOptions } from "playwright-core";
-
-export interface PageProps {
-  page: Page;
-  browserName?: string;
-  contextOptions?: BrowserContextOptions;
-}
