03224450f82eb586ef58ae6d7493819074b817a7
Emily <emilycarlsen@navapbc.com>
2024-11-04 16:11:14 -0500

    add test and story

---
+++ frontend/tests/components/opportunity/OpportunityDocuments.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import OpportunityDocuments from "src/components/opportunity/OpportunityDocuments";
+
+import { useTranslationsMock } from "src/utils/testing/intlMocks";
+
+jest.mock("next-intl", () => ({
+    useTranslations: () => useTranslationsMock(),
+}));
+
+const mockData = [
+    {
+        opportunity_attachment_type: "notice_of_funding_opportunity",
+        file_name: "FundingInformation.pdf",
+        download_path: "https://example.com",
+        updated_at: "2021-10-01T00:00:00Z",
+    },
+    {
+        opportunity_attachment_type: "other",
+        file_name: "File2_ExhibitB.pdf",
+        download_path: "https://example.com",
+        updated_at: "2021-10-01T00:00:00Z",
+    },
+];
+
+describe("OpportunityDocuments", () => {
+  it("renders", () => {
+    render(<OpportunityDocuments documents={mockData} />);
+
+    const fundLink = screen.getByRole("link", {
+        name: "FundingInformation.pdf",
+    });
+    const otherLink = screen.getByRole("link", {
+        name: "File2_ExhibitB.pdf",
+    });
+
+    expect(fundLink).toBeInTheDocument();
+    expect(fundLink).toHaveAttribute("href", "https://example.com");
+
+    expect(otherLink).toBeInTheDocument();
+    expect(otherLink).toHaveAttribute("href", "https://example.com");
+  });
+});
