1b0397feb7916f8fdfa40db0dc981dd76f7d9921
Aaron Couch <acinternets@gmail.com>
2024-11-07 11:11:34 -0500

    Update for formatting

--- frontend/tests/artillery/processor.ts
+++ frontend/tests/artillery/processor.ts
@@ -1,19 +1,16 @@
 import { random } from "lodash";
 
-
-async function getOppId(context: { vars: { id: number } }) {
-  console.log(context.vars.ids.length)
-  context.vars.id = context.vars.ids[random(context.vars.ids.length-1)]
-
+// eslint-disable-next-line @typescript-eslint/require-await
+async function getOppId(context: { vars: { id: number; ids: Array<number> } }) {
+  context.vars.id = context.vars.ids[random(context.vars.ids.length - 1)];
 }
 
-async function loadOppIds(context: { vars: { ids: Array<number>} }) {
-
-  context.vars.ids = [1,2,3,4,5];
-  console.log('this is loaded!')
+// eslint-disable-next-line @typescript-eslint/require-await
+async function loadOppIds(context: { vars: { ids: Array<number> } }) {
+  context.vars.ids = [1, 2, 3, 4, 5];
 }
 
 module.exports = {
   getOppId,
-  loadOppIds
+  loadOppIds,
 };
