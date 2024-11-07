20b20ed93ea9829577aa457ef0e9249629a203d4
bruk <brukty@gmail.com>
2024-11-07 16:09:30 -0500

    clean

--- api/src/db/models/user_models.py
+++ api/src/db/models/user_models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
