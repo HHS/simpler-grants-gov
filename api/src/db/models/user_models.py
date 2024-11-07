32a30cb14614a3fff03b93e99a836779d1ca5f8d
bruk <brukty@gmail.com>
2024-11-07 15:54:07 -0500

    fix lookup column, add migration script

--- api/src/db/models/user_models.py
+++ api/src/db/models/user_models.py
@@ -4,6 +4,7 @@ from sqlalchemy import BigInteger, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.adapters.db.type_decorators.postgres_type_decorators import LookupColumn
 from src.db.models.base import ApiSchemaTable, TimestampMixin
 from src.db.models.lookup_models import LkExternalUserType
 
@@ -24,8 +25,11 @@ class LinkExternalUser(ApiSchemaTable, TimestampMixin):
     user_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey(User.user_id), index=True)
     user: Mapped[User] = relationship(User)
 
-    external_user_type_id: Mapped[int] = mapped_column(
-        BigInteger, ForeignKey(LkExternalUserType.external_user_type_id)
+    external_user_type: Mapped[int] = mapped_column(
+        "external_user_type_id",
+        LookupColumn(LkExternalUserType),
+        ForeignKey(LkExternalUserType.external_user_type_id),
+        index=True,
     )
 
     email: Mapped[str]
