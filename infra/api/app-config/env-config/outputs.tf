88cb722c6f7cb5c2f83a1a68ee63f52a71fc4f77
kai [they] <coilysiren@gmail.com>
2024-11-12 10:50:01 -0800

    [Issue #2600] fix availability zone count variable (#2807)
    ## Summary
    
    Contributes to #2600
    
    ### Time to review: __0.1 mins__
    
    ## Context for reviewers
    
    
    https://github.com/HHS/simpler-grants-gov/actions/runs/11802929736/job/32879723263#step:5:143

--- infra/api/app-config/env-config/outputs.tf
+++ infra/api/app-config/env-config/outputs.tf
@@ -1,11 +1,11 @@
 output "search_config" {
   value = var.has_search ? {
-    instance_type                  = var.search_data_instance_type
-    instance_count                 = var.search_data_instance_count
-    dedicated_master_type          = var.search_master_instance_type
-    engine_version                 = var.search_engine_version
-    volume_size                    = var.search_data_volume_size
-    search_availability_zone_count = var.search_availability_zone_count
+    instance_type           = var.search_data_instance_type
+    instance_count          = var.search_data_instance_count
+    dedicated_master_type   = var.search_master_instance_type
+    engine_version          = var.search_engine_version
+    volume_size             = var.search_data_volume_size
+    availability_zone_count = var.search_availability_zone_count
   } : null
 }
 
