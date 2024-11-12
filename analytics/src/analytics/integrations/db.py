bb7a2b35e004d66d37aafd77a66945378a107263
David Dudas <david.dudas@intuitial.com>
2024-11-12 13:18:47 -0800

    added temp logging to help troubleshoot db connection failure

--- analytics/src/analytics/integrations/db.py
+++ analytics/src/analytics/integrations/db.py
@@ -25,11 +25,15 @@ def get_db() -> Engine:
     db = get_db_settings()
     # inspired by simpler-grants-gov/blob/main/api/src/adapters/db/clients/postgres_client.py
     token = db.password if db.local_env is True else generate_iam_auth_token(db)
+    url = f"postgresql+psycopg://{db.user}:{token}@{db.db_host}:{db.port}?sslmode={db.ssl_mode}"
+    print(f"TEMP DEBUG: db settings = {db}")
+    print(f"TEMP DEBUG: token = {token}")
+    print(f"TEMP DEBUG: url = {url}")
 
     return create_engine(
-        f"postgresql+psycopg://{db.user}:{token}@{db.db_host}:{db.port}",
+        url,
         pool_pre_ping=True,
-        hide_parameters=True,
+        hide_parameters=False,
     )
 
 
