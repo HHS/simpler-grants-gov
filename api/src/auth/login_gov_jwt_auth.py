f83f0d13e858636244d9281329f914945ca63efe
Michael Chouinard <chouinar6@gmail.com>
2024-11-13 12:26:46 -0500

    Tests and cleanup

--- api/src/auth/login_gov_jwt_auth.py
+++ api/src/auth/login_gov_jwt_auth.py
@@ -1,18 +1,24 @@
-
 import dataclasses
+import logging
+
 import jwt
 from pydantic import Field
+
 from src.util.env_config import PydanticBaseEnvConfig
+
+logger = logging.getLogger(__name__)
 
 
 class LoginGovConfig(PydanticBaseEnvConfig):
+    """
+    Configuration for login.gov JWT auth
 
+    Do not create this directly, instead call get_login_gov_config
+    which will handle setting it up for you.
+    """
 
-    # TODO - actually hook this up to env vars in some manner
-
-    # TODO - public keys should be something we can configure from
-    # an env var but only ever do so from local - otherwise
-    # when we create it it will load.
+    # Public keys likely won't ever be set by an env var, so it defaults
+    # to an empty list and gets overriden by any call to _refresh_keys
     public_keys: list[jwt.PyJWK | str] = Field(alias="LOGIN_GOV_PUBLIC_KEYS", default_factory=list)
     login_gov_jwk_endpoint: str = Field(alias="LOGIN_GOV_JWK_ENDPOINT")
 
@@ -21,39 +27,65 @@ class LoginGovConfig(PydanticBaseEnvConfig):
     login_gov_endpoint: str = Field(alias="LOGIN_GOV_ENDPOINT")
     client_id: str = Field(alias="LOGIN_GOV_CLIENT_ID")
 
+
 def get_login_gov_config() -> LoginGovConfig:
     config = LoginGovConfig()
-    refresh_keys(config)
+    _refresh_keys(config)
     return config
+
 
 @dataclasses.dataclass
 class LoginGovUser:
     user_id: str
     email: str
 
+
 class JwtValidationError(Exception):
     """
     Exception we will reraise if there are
     any issues processing a JWT that should
-    cause the validation to error out
+    cause the endpoint to raise a 401
     """
+
     pass
 
-def refresh_keys(config: LoginGovConfig) -> None:
+
+def _refresh_keys(config: LoginGovConfig) -> None:
+    """
+    WARNING:
+        This implementation is technically incorrect as it does
+        not account for thread safety. If multiple threads attempt to
+        refresh the token at the same time, they will all set it separately.
+
+        Assignment in python should be atomic, and the Python global-interpreter-lock
+        likely make this less risky, but there is no guarantee that this won't
+        cause issues as we use it.
+
+        We will evaluate this behavior over time and see if it causes us any issues.
+        For now, we are fine accepting this risk as the complexity of caching this
+        in a thread-safe way (eg. database, redis, or using Python locks)
+        isn't seen as worthwhile at the moment.
+    """
+    logger.info("Refreshing login.gov JWKs")
     jwk_client = jwt.PyJWKClient(config.login_gov_jwk_endpoint)
     public_keys = jwk_client.get_jwk_set().keys
-    config.public_keys = public_keys
+
+    # This line is possibly an issue for the reasons described above.
+    config.public_keys = list(public_keys)
+
 
 def validate_token(token: str, config: LoginGovConfig) -> LoginGovUser:
 
     # TODO - this iteration approach won't be necessary if the JWT we get
     #        from login.gov does actually set the KID in the header
+    # Iterate over the public keys we have and check each
+    # to determine if we have a valid key.
     for public_key in config.public_keys:
         user = _validate_token_with_key(token, public_key, config)
         if user is not None:
             return user
 
-    refresh_keys(config)
+    _refresh_keys(config)
 
     for public_key in config.public_keys:
         user = _validate_token_with_key(token, public_key, config)
@@ -62,25 +94,30 @@ def validate_token(token: str, config: LoginGovConfig) -> LoginGovUser:
 
     raise JwtValidationError("Token could not be validated against any public keys from login.gov")
 
-def _validate_token_with_key(token: str, public_key: jwt.PyJWK, config: LoginGovConfig) -> LoginGovUser | None:
+
+def _validate_token_with_key(
+    token: str, public_key: jwt.PyJWK | str, config: LoginGovConfig
+) -> LoginGovUser | None:
     # We are processing the id_token as described on:
     # https://developers.login.gov/oidc/token/#token-response
     try:
-        data = jwt.api_jwt.decode_complete(token,
-                                           public_key,
-                                           algorithms=[config.encryption_algorithm],
-                                           issuer=config.login_gov_endpoint,
-                                           audience=config.client_id,
-                                           # By default these options are already set to validate
-                                           # but making it very clear / explicit the validations we are doing
-                                           options={
-                                               "verify_signature": True,
-                                               "verify_exp": True,
-                                               "verify_iat": True,
-                                               "verify_nbf": True,
-                                               "verify_aud": True,
-                                               "verify_iss": True,
-        })
+        data = jwt.api_jwt.decode_complete(
+            token,
+            public_key,
+            algorithms=[config.encryption_algorithm],
+            issuer=config.login_gov_endpoint,
+            audience=config.client_id,
+            # By default these options are already set to validate
+            # but making it very clear / explicit the validations we are doing
+            options={
+                "verify_signature": True,
+                "verify_exp": True,
+                "verify_iat": True,
+                "verify_nbf": True,
+                "verify_aud": True,
+                "verify_iss": True,
+            },
+        )
         payload = data.get("payload", {})
 
         user_id = payload["sub"]
@@ -96,7 +133,7 @@ def _validate_token_with_key(token: str, public_key: jwt.PyJWK, config: LoginGov
         raise JwtValidationError("Token Missing Required Field(s)") from e
     except jwt.ExpiredSignatureError as e:
         raise JwtValidationError("Expired Token") from e
-    except jwt.ImmatureSignatureError as e: # IAT and NBF errors hit this
+    except jwt.ImmatureSignatureError as e:  # IAT and NBF errors hit this
         raise JwtValidationError("Token not yet valid") from e
     except jwt.InvalidIssuerError as e:
         raise JwtValidationError("Unknown Issuer") from e
@@ -107,5 +144,5 @@ def _validate_token_with_key(token: str, public_key: jwt.PyJWK, config: LoginGov
         # Since we might be checking against the wrong key (unless we get a KID)
         # we don't want to error necessarily
         return None
-    except jwt.PyJWTError as e: # Every other type of JWT error not caught above
+    except jwt.PyJWTError as e:  # Every other type of JWT error not caught above
         raise JwtValidationError("Unable to process token") from e
--- api/tests/src/auth/test_login_gov_jwt_auth.py
+++ api/tests/src/auth/test_login_gov_jwt_auth.py
@@ -1,26 +1,76 @@
-import jwt
 from datetime import datetime, timedelta, timezone
 
+import jwt
 import pytest
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
-from src.auth.login_gov_jwt_auth import validate_token, LoginGovConfig, LoginGovUser, JwtValidationError
-
-# TODO - make our own RSA key
-private_key = b"-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAwhvqCC+37A+UXgcvDl+7nbVjDI3QErdZBkI1VypVBMkKKWHM\nNLMdHk0bIKL+1aDYTRRsCKBy9ZmSSX1pwQlO/3+gRs/MWG27gdRNtf57uLk1+lQI\n6hBDozuyBR0YayQDIx6VsmpBn3Y8LS13p4pTBvirlsdX+jXrbOEaQphn0OdQo0WD\noOwwsPCNCKoIMbUOtUCowvjesFXlWkwG1zeMzlD1aDDS478PDZdckPjT96ICzqe4\nO1Ok6fRGnor2UTmuPy0f1tI0F7Ol5DHAD6pZbkhB70aTBuWDGLDR0iLenzyQecmD\n4aU19r1XC9AHsVbQzxHrP8FveZGlV/nJOBJwFwIDAQABAoIBAFCVFBA39yvJv/dV\nFiTqe1HahnckvFe4w/2EKO65xTfKWiyZzBOotBLrQbLH1/FJ5+H/82WVboQlMATQ\nSsH3olMRYbFj/NpNG8WnJGfEcQpb4Vu93UGGZP3z/1B+Jq/78E15Gf5KfFm91PeQ\nY5crJpLDU0CyGwTls4ms3aD98kNXuxhCGVbje5lCARizNKfm/+2qsnTYfKnAzN+n\nnm0WCjcHmvGYO8kGHWbFWMWvIlkoZ5YubSX2raNeg+YdMJUHz2ej1ocfW0A8/tmL\nwtFoBSuBe1Z2ykhX4t6mRHp0airhyc+MO0bIlW61vU/cPGPos16PoS7/V08S7ZED\nX64rkyECgYEA4iqeJZqny/PjOcYRuVOHBU9nEbsr2VJIf34/I9hta/mRq8hPxOdD\n/7ES/ZTZynTMnOdKht19Fi73Sf28NYE83y5WjGJV/JNj5uq2mLR7t2R0ZV8uK8tU\n4RR6b2bHBbhVLXZ9gqWtu9bWtsxWOkG1bs0iONgD3k5oZCXp+IWuklECgYEA27bA\n7UW+iBeB/2z4x1p/0wY+whBOtIUiZy6YCAOv/HtqppsUJM+W9GeaiMpPHlwDUWxr\n4xr6GbJSHrspkMtkX5bL9e7+9zBguqG5SiQVIzuues9Jio3ZHG1N2aNrr87+wMiB\nxX6Cyi0x1asmsmIBO7MdP/tSNB2ebr8qM6/6mecCgYBA82ZJfFm1+8uEuvo6E9/R\nyZTbBbq5BaVmX9Y4MB50hM6t26/050mi87J1err1Jofgg5fmlVMn/MLtz92uK/hU\nS9V1KYRyLc3h8gQQZLym1UWMG0KCNzmgDiZ/Oa/sV5y2mrG+xF/ZcwBkrNgSkO5O\n7MBoPLkXrcLTCARiZ9nTkQKBgQCsaBGnnkzOObQWnIny1L7s9j+UxHseCEJguR0v\nXMVh1+5uYc5CvGp1yj5nDGldJ1KrN+rIwMh0FYt+9dq99fwDTi8qAqoridi9Wl4t\nIXc8uH5HfBT3FivBtLucBjJgOIuK90ttj8JNp30tbynkXCcfk4NmS23L21oRCQyy\nlmqNDQKBgQDRvzEB26isJBr7/fwS0QbuIlgzEZ9T3ZkrGTFQNfUJZWcUllYI0ptv\ny7ShHOqyvjsC3LPrKGyEjeufaM5J8EFrqwtx6UB/tkGJ2bmd1YwOWFHvfHgHCZLP\n34ZNURCvxRV9ZojS1zmDRBJrSo7+/K0t28hXbiaTOjJA18XAyyWmGg==\n-----END RSA PRIVATE KEY-----\n"
-public_key = b"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwhvqCC+37A+UXgcvDl+7\nnbVjDI3QErdZBkI1VypVBMkKKWHMNLMdHk0bIKL+1aDYTRRsCKBy9ZmSSX1pwQlO\n/3+gRs/MWG27gdRNtf57uLk1+lQI6hBDozuyBR0YayQDIx6VsmpBn3Y8LS13p4pT\nBvirlsdX+jXrbOEaQphn0OdQo0WDoOwwsPCNCKoIMbUOtUCowvjesFXlWkwG1zeM\nzlD1aDDS478PDZdckPjT96ICzqe4O1Ok6fRGnor2UTmuPy0f1tI0F7Ol5DHAD6pZ\nbkhB70aTBuWDGLDR0iLenzyQecmD4aU19r1XC9AHsVbQzxHrP8FveZGlV/nJOBJw\nFwIDAQAB\n-----END PUBLIC KEY-----\n"
+import src.auth.login_gov_jwt_auth as login_gov_jwt_auth
+from src.auth.login_gov_jwt_auth import JwtValidationError, LoginGovConfig, validate_token
 
 DEFAULT_CLIENT_ID = "urn:gov:unit-test"
 DEFAULT_ISSUER = "http://localhost:3000"
 
+
+def _generate_rsa_key_pair():
+    # Rather than define a private/public key, generate one for the tests
+    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+
+    private_key = key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+    public_key = key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return private_key, public_key
+
+
+@pytest.fixture(scope="session")
+def rsa_key_pair():
+    return _generate_rsa_key_pair()
+
+
+@pytest.fixture(scope="session")
+def private_rsa_key(rsa_key_pair):
+    return rsa_key_pair[0]
+
+
+@pytest.fixture(scope="session")
+def public_rsa_key(rsa_key_pair):
+    return rsa_key_pair[1]
+
+
+@pytest.fixture(scope="session")
+def other_rsa_key_pair():
+    return _generate_rsa_key_pair()
+
+
+@pytest.fixture
+def login_gov_config(public_rsa_key):
+    # Note this isn't session scoped so it gets remade
+    # for every test in the event of changes to it
+    return LoginGovConfig(
+        LOGIN_GOV_PUBLIC_KEYS=[public_rsa_key],
+        LOGIN_GOV_JWK_ENDPOINT="not_used",
+        LOGIN_GOV_ENDPOINT=DEFAULT_ISSUER,
+        LOGIN_GOV_CLIENT_ID=DEFAULT_CLIENT_ID,
+    )
+
+
 def create_jwt(
-        user_id: str,
-        email: str,
-        expires_at: datetime,
-        issued_at: datetime,
-        not_before: datetime,
-        issuer: str = DEFAULT_ISSUER,
-        audience: str = DEFAULT_CLIENT_ID,
-        acr: str = "urn:acr.login.gov:auth-only",
+    user_id: str,
+    email: str,
+    expires_at: datetime,
+    issued_at: datetime,
+    not_before: datetime,
+    private_key: str | bytes,
+    issuer: str = DEFAULT_ISSUER,
+    audience: str = DEFAULT_CLIENT_ID,
+    acr: str = "urn:acr.login.gov:auth-only",
 ):
     payload = {
         "sub": user_id,
@@ -43,23 +93,15 @@ def create_jwt(
 
     return jwt.encode(payload, private_key, algorithm="RS256")
 
-@pytest.fixture
-def login_gov_config():
-    return LoginGovConfig(
-        LOGIN_GOV_PUBLIC_KEYS=[public_key],
-        LOGIN_GOV_JWK_ENDPOINT="not_used",
-        LOGIN_GOV_ENDPOINT=DEFAULT_ISSUER,
-        LOGIN_GOV_CLIENT_ID=DEFAULT_CLIENT_ID
-    )
 
-
-def test_validate_token_happy_path(login_gov_config):
+def test_validate_token_happy_path(login_gov_config, private_rsa_key):
     user_id = "12345678-abc"
     email = "fake@mail.com"
 
     token = create_jwt(
         user_id=user_id,
         email=email,
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
         issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
         not_before=datetime.now(tz=timezone.utc) - timedelta(days=1),
@@ -70,10 +112,12 @@ def test_validate_token_happy_path(login_gov_config):
     assert login_gov_user.user_id == user_id
     assert login_gov_user.email == email
 
-def test_validate_token_expired(login_gov_config):
+
+def test_validate_token_expired(login_gov_config, private_rsa_key):
     token = create_jwt(
         user_id="abc123",
         email="mail@fake.com",
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
         issued_at=datetime.now(tz=timezone.utc) - timedelta(days=30),
         not_before=datetime.now(tz=timezone.utc) - timedelta(days=30),
@@ -82,10 +126,12 @@ def test_validate_token_expired(login_gov_config):
     with pytest.raises(JwtValidationError, match="Expired Token"):
         validate_token(token, login_gov_config)
 
-def test_validate_token_issued_at_future(login_gov_config):
+
+def test_validate_token_issued_at_future(login_gov_config, private_rsa_key):
     token = create_jwt(
         user_id="abc123",
         email="mail@fake.com",
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) + timedelta(days=1),
         issued_at=datetime.now(tz=timezone.utc) + timedelta(days=1),
         not_before=datetime.now(tz=timezone.utc) - timedelta(days=30),
@@ -95,10 +141,11 @@ def test_validate_token_issued_at_future(login_gov_config):
         validate_token(token, login_gov_config)
 
 
-def test_validate_token_not_before_future(login_gov_config):
+def test_validate_token_not_before_future(login_gov_config, private_rsa_key):
     token = create_jwt(
         user_id="abc123",
         email="mail@fake.com",
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
         issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
         not_before=datetime.now(tz=timezone.utc) + timedelta(days=1),
@@ -107,11 +154,13 @@ def test_validate_token_not_before_future(login_gov_config):
     with pytest.raises(JwtValidationError, match="Token not yet valid"):
         validate_token(token, login_gov_config)
 
-def test_validate_token_unknown_issuer(login_gov_config):
+
+def test_validate_token_unknown_issuer(login_gov_config, private_rsa_key):
     token = create_jwt(
         user_id="abc123",
         email="mail@fake.com",
         issuer="fred",
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
         issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
         not_before=datetime.now(tz=timezone.utc) - timedelta(days=1),
@@ -120,11 +169,13 @@ def test_validate_token_unknown_issuer(login_gov_config):
     with pytest.raises(JwtValidationError, match="Unknown Issuer"):
         validate_token(token, login_gov_config)
 
-def test_validate_token_unknown_audience(login_gov_config):
+
+def test_validate_token_unknown_audience(login_gov_config, private_rsa_key):
     token = create_jwt(
         user_id="abc123",
         email="mail@fake.com",
         audience="fred",
+        private_key=private_rsa_key,
         expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
         issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
         not_before=datetime.now(tz=timezone.utc) - timedelta(days=1),
@@ -132,3 +183,45 @@ def test_validate_token_unknown_audience(login_gov_config):
 
     with pytest.raises(JwtValidationError, match="Unknown Audience"):
         validate_token(token, login_gov_config)
+
+
+def test_validate_token_invalid_signature(login_gov_config, other_rsa_key_pair, monkeypatch):
+    token = create_jwt(
+        user_id="abc123",
+        email="mail@fake.com",
+        private_key=other_rsa_key_pair[0],  # Create it with a different key
+        expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
+        issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
+        not_before=datetime.now(tz=timezone.utc) - timedelta(days=1),
+    )
+
+    # Need to override the refresh logic so it doesn't try to reach out to anything
+    # We don't need to set the keys to anything else here.
+    def override_method(config):
+        pass
+
+    monkeypatch.setattr(login_gov_jwt_auth, "_refresh_keys", override_method)
+
+    with pytest.raises(
+        JwtValidationError,
+        match="Token could not be validated against any public keys from login.gov",
+    ):
+        validate_token(token, login_gov_config)
+
+
+def test_something_with_the_refresh(login_gov_config, other_rsa_key_pair, monkeypatch):
+    token = create_jwt(
+        user_id="abc123",
+        email="mail@fake.com",
+        private_key=other_rsa_key_pair[0],  # Create it with a different key
+        expires_at=datetime.now(tz=timezone.utc) + timedelta(days=30),
+        issued_at=datetime.now(tz=timezone.utc) - timedelta(days=1),
+        not_before=datetime.now(tz=timezone.utc) - timedelta(days=1),
+    )
+
+    def override_method(config):
+        config.public_keys = [other_rsa_key_pair[1]]
+
+    monkeypatch.setattr(login_gov_jwt_auth, "_refresh_keys", override_method)
+
+    validate_token(token, login_gov_config)
