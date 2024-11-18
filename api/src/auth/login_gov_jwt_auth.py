6f81b254da89578c979c573d6e51ff6f13dc16ea
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2024-11-18 15:28:52 -0500

    [Issue #2721] Setup logic to validate login.gov tokens against their public keys (#2837)
    ## Summary
    Fixes #2721
    
    ### Time to review: __15 mins__
    
    ## Changes proposed
    Add logic to validate the authenticity of tokens from login.gov
    
    Add rough logic for fetching public keys from login.gov
    
    Add logic for parsing tokens from login.gov, extracting relevant info
    
    ## Context for reviewers
    We will be processing the id_token returned by this login.gov endpoint
    https://developers.login.gov/oidc/token/
    
    This token follows most general JWT conventions. In order to validate
    the authenticity of the token, we need to fetch the public key from
    login.gov (see: https://developers.login.gov/oidc/certificates/ ). This
    is all standard Oauth stuff, we just have to deal with complexities
    surrounding the caching of these public keys.
    
    We assume that if a token fails to validate with the public key, that it
    might be with a key we don't yet know about, and refresh the keys (this
    is a pretty standard approach), however this refresh runs into
    thread-safety issues that are documented inline. In short, the API runs
    multiple threads and they might all try to refresh simultaneously.
    Whether that is an issue, we'll have to see.
    
    Most of the actual validation on the token is done inside of the library
    and just boils down to:
    * Proving it was generated with the key (the public key verification the
    JWT library handles
    * Verifying timestamps are in the right order
    * Verifying who sent us this token (clientID will need to be configured
    to match whatever the frontend sets up).
    
    After all this, we're certain we know who they are, and can use the
    token, although the current logic stops after parsing.
    
    ## Additional information
    More thorough testing of this behavior needs the frontend / integration
    with login.gov to be a bit further along. There are a few small unknowns
    in the details of the token we'll be parsing that might require small
    adjustments.
    
    To avoid committing tokens, I make the tests fully generate the RSA
    tokens in the tests. This is the same as defining a public and private
    key in a very long form (the "BEGIN PUBLIC KEY..." files).

---
+++ api/src/auth/login_gov_jwt_auth.py
@@ -0,0 +1,148 @@
+import dataclasses
+import logging
+
+import jwt
+from pydantic import Field
+
+from src.util.env_config import PydanticBaseEnvConfig
+
+logger = logging.getLogger(__name__)
+
+
+class LoginGovConfig(PydanticBaseEnvConfig):
+    """
+    Configuration for login.gov JWT auth
+
+    Do not create this directly, instead call get_login_gov_config
+    which will handle setting it up for you.
+    """
+
+    # Public keys likely won't ever be set by an env var, so it defaults
+    # to an empty list and gets overriden by any call to _refresh_keys
+    public_keys: list[jwt.PyJWK | str] = Field(alias="LOGIN_GOV_PUBLIC_KEYS", default_factory=list)
+    login_gov_jwk_endpoint: str = Field(alias="LOGIN_GOV_JWK_ENDPOINT")
+
+    encryption_algorithm: str = Field(alias="LOGIN_GOV_ENCRYPTION_ALGORITHM", default="RS256")
+
+    login_gov_endpoint: str = Field(alias="LOGIN_GOV_ENDPOINT")
+    client_id: str = Field(alias="LOGIN_GOV_CLIENT_ID")
+
+
+def get_login_gov_config() -> LoginGovConfig:
+    config = LoginGovConfig()
+    _refresh_keys(config)
+    return config
+
+
+@dataclasses.dataclass
+class LoginGovUser:
+    user_id: str
+    email: str
+
+
+class JwtValidationError(Exception):
+    """
+    Exception we will reraise if there are
+    any issues processing a JWT that should
+    cause the endpoint to raise a 401
+    """
+
+    pass
+
+
+def _refresh_keys(config: LoginGovConfig) -> None:
+    """
+    WARNING:
+        This implementation is technically incorrect as it does
+        not account for thread safety. If multiple threads attempt to
+        refresh the token at the same time, they will all set it separately.
+
+        Assignment in python should be atomic, and the Python global-interpreter-lock
+        likely make this less risky, but there is no guarantee that this won't
+        cause issues as we use it.
+
+        We will evaluate this behavior over time and see if it causes us any issues.
+        For now, we are fine accepting this risk as the complexity of caching this
+        in a thread-safe way (eg. database, redis, or using Python locks)
+        isn't seen as worthwhile at the moment.
+    """
+    logger.info("Refreshing login.gov JWKs")
+    jwk_client = jwt.PyJWKClient(config.login_gov_jwk_endpoint)
+    public_keys = jwk_client.get_jwk_set().keys
+
+    # This line is possibly an issue for the reasons described above.
+    config.public_keys = list(public_keys)
+
+
+def validate_token(token: str, config: LoginGovConfig) -> LoginGovUser:
+
+    # TODO - this iteration approach won't be necessary if the JWT we get
+    #        from login.gov does actually set the KID in the header
+    # Iterate over the public keys we have and check each
+    # to determine if we have a valid key.
+    for public_key in config.public_keys:
+        user = _validate_token_with_key(token, public_key, config)
+        if user is not None:
+            return user
+
+    _refresh_keys(config)
+
+    for public_key in config.public_keys:
+        user = _validate_token_with_key(token, public_key, config)
+        if user is not None:
+            return user
+
+    raise JwtValidationError("Token could not be validated against any public keys from login.gov")
+
+
+def _validate_token_with_key(
+    token: str, public_key: jwt.PyJWK | str, config: LoginGovConfig
+) -> LoginGovUser | None:
+    # We are processing the id_token as described on:
+    # https://developers.login.gov/oidc/token/#token-response
+    try:
+        data = jwt.api_jwt.decode_complete(
+            token,
+            public_key,
+            algorithms=[config.encryption_algorithm],
+            issuer=config.login_gov_endpoint,
+            audience=config.client_id,
+            # By default these options are already set to validate
+            # but making it very clear / explicit the validations we are doing
+            options={
+                "verify_signature": True,
+                "verify_exp": True,
+                "verify_iat": True,
+                "verify_nbf": True,
+                "verify_aud": True,
+                "verify_iss": True,
+            },
+        )
+        payload = data.get("payload", {})
+
+        user_id = payload["sub"]
+        email = payload["email"]
+
+        return LoginGovUser(user_id=user_id, email=email)
+
+    # Most exceptions will result in an outright error
+    # as the only change to calls to this function are the public keys
+    # we use to validate. Unless it is a public-key-validation related error
+    # just reraise as a JwtValidationError here
+    except KeyError as e:
+        raise JwtValidationError("Token Missing Required Field(s)") from e
+    except jwt.ExpiredSignatureError as e:
+        raise JwtValidationError("Expired Token") from e
+    except jwt.ImmatureSignatureError as e:  # IAT and NBF errors hit this
+        raise JwtValidationError("Token not yet valid") from e
+    except jwt.InvalidIssuerError as e:
+        raise JwtValidationError("Unknown Issuer") from e
+    except jwt.InvalidAudienceError as e:
+        raise JwtValidationError("Unknown Audience") from e
+    except jwt.InvalidSignatureError:
+        # This occurs if the validation fails for the key.
+        # Since we might be checking against the wrong key (unless we get a KID)
+        # we don't want to error necessarily
+        return None
+    except jwt.PyJWTError as e:  # Every other type of JWT error not caught above
+        raise JwtValidationError("Unable to process token") from e
