9abb20eb9e1ea4785eef0f0f11d4c5b80292939c
David Dudas <david.dudas@intuitial.com>
2024-11-19 14:28:58 -0800

    combined sql files

--- analytics/src/analytics/integrations/etldb/migrations/versions/0004_create_table_deliv_hist.sql
+++ analytics/src/analytics/integrations/etldb/migrations/versions/0004_create_table_deliv_hist.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS gh_deliverable_history (
 	UNIQUE(deliverable_id, d_effective)
 );
 CREATE INDEX IF NOT EXISTS gh_dh_i1 on gh_deliverable_history(deliverable_id, d_effective);
+
+CREATE TABLE IF NOT EXISTS gh_project (
+	id SERIAL PRIMARY KEY,
+	ghid INTEGER UNIQUE NOT NULL,
+	name TEXT NOT NULL,
+	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	t_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
