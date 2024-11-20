a3b77f6f61e74fdbe0a6c565a98fb269770f8d54
Emily <emilycarlsen@navapbc.com>
2024-11-20 15:59:48 -0500

    add tests for dl component

---
+++ frontend/tests/components/opportunity/OpportunityDownload.test.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useTranslationsMock } from "src/utils/testing/intlMocks";
+
+import OpportunityDownload from "../../../src/components/opportunity/OpportunityDownload";
+
+jest.mock("next-intl", () => ({
+  useTranslations: () => useTranslationsMock(),
+}));
+
+describe("OpportunityDownload Component", () => {
+  const nofoPath = "http://example.com/nofo.pdf";
+
+  it("renders if nofoPath is present", () => {
+    render(<OpportunityDownload nofoPath={nofoPath} />);
+
+    expect(
+      screen.getByRole("button", { name: "nofo_download" }),
+    ).toBeInTheDocument();
+  });
+
+  it("does not render if nofoPath is not present", () => {
+    render(<OpportunityDownload nofoPath="" />);
+
+    expect(
+      screen.queryByRole("button", { name: "nofo_download" }),
+    ).not.toBeInTheDocument();
+  });
+
+  it("opens a new tab when download button is clicked", () => {
+    window.open = jest.fn();
+
+    render(<OpportunityDownload nofoPath={nofoPath} />);
+
+    const downloadButton = screen.getByRole("button", {
+      name: "nofo_download",
+    });
+
+    fireEvent.click(downloadButton);
+
+    expect(window.open).toHaveBeenCalledWith(nofoPath, "_blank");
+  });
+});
