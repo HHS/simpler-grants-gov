4e45c18c91bc1896687c6910887910dcdff66389
bruk <brukty@gmail.com>
2024-11-19 22:08:00 -0500

    validate response data

--- api/tests/src/api/users/test_user_route_token.py
+++ api/tests/src/api/users/test_user_route_token.py
@@ -1,16 +1,27 @@
+##################
+# POST /user/token
+##################
 
-
-#####################################
-# POST user token tests
-#####################################
 
 def test_post_user_route_token_200(client, api_auth_token):
-    resp= client.post(f"/v1/users/user/token", headers={"X-Auth": api_auth_token, "X-OAuth-login-gov": "test_token" })
-    assert resp.status_code == 200
+    resp = client.post(
+        "/v1/users/user/token", headers={"X-Auth": api_auth_token, "X-OAuth-login-gov": "test"}
+    )
     response_data = resp.get_json()["data"]
+    expected_response_data = {
+        "token": "the token goes here!",
+        "user": {
+            "user_id": "abc-...",
+            "email": "example@gmail.com",
+            "external_user_type": "login_gov",
+        },
+        "is_user_new": True,
+    }
+    assert resp.status_code == 200
+    assert response_data == expected_response_data
 
 
 def test_post_user_route_token_400(client, api_auth_token):
-    resp= client.post(f"v1/users/user/token", headers={"X-Auth": api_auth_token})
+    resp = client.post("v1/users/user/token", headers={"X-Auth": api_auth_token})
     assert resp.status_code == 400
     assert resp.get_json()["message"] == "Missing X-OAuth-login-gov header"
