35fb5d6acbd08606544a27810368eba47713afa2
babebe <brukty@gmail.com>
2024-11-22 16:36:42 -0500

    [Issue 2648] Add signout endpoint (#2997)
    ## Summary
    Fixes #{[2648](https://github.com/HHS/simpler-grants-gov/issues/2648)}
    
    ### Time to review: __5 mins__
    
    ## Changes proposed.
    
    Added `/token/logout` endpoint 
    Added `LogoutResponseSchema` response schema
    Added success and invalid token tests
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

--- api/src/api/users/user_routes.py
+++ api/src/api/users/user_routes.py
@@ -1,10 +1,15 @@
 import logging
 
+from src.adapters import db
+from src.adapters.db import flask_db
 from src.api import response
 from src.api.route_utils import raise_flask_error
 from src.api.users import user_schemas
 from src.api.users.user_blueprint import user_blueprint
+from src.api.users.user_schemas import UserTokenLogoutResponseSchema
+from src.auth.api_jwt_auth import api_jwt_auth
 from src.auth.api_key_auth import api_key_auth
+from src.db.models.user_models import UserTokenSession
 
 logger = logging.getLogger(__name__)
 
@@ -34,3 +39,27 @@ def user_token(x_oauth_login_gov: dict) -> response.ApiResponse:
     logger.info(message)
 
     raise_flask_error(400, message)
+
+
+@user_blueprint.post("/token/logout")
+@user_blueprint.output(UserTokenLogoutResponseSchema)
+@user_blueprint.doc(responses=[200, 401])
+@user_blueprint.auth_required(api_jwt_auth)
+@flask_db.with_db_session()
+def user_token_logout(db_session: db.Session) -> response.ApiResponse:
+    logger.info("POST /v1/users/token/logout")
+
+    user_token_session: UserTokenSession = api_jwt_auth.current_user  # type: ignore
+    with db_session.begin():
+        user_token_session.is_valid = False
+        db_session.add(user_token_session)
+
+    logger.info(
+        "Logged out a user",
+        extra={
+            "user_token_session.token_id": str(user_token_session.token_id),
+            "user_token_session.user_id": str(user_token_session.user_id),
+        },
+    )
+
+    return response.ApiResponse(message="Success")
