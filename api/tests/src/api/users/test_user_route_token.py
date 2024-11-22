ad4b5936128e9d7788edf096006b67f4d386532b
bruk <brukty@gmail.com>
2024-11-22 15:56:16 -0500

    add suceess and expired token test

--- api/tests/src/api/users/test_user_route_token.py
+++ api/tests/src/api/users/test_user_route_token.py
@@ -1,6 +1,10 @@
 ##################
 # POST /token
 ##################
+from datetime import datetime
+
+from src.auth.api_jwt_auth import create_jwt_for_user
+from tests.src.db.models.factories import UserFactory
 
 
 def test_post_user_route_token_200(client, api_auth_token):
@@ -25,3 +29,34 @@ def test_post_user_route_token_400(client, api_auth_token):
     resp = client.post("v1/users/token", headers={"X-Auth": api_auth_token})
     assert resp.status_code == 400
     assert resp.get_json()["message"] == "Missing X-OAuth-login-gov header"
+
+
+def test_post_user_route_token_refresh_200(
+    enable_factory_create, client, db_session, api_auth_token
+):
+    user = UserFactory.create()
+    token, user_token_session = create_jwt_for_user(user, db_session)
+    expiration = user_token_session.expires_at
+    db_session.commit()
+
+    resp = client.post("v1/users/token/refresh", headers={"X-SGG-Token": token})
+
+    db_session.refresh(user_token_session)
+
+    assert resp.status_code == 200
+    assert not user_token_session.expired_at != expiration
+
+
+def test_post_user_route_token_refresh_expired(
+    enable_factory_create, client, db_session, api_auth_token
+):
+    user = UserFactory.create()
+
+    token, session = create_jwt_for_user(user, db_session)
+    session.expires_at = datetime.fromisoformat("1980-01-01 12:00:00+00:00")
+    db_session.commit()
+
+    resp = client.post("v1/users/token/refresh", headers={"X-SGG-Token": token})
+
+    assert resp.status_code == 401
+    assert resp.get_json()["message"] == "Token expired"
