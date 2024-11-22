ee81a009de25b936a42f744b1483d4966bd33fdd
bruk <brukty@gmail.com>
2024-11-22 14:05:34 -0500

    add 200 and 401 invalid token error

--- api/tests/src/api/users/test_user_route_token.py
+++ api/tests/src/api/users/test_user_route_token.py
@@ -1,3 +1,6 @@
+from src.auth.api_jwt_auth import create_jwt_for_user
+from tests.src.db.models.factories import UserFactory
+
 ##################
 # POST /token
 ##################
@@ -25,3 +28,30 @@ def test_post_user_route_token_400(client, api_auth_token):
     resp = client.post("v1/users/token", headers={"X-Auth": api_auth_token})
     assert resp.status_code == 400
     assert resp.get_json()["message"] == "Missing X-OAuth-login-gov header"
+
+
+def test_post_user_route_token_logout_200(
+    enable_factory_create, client, db_session, api_auth_token
+):
+    user = UserFactory.create()
+    token, _ = create_jwt_for_user(user, db_session)
+    db_session.commit()
+
+    resp = client.post("v1/users/token/logout", headers={"X-SGG-Token": token})
+
+    assert resp.status_code == 200
+
+
+def test_post_user_route_token_logout_invalid(
+    enable_factory_create, client, db_session, api_auth_token
+):
+    user = UserFactory.create()
+
+    token, session = create_jwt_for_user(user, db_session)
+    session.is_valid = False
+    db_session.commit()
+
+    resp = client.post("v1/users/token/logout", headers={"X-SGG-Token": token})
+
+    assert resp.status_code == 401
+    assert resp.get_json()["message"] == "Token is no longer valid"
