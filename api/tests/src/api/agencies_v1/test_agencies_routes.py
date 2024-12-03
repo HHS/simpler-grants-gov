6f217a0f3c575cea4a5fbb65c0869ebe46146785
Michael Huneke <Mike.Huneke@mass.gov>
2024-12-03 10:32:09 -0500

    Use class approach

--- api/tests/src/api/agencies_v1/test_agencies_routes.py
+++ api/tests/src/api/agencies_v1/test_agencies_routes.py
@@ -1,32 +1,39 @@
 import pytest
 
-from src.db.models.agency_models import Agency, LinkAgencyDownloadFileType
+from tests.conftest import BaseTestClass
+from src.db.models.agency_models import Agency
 from tests.src.db.models.factories import AgencyFactory
 
 
-@pytest.fixture(autouse=True)
-def cleanup_agencies(db_session):
-    yield
-    agencies = db_session.query(Agency).all()
-    db_session.delete(agencies)
-    db_session.commit()
+class TestAgenciesRoutes(BaseTestClass):
+    @pytest.fixture(scope="class", autouse=True)
+    def cleanup_agencies(self, db_session):
+        yield
 
+        # Fetch all agencies
+        agencies = db_session.query(Agency).all()
+        # Delete each agency
+        for agency in agencies:
+            db_session.delete(agency)
+        db_session.commit()
 
-def test_agencies_get_default_dates(client, api_auth_token, enable_factory_create, db_session):
-    # These should return in the default date range
-    AgencyFactory.create_batch(20)
+    def test_agencies_get_default_dates(
+        self, client, api_auth_token, enable_factory_create, db_session
+    ):
+        # These should return in the default date range
+        AgencyFactory.create_batch(20)
 
-    payload = {
-        "filters": {},
-        "pagination": {
-            "page": 1,
-            "page_size": 10,
-            "page_offset": 1,
-            "order_by": "created_at",
-            "sort_direction": "descending",
-        },
-    }
-    response = client.post("/v1/agencies", headers={"X-Auth": api_auth_token}, json=payload)
-    assert response.status_code == 200
-    data = response.json["data"]
-    assert len(data) == 10
+        payload = {
+            "filters": {},
+            "pagination": {
+                "page": 1,
+                "page_size": 10,
+                "page_offset": 1,
+                "order_by": "created_at",
+                "sort_direction": "descending",
+            },
+        }
+        response = client.post("/v1/agencies", headers={"X-Auth": api_auth_token}, json=payload)
+        assert response.status_code == 200
+        data = response.json["data"]
+        assert len(data) == 10
