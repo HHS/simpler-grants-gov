b50c042d746e4d126c6d5a601141693cd0cd3637
Doug Schrashun <dougschrashun@navapbc.com>
2024-12-05 14:37:21 -0500

    finish maintenance page

--- frontend/src/app/[locale]/maintenance/page.tsx
+++ frontend/src/app/[locale]/maintenance/page.tsx
@@ -1,3 +1,18 @@
+import { useTranslations } from "next-intl";
+import { GridContainer } from "@trussworks/react-uswds";
+
 export default function Maintenance() {
-  return <div>maintenance mode</div>;
+  const t = useTranslations("Maintenance");
+
+  const body = t.rich("body", {
+    LinkToGrants: (content) => <a href="https://www.grants.gov">{content}</a>,
+  });
+
+  return (
+    <GridContainer className="padding-y-1 tablet:padding-y-3 desktop-lg:padding-y-6 padding-x-5 tablet:padding-x-7 desktop-lg:padding-x-10 text-center">
+      <h2 className="margin-bottom-0">{t("heading")}</h2>
+      <p>{body}</p>
+      <p>{t("signOff")}</p>
+    </GridContainer>
+  );
 }
