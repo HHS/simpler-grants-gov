95554fa3d74c11e2ca83a73af1b27f13a5491124
Doug Schrashun <dougschrashun@navapbc.com>
2024-12-06 16:39:10 -0500

    dockerfile changes and cleanup

--- frontend/src/hoc/search/withFeatureFlag.tsx
+++ frontend/src/hoc/search/withFeatureFlag.tsx
@@ -5,20 +5,6 @@ import { WithFeatureFlagProps } from "src/types/uiTypes";
 import { cookies } from "next/headers";
 import React, { ComponentType } from "react";
 
-// const withSearchParams = <P extends WithFeatureFlagProps, R>(
-//   WrappedComponent: ComponentType<P>,
-//   featureFlagName: string,
-//   onEnabled: () => R,
-// ) => ({ searchParams }: WithFeatureFlagProps) {
-//   const featureFlagsManager = new FeatureFlagsManager(cookies());
-
-//   if (featureFlagsManager.isFeatureEnabled(featureFlagName, searchParams)) {
-//     return onEnabled();
-//   }
-
-//   return <WrappedComponent {...props} />;
-// }
-
 // since this relies on search params coming in as a prop, this can only be used on a top level page component
 // for other components we'll need a different implementation
 const withFeatureFlag = <P, R>(
@@ -27,15 +13,10 @@ const withFeatureFlag = <P, R>(
   onEnabled: () => R,
 ) => {
   // if we're in the middle of a build, that means this is an ssg rendering pass.
-  // in that case we can skip this feature flag business
+  // in that case we can skip this whole feature flag business and move on with our lives
   if (environment.NEXT_BUILD === "true") {
     return WrappedComponent;
   }
-  // // eslint-disable-next-line
-  // console.log("____", featureFlagName, onEnabled);
-
-  // the problem is not that this returns a function
-  // i guess it wants the function it returns to return an element
 
   // top level component to grab search params from the top level page props
   return (props: P & WithFeatureFlagProps) => {
@@ -53,13 +34,6 @@ const withFeatureFlag = <P, R>(
     };
     return <ComponentWithFeatureFlag {...props} searchParams={searchParams} />;
   };
-
-  // const Dumb = (props: P & WithFeatureFlagProps) => {
-  //   // eslint-disable-next-line
-  //   console.log("+++", props.searchParams);
-  //   return <div>hi</div>;
-  // };
-  // return Dumb;
 };
 
 export default withFeatureFlag;
