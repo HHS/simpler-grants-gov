0fc834ef54be9482fa96deb02ab310c6fdfa9508
Doug Schrashun <dougschrashun@navapbc.com>
2024-12-06 16:43:45 -0500

    more cleanup

--- frontend/src/constants/featureFlags.ts
+++ frontend/src/constants/featureFlags.ts
@@ -2,11 +2,11 @@ import { FeatureFlags } from "src/services/FeatureFlagManager";
 
 // Feature flags should default to false
 export const featureFlags: FeatureFlags = {
-  // This is for showing the search page as it is being developed and user tested
+  // This is for hiding the search page as it is being developed and user tested
   // This should be removed when the search page goes live, before May 2024
   hideSearchV0: false,
+  // Kill switches for search and opportunity pages, will show maintenance page when turned on
   searchOff: false,
   opportunityOff: false,
 };
 
-// http://localhost:3000/es/opportunity/33?_ff=opportunityOff%3Afalse
