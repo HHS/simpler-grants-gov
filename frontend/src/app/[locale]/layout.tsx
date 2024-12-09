HHS/simpler-grants-gov

commit 4acc5cad98b1b94e5052938237cd50afc09997ef

[Issue #2819] disable ga in test (#3001)
* moves GA IDs from Next code to SSM / terraform to solve issues where non-production analytics data was making it into the production GA account
* adds environment documentation, and removes an unused environment variable

frontend/src/app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/layout
  */
 import { Metadata } from "next";
-import { environment, PUBLIC_ENV } from "src/constants/environments";
+import { environment } from "src/constants/environments";
 
 import "src/styles/styles.scss";
 
@@ -43,7 +43,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
-        <GoogleAnalytics gaId={PUBLIC_ENV.GOOGLE_ANALYTICS_ID} />
+        <GoogleAnalytics gaId={environment.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
       </head>
       <body>
         <NextIntlClientProvider messages={messages}>

frontend/src/constants/environments.ts
@@ -1,22 +1,3 @@
-/**
- * Define environment variables that you need exposed in the client-side bundle.
- * These should not include sensitive secrets!
- */
-const PUBLIC_ENV_VARS_BY_ENV = {
-  development: {
-    GOOGLE_ANALYTICS_ID: "G-GWJZD3DL8W",
-  },
-  test: {
-    GOOGLE_ANALYTICS_ID: "G-6MDCC5EZW2",
-  },
-  production: {
-    GOOGLE_ANALYTICS_ID: "G-6MDCC5EZW2",
-  },
-} as const;
-
-const NEXT_ENVS = ["development", "test", "production"] as const;
-export type NextPublicAppEnv = (typeof NEXT_ENVS)[number];
-
 const {
   NODE_ENV,
   NEXT_PUBLIC_BASE_PATH,
@@ -27,11 +8,11 @@ const {
   API_URL,
   API_AUTH_TOKEN = "",
   NEXT_PUBLIC_BASE_URL,
+  // for deployed envs it will come from ECS task def env var
+  // it will not be populated locally, or in Jest or E2E tests
+  // if it is needed locally or tests, add it in .env.local or command line
+  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = "",
 } = process.env;
-
-const CURRENT_ENV = NODE_ENV ?? "development";
-
-export const PUBLIC_ENV = PUBLIC_ENV_VARS_BY_ENV[CURRENT_ENV];
 
 // home for all interpreted server side environment variables
 export const environment: { [key: string]: string } = {
@@ -47,4 +28,5 @@ export const environment: { [key: string]: string } = {
   API_URL: API_URL || "",
   API_AUTH_TOKEN,
   NEXT_PUBLIC_BASE_URL: NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
 };

infra/frontend/app-config/env-config/environment-variables.tf
@@ -43,6 +43,10 @@ locals {
     API_AUTH_TOKEN = {
       manage_method     = "manual"
       secret_store_name = "/${var.app_name}/${var.environment}/api-auth-token"
+    },
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = {
+      manage_method     = "manual"
+      secret_store_name = "/${var.app_name}/${var.environment}/google-analytics-id"
     }
   }
 }

frontend/.env.development
@@ -3,9 +3,6 @@
 # env vars when running `next dev` only.
 # Learn more: https://nextjs.org/docs/app/building-your-application/configuring/environment-variables
 
-# This is used to determin if this is a prod or a non prod environment for deployed environments. Currently used for noindex and google tag manager logic
-NEXT_PUBLIC_ENVIRONMENT=dev
-
 # If you deploy to a subpath, change this to the subpath so relative paths work correctly.
 NEXT_PUBLIC_BASE_PATH=
 

frontend/.env.production
@@ -3,9 +3,6 @@
 # env vars when running `next dev` only.
 # Learn more: https://nextjs.org/docs/app/building-your-application/configuring/environment-variables
 
-# This is used to determin if this is a prod or a non prod environment for deployed environments. Currently used for noindex and google tag manager logic
-NEXT_PUBLIC_ENVIRONMENT=prod
-
 # If you deploy to a subpath, change this to the subpath so relative paths work correctly.
 NEXT_PUBLIC_BASE_PATH=
 

frontend/Dockerfile
@@ -43,16 +43,6 @@ COPY *.config.js .
 COPY *.d.ts .
 COPY src ./src
 
-# Environment variables must be present at build time
-# https://github.com/vercel/next.js/discussions/14030
-# ARG ENV_VARIABLE
-# ENV ENV_VARIABLE=${ENV_VARIABLE}
-# ARG NEXT_PUBLIC_ENV_VARIABLE
-# ENV NEXT_PUBLIC_ENV_VARIABLE=${NEXT_PUBLIC_ENV_VARIABLE}
-
-ARG NEXT_PUBLIC_ENVIRONMENT
-ENV NEXT_PUBLIC_ENVIRONMENT=${NEXT_PUBLIC_ENVIRONMENT}
-
 ENV NEXT_TELEMETRY_DISABLED 1
 
 # Skip lint because it should have happened in the CI already
@@ -117,8 +107,6 @@ ARG SENDY_API_URL
 ENV SENDY_API_URL=${SENDY_API_URL}
 ARG SENDY_LIST_ID
 ENV SENDY_LIST_ID=${SENDY_LIST_ID}
-ARG NEXT_PUBLIC_ENVIRONMENT
-ENV NEXT_PUBLIC_ENVIRONMENT=${NEXT_PUBLIC_ENVIRONMENT}
 
 ENV NEXT_TELEMETRY_DISABLED 1
 ENV PORT 3000

frontend/Makefile
@@ -22,8 +22,6 @@ endif
 export RUN_USER
 export RUN_UID
 
-ENVIRONMENT ?= dev
-
 ##################################################
 # Release
 ##################################################
@@ -33,7 +31,6 @@ release-build:
 		--platform=linux/amd64 \
 		--build-arg RUN_USER=$(RUN_USER) \
 		--build-arg RUN_UID=$(RUN_UID) \
-		--build-arg NEXT_PUBLIC_ENVIRONMENT=${ENVIRONMENT} \
 		$(OPTS) \
 		.
 

commit a805bea1acfdfef610759b7be2760fb569145a64

[Unticketed] clean up messy GA env var situation (#3025)
* Cleans up after bug introduced in
https://github.com/HHS/simpler-grants-gov/pull/3001/files and fixed in
https://github.com/HHS/simpler-grants-gov/pull/3013
* Adds some more detail to the documentation and a diagram

frontend/src/app/[locale]/layout.tsx
@@ -84,7 +84,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
-        <GoogleAnalytics gaId={environment.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+        <GoogleAnalytics gaId={environment.GOOGLE_TAG_MANAGER_ID} />
         <meta
           name="google-site-verification"
           content="jFShzxCTiLzv8gvEW4ft7fCaQkluH229-B-tJKteYJY"

frontend/src/constants/environments.ts
@@ -24,5 +24,5 @@ export const environment: { [key: string]: string } = {
   API_URL: API_URL || "",
   API_AUTH_TOKEN,
   NEXT_PUBLIC_BASE_URL: NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-  NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: "GTM-MV57HMHS",
+  GOOGLE_TAG_MANAGER_ID: "GTM-MV57HMHS",
 };

infra/frontend/app-config/env-config/environment-variables.tf
@@ -44,10 +44,6 @@ locals {
       manage_method     = "manual"
       secret_store_name = "/${var.app_name}/${var.environment}/api-auth-token"
     },
-    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = {
-      manage_method     = "manual"
-      secret_store_name = "/${var.app_name}/${var.environment}/google-analytics-id"
-    },
     NEW_RELIC_APP_NAME = {
       manage_method     = "manual"
       secret_store_name = "/${var.app_name}/${var.environment}/new-relic-app-name"