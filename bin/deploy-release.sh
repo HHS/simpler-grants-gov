99129d713407cfeda735276f3e9ab74392c6a46f
Kai Siren <kaisiren@navapbc.com>
2024-12-09 11:13:40 -0800

    deploy info is input via CLI now

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 APP_NAME=$1
 IMAGE_TAG=$2
 ENVIRONMENT=$3
-DEPLOY_GITHUB_REF=$4
-DEPLOY_GITHUB_SHA=$5
 
 echo "--------------"
 echo "Deploy release"
@@ -14,11 +12,9 @@ echo "Input parameters:"
 echo "  APP_NAME=$APP_NAME"
 echo "  IMAGE_TAG=$IMAGE_TAG"
 echo "  ENVIRONMENT=$ENVIRONMENT"
-echo "  DEPLOY_GITHUB_REF=$DEPLOY_GITHUB_REF"
-echo "  DEPLOY_GITHUB_SHA=$DEPLOY_GITHUB_SHA"
 echo
 echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
 
-TF_CLI_ARGS_apply="-input=false -auto-approve -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT" DEPLOY_GITHUB_REF="$DEPLOY_GITHUB_REF" DEPLOY_GITHUB_SHA="$DEPLOY_GITHUB_SHA"
+TF_CLI_ARGS_apply="-input=false -auto-approve -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT"
 
 echo "Completed $APP_NAME deploy of $IMAGE_TAG to $ENVIRONMENT"
