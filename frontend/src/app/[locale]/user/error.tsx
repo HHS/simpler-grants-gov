8dafbdf23f946175d52268bab01cc637f7f2a3d2
Doug Schrashun <dougschrashun@navapbc.com>
2024-12-11 11:56:23 -0500

    get error page working

--- frontend/src/app/[locale]/user/error.tsx
+++ frontend/src/app/[locale]/user/error.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import Cookies from "js-cookie";
 import { ErrorProps } from "src/types/uiTypes";
 
+import { useTranslations } from "next-intl";
 import { GridContainer } from "@trussworks/react-uswds";
 
 export default function UserError({ error }: ErrorProps) {
-  if (!Cookies.get("session")) {
-    throw new Error("not logged in!");
-  }
+  const t = useTranslations("User");
   return (
     <GridContainer className="padding-y-1 tablet:padding-y-3 desktop-lg:padding-y-15 measure-2">
-      <h1>Error</h1>
+      <h1>{t("errorHeading")}</h1>
       {error.message && <div>{error.message}</div>}
     </GridContainer>
   );
