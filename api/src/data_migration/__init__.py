984d15db72269183b228804625e210715ff9d8cd
bruk <brukty@gmail.com>
2024-12-12 11:59:26 -0500

    rm change on api

--- api/src/data_migration/__init__.py
+++ api/src/data_migration/__init__.py
@@ -5,6 +5,5 @@ from . import command  # noqa: F401
 # import any of the other files so they get initialized and attached to the blueprint
 import src.data_migration.copy_oracle_data  # noqa: F401 E402 isort:skip
 import src.data_migration.setup_foreign_tables  # noqa: F401 E402 isort:skip
-import src.data_migration.copy_data_between_dbs # noqa: F401 E402 isort:skip
 
 __all__ = ["data_migration_blueprint"]
