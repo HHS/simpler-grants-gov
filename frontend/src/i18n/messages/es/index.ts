b10c912928e83974e39e2ab22167d9e46a7eafcc
Doug Schrashun <dougschrashun@navapbc.com>
2024-12-12 10:32:25 -0500

    revert spanish testing

--- frontend/src/i18n/messages/es/index.ts
+++ frontend/src/i18n/messages/es/index.ts
@@ -1,13 +1,1 @@
-export const messages = {
-  ErrorPages: {
-    page_not_found: {
-      title: "No pagina nunca",
-    },
-  },
-  Subscribe: {
-    form: {
-      name: "Nombre Primero",
-      lastName: "Nombre Ultimo",
-    },
-  },
-};
+export const messages = {};
