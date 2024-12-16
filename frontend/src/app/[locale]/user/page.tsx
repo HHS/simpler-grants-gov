b13918a828ca2b7f7ac29f6f2b9827eec80e60ca
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2024-12-16 11:09:21 -0500

    [Issue #2654] Next login flow backend (#3182)
    * adds route at /api/auth/callback to be hit by the flask API at the completion of the login flow
    * adds jwt token parsing logic
    * adds support for a "SESSION_SECRET" env var
    * adds a User page for temporary testing

---
+++ frontend/src/app/[locale]/user/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+import { LocalizedPageProps } from "src/types/intl";
+
+import { getTranslations } from "next-intl/server";
+import { GridContainer } from "@trussworks/react-uswds";
+
+export async function generateMetadata({
+  params: { locale },
+}: LocalizedPageProps) {
+  const t = await getTranslations({ locale });
+  const meta: Metadata = {
+    title: t("User.pageTitle"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
+
+// this is a placeholder page used as temporary landing page for login redirects.
+// Note that this page only functions to display the message passed down in query params from
+// the /api/auth/callback route, and it does not handle errors.
+// How to handle errors or failures from the callback route in the UI will need to be revisited
+// later on, but note that throwing to an error page won't be an option, as that produces a 500
+// response in the client.
+export default async function UserDisplay({
+  searchParams,
+  params: { locale },
+}: LocalizedPageProps & { searchParams: { message?: string } }) {
+  const { message } = searchParams;
+
+  const t = await getTranslations({ locale, namespace: "User" });
+  return (
+    <GridContainer>
+      <h1>{t("heading")}</h1>
+      {message && <div>{message}</div>}
+    </GridContainer>
+  );
+}
--- frontend/src/i18n/messages/en/index.ts
+++ frontend/src/i18n/messages/en/index.ts
@@ -608,4 +608,9 @@ export const messages = {
     signOff: "Thank you for your patience.",
     pageTitle: "Simpler.Grants.gov - Maintenance",
   },
+  User: {
+    heading: "User",
+    pageTitle: "User | Simpler.Grants.Gov",
+    errorHeading: "Error",
+  },
 };
