29c3fe0036bda3618bfc35c70d46797d1db50c7f
Andy Cochran <andrewthomascochran@gmail.com>
2024-12-17 16:54:22 -0500

    fix e2e scroll test; z-index skip link on top

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -25,7 +25,7 @@ export default function Layout({ children, locale }: Props) {
   return (
     // Stick the footer to the bottom of the page
     <div className="display-flex flex-column minh-viewport">
-      <a className="usa-skipnav" href="#main-content">
+      <a className="usa-skipnav z-top" href="#main-content">
         {t("Layout.skip_to_main")}
       </a>
       <NextIntlClientProvider
