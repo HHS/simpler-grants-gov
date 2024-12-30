2d0c24d3b2e04ffd7bfe8c1564f652a439e0ac46
bruk <brukty@gmail.com>
2024-12-29 19:28:38 -0500

    use retry on backendtask

--- api/src/search/backend/load_search_data.py
+++ api/src/search/backend/load_search_data.py
@@ -7,6 +7,8 @@ from src.adapters.search import flask_opensearch
 from src.search.backend.load_opportunities_to_index import LoadOpportunitiesToIndex
 from src.search.backend.load_search_data_blueprint import load_search_data_blueprint
 from src.task.ecs_background_task import ecs_background_task
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+from opensearchpy.exceptions import ConnectionTimeout, TransportError
 
 
 @load_search_data_blueprint.cli.command(
@@ -20,6 +22,11 @@ from src.task.ecs_background_task import ecs_background_task
 @flask_db.with_db_session()
 @flask_opensearch.with_search_client()
 @ecs_background_task(task_name="load-opportunity-data-opensearch")
+@retry(
+    stop=stop_after_attempt(3),  # Retry up to 3 times
+    wait=wait_fixed(2),  # Wait 2 seconds between retries
+    retry=retry_if_exception_type((TransportError, ConnectionTimeout)),  # Retry on TransportError (including timeouts)
+)
 def load_opportunity_data(
     search_client: search.SearchClient, db_session: db.Session, full_refresh: bool
 ) -> None:
