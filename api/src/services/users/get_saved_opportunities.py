3b639681d2c9a43c0b8f0f0b18899a682953c6bf
Michael Huneke <mike.huneke@focusconsulting.io>
2025-01-02 10:06:21 -0500

    Update to new select api

--- api/src/services/users/get_saved_opportunities.py
+++ api/src/services/users/get_saved_opportunities.py
@@ -1,6 +1,8 @@
 import logging
 from uuid import UUID
 
+from sqlalchemy import select
+
 from src.adapters import db
 from src.db.models.opportunity_models import Opportunity
 from src.db.models.user_models import UserSavedOpportunity
@@ -11,9 +13,11 @@ logger = logging.getLogger(__name__)
 def get_saved_opportunities(db_session: db.Session, user_id: UUID) -> list[UserSavedOpportunity]:
     logger.info(f"Getting saved opportunities for user {user_id}")
 
-    return (
-        db_session.query(UserSavedOpportunity)
+    stmt = (
+        select(UserSavedOpportunity)
         .join(Opportunity)
-        .filter(UserSavedOpportunity.user_id == user_id)
-        .all()
+        .where(UserSavedOpportunity.user_id == user_id)
     )
+
+    result = db_session.execute(stmt)
+    return list(result.scalars().all())
