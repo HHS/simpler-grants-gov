f53e715947201133b0baada125fbbcb68c7f2242
Doug Schrashun <dougschrashun@navapbc.com>
2025-01-02 11:34:59 -0500

    some fix ups for e2e, hydration and accessibility

--- frontend/src/hooks/useFeatureFlags.ts
+++ frontend/src/hooks/useFeatureFlags.ts
@@ -2,13 +2,16 @@
 
 import Cookies from "js-cookie";
 import { isBoolean } from "lodash";
-import { FeatureFlags } from "src/constants/defaultFeatureFlags";
+import {
+  defaultFeatureFlags,
+  FeatureFlags,
+} from "src/constants/defaultFeatureFlags";
 import {
   FEATURE_FLAGS_KEY,
   getCookieExpiration,
 } from "src/services/featureFlags/featureFlagHelpers";
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 /**
  * Allows client components to access feature flags by
@@ -21,9 +24,17 @@ export function useFeatureFlags(): {
   checkFeatureFlag: (flagName: string) => boolean;
   featureFlags: FeatureFlags;
 } {
-  const [featureFlags, setFeatureFlags] = useState<FeatureFlags>(
-    JSON.parse(Cookies.get(FEATURE_FLAGS_KEY) || "{}") as FeatureFlags,
-  );
+  const [featureFlags, setFeatureFlags] =
+    useState<FeatureFlags>(defaultFeatureFlags);
+
+  // a workaround, as setting this in default state value results in hydration error
+  // on feature flag admin page. Does it cause a blip on other pages?
+  useEffect(() => {
+    const flagsFromCookie = JSON.parse(
+      Cookies.get(FEATURE_FLAGS_KEY) || "{}",
+    ) as FeatureFlags;
+    setFeatureFlags(flagsFromCookie);
+  }, []);
 
   const setFeatureFlag = useCallback(
     (flagName: string, value: boolean) => {
@@ -41,15 +52,6 @@ export function useFeatureFlags(): {
 
   const checkFeatureFlag = useCallback(
     (flagName: string): boolean => {
-      if (!featureFlags || !Object.keys(featureFlags).length) {
-        // note that this hook will run on the server when building client components server side
-        // but will only return initial default values and will not have access to client side cookies
-        // eslint-disable-next-line
-        console.debug(
-          "Feature flags not present. Likely attempting to run client side hook on server",
-        );
-        return false;
-      }
       const value = featureFlags[flagName];
       if (!isBoolean(value)) {
         console.error("Unknown or misconfigured feature flag: ", flagName);
