d3a7d7c3288cdb5c3efce8e968c857be19b65673
acouch <acinternets@gmail.com>
2025-01-03 13:55:54 -0500

    Update route.ts to remove unused vars

--- frontend/src/app/api/env/route.ts
+++ frontend/src/app/api/env/route.ts
@@ -4,8 +4,6 @@ export const dynamic = "force-dynamic";
 
 export function GET() {
   return Response.json({
-    api_url: environment.API_URL,
     auth_login_url: environment.AUTH_LOGIN_URL,
-    node_env: environment.NODE_ENV,
   });
 }
