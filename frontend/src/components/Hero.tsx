42c8f343acef752826d2aa07fe2fd8f46ec456e1
Matt Dragon <mattdragon@navapbc.com>
2025-01-06 10:57:41 -0500

    [Issue #3216] Feature/brand rebase (#3385)
    ## Summary
    Fixes #3216
    
    ### Time to review: __0 mins__
    
    ## Changes proposed
    Merges feature/brand into main after rebasing against main to allow the
    merge to succeed
    
    ## Context for reviewers
    Reviews have already occurred as work was being brought into the
    feature/brand branch.
    
    ---------
    
    Co-authored-by: Andy Cochran <andycochran@users.noreply.github.com>
    Co-authored-by: acouch <acinternets@gmail.com>

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -10,26 +10,40 @@ const Hero = () => {
   const t = useTranslations("Hero");
 
   return (
-    <div data-testid="hero" className="usa-dark-background bg-primary">
-      <GridContainer className="padding-y-1 tablet:padding-y-3 tablet-lg:padding-y-10 desktop-lg:padding-y-15 position-relative">
-        <h1 className="text-ls-neg-2 tablet:font-sans-2xl desktop-lg:font-sans-3xl desktop-lg:margin-top-2">
-          <span>{t("title")}</span>
-        </h1>
-        <p className="usa-intro line-height-sans-3 font-sans-md tablet:font-sans-lg desktop-lg:font-sans-xl desktop-lg:margin-bottom-0">
-          {t("content")}
-        </p>
+    <div
+      data-testid="hero"
+      className="hero bg-primary-darkest text-white overflow-hidden"
+    >
+      <GridContainer className="hero--container padding-y-1 tablet:padding-y-3 tablet-lg:padding-y-10 desktop-lg:padding-top-15 desktop-lg:padding-bottom-10 position-relative">
+        <div className="hero--text position-relative z-100">
+          <h1 className="text-ls-neg-2 tablet:font-sans-2xl desktop-lg:font-sans-3xl desktop-lg:margin-top-2 text-balance">
+            <span>{t("title")}</span>
+          </h1>
+          <p className="usa-intro line-height-sans-3 font-sans-md tablet:font-sans-lg text-balance">
+            {t("content")}
+          </p>
+        </div>
         <Link
-          className="usa-button usa-button--outline usa-button--inverse font-sans-2xs tablet:font-sans-md margin-bottom-3 desktop:position-absolute top-3 right-0 desktop:margin-y-3 desktop:margin-x-4"
-          href={ExternalRoutes.GITHUB_REPO}
-          target="_blank"
+          href="/search"
+          passHref
+          className="usa-button usa-button--secondary usa-button--big margin-bottom-2"
         >
-          <USWDSIcon
-            name="github"
-            className="usa-icon usa-icon--size-3 margin-right-1 text-middle tablet:margin-top-neg-2px"
-            aria-label="Github"
-          />
-          {t("github_link")}
+          {t("search_link")}
         </Link>
+        <span className="usa-dark-background bg-transparent">
+          <Link
+            className="usa-button usa-button--small usa-button--unstyled usa-button--inverse desktop:position-absolute top-3 right-0 margin-y-1 desktop:margin-y-3 desktop:margin-x-4"
+            href={ExternalRoutes.GITHUB_REPO}
+            target="_blank"
+          >
+            <USWDSIcon
+              name="github"
+              className="usa-icon usa-icon--size-3"
+              aria-label="Github"
+            />
+            {t("github_link")}
+          </Link>
+        </span>
       </GridContainer>
     </div>
   );
