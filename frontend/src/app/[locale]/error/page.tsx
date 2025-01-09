ac898465358b98b8cc995717cd85aa8110f8a85c
Doug Schrashun <dougschrashun@navapbc.com>
2025-01-09 10:27:24 -0500

    fix up new pages with translations and meta

--- frontend/src/app/[locale]/error/page.tsx
+++ frontend/src/app/[locale]/error/page.tsx
@@ -1,14 +1,29 @@
-"use client";
+import { Metadata } from "next";
 
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 import { GridContainer } from "@trussworks/react-uswds";
 
-const Unauthorized = () => {
+import ServerErrorAlert from "src/components/ServerErrorAlert";
+
+export async function generateMetadata() {
+  const t = await getTranslations();
+  const meta: Metadata = {
+    title: t("ErrorPages.generic_error.page_title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
+
+// not a NextJS error page - this is here to be redirected to manually in cases
+// where Next's error handling situation doesn't quite do what we need.
+const TopLevelError = () => {
+  const t = useTranslations("Errors");
   return (
     <GridContainer>
-      <h3>Error</h3>
-      <div>Oops, something went wrong. Please try again.</div>
+      <ServerErrorAlert callToAction={t("try_again")} />
     </GridContainer>
   );
 };
 
-export default Unauthorized;
+export default TopLevelError;
--- frontend/src/app/[locale]/unauthorized/page.tsx
+++ frontend/src/app/[locale]/unauthorized/page.tsx
@@ -1,12 +1,25 @@
-"use client";
+import { Metadata } from "next";
 
-import { GridContainer } from "@trussworks/react-uswds";
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+import { Alert, GridContainer } from "@trussworks/react-uswds";
+
+export async function generateMetadata() {
+  const t = await getTranslations();
+  const meta: Metadata = {
+    title: t("ErrorPages.unauthorized.page_title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
 
 const Unauthorized = () => {
+  const t = useTranslations("Errors");
   return (
     <GridContainer>
-      <h3>Unauthorized</h3>
-      <div>Unable to log in</div>
+      <Alert type="error" heading={t("unauthorized")} headingLevel="h4">
+        {t("authorization_fail")}
+      </Alert>
     </GridContainer>
   );
 };
