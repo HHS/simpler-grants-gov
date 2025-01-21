abbca5ece271d6e035420d083c57b0421e18b0c0
Doug Schrashun <dougschrashun@navapbc.com>
2025-01-21 14:37:51 -0500

    more reorg and fix high level test

--- frontend/src/components/search/SearchFilters.tsx
+++ frontend/src/components/search/SearchFilters.tsx
@@ -1,43 +1,16 @@
 import { getAgenciesForFilterOptions } from "src/services/fetch/fetchers/agenciesFetcher";
 
 import { useTranslations } from "next-intl";
-import { Suspense } from "react";
-import { Accordion } from "@trussworks/react-uswds";
 
-import SearchFilterAccordion, {
-  FilterAccordionProps,
-  FilterOption,
-} from "src/components/search/SearchFilterAccordion/SearchFilterAccordion";
+import SearchFilterAccordion from "src/components/search/SearchFilterAccordion/SearchFilterAccordion";
 import {
   categoryOptions,
   eligibilityOptions,
   fundingOptions,
 } from "src/components/search/SearchFilterAccordion/SearchFilterOptions";
 import SearchOpportunityStatus from "src/components/search/SearchOpportunityStatus";
+import { AgencyFilterAccordion } from "./SearchFilterAccordion/AgencyFilterAccordion";
 
-interface FetchedOptionsFilterAccordionProps extends FilterAccordionProps {
-  filterOptionsPromise: Promise<FilterOption[]>;
-}
-
-// async component used to handle fetching agencies
-export async function FetchedOptionsFilterAccordion({
-  filterOptionsPromise,
-  title,
-  queryParamKey,
-  query,
-}: FetchedOptionsFilterAccordionProps) {
-  const filterOptions = await filterOptionsPromise;
-  return (
-    <SearchFilterAccordion
-      filterOptions={filterOptions}
-      query={query}
-      queryParamKey={queryParamKey}
-      title={title}
-    />
-  );
-}
-
-// TODO: disabled styling for suspended component
 export default function SearchFilters({
   fundingInstrument,
   eligibility,
@@ -53,7 +26,7 @@ export default function SearchFilters({
 }) {
   const t = useTranslations("Search");
   const agenciesPromise = getAgenciesForFilterOptions();
-
+  // console.log("!!! render", agency, agenciesPromise);
   return (
     <>
       <SearchOpportunityStatus query={opportunityStatus} />
@@ -69,31 +42,7 @@ export default function SearchFilters({
         queryParamKey={"eligibility"}
         title={t("accordion.titles.eligibility")}
       />
-      <Suspense
-        fallback={
-          <Accordion
-            bordered={true}
-            items={[
-              {
-                title: `${t("accordion.titles.agency")}`,
-                content: [],
-                expanded: false,
-                id: `opportunity-filter-agency-disabled`,
-                headingLevel: "h2",
-              },
-            ]}
-            multiselectable={true}
-            className="margin-top-4"
-          />
-        }
-      >
-        <FetchedOptionsFilterAccordion
-          filterOptionsPromise={agenciesPromise}
-          query={agency}
-          queryParamKey={"agency"}
-          title={t("accordion.titles.agency")}
-        />
-      </Suspense>
+      <AgencyFilterAccordion query={agency} agenciesPromise={agenciesPromise} />
       <SearchFilterAccordion
         filterOptions={categoryOptions}
         query={category}
