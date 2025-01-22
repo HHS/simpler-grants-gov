27c275b518ef7d65aa178f6485eef93db6febd25
Michael Huneke <mike.huneke@focusconsulting.io>
2025-01-22 17:30:05 -0500

    Lint

--- api/tests/src/task/notifications/test_generate_notifications.py
+++ api/tests/src/task/notifications/test_generate_notifications.py
@@ -1,6 +1,3 @@
-from datetime import timedelta
-
-
 def test_via_cli(cli_runner, db_session, enable_factory_create, user):
     """Simple test that verifies we can invoke the notification task via CLI"""
     result = cli_runner.invoke(args=["task", "generate-notifications"])
