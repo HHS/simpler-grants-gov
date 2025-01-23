e5cbbad44d57a7a0bdb919537dd575ef48590e45
Mike H <mike.huneke@focusconsulting.io>
2025-01-23 09:50:52 -0500

    [Issue #3535] Add generate notifications task (#3608)
    ## Summary
    Fixes #3535 
    
    ### Time to review: 10 mins
    
    ## Changes proposed
    Add scaffolding for task
    Propose some basic metrics to track (TBD)
    Drop other TODOs for consideration down the road
    
    ## Context for reviewers
    This provides only the structure of the task, no exact logic inside the
    task yet.
    
    ## Additional information
    See unit test of task running from CLI.

---
+++ api/tests/src/task/notifications/test_generate_notifications.py
@@ -0,0 +1,5 @@
+def test_via_cli(cli_runner, db_session, enable_factory_create, user):
+    """Simple test that verifies we can invoke the notification task via CLI"""
+    result = cli_runner.invoke(args=["task", "generate-notifications"])
+
+    assert result.exit_code == 0
