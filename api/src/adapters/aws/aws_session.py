2284a32e569fade47f18e90f87467a25416beaa6
Michael Chouinard <chouinar6@gmail.com>
2025-01-27 15:35:02 -0500

    [Issue #3540] AWS Pinpoint Mock setup

--- api/src/adapters/aws/aws_session.py
+++ api/src/adapters/aws/aws_session.py
@@ -1,11 +1,30 @@
-import os
-
 import boto3
+
+from src.util.env_config import PydanticBaseEnvConfig
+
+
+class BaseAwsConfig(PydanticBaseEnvConfig):
+    is_local_aws: bool = False
+
+
+_base_aws_config: BaseAwsConfig | None = None
+
+
+def get_base_aws_config() -> BaseAwsConfig:
+    global _base_aws_config
+    if _base_aws_config is None:
+        _base_aws_config = BaseAwsConfig()
+
+    return _base_aws_config
+
+
+def is_local_aws() -> bool:
+    """Whether we are running against local AWS which affects the credentials we use (forces them to be not real)"""
+    return get_base_aws_config().is_local_aws
 
 
 def get_boto_session() -> boto3.Session:
-    is_local = bool(os.getenv("IS_LOCAL_AWS", False))
-    if is_local:
+    if is_local_aws():
         return boto3.Session(aws_access_key_id="NO_CREDS", aws_secret_access_key="NO_CREDS")
 
     return boto3.Session()
