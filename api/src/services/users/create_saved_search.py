31eb2c0a68e67b5a633900a5216348a1b4059070
babebe <brukty@gmail.com>
2025-01-28 16:53:49 -0500

    3537/save search get opp (#3673)
    ## Summary
    Fixes #{3537}
    
    ### Time to review: __1 mins__
    
    ## Changes proposed
    Fix factory field name 
    Pass in searched_opportunity_ids to UserSavedSearch

--- api/src/services/users/create_saved_search.py
+++ api/src/services/users/create_saved_search.py
@@ -9,7 +9,10 @@ logger = logging.getLogger(__name__)
 
 def create_saved_search(db_session: db.Session, user_id: UUID, json_data: dict) -> UserSavedSearch:
     saved_search = UserSavedSearch(
-        user_id=user_id, name=json_data["name"], search_query=json_data["search_query"]
+        user_id=user_id,
+        name=json_data["name"],
+        search_query=json_data["search_query"],
+        searched_opportunity_ids=[],
     )
 
     db_session.add(saved_search)
