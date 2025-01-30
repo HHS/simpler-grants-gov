d640e23d7b6b6fbbddf39ff9bc4b051992135cd2
bruk <brukty@gmail.com>
2025-01-30 15:36:47 -0500

    add optional arg includes to search

--- api/src/adapters/search/opensearch_client.py
+++ api/src/adapters/search/opensearch_client.py
@@ -216,6 +216,7 @@ class SearchClient:
         search_query: dict,
         include_scores: bool = True,
         params: dict | None = None,
+        includes: list[str] | None = None,
         excludes: list[str] | None = None,
     ) -> SearchResponse:
         if params is None:
@@ -225,6 +226,7 @@ class SearchClient:
             index=index_name,
             body=search_query,
             params=params,
+            _source_includes=includes,
             _source_excludes=excludes,
         )
         return SearchResponse.from_opensearch_response(response, include_scores)
