c08cfb89f58595484e5f37a22f13ab9b6d53bcb2
bruk <brukty@gmail.com>
2025-02-03 14:31:34 -0500

    update test

--- api/tests/src/api/users/test_user_update_saved_search.py
+++ api/tests/src/api/users/test_user_update_saved_search.py
@@ -3,7 +3,7 @@ import uuid
 import pytest
 
 from src.db.models.user_models import UserSavedSearch, UserTokenSession
-from tests.src.db.models.factories import UserSavedSearchFactory
+from tests.src.db.models.factories import UserSavedSearchFactory, UserFactory
 
 
 @pytest.fixture
@@ -11,7 +11,6 @@ def saved_search(enable_factory_create, user, db_session):
     search = UserSavedSearchFactory.create(
         user=user, name="Save Search", search_query={"keywords": "python"}
     )
-    db_session.commit()
     return search
 
 
@@ -24,7 +23,7 @@ def clear_data(db_session):
 
 
 def test_user_update_saved_search(client, db_session, user, user_auth_token, saved_search):
-    response = client.patch(
+    response = client.put(
         f"/v1/users/{user.user_id}/saved-searches/{saved_search.saved_search_id}",
         headers={"X-SGG-Token": user_auth_token},
         json={"name": "Update Search"},
@@ -50,7 +49,7 @@ def test_user_update_saved_search_not_found(
     user_auth_token,
 ):
     # Try to update a non-existent search
-    response = client.patch(
+    response = client.put(
         f"/v1/users/{user.user_id}/saved-searches/{uuid.uuid4()}",
         headers={"X-SGG-Token": user_auth_token},
         json={"name": "Update Search"},
@@ -60,11 +59,30 @@ def test_user_update_saved_search_not_found(
     assert response.json["message"] == "Saved search not found"
 
 
-def test_user_update_saved_search_no_auth(
+def test_user_update_saved_search_unauthorized(
     client, enable_factory_create, db_session, user, user_auth_token, saved_search
 ):
+    # Try to update a search with another user
+    unauthorized_user= UserFactory.create()
+    response = client.put(
+        f"/v1/users/{unauthorized_user.user_id}/saved-searches/{saved_search.saved_search_id}",
+        headers={"X-SGG-Token": user_auth_token},
+        json={"name": "Update Search"},
+    )
+
+    db_session.refresh(saved_search)
+
+    assert response.status_code == 401
+    assert response.json["message"] == "Unauthorized user"
+
+    # Verify search was not updated
+    saved_searches = db_session.query(UserSavedSearch).first()
+    assert saved_searches.name == "Save Search"
+
+def test_user_update_saved_search_no_auth(client, enable_factory_create, db_session, user, user_auth_token, saved_search
+):
     # Try to update a search without authentication
-    response = client.patch(
+    response = client.put(
         f"/v1/users/{user.user_id}/saved-searches/{saved_search.saved_search_id}",
         json={"name": "Update Search"},
     )
