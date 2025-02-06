57f9ac61c1b9b6e29b21441193179b8c633ae9a8
David Dudas <david.dudas@intuitial.com>
2025-02-06 15:26:44 -0800

    rename exported functions and add new exported functions

--- analytics/src/analytics/integrations/github/__init__.py
+++ analytics/src/analytics/integrations/github/__init__.py
@@ -1,11 +1,15 @@
 """Export data from GitHub."""
 
 __all__ = [
-    "export_roadmap_data",
-    "export_sprint_data",
+    "export_roadmap_data_to_file",
+    "export_roadmap_data_to_object",
+    "export_sprint_data_to_file",
+    "export_sprint_data_to_object",
 ]
 
 from analytics.integrations.github.main import (
-    export_roadmap_data,
-    export_sprint_data,
+    export_roadmap_data_to_file,
+    export_roadmap_data_to_object,
+    export_sprint_data_to_file,
+    export_sprint_data_to_object,
 )
