8a7b0b5667ebebba5b52b54991fdca317609a6cd
jcrichlake <jeff.crichlake@agile6.com>
2025-02-11 13:33:48 -0500

    Adding initial tests for snapshot

---
+++ analytics/tests/dataquality/inputs/__init__.py
@@ -0,0 +1,1 @@
+"""Inputs for the snapshot comparisons."""
--- analytics/tests/dataquality/test_data_quality.py
+++ analytics/tests/dataquality/test_data_quality.py
@@ -1,7 +1,31 @@
+from datetime import datetime
+from pathlib import Path
+from unittest import mock
+
+from analytics.cli import extract_transform_and_load
+from analytics.etl.github import GitHubProjectConfig, GitHubProjectETL
+from analytics.etl.utils import load_config
+
+from tests.dataquality.inputs.roadmap import mock_graphql_roadmap_data
+from tests.dataquality.inputs.sprintboards import mock_graphql_sprintboard_data
+
+
 def test_snapshot(snapshot):
     """Compare pipeline to pre-committed snapshot."""
     assert output() == snapshot()
 
-def output() -> list[dict]:
+@mock.patch("analytics.integrations.github.export_roadmap_data_to_object",
+             mock_graphql_roadmap_data)
+@mock.patch("analytics.integrations.github.export_sprint_data_to_object",
+            mock_graphql_sprintboard_data)
+def output() -> list[dict]|None:
     """Call the new pipeline code to be used for comparison."""
-    return [{"name": "Test"}, {"output": "correct"}]
+    #extract_transform_and_load("config/github-projects.json", "2025-02-11")
+
+    config_path = Path("config/github-projects.json")
+    effective_date = "2025-02-11"
+    config = load_config(config_path, GitHubProjectConfig)
+
+    # validate effective_date
+
+    return GitHubProjectETL(config).extract_and_transform_in_memory()
