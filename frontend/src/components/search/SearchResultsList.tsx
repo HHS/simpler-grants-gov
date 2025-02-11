d2519bc18cbe53b3b57818337a8c8e9575fc24d9
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-02-11 11:17:35 -0500

    [Issue #1485] Search page suspense refactor (#3804)
    Simplifies how the search page is structured:
    * 1 promise, 1 resolution
    * removes `fetch` components
    * introduces search one results skeleton for suspense fallback

--- frontend/src/components/search/SearchResultsList.tsx
+++ frontend/src/components/search/SearchResultsList.tsx
@@ -4,33 +4,17 @@ import { SearchAPIResponse } from "src/types/search/searchResponseTypes";
 
 import { getTranslations } from "next-intl/server";
 
-import { ClientSideUrlUpdater } from "src/components/ClientSideUrlUpdater";
 import SearchResultsListItem from "src/components/search/SearchResultsListItem";
 import ServerErrorAlert from "src/components/ServerErrorAlert";
 
 interface ServerPageProps {
-  searchResultsPromise: Promise<SearchAPIResponse>;
+  searchResults: SearchAPIResponse;
 }
 
-export default async function SearchResultsListFetch({
-  searchResultsPromise,
+export default async function SearchResultsList({
+  searchResults,
 }: ServerPageProps) {
-  const searchResults = await searchResultsPromise;
   const t = await getTranslations("Search");
-
-  if (
-    !searchResults.data.length &&
-    searchResults.pagination_info.total_pages > 0 &&
-    searchResults.pagination_info.page_offset >
-      searchResults.pagination_info.total_pages
-  ) {
-    return (
-      <ClientSideUrlUpdater
-        param={"page"}
-        value={searchResults.pagination_info.total_pages.toString()}
-      />
-    );
-  }
 
   if (searchResults.status_code !== 200) {
     return <ServerErrorAlert callToAction={t("generic_error_cta")} />;
@@ -41,9 +25,10 @@ export default async function SearchResultsListFetch({
       <div>
         <h2>{t("resultsListFetch.noResultsTitle")}</h2>
         <ul>
-          {t.rich("resultsListFetch.noResultsBody", {
-            li: (chunks) => <li>{chunks}</li>,
-          })}
+          <li>{t("resultsListFetch.noResultsBody.0")}</li>
+          <li>{t("resultsListFetch.noResultsBody.1")}</li>
+          <li>{t("resultsListFetch.noResultsBody.2")}</li>
+          <li>{t("resultsListFetch.noResultsBody.3")}</li>
         </ul>
       </div>
     );
