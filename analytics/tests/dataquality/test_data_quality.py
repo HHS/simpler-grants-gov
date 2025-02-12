13252669de57ade4fa99c96c14fbb18af1e50b46
jcrichlake <jeff.crichlake@agile6.com>
2025-02-12 11:20:53 -0500

    Adding second test for graphQL output

--- analytics/tests/dataquality/test_data_quality.py
+++ analytics/tests/dataquality/test_data_quality.py
@@ -11,7 +11,7 @@ from tests.dataquality.inputs.roadmap import mock_graphql_roadmap_data
 from tests.dataquality.inputs.sprintboards import mock_graphql_sprintboard_data
 
 
-def test_snapshot(snapshot):
+def test_roadmap_snapshot(snapshot):
     """Compare pipeline to pre-committed snapshot."""
     test_data = output()
 
@@ -22,13 +22,29 @@ def test_snapshot(snapshot):
              mock_graphql_roadmap_data)
 def output() -> list[dict]|None:
     """Call the new pipeline code to be used for comparison."""
-    #extract_transform_and_load("config/github-projects.json", "2025-02-11")
-
     config_path = Path("config/github-projects.json")
-    effective_date = "2025-02-11"
     config = load_config(config_path, GitHubProjectConfig)
 
     # validate effective_date
 
     return GitHubProjectETL(config).extract_and_transform_in_memory()
 
+
+
+
+def test_sprint_snapshot(snapshot):
+    """Compare pipeline for sprintboard snapshot."""
+    assert sprint_board_output == snapshot
+
+
+@mock.patch.object(GitHubGraphqlClient,
+                "execute_paginated_query",
+             mock_graphql_sprintboard_data)
+def sprint_board_output() -> list[dict]|None:
+    """Call the new pipeline code to be used for comparison."""
+    config_path = Path("config/github-projects.json")
+    config = load_config(config_path, GitHubProjectConfig)
+
+    # validate effective_date
+
+    return GitHubProjectETL(config).extract_and_transform_in_memory()
