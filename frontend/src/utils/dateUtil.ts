46f85f51d1f1e95e7d459ce05ae7142e7ed3499f
karinamzalez <karinamzalez@gmail.com>
2025-02-13 15:59:23 -0600

    dateUtils: update formatDate to use dayjs

--- frontend/src/utils/dateUtil.ts
+++ frontend/src/utils/dateUtil.ts
@@ -7,28 +7,12 @@ dayjs.extend(advancedFormat);
 
 // Convert "2024-02-21" to "February 21, 2024"
 export function formatDate(dateStr: string | null): string {
-  if (!dateStr || dateStr.length !== 10) {
+  if (!dateStr || !dayjs(dateStr).isValid()) {
     console.warn("invalid date string provided for parse");
     return "";
   }
 
-  const parts = dateStr.split("-");
-
-  if (parts.length !== 3) {
-    console.warn("invalid date string provided for parse");
-    return "";
-  }
-
-  const [year, month, day] = parts.map(Number);
-  // Create a new Date object using the local time
-  const date = new Date(year, month - 1, day);
-
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return date.toLocaleDateString("en-US", options);
+  return dayjs(dateStr).format('LL');
 }
 
 export const getConfiguredDayJs = () => dayjs;
