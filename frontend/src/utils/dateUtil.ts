8e836004dd56eeca25ea9f67a41794dfb28380c4
karinamzalez <karinamzalez@gmail.com>
2025-02-14 13:29:18 -0600

    dateUtil: update test

--- frontend/src/utils/dateUtil.ts
+++ frontend/src/utils/dateUtil.ts
@@ -1,15 +1,17 @@
 import dayjs from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(advancedFormat);
+dayjs.extend(customParseFormat);
 dayjs.extend(localizedFormat);
 dayjs.extend(timezone);
 
 // Convert "2024-02-21" to "February 21, 2024"
 export function formatDate(dateStr: string | null): string {
-  if (!dateStr || !dayjs(dateStr).isValid()) {
+  if (!dateStr || !dayjs(dateStr, 'YYYY-MM-DD', true).isValid()) {
     console.warn("invalid date string provided for parse");
     return "";
   }
--- frontend/tests/utils/dateUtil.test.ts
+++ frontend/tests/utils/dateUtil.test.ts
@@ -9,14 +9,13 @@ describe("formatDate", () => {
     expect(formatDate(null)).toEqual("");
     expect(formatDate("")).toEqual("");
     expect(formatDate("Wednesday")).toEqual("");
-    expect(formatDate("20")).toEqual("");
+    expect(formatDate("20241010")).toBe("");
     expect(formatDate("24-10-10")).toEqual("");
+    expect(formatDate(Date.now().toString())).toEqual("");
   });
-
+  
   it("returns a human readable string for properly formatted dates", () => {
     expect(formatDate("2024-10-10")).toBe("October 10, 2024");
-    expect(formatDate("20241010")).toBe("October 10, 2024");
-    expect(formatDate(Date.now().toString())).toEqual("");
   });
 
   it("invokes console warn when date string does not contain 3 parts", () => {
