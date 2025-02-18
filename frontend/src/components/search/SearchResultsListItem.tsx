a3a2a15abf2ecad5d634bd91cdb218bed2f8e220
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-02-18 14:55:03 -0500

    [Issue #3156] fetch options for agency filter from api (#3637)

--- frontend/src/components/search/SearchResultsListItem.tsx
+++ frontend/src/components/search/SearchResultsListItem.tsx
@@ -2,40 +2,38 @@
 
 import { Opportunity } from "src/types/search/searchResponseTypes";
 import { formatDate } from "src/utils/dateUtil";
-import { AgencyNamyLookup } from "src/utils/search/generateAgencyNameLookup";
+import { getAgencyDisplayName } from "src/utils/search/searchUtils";
 
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 interface SearchResultsListItemProps {
   opportunity: Opportunity;
-  agencyNameLookup?: AgencyNamyLookup;
 }
+
+const metadataBorderClasses = `
+  display-block
+  tablet:display-inline-block
+  tablet:border-left-1px
+  tablet:padding-x-1
+  tablet:margin-left-neg-1
+  tablet:margin-right-1
+  tablet:border-base-lighter
+  `;
+
+const resultBorderClasses = `
+  border-1px
+  border-base-lighter
+  padding-x-2
+  padding-y-105
+  margin-bottom-2
+  text-base-darker
+  `;
 
 export default function SearchResultsListItem({
   opportunity,
-  agencyNameLookup,
 }: SearchResultsListItemProps) {
   const t = useTranslations("Search");
-
-  const metadataBorderClasses = `
-    display-block
-    tablet:display-inline-block
-    tablet:border-left-1px
-    tablet:padding-x-1
-    tablet:margin-left-neg-1
-    tablet:margin-right-1
-    tablet:border-base-lighter
-  `;
-
-  const resultBorderClasses = `
-    border-1px
-    border-base-lighter
-    padding-x-2
-    padding-y-105
-    margin-bottom-2
-    text-base-darker
-  `;
 
   return (
     <div className={resultBorderClasses}>
@@ -100,15 +98,7 @@ export default function SearchResultsListItem({
             <div className="grid-col tablet:order-2 overflow-hidden font-body-xs">
               <span className={metadataBorderClasses}>
                 <strong>{t("resultsListItem.summary.agency")}</strong>
-                {opportunity?.top_level_agency_name &&
-                opportunity?.agency_name &&
-                opportunity?.top_level_agency_name !== opportunity?.agency_name
-                  ? `${opportunity?.top_level_agency_name} - ${opportunity?.agency_name}`
-                  : opportunity?.agency_name ||
-                    (agencyNameLookup && opportunity?.summary?.agency_code
-                      ? // Use same exact label we're using for the agency filter list
-                        agencyNameLookup[opportunity?.summary?.agency_code]
-                      : "--")}
+                {getAgencyDisplayName(opportunity)}
               </span>
             </div>
             <div className="grid-col tablet:order-3 overflow-hidden font-body-xs">
