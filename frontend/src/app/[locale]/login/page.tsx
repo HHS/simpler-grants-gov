c90da789f163b584358206e26203c6f4a05cf453
Matt Dragon <mattdragon@navapbc.com>
2025-02-20 17:18:15 -0500

    Working without the ref

--- frontend/src/app/[locale]/login/page.tsx
+++ frontend/src/app/[locale]/login/page.tsx
@@ -3,33 +3,28 @@
 import SessionStorage from "src/utils/sessionStorage";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export default function Login() {
-  const redirecting = useRef<boolean>(false);
   const router = useRouter();
 
-  useEffect((): void => {
+  useEffect(() => {
     if (typeof window !== "undefined") {
-      // without this check, even wrapped in a useEffect this would fire twice and the second time redirect to / because sessionStorage was empty.
-      if (!redirecting.current) {
-        redirecting.current = true;
-        const redirectURL = SessionStorage.getItem("login-redirect");
-        SessionStorage.removeItem("login-redirect");
+      const redirectURL = SessionStorage.getItem("login-redirect");
+      SessionStorage.removeItem("login-redirect");
 
-        if (
-          redirectURL === null ||
-          redirectURL === "" ||
-          redirectURL.substring(0, 1) !== "/"
-        ) {
-          router.push("/");
-          return;
-        }
-
-        router.push(redirectURL);
+      if (
+        redirectURL === null ||
+        redirectURL === "" ||
+        redirectURL.substring(0, 1) !== "/"
+      ) {
+        router.push("/");
       }
+      router.push(redirectURL || "/");
+      return () => SessionStorage.removeItem("login-redirect");
+    } else {
+      console.error("window is undefined");
     }
   }, [router]);
-
   return <>Redirecting...</>;
 }
