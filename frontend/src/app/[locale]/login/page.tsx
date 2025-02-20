4d0ab96fdb7bddbb911f565af188bdfbee11391a
Matt Dragon <mattdragon@navapbc.com>
2025-02-20 08:30:02 -0500

    Add a useEffect to avoid re-render during render error

--- frontend/src/app/[locale]/login/page.tsx
+++ frontend/src/app/[locale]/login/page.tsx
@@ -2,30 +2,34 @@
 
 import SessionStorage from "src/utils/sessionStorage";
 
-import { useRef } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 export default function Login() {
   const redirecting = useRef<boolean>(false);
+  const router = useRouter();
 
-  if (typeof window !== "undefined") {
-    // without this check, even wrapped in a useEffect this would fire twice and the second time redirect to / because sessionStorage was empty.
-    if (!redirecting.current) {
-      redirecting.current = true;
-      const redirectURL = SessionStorage.getItem("login-redirect");
-      SessionStorage.removeItem("login-redirect");
+  useEffect((): void => {
+    if (typeof window !== "undefined") {
+      // without this check, even wrapped in a useEffect this would fire twice and the second time redirect to / because sessionStorage was empty.
+      if (!redirecting.current) {
+        redirecting.current = true;
+        const redirectURL = SessionStorage.getItem("login-redirect");
+        SessionStorage.removeItem("login-redirect");
 
-      if (
-        redirectURL === null ||
-        redirectURL === "" ||
-        redirectURL.substring(0, 1) !== "/"
-      ) {
-        window.location.assign("/");
-        return;
+        if (
+          redirectURL === null ||
+          redirectURL === "" ||
+          redirectURL.substring(0, 1) !== "/"
+        ) {
+          router.push("/");
+          return;
+        }
+
+        router.push(redirectURL);
       }
-
-      window.location.assign(redirectURL);
-
-      return <>Redirecting...</>;
     }
-  }
+  }, [router]);
+
+  return <>Redirecting...</>;
 }
