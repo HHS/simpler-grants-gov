a305237bacaa2a11b2ca30356370db0e638827cd
bruk <brukty@gmail.com>
2025-02-20 11:43:08 -0500

    add test

---
+++ api/tests/src/services/opportunities_v1/test_opportunity_version.py
@@ -0,0 +1,20 @@
+from src.db.models.opportunity_models import OpportunityVersion
+from src.services.opportunities_v1.opportuntity_version import save_opportunity_version
+from tests.src.db.models.factories import OpportunityFactory
+
+
+def test_save_opportunity_version(db_session, enable_factory_create):
+    # Create an opportunity
+    opportunity = OpportunityFactory.create()
+
+    # Save opportunity into opportunity_version table
+    save_opportunity_version(db_session, opportunity)
+
+    # db_session.refresh(OpportunityVersion)
+
+    # Verify Record created
+    saved_opp_version = db_session.query(OpportunityVersion).all()
+
+    assert len(saved_opp_version) == 1
+    assert saved_opp_version[0].opportunity_id == opportunity.opportunity_id
+    assert saved_opp_version[0].opportunity_data == opportunity.for_json()
