13483003489b48e6439728a978e7d5b679dc6d7c
bruk <brukty@gmail.com>
2025-02-20 16:25:54 -0500

    add test

--- api/tests/src/util/test_dict_util.py
+++ api/tests/src/util/test_dict_util.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.util.dict_util import flatten_dict
+from src.util.dict_util import flatten_dict, diff_nested_dicts
 
 
 @pytest.mark.parametrize(
@@ -49,5 +49,40 @@ from src.util.dict_util import flatten_dict
         ({"a": {0: {"b": "b_value"}, 1: "c"}}, {"a.0.b": "b_value", "a.1": "c"}),
     ],
 )
+
 def test_flatten_dict(data, expected_output):
     assert flatten_dict(data) == expected_output
+
+def test_diff_nested_dicts() -> []:
+    dict_a = {
+        "name": "bob",
+        "nested": {
+            "sub_value1": [1, 2, 3, 4],
+            "sub_value2": "hello",
+            "sub_value3": "initial value",
+        }
+    }
+
+    dict_b = {
+        "name": "steve",
+        "nested": {
+            "sub_value1": [1, 2, 5],
+            "sub_value2": "hello",
+            "sub_value3": "new value"
+        },
+        "new_field": "yay"
+    }
+
+    expected = [
+        {'field': 'nested.sub_value3',
+         'before': 'initial value',
+         'after': 'new value'},
+        {'field': 'name', 'before': 'bob', 'after': 'steve'},
+        {'field': 'new_field', 'before': None, 'after': 'yay'},
+        {'field': 'nested.sub_value1', 'before': [1, 2, 3, 4], 'after': [1, 2, 5]}
+    ]
+    diffs = diff_nested_dicts(dict_a, dict_b)
+
+    assert diffs == expected
+
+
