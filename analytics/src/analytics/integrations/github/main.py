c1012652ed76b887adf370cf1b708421bc8813e4
David Dudas <david.dudas@intuitial.com>
2025-02-26 15:02:27 -0800

    log the validation failure rate

--- analytics/src/analytics/integrations/github/main.py
+++ analytics/src/analytics/integrations/github/main.py
@@ -27,8 +27,11 @@ def transform_project_data(
 ) -> list[dict]:
     """Pluck and reformat relevant fields for each item in the raw data."""
     transformed_data = []
+    count = 0
+    fail = 0
 
     for i, item in enumerate(raw_data):
+        count += 1
         try:
             # Filter out invalid content from boards local user may not have permission to
             if item.get("content") is None:
@@ -75,11 +78,17 @@ def transform_project_data(
             transformed_data.append(transformed)
 
         except ValidationError as err:
+            fail += 1
             message = f"project item {i} cannot be validated; skipping"
             logger.info(message)
             logger.debug(err)
             logger.debug(item)
             continue
+
+    if count > 0:
+        failure_rate = round(100*fail/count, 3)
+        message = f"validation failure rate: {failure_rate} %"
+        logger.info(message)
 
     return transformed_data
 
