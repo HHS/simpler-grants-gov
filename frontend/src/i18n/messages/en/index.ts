2c76c6cda8ee5499514de081aeae87dfe7429c74
Freddie Yebra <freddieyebra@navapbc.com>
2025-02-25 22:37:22 -0600

    Add content items.

--- frontend/src/i18n/messages/en/index.ts
+++ frontend/src/i18n/messages/en/index.ts
@@ -680,6 +680,33 @@ export const messages = {
     sections: {
       progress: {
         title: "What we're working on right now",
+        contentItems: [
+          [
+            {
+              title: "Simpler application workflow prototype",
+              content:
+                "We will create a comprehensive service blueprint showing how the existing Grants.gov application process could be simplified. Then, we'll prototype an application form with persistent data storage and scope a pilot for a small subset of opportunities.<p><linkGithub3348>Follow #3348 on GitHub</linkGithub3348></p>",
+            },
+            {
+              title:
+                "Full support for Opportunity Listing attachments (NOFOs/downloads)",
+              content:
+                "The opportunity listings on Simpler.Grants.gov will include all information and file attachments available on Grants.gov. Design updates will make the Notice of Funding Opportunity (NOFO) easier to access.<p><linkGithub3045>Follow #3045 on GitHub</linkGithub3045></p>",
+            },
+          ],
+          [
+            {
+              title: "Authenticate via Login.gov",
+              content:
+                "Enable grant seekers to create an account on Simpler.Grants.gov using the Login.gov single sign-on platform to minimize the steps and friction users experience when signing up.<p><linkGithub2640>Follow #2640 on GitHub</linkGithub2640></p>",
+            },
+            {
+              title: "Search & Opportunity Listing improvements",
+              content:
+                "With feedback from the community, we will iterate on improvements that make it easier to adjust search filter criteria, share search results, and save relevant results and opportunities.<p><linkGithub2875>Follow #2875 on GitHub</linkGithub2875></p>",
+            },
+          ],
+        ],
       },
       milestones: {
         title: "Recent milestones reached",
