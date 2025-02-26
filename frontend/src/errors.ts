7a7f9b107f54eb4cd54d8214a5e739e375e30035
acouch <acinternets@gmail.com>
2025-02-26 12:46:22 -0500

    Update route and error

--- frontend/src/errors.ts
+++ frontend/src/errors.ts
@@ -199,6 +199,7 @@ export const readError = (e: Error, defaultStatus: number) => {
 
   return {
     status: Number(status),
-    message: cause ? JSON.stringify(cause) : message,
+    message,
+    cause: cause as FrontendErrorDetails,
   };
 };
