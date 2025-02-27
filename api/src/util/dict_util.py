7460f037ca5ae7497e086dcb543be63ca9a92974
bruk <brukty@gmail.com>
2025-02-27 13:47:51 -0500

    covert to set before comparing

--- api/src/util/dict_util.py
+++ api/src/util/dict_util.py
@@ -61,14 +61,19 @@ def diff_nested_dicts(dict1: dict, dict2: dict) -> list:
     all_keys = set(flatt_dict1.keys()).union(flatt_dict2.keys())  # Does not keep order
 
     for k in all_keys:
-        v_a = flatt_dict1.get(k, None)
-        v_b = flatt_dict2.get(k, None)
+        # convert values to set for comparison
+        v_a = _convert_to_set(flatt_dict1.get(k, None))
+        v_b = _convert_to_set(flatt_dict2.get(k, None))
 
-        # Store diff if values are different
-        if all(isinstance(v, Iterable) and not isinstance(v, str) for v in (v_a, v_b)): # Check if values are iterable and not strings
-            if set(v_a) != set(v_b):
-                diffs.append({"field": k, "before": v_a, "after": v_b})
-        elif v_a != v_b:
+        if v_a != v_b:
             diffs.append({"field": k, "before": v_a, "after": v_b})
 
     return diffs
+
+
+def _convert_to_set(data: Any) -> set:
+    if data is None:
+        return set()
+    if isinstance(data, Iterable) and not isinstance(data, str):  # Check if iterable exclude strings
+        return set(data)
+    return {data}
