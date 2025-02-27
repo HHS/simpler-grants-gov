ba8ca9bcc305b38b37a43eb15511c02b517042c9
bruk <brukty@gmail.com>
2025-02-27 11:08:32 -0500

    use schema dump

--- api/src/services/opportunities_v1/opportunity_version.py
+++ api/src/services/opportunities_v1/opportunity_version.py
@@ -1,95 +1,33 @@
 import logging
-from datetime import date, datetime
-from decimal import Decimal
-from importlib.metadata.diagnose import inspect
-from uuid import UUID
-
-from sqlalchemy.orm import RelationshipProperty, class_mapper
 
 from src.adapters import db
-from src.adapters.db import PostgresDBClient
-from src.db.models.base import Base
-from src.db.models.opportunity_models import Opportunity
-from tests.src.db.models.factories import OpportunityFactory
+from src.api.opportunities_v1.opportunity_schemas import OpportunityV1Schema
+from src.db.models.opportunity_models import Opportunity, OpportunityVersion
 
 logger = logging.getLogger(__name__)
 
-all_model_dict = {}
 
+def save_opportunity_version(db_session: db.Session, opportunity: Opportunity) -> None:
+    """
+    Saves a new version of an Opportunity record in the OpportunityVersion table.
 
-def save_opportunity_json(
-    db_session: db.Session, opportunity_model: Base, visited=None, relationship_name=None
-) -> None:
-    # Initialize visited dictionary if it doesn't exist
+    This function extracts the opportunity data from the provided Opportunity model instance,
+    creates a new OpportunityVersion record with the data, and saves it in the database.
 
-    if visited is None:
-        visited = {}
+    :param  db_session: The active SQLAlchemy session used to interact with the database.
+    :param opportunity: An instance of the Opportunity model containing the data to be saved.
+    :return: This function does not return a value. It saves a new version of the opportunity in the database.
+    """
+    #  Extracts the opportunity data as JSON object
+    schema = OpportunityV1Schema()
+    schema_data = schema.dump(opportunity)
 
-    # Get the class of the instance
-    model_class = opportunity_model.__class__
+    # Add new OpportunityVersion instance to the database session
+    opportunity_version = OpportunityVersion(
+        opportunity_id=opportunity.opportunity_id,
+        opportunity_data=schema_data,
+    )
 
-    # Prevent infinite recursion in case of circular relationships
-    if model_class in visited:
-        if opportunity_model not in visited[model_class]:
-            visited[model_class].append(opportunity_model)
+    db_session.add(opportunity_version)
 
-    else:
-        visited[model_class] = [opportunity_model]
-
-
-    global all_model_dict
-    try:
-        model_dict = {}
-        for column in class_mapper(model_class).columns:
-            # skip over relationships
-            if column.foreign_keys:
-                continue
-
-            value = getattr(opportunity_model, column.name)
-            # can maybe use for_json() for these
-            if isinstance(value, UUID) or isinstance(value, Decimal):
-                model_dict[column.name] = str(value)
-            if isinstance(value, date) or isinstance(value, datetime):
-                model_dict[column.name] = value.isoformat()
-            elif isinstance(value, list):  # Handle relationships (like many-to-many or one-to-many)
-                model_dict[column.name] = [
-                    save_opportunity_json(db_session, item, visited) for item in value
-                ]
-            else:
-                model_dict[column.name] = value
-
-        if isinstance(opportunity_model, Opportunity):
-            all_model_dict = model_dict
-        elif isinstance(all_model_dict[relationship_name], list):
-            all_model_dict[relationship_name].append(model_dict)
-        else:
-            all_model_dict[relationship_name] = model_dict
-
-
-
-        for relationship_name, relationship_property in class_mapper(
-            opportunity_model.__class__
-        ).relationships.items():
-            if relationship_name != "opportunity":
-                relationship_value = getattr(opportunity_model, relationship_name, None)
-
-                if relationship_value:
-
-                    if isinstance(
-                        relationship_value, list
-                    ):  # Handle one-to-many or many-to-many relationships
-                        all_model_dict[relationship_name] = []
-                        for item in relationship_value:
-                            save_opportunity_json(db_session, item, visited, relationship_name)
-
-                    else:  # Handle one-to-one or many-to-one relationships
-                        all_model_dict[relationship_name] = None
-                        save_opportunity_json(
-                            db_session, relationship_value, visited, relationship_name
-                        )
-                        logger.error(model_dict[relationship_name])
-
-
-    except Exception as e:
-        print(f"Error : {e}")
-        raise e
+    db_session.commit()
