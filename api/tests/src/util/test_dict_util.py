bf551d27cefbd23ce2d29a0bc2e10e4bd7e11340
babebe <brukty@gmail.com>
2025-02-28 09:37:06 -0500

    [Issue #3897] Get the diff of two nested dict (#3968)
    ## Summary
    Fixes #{[3897](https://github.com/HHS/simpler-grants-gov/issues/3897)}
    
    ### Time to review: __5 mins__
    
    ## Changes proposed
    Util Function: Given two complex nested dictionaries can provide a set
    of changes between the two
    Test Added

--- api/tests/src/util/test_dict_util.py
+++ api/tests/src/util/test_dict_util.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.util.dict_util import flatten_dict
+from src.util.dict_util import diff_nested_dicts, flatten_dict
 
 
 @pytest.mark.parametrize(
@@ -51,3 +51,91 @@ from src.util.dict_util import flatten_dict
 )
 def test_flatten_dict(data, expected_output):
     assert flatten_dict(data) == expected_output
+
+
+@pytest.mark.parametrize(
+    "dict1,dict2,expected_output",
+    [
+        (
+            # dict1
+            {"a": "apple", "b": {"x": 1, "y": 2}, "c": 100},  # additional field a
+            # dict2
+            {
+                "b": {"x": 1, "y": 3},  # changed y
+                "c": 200,  # changed c
+                "d": "dog",  # new field added
+            },
+            # expected output
+            [
+                {"field": "a", "before": "apple", "after": None},
+                {"field": "b.y", "before": 2, "after": 3},
+                {"field": "c", "before": 100, "after": 200},
+                {"field": "d", "before": None, "after": "dog"},
+            ],
+        ),
+        (
+            # dict1
+            {"a": "ball", "b": {"p": 5, "q": 10}, "e": "elephant"},
+            # dict2
+            {
+                "a": "bat",  # changed a
+                "b": {"p": 5, "q": 1.1},  # no change  # changed q
+                "e": "elephant",  # no change
+            },
+            # expected output
+            [
+                {"field": "a", "before": "ball", "after": "bat"},
+                {"field": "b.q", "before": 10, "after": 1.1},
+            ],
+        ),
+        (
+            # dict1
+            {"x": 10, "y": "yellow", "z": {"m": "mouse", "n": True}},
+            # dict2
+            {
+                "x": 10,  # no change
+                "y": "yellow",  # no change
+                "z": {"m": "mouse", "n": False},  # no change  # changed n
+            },
+            # expected output
+            [{"field": "z.n", "before": True, "after": False}],
+        ),
+        (
+            # dict1
+            {"x": {"x": {"x": [1, 2, True]}}},
+            # dict2
+            {"x": {"x": {"x": [1, 2, True]}}},  # no change
+            # expected output
+            [],
+        ),
+        (
+            # dict1
+            {"x": {"x": {"x": [1, 2, True]}}},
+            # dict2
+            {"x": {"x": {"x": [1, True, 2]}}},  # re-ordered list
+            # expected output
+            [],
+        ),
+        (
+            # dict1
+            {"x": {"x": [1, 2], "z": None}},  # missing y
+            # dict2
+            {"x": {"y": [1, 2], "z": 4}},  # missing x
+            # expected output
+            [
+                {"field": "x.x", "before": [1, 2], "after": None},
+                {"field": "x.y", "before": None, "after": [1, 2]},
+                {"field": "x.z", "before": None, "after": 4},
+            ],
+        ),
+    ],
+)
+def test_diff_nested_dicts(dict1, dict2, expected_output):
+    result = diff_nested_dicts(dict1, dict2)
+
+    assert len(result) == len(expected_output)
+
+    expected_sorted = sorted(expected_output, key=lambda x: x["field"])
+    sorted_result = sorted(result, key=lambda x: x["field"])
+
+    assert expected_sorted == sorted_result
