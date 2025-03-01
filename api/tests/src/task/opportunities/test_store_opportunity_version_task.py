e9f05d5d3145b3a0c6a0e80e8f6c099648ca79d0
bruk <brukty@gmail.com>
2025-02-28 19:49:56 -0500

    update test

--- api/tests/src/task/opportunities/test_store_opportunity_version_task.py
+++ api/tests/src/task/opportunities/test_store_opportunity_version_task.py
@@ -1,8 +1,12 @@
-import pytest
+from datetime import timedelta
 
-from src.db.models.opportunity_models import OpportunityVersion, OpportunitySummary, Opportunity
+import pytest
+from sqlalchemy import update
+
+from src.db.models.opportunity_models import OpportunityVersion, OpportunitySummary, Opportunity, OpportunityChangeAudit
 from src.task.opportunities.store_opportunity_version_task import StoreOpportunityVersionTask
 from src.util import datetime_util
+from src.util.datetime_util import get_now_us_eastern_datetime
 from tests.conftest import BaseTestClass
 from tests.src.db.models.factories import OpportunityFactory, OpportunityChangeAuditFactory, OpportunitySummaryFactory, \
     OpportunityAssistanceListingFactory
@@ -20,7 +24,7 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
         db_session.commit()
 
         # create opportunities
-        opportunities = OpportunityFactory.create_batch(5)
+        opportunities = OpportunityFactory.create_batch(3)
 
         OpportunityChangeAuditFactory.create(
             opportunity=opportunities[0]
@@ -39,15 +43,51 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
             opportunity=opportunities[1],
 
         )
+        OpportunityAssistanceListingFactory.create(
+            opportunity=opportunities[2],
+
+        )
 
         OpportunityChangeAuditFactory.create(
             opportunity=opportunities[1]
         )
+        # Updated before the last job run
+        OpportunityChangeAuditFactory.create(
+            opportunity=opportunities[2],
+            updated_at=get_now_us_eastern_datetime() - timedelta(hours=1),
+        )
+
         store_opportunity_version_task.run()
 
         saved_opp_version = db_session.query(OpportunityVersion).all()
         assert len(saved_opp_version) == 2
         assert saved_opp_version[1].opportunity_id == opportunities[1].opportunity_id
+
+
+        # Verify previously versioned opportunity that was updated is entered into OpportunityVersion table
+        OpportunityAssistanceListingFactory.create(
+            opportunity=opportunities[0],
+
+        )
+        db_session.execute(
+            update(OpportunityChangeAudit)
+            .where(OpportunityChangeAudit.opportunity_id == opportunities[0].opportunity_id)
+            .values(updated_at=get_now_us_eastern_datetime())
+        )
+
+        store_opportunity_version_task.run()
+        saved_opp_version = db_session.query(OpportunityVersion).all()
+
+        assert len(saved_opp_version) == 3
+        assert len([opp for opp in saved_opp_version if opp.opportunity_id == opportunities[0].opportunity_id] ) == 2 # updated twice
+        assert saved_opp_version[2].opportunity_id == opportunities[0].opportunity_id
+
+
+
+
+
+
+
 
 
 
