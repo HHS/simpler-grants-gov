cff9f696bbaa153000af3714dbf77b4df8aae0b9
bruk <brukty@gmail.com>
2025-02-28 20:08:34 -0500

    cleanup

--- api/tests/src/task/opportunities/test_store_opportunity_version_task.py
+++ api/tests/src/task/opportunities/test_store_opportunity_version_task.py
@@ -3,13 +3,15 @@ from datetime import timedelta
 import pytest
 from sqlalchemy import update
 
-from src.db.models.opportunity_models import OpportunityVersion, OpportunitySummary, Opportunity, OpportunityChangeAudit
+from src.db.models.opportunity_models import Opportunity, OpportunityChangeAudit, OpportunityVersion
 from src.task.opportunities.store_opportunity_version_task import StoreOpportunityVersionTask
-from src.util import datetime_util
 from src.util.datetime_util import get_now_us_eastern_datetime
 from tests.conftest import BaseTestClass
-from tests.src.db.models.factories import OpportunityFactory, OpportunityChangeAuditFactory, OpportunitySummaryFactory, \
-    OpportunityAssistanceListingFactory
+from tests.src.db.models.factories import (
+    OpportunityAssistanceListingFactory,
+    OpportunityChangeAuditFactory,
+    OpportunityFactory,
+)
 
 
 class TestStoreOpportunityVersionTask(BaseTestClass):
@@ -17,7 +19,9 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
     def store_opportunity_version_task(self, db_session):
         return StoreOpportunityVersionTask(db_session)
 
-    def test_store_opportunity_version_task(self, db_session, enable_factory_create, store_opportunity_version_task):
+    def test_store_opportunity_version_task(
+        self, db_session, enable_factory_create, store_opportunity_version_task
+    ):
         db_session.query(OpportunityVersion).delete()
         db_session.query(Opportunity).delete()
         db_session.query(Opportunity).delete()
@@ -26,9 +30,7 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
         # create opportunities
         opportunities = OpportunityFactory.create_batch(3)
 
-        OpportunityChangeAuditFactory.create(
-            opportunity=opportunities[0]
-        )
+        OpportunityChangeAuditFactory.create(opportunity=opportunities[0])
 
         store_opportunity_version_task.run()
 
@@ -41,16 +43,12 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
         # Verify only opportunities updated after latest job were entered into OpportunityVersion table
         OpportunityAssistanceListingFactory.create(
             opportunity=opportunities[1],
-
         )
         OpportunityAssistanceListingFactory.create(
             opportunity=opportunities[2],
-
         )
 
-        OpportunityChangeAuditFactory.create(
-            opportunity=opportunities[1]
-        )
+        OpportunityChangeAuditFactory.create(opportunity=opportunities[1])
         # Updated before the last job run
         OpportunityChangeAuditFactory.create(
             opportunity=opportunities[2],
@@ -63,11 +61,9 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
         assert len(saved_opp_version) == 2
         assert saved_opp_version[1].opportunity_id == opportunities[1].opportunity_id
 
-
         # Verify previously versioned opportunity that was updated is entered into OpportunityVersion table
         OpportunityAssistanceListingFactory.create(
             opportunity=opportunities[0],
-
         )
         db_session.execute(
             update(OpportunityChangeAudit)
@@ -79,22 +75,14 @@ class TestStoreOpportunityVersionTask(BaseTestClass):
         saved_opp_version = db_session.query(OpportunityVersion).all()
 
         assert len(saved_opp_version) == 3
-        assert len([opp for opp in saved_opp_version if opp.opportunity_id == opportunities[0].opportunity_id] ) == 2 # updated twice
+        assert (
+            len(
+                [
+                    opp
+                    for opp in saved_opp_version
+                    if opp.opportunity_id == opportunities[0].opportunity_id
+                ]
+            )
+            == 2
+        )  # updated twice
         assert saved_opp_version[2].opportunity_id == opportunities[0].opportunity_id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
