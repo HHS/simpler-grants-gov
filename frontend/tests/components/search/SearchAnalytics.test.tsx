37ff8c9f32d6cf590f5378e32b6c0cfd8bef6978
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-03-03 09:14:47 -0500

    [Issue #3764] add search query params to new relic (#4039)
    * sends search page query params to new relic
    * adds a typing package for the new relic client side object
    * some moving around of new relic and query param related typing
    * enables new relic in staging
    * updates docs

--- frontend/tests/components/search/SearchAnalytics.test.tsx
+++ frontend/tests/components/search/SearchAnalytics.test.tsx
@@ -3,24 +3,38 @@ import { render } from "@testing-library/react";
 import SearchAnalytics from "src/components/search/SearchAnalytics";
 
 const sendGAEventMock = jest.fn();
+const waitForNewRelicMock = jest.fn();
+const setNewRelicCustomAttributeMock = jest.fn();
+const unsetAllNewRelicQueryAttributesMock = jest.fn();
 
 jest.mock("@next/third-parties/google", () => ({
   /* eslint-disable-next-line @typescript-eslint/no-unsafe-return */
   sendGAEvent: (...args: unknown[]) => sendGAEventMock(...args),
 }));
 
+jest.mock("src/utils/analyticsUtil", () => ({
+  waitForNewRelic: () => waitForNewRelicMock() as unknown,
+  setNewRelicCustomAttribute: (...args: unknown[]): unknown =>
+    setNewRelicCustomAttributeMock(...args),
+  unsetAllNewRelicQueryAttributes: () =>
+    unsetAllNewRelicQueryAttributesMock() as unknown,
+}));
+
+const basicParams = {
+  fundingInstrument: "cooperative_agreement",
+  status: "posted, archived",
+  agency: "AC,PAMS-SC",
+  page: "1",
+  query: "a random search term",
+};
+
 describe("SearchAnalytics", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
   it("calls sendGAEvent with expected params on render", () => {
     const { rerender } = render(
-      <SearchAnalytics
-        params={{
-          fundingInstrument: "cooperative_agreement",
-          status: "posted, archived",
-          agency: "AC,PAMS-SC",
-          page: "1",
-          query: "a random search term",
-        }}
-      />,
+      <SearchAnalytics params={basicParams} newRelicEnabled={false} />,
     );
     expect(sendGAEventMock).toHaveBeenCalledWith("event", "search_attempt", {
       search_filters:
@@ -36,11 +50,93 @@ describe("SearchAnalytics", () => {
           page: "1",
           query: "a random search term",
         }}
+        newRelicEnabled={false}
       />,
     );
     expect(sendGAEventMock).toHaveBeenCalledWith("event", "search_attempt", {
       search_filters:
         '{"status":"posted, archived, closed","agency":"AC","category":"recovery_act"}',
     });
+  });
+
+  it("does not waitForNewRelic if New Relic is not enabled", () => {
+    render(<SearchAnalytics params={basicParams} newRelicEnabled={false} />);
+    expect(waitForNewRelicMock).not.toHaveBeenCalled();
+  });
+
+  it("does not attempt to set custom attributes if New Relic is not enabled", () => {
+    const { rerender } = render(
+      <SearchAnalytics params={basicParams} newRelicEnabled={false} />,
+    );
+
+    rerender(
+      <SearchAnalytics
+        params={{
+          ...basicParams,
+          page: "2",
+        }}
+        newRelicEnabled={false}
+      />,
+    );
+    expect(setNewRelicCustomAttributeMock).not.toHaveBeenCalled();
+  });
+
+  it("does not attempt to set custom attributes if New Relic is not initialized", () => {
+    waitForNewRelicMock.mockResolvedValue(false);
+    const { rerender } = render(
+      <SearchAnalytics params={basicParams} newRelicEnabled={true} />,
+    );
+
+    rerender(
+      <SearchAnalytics
+        params={{
+          ...basicParams,
+          page: "2",
+        }}
+        newRelicEnabled={true}
+      />,
+    );
+    expect(setNewRelicCustomAttributeMock).not.toHaveBeenCalled();
+  });
+
+  it("calls setNewRelicCustomAttribute for all search params when params change", async () => {
+    waitForNewRelicMock.mockImplementation(() => {
+      return Promise.resolve(true);
+    });
+    const { rerender } = render(
+      <SearchAnalytics params={basicParams} newRelicEnabled={true} />,
+    );
+
+    // have to wait a tick for the wait promise to resolve
+    const animationFramePromise = new Promise((resolve) => {
+      requestAnimationFrame(resolve);
+    });
+
+    await animationFramePromise;
+
+    rerender(
+      <SearchAnalytics
+        params={{
+          ...basicParams,
+          page: "2",
+        }}
+        newRelicEnabled={true}
+      />,
+    );
+    Object.entries({
+      ...basicParams,
+      page: "2",
+    }).forEach(([key, value]) => {
+      expect(setNewRelicCustomAttributeMock).toHaveBeenCalledWith(key, value);
+    });
+  });
+
+  it("calls unsetAllNewRelicQueryAttributes on cleanup", () => {
+    waitForNewRelicMock.mockImplementation(() => {
+      return Promise.resolve(true);
+    });
+    render(<SearchAnalytics params={basicParams} newRelicEnabled={true} />);
+
+    expect(unsetAllNewRelicQueryAttributesMock).toHaveBeenCalled();
   });
 });
