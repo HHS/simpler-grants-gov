c971fad5055e435fa03e8a239e7bdb6cba542428
Piyush Saravagi <saravagipiyush@gmail.com>
2025-03-10 09:23:10 -0700

    Fix formatting

--- frontend/tests/e2e/opportunity.spec.ts
+++ frontend/tests/e2e/opportunity.spec.ts
@@ -19,40 +19,52 @@ test("has page attributes", async ({ page }) => {
 
 test("can expand and collapse summary", async ({ page }) => {
   await expect(page.getByText("Show full summary")).toBeVisible();
-  await expect(page.getByText(/.*From single present little building.*/)).not.toBeVisible();
+  await expect(
+    page.getByText(/.*From single present little building.*/),
+  ).not.toBeVisible();
   await expect(page.getByText("Hide full description")).not.toBeVisible();
 
   await page.getByRole("button", { name: /^Show full summary$/ }).click();
 
   // validate that summary has been expanded
   await expect(page.getByText("Show full summary")).not.toBeVisible();
-  await expect(page.getByText(/.*From single present little building.*/)).toBeVisible();
+  await expect(
+    page.getByText(/.*From single present little building.*/),
+  ).toBeVisible();
   await expect(page.getByText("Hide full description")).toBeVisible();
 
   await page.getByRole("button", { name: /^Hide full description$/ }).click();
 
   // validate that summary has been collapsed
   await expect(page.getByText("Show full summary")).toBeVisible();
-  await expect(page.getByText(/.*From single present little building.*/)).not.toBeVisible();
+  await expect(
+    page.getByText(/.*From single present little building.*/),
+  ).not.toBeVisible();
   await expect(page.getByText("Hide full description")).not.toBeVisible();
 });
 
 test("can expand and collapse description", async ({ page }) => {
   await expect(page.getByText("Show full description")).toBeVisible();
-  await expect(page.getByText(/.*Year section value bag none clear.*/)).not.toBeVisible();
+  await expect(
+    page.getByText(/.*Year section value bag none clear.*/),
+  ).not.toBeVisible();
   await expect(page.getByText("Hide full description")).not.toBeVisible();
 
   await page.getByRole("button", { name: /^Show full description$/ }).click();
 
   // validate that description has been expanded
   await expect(page.getByText("Show full description")).not.toBeVisible();
-  await expect(page.getByText(/.*Year section value bag none clear.*/)).toBeVisible();
+  await expect(
+    page.getByText(/.*Year section value bag none clear.*/),
+  ).toBeVisible();
   await expect(page.getByText("Hide full description")).toBeVisible();
 
   await page.getByRole("button", { name: /^Hide full description$/ }).click();
 
   // validate that description has been collapsed
   await expect(page.getByText("Show full description")).toBeVisible();
-  await expect(page.getByText(/.*Year section value bag none clear.*/)).not.toBeVisible();
+  await expect(
+    page.getByText(/.*Year section value bag none clear.*/),
+  ).not.toBeVisible();
   await expect(page.getByText("Hide full description")).not.toBeVisible();
 });
