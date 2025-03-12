1dc05506cfb9bc737d6f9fa1ea9fc6dde5ef29f6
Kai Siren <kaisiren@navapbc.com>
2025-03-12 16:17:53 -0700

    pin version

--- infra/accounts/monitoring.tf
+++ infra/accounts/monitoring.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "newrelic_account_id" {
 }
 
 module "newrelic-aws-cloud-integrations" {
-  source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
+  source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws?ref=v3.58.1"
 
   newrelic_account_id     = data.aws_ssm_parameter.newrelic_account_id.value
   newrelic_account_region = "US"
