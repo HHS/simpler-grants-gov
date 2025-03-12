203830fea72a0b27531bedbc23d88dff09bc2910
Aaron Couch <aaroncouch@navapbc.com>
2025-03-12 10:28:06 -0400

    [Issue #3828] Update copy button to support non-https (#4095)

--- frontend/src/components/user/SaveSearchPanel.tsx
+++ frontend/src/components/user/SaveSearchPanel.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { environment } from "src/constants/environments";
-
 import { useTranslations } from "next-intl";
 import { usePathname, useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 
 import SearchQueryCopyButton from "src/components/search/SearchQueryCopyButton";
 
 export const SaveSearchPanel = () => {
   const path = usePathname();
   const searchParams = useSearchParams();
-  const query = searchParams?.toString() ? `?${searchParams.toString()}` : "";
-  const url = `${environment.NEXT_PUBLIC_BASE_URL}${path}${query}`;
+
+  const url = useMemo(() => {
+    const query = searchParams?.toString() ? `?${searchParams.toString()}` : "";
+    const origin = typeof window !== "undefined" ? window.location.origin : "";
+    return `${origin}${path}${query}`;
+  }, [searchParams, path]);
+
   const t = useTranslations("Search.savedQuery");
   return (
     <div className="border-base-lighter border-1px padding-2 flex-align-start text-primary-darker text-underline display-flex">
--- frontend/src/hooks/useCopyToClipboard.ts
+++ frontend/src/hooks/useCopyToClipboard.ts
@@ -6,16 +6,42 @@ export const useCopyToClipboard = () => {
   const [copied, setCopied] = useState(false);
   const [copying, setCopying] = useState(false);
 
+  // Provides https fallback to clipboard API
+  // credit to: https://stackoverflow.com/a/65996386
+  const copyWithFallback = async (content: string) => {
+    // Use fallback if https is not supported
+    if (navigator.clipboard && window.isSecureContext) {
+      await navigator.clipboard.writeText(content);
+    } else {
+      const textArea = document.createElement("textarea");
+      textArea.value = content;
+      textArea.style.position = "absolute";
+      textArea.style.left = "-999999px";
+      document.body.prepend(textArea);
+      textArea.select();
+      try {
+        // This is identified as deprecated https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+        // though no standard has replaced it and the "copy" command is supported
+        // in all browsers.
+        document.execCommand("copy");
+      } catch (error) {
+        console.error(error);
+      } finally {
+        textArea.remove();
+      }
+    }
+  };
+
   const copyToClipboard = async (content: string, contentTime: number) => {
     try {
       setCopying(true);
-      await navigator.clipboard.writeText(content);
+      await copyWithFallback(content);
       setCopied(true);
       setCopying(false);
     } catch (error) {
       setCopied(false);
       setCopying(false);
-      console.error(`Error copying to clipboard: ${content}`, error);
+      throw new Error(`Error copying to clipboard: ${error as string}`);
     } finally {
       setTimeout(() => {
         setCopied(false);
