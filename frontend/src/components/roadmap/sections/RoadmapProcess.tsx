71f7f9ef0013ad5be7d472bbbc37eff6df1bd605
Freddie Yebra <freddieyebra@navapbc.com>
2025-03-12 08:20:40 -0500

    Update var names.

--- frontend/src/components/roadmap/sections/RoadmapProcess.tsx
+++ frontend/src/components/roadmap/sections/RoadmapProcess.tsx
@@ -46,14 +46,17 @@ export default function RoadmapProcess() {
           <p className="margin-0 tablet-lg:margin-bottom-2 line-height-sans-4 tablet:font-sans-md">
             {t("sectionSummary")}
           </p>
-          {roadmapProcesSectionGridRows.map((i, k) => (
-            <div className="grid-row" key={`roadmap-process-row-${k}`}>
-              {i.map((j) => (
+          {roadmapProcesSectionGridRows.map((sectionRow, sectionRoadIdx) => (
+            <div
+              className="grid-row"
+              key={`roadmap-process-row-${sectionRoadIdx}`}
+            >
+              {sectionRow.map((sectionRowItem) => (
                 <div
                   className="grid-col-12 tablet-lg:grid-col-6 tablet-lg:padding-right-5"
-                  key={`roadmap-process-${j.title}`}
+                  key={`roadmap-process-${sectionRowItem.title}`}
                 >
-                  <RoadmapProcessSectionContent {...j} />
+                  <RoadmapProcessSectionContent {...sectionRowItem} />
                 </div>
               ))}
             </div>
