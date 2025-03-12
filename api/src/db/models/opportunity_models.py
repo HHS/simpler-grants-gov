42e9893fcac58ed127305f75787b0671ce447f80
bruk <brukty@gmail.com>
2025-03-12 13:45:14 -0400

    add loaded col

--- api/src/db/models/opportunity_models.py
+++ api/src/db/models/opportunity_models.py
@@ -456,6 +456,7 @@ class OpportunityChangeAudit(ApiSchemaTable, TimestampMixin):
         BigInteger, ForeignKey(Opportunity.opportunity_id), primary_key=True, index=True
     )
     opportunity: Mapped[Opportunity] = relationship(Opportunity)
+    loaded: Mapped[bool| None]
 
 
 class OpportunityVersion(ApiSchemaTable, TimestampMixin):
