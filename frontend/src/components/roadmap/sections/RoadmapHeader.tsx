300f51d83e2cb1e09d7ba2c7c7da69ceb5bd08ef
Freddie Yebra <freddieyebra@navapbc.com>
2025-03-12 12:49:24 -0500

    Only render 1 image.

--- frontend/src/components/roadmap/sections/RoadmapHeader.tsx
+++ frontend/src/components/roadmap/sections/RoadmapHeader.tsx
@@ -7,49 +7,40 @@ export default function RoadmapHeader() {
 
   return (
     <div className="text-white bg-primary-darkest padding-top-4 tablet:padding-y-6">
-      <GridContainer>
+      <GridContainer className="padding-0">
         <Grid row>
           <Grid
             tablet={{ col: 6 }}
-            className="display-flex flex-column flex-justify-center"
+            className="grid-container display-flex flex-column flex-justify-center"
           >
             <h1 className="font-sans-xl margin-0 tablet:font-sans-2xl tablet:maxw-2">
               {t("pageHeaderTitle")}
             </h1>
             <p
               className={
-                "font-sans-sm line-height-sans-4 margin-bottom-4 tablet:margin-bottom-0"
+                "font-sans-sm line-height-sans-4 margin-bottom-4 tablet:padding-right-2 tablet:margin-bottom-0"
               }
             >
               {t("pageHeaderParagraph")}
             </p>
           </Grid>
           <Grid
-            className="display-none tablet:display-flex tablet:flex-justify-end tablet:flex-align-center"
+            className="tablet:display-flex tablet:flex-justify-end tablet:flex-align-center tablet:grid-container"
             tablet={{ col: 6 }}
           >
-            <div className="display-flex flex-justify-center flex-align-center">
+            <Grid className="display-flex flex-justify-center flex-align-center  ">
               <Image
                 src="/img/roadmap-header-image.png"
-                className="minh-full maxh-mobile maxw-mobile"
+                className="minh-full maxh-mobile width-full tablet:maxw-mobile"
                 alt="Picture of person on a journey"
                 layout="responsive"
                 width={360}
                 height={350}
               />
-            </div>
+            </Grid>
           </Grid>
         </Grid>
       </GridContainer>
-      <div className="tablet:display-none width-full">
-        <Image
-          src="/img/roadmap-header-image.png"
-          alt="Picture of person on a journey"
-          layout="responsive"
-          width={360}
-          height={350}
-        />
-      </div>
     </div>
   );
 }
