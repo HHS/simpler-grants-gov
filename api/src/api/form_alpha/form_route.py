6d3b46ee6aacd33da06c446fea7c611ed68c4dec
Michael Chouinard <chouinar6@gmail.com>
2025-03-14 12:08:37 -0400

    Logging

--- api/src/api/form_alpha/form_route.py
+++ api/src/api/form_alpha/form_route.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 
 import src.adapters.db as db
@@ -6,7 +7,10 @@ import src.api.form_alpha.form_schema as form_schema
 import src.api.response as response
 from src.api.form_alpha.form_blueprint import form_blueprint
 from src.auth.api_key_auth import api_key_auth
+from src.logging.flask_logger import add_extra_data_to_current_request_logs
 from src.services.form_alpha.get_form import get_form
+
+logger = logging.getLogger(__name__)
 
 
 @form_blueprint.get("/forms/<uuid:form_id>")
@@ -14,7 +18,10 @@ from src.services.form_alpha.get_form import get_form
 @form_blueprint.auth_required(api_key_auth)
 @flask_db.with_db_session()
 def form_get(db_session: db.Session, form_id: uuid.UUID) -> response.ApiResponse:
+    add_extra_data_to_current_request_logs({"form.form_id": form_id})
+    logger.info("GET /alpha/forms/:form_id")
 
-    form = get_form(db_session, form_id)
+    with db_session.begin():
+        form = get_form(db_session, form_id)
 
     return response.ApiResponse(message="Success", data=form)
