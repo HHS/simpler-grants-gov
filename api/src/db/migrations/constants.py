1f08b0735b3690e487bc71f3605e975b87074145
babebe <brukty@gmail.com>
2025-03-14 10:03:31 -0400

    [ISSUE #4014] Update opportunity change audit [1/2] (#4162)
    ## Summary
    Fixes #{4014}
    
    ### Time to review: __2 mins__
    
    ## Changes proposed
    Migration File: 
    Add new col `is_loaded_to_search ` to OpportunityChangeAudit. [Nullable,
    Bool]
    Update trigger function to se `is_loaded_to_search ` to False. Moved
    trigger func to constants.py
    Update Factory
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

---
+++ api/src/db/migrations/constants.py
@@ -0,0 +1,31 @@
+opportunity_search_index_queue_trigger_function = """
+CREATE OR REPLACE FUNCTION update_opportunity_search_queue()
+RETURNS TRIGGER AS $$
+DECLARE
+    opp_id bigint;
+BEGIN
+    -- Determine the opportunity_id based on the table
+    CASE TG_TABLE_NAME
+        WHEN 'link_opportunity_summary_funding_instrument' THEN
+            opp_id := (SELECT opportunity_id FROM api.opportunity_summary WHERE opportunity_summary_id = NEW.opportunity_summary_id);
+        WHEN 'link_opportunity_summary_funding_category' THEN
+            opp_id := (SELECT opportunity_id FROM api.opportunity_summary WHERE opportunity_summary_id = NEW.opportunity_summary_id);
+        WHEN 'link_opportunity_summary_applicant_type' THEN
+            opp_id := (SELECT opportunity_id FROM api.opportunity_summary WHERE opportunity_summary_id = NEW.opportunity_summary_id);
+        WHEN 'opportunity_summary' THEN
+            opp_id := NEW.opportunity_id;
+        WHEN 'current_opportunity_summary' THEN
+            opp_id := NEW.opportunity_id;
+        ELSE
+            opp_id := NEW.opportunity_id;
+    END CASE;
+
+    INSERT INTO api.opportunity_change_audit (opportunity_id)
+    VALUES (opp_id)
+    ON CONFLICT (opportunity_id)
+    DO UPDATE SET updated_at = CURRENT_TIMESTAMP, is_loaded_to_search = FALSE;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+"""
