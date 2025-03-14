bc5abb32c6b0e7e7b86707e549cadde469a50351
Michael Huneke <mike.huneke@focusconsulting.io>
2025-03-14 13:35:01 -0400

    Update schema names / defs

--- api/src/api/application_alpha/application_schemas.py
+++ api/src/api/application_alpha/application_schemas.py
@@ -6,6 +6,8 @@ class ApplicationStartRequestSchema(Schema):
     competition_id = fields.UUID(required=True)
 
 
+class ApplicationStartResponseDataSchema(Schema):
+     application_id = fields.UUID()
+
 class ApplicationStartResponseSchema(AbstractResponseSchema):
-    message = fields.String()
-    data = fields.Dict()
+    data = fields.Nested(ApplicationStartResponseDataSchema())
