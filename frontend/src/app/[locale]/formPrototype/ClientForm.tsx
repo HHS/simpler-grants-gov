a96676723ae4ba2ab752606e127d0380f2ff3539
Aaron Couch <acinternets@gmail.com>
2025-03-17 02:14:29 -0400

    Update form

--- frontend/src/app/[locale]/formPrototype/ClientForm.tsx
+++ frontend/src/app/[locale]/formPrototype/ClientForm.tsx
@@ -37,9 +37,9 @@ type TextTypes =
   | "tel"
   | "url";
 
-const createTextField = (fieldName: string, title: string, type: TextTypes) => {
+const createTextField = (fieldName: string, title: string, type: TextTypes, parentId: string) => {
   return (
-    <div key={`wrapper-for-${fieldName}`}>
+    <div key={`wrapper-for-${fieldName}`} id={parentId}>
       <Label key={`label-for-${fieldName}`} htmlFor={fieldName}>
         {title}
       </Label>
@@ -48,80 +48,74 @@ const createTextField = (fieldName: string, title: string, type: TextTypes) => {
   );
 };
 
-const wrapSection = (label: string, fieldName: string, tree: JSX.Element[]) => {
+const wrapSection = (label: string, fieldName: string, tree: JSX.Element) => {
   return (
     <Fieldset key={`${fieldName}-row`}> <FormGroup key={`${fieldName}-group`}>
-      <Label htmlFor={fieldName}>{label}</Label>
-      {tree.map((field) => field)}
+      <Label htmlFor={`${fieldName}-fields`}>{label}</Label>
+      {tree}
   </FormGroup>  </Fieldset>
   );
 };
 
-const createField = (fieldName: string, title: string, type: string) => {
+const createField = (fieldName: string, title: string, type: string, parentId: string) => {
   switch (type) {
     case "string":
-      return createTextField(fieldName, title, "text");
+      return createTextField(fieldName, title, "text",parentId);
       break;
     case "number":
-      return createTextField(fieldName, title, type);
+      return createTextField(fieldName, title, type,parentId);
       break;
     default:
       throw new Error(`Error rendering field ${fieldName}`);
   }
 };
 
-const buildFormTree = (
-  schema: object,
-  uiSchema: uiSchemaType,
-  formData: FormData,
-  wrap: {
-    wrapped: boolean;
-    label: string;
-    fieldName: string;
-    count: number;
-  } = {
-    wrapped: false,
-    label: "",
-    fieldName: "",
-    count: 0,
-  },
-  tree: JSX.Element[] = [],
-) => {
-  for (const fieldName in uiSchema) {
-    if (typeof uiSchema[fieldName] === "object") {
-      if (uiSchema[fieldName].type === "field") {
-        const { definition } = uiSchema[fieldName] as { definition: string };
-        const { title, type } = getSchemaObjectFromPointer(
-          schema,
-          definition,
-        ) as SchemaField;
-        const field = createField(fieldName, title, type);
-        tree.push(field);
-      } else if (
-        uiSchema[fieldName].type === "section" &&
-        uiSchema[fieldName].children
-      ) {
-        // TODO: sections after rendering fields in separate rec func
-        if (wrap.wrapped) {
-          const prev = tree.splice(tree.length - wrap.count);
-          tree.push(wrapSection(wrap.label, wrap.fieldName, prev));
+const parentHasChild = (acc, uiSchema): boolean => {
+  // TODO: check JSX elements
+  return false;
+}
+
+function buildFormTreeClosure(schema: object, uiSchema: object, formData: FormData) {
+  let acc: JSX.Element | JSX.Element[] = <></>;
+
+  const buildFormTree = (
+    uiSchema: uiSchemaType,
+    parent: {label: string, name: string} | null,
+  ) => {
+    if (typeof uiSchema === 'object' && uiSchema.children) {
+      buildFormTree(uiSchema.children, {label: uiSchema.label, name: uiSchema.name});
+    }
+    else if (Array.isArray(uiSchema)) {
+      for (const i in uiSchema) {
+        if (uiSchema[i].children) {
+          buildFormTree(uiSchema[i].children, {label: uiSchema[i].label, name: uiSchema[i].name});
         }
-        wrap.wrapped = true;
-        wrap.count = Object.keys(uiSchema[fieldName].children).length;
-        wrap.label = uiSchema[fieldName].label ?? "";
-        wrap.fieldName = fieldName;
-        buildFormTree(
-          schema,
-          uiSchema[fieldName].children,
-          formData,
-          wrap,
-          tree,
-        );
+      }
+      if (parent) {
+        const row = uiSchema.map((node) => {
+          if (node.children) {
+            // TODO: test
+            return acc;
+          }
+          else {
+            const { definition } = node as { definition: string };
+            const name = definition?.split('/')[2];
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            const { title, type } = getSchemaObjectFromPointer(
+              schema,
+              definition,
+            ) as SchemaField;
+            const field = createField(name, title, type, `${parent.name}-fields`);
+            return field;
+          }
+        });
+        acc = parentHasChild(acc, uiSchema) ?  wrapSection(parent.label, parent.name, row) : [acc,wrapSection(parent.label, parent.name, row)];  
       }
     }
   }
-  return tree;
-};
+  buildFormTree(uiSchema, null);
+  return acc;
+}
 
 const handleSubmit =
   (setFormData: SetFormDataFunction): React.FormEventHandler<HTMLFormElement> =>
@@ -143,10 +137,10 @@ const ClientForm = ({
   uiSchema: object;
 }) => {
   const [formData, setFormData] = useState({});
-  const fields = buildFormTree(schema, uiSchema as uiSchemaType, formData);
+  const fields = buildFormTreeClosure(schema, uiSchema as uiSchemaType, formData);
   return (
     <form onChange={handleSubmit(setFormData)}>
-      <FormGroup>{fields.map((field) => field)}</FormGroup>
+      <FormGroup>{fields}</FormGroup>
     </form>
   );
 };
