32d63b3db3e35bb28ea37e1c9a3bbd4296d98837
Kai Siren <kaisiren@navapbc.com>
2025-03-18 11:07:02 -0700

    nava-platform infra update-app --version platform-cli-migration/v0.14.3 . analytics

--- infra/analytics/app-config/dev.tf
+++ infra/analytics/app-config/dev.tf
@@ -14,6 +14,7 @@ module "dev_config" {
   enable_https                    = false
   has_database                    = local.has_database
   has_incident_management_service = local.has_incident_management_service
+  enable_notifications            = local.enable_notifications
 
   service_cpu    = 256
   service_memory = 2048
