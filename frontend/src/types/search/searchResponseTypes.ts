940332f11c352e87657513259c30338422b4c193
Doug Schrashun <dougschrashun@navapbc.com>
2025-03-20 15:21:07 -0400

    add facet count typing

--- frontend/src/types/search/searchResponseTypes.ts
+++ frontend/src/types/search/searchResponseTypes.ts
@@ -4,12 +4,28 @@ import { SearchFetcherActionType } from "src/types/search/searchRequestTypes";
 
 export type SearchResponseData = BaseOpportunity[];
 
+export type FacetCounts = {
+  [key in BackendFilterNames]: {
+    [key: string]: number;
+  };
+};
+
 export interface SearchAPIResponse extends APIResponse {
   data: SearchResponseData;
   pagination_info: PaginationInfo;
+  facet_counts: FacetCounts;
+  // these are set on the frontend after fetch, not coming back from API
   actionType?: SearchFetcherActionType;
   fieldChanged?: string;
 }
+
+export const backendFilterNames = [
+  "opportunity_status",
+  "funding_instrument",
+  "applicant_type",
+  "agency",
+  "funding_category",
+] as const;
 
 export const searchFilterNames = [
   "status",
@@ -32,3 +48,4 @@ export const validSearchQueryParamKeys = [
 export type ValidSearchQueryParam = (typeof validSearchQueryParamKeys)[number];
 
 export type FrontendFilterNames = (typeof searchFilterNames)[number];
+export type BackendFilterNames = (typeof backendFilterNames)[number];
