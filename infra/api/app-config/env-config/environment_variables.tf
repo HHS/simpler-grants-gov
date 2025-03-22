452a0f5bae9a336bcd080dd3a8bd8a5297368490
Matt Dragon <mattdragon@navapbc.com>
2025-03-22 17:03:38 -0400

    Typo

--- infra/api/app-config/env-config/environment_variables.tf
+++ infra/api/app-config/env-config/environment_variables.tf
@@ -19,7 +19,7 @@ locals {
     LOGIN_GOV_JWK_ENDPOINT    = "https://idp.int.identitysandbox.gov/api/openid_connect/certs"
     LOGIN_GOV_AUTH_ENDPOINT   = "https://idp.int.identitysandbox.gov/openid_connect/authorize"
     LOGIN_GOV_TOKEN_ENDPOINT  = "https://idp.int.identitysandbox.gov/api/openid_connect/token"
-    LOGIN_GOV_REDIRECT_SCHEMA = var.enable_https ? "https" : "http"
+    LOGIN_GOV_REDIRECT_SCHEME = var.enable_https ? "https" : "http"
     API_JWT_ISSUER            = "simpler-grants-api-${var.environment}"
     API_JWT_AUDIENCE          = "simpler-grants-api-${var.environment}"
 
