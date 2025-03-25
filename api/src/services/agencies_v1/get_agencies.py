4ce5bcffa72ae26a4e5a2960671d22b24554546e
joshtonava <201659582+joshtonava@users.noreply.github.com>
2025-03-25 08:48:15 -0500

    Updated agency API models to use UUID

--- api/src/services/agencies_v1/get_agencies.py
+++ api/src/services/agencies_v1/get_agencies.py
@@ -1,11 +1,11 @@
 import logging
+import uuid
 from typing import Sequence, Tuple
 
+import src.adapters.db as db
 from pydantic import BaseModel, Field
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
-
-import src.adapters.db as db
 from src.db.models.agency_models import Agency
 from src.pagination.pagination_models import PaginationInfo, PaginationParams
 from src.pagination.paginator import Paginator
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 class AgencyFilters(BaseModel):
-    agency_id: int | None = None
+    agency_id: uuid.UUID | None = None
     agency_name: str | None = None
 
 
