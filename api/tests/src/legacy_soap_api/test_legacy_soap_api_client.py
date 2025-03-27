292d175606c503972e5c4cfc37165964a898df06
Freddie Yebra <freddieyebra@navapbc.com>
2025-03-27 16:54:11 -0500

    Lint

--- api/tests/src/legacy_soap_api/test_legacy_soap_api_client.py
+++ api/tests/src/legacy_soap_api/test_legacy_soap_api_client.py
@@ -4,9 +4,9 @@ from src.legacy_soap_api.legacy_soap_api_client import LegacySOAPClient
 
 
 class TestSOAPClient:
-     @pytest.fixture(scope="class")
-     def legacy_soap_client(self):
+    @pytest.fixture(scope="class")
+    def legacy_soap_client(self):
         return LegacySOAPClient()
 
-     def test_can_instantiate(self, legacy_soap_client) -> None:
+    def test_can_instantiate(self, legacy_soap_client) -> None:
         assert isinstance(legacy_soap_client, LegacySOAPClient)
