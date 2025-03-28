b63bd1802460d33989be0fa3e6e6036992909641
Michael Chouinard <chouinar6@gmail.com>
2025-03-28 11:40:58 -0400

    [Untickted] Try to fix CI/CD issues with mock oauth2 server

--- frontend/tests/e2e/sign-in.spec.ts
+++ frontend/tests/e2e/sign-in.spec.ts
@@ -8,7 +8,7 @@ test.afterEach(async ({ context }) => {
 });
 
 // reenable after https://github.com/HHS/simpler-grants-gov/issues/3791
-test.skip("signs in successfully", async ({ page }, { project }) => {
+test("signs in successfully", async ({ page }, { project }) => {
   await page.goto("http://localhost:3000/?_ff=authOn:true");
   await performSignIn(page, project);
 });
