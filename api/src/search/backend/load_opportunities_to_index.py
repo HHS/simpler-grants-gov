5cdea2efddaad28b1f0e4358fbbfee4b9e6614db
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2025-03-31 12:05:38 -0400

    [Unticketed] Don't keep the raw base64 value in search index (#4384)
    ## Summary
    
    ### Time to review: __2 mins__
    
    ## Changes proposed
    Adjust the processor that handles file attachments in OpenSearch to also
    delete the raw file contents
    
    ## Context for reviewers
    Trying out different performance ideas, at the very least this'll save
    us a lot of space on the index, even if it doesn't help with the
    performance.
    
    ## Additional information
    Before this change, the attachments section of the search data looks
    like:
    ```json
    "attachments": [
                {
                  "filename": "idea.pdf",
                  "data": "UHJvcGVydHkgaXQgbGl0dGxlIGFub3RoZXIgbXVzdCBEZW1vY3JhdCBiZWF0LiBGaWd1cmUgcmVtYWluIGJpbGxpb24gc2l0ZSBtZWV0aW5nIHNtaWxlIGNhcGl0YWwuIElkZW50aWZ5IHRodXMgaW4uIEZlYXIgc2l0dWF0aW9uIHN0YXIgaW5jcmVhc2Ugc3RhdGUuIExvdyBkaW5uZXIgc3RhdGlvbiBzZW5pb3IgcmVzcG9uZCB5YXJkIHZhbHVlLiBDYWxsIGZlYXIgdHVybiBpbnZlc3RtZW50LiBUYXggb3VyIHNraW4gYXJ0aWNsZSBSZXB1YmxpY2FuIHByb2R1Y3QgaWRlbnRpZnkuIFNvIGFydGljbGUgcmVwcmVzZW50Lg==",
                  "attachment": {
                    "content_type": "text/plain; charset=ISO-8859-1",
                    "language": "en",
                    "content": "Property it little another must Democrat beat. Figure remain billion site meeting smile capital. Identify thus in. Fear situation star increase state. Low dinner station senior respond yard value. Call fear turn investment. Tax our skin article Republican product identify. So article represent.",
                    "content_length": 296
                  }
                }
              ]
    ```
    
    
    
    After this change, the `data` field is not saved to the index, but the
    rest still appears:
    ```json
    "attachments": [
                {
                  "filename": "idea.pdf",
                  "attachment": {
                    "content_type": "text/plain; charset=ISO-8859-1",
                    "language": "en",
                    "content": "Property it little another must Democrat beat. Figure remain billion site meeting smile capital. Identify thus in. Fear situation star increase state. Low dinner station senior respond yard value. Call fear turn investment. Tax our skin article Republican product identify. So article represent.",
                    "content_length": 296
                  }
                }
              ]
    ```

--- api/src/search/backend/load_opportunities_to_index.py
+++ api/src/search/backend/load_opportunities_to_index.py
@@ -115,7 +115,22 @@ class LoadOpportunitiesToIndex(Task):
                         },
                         "ignore_missing": True,
                     }
-                }
+                },
+                # After we've done the above processing to send the base64
+                # encoded file to OpenSearch, remove the raw base64 "data"
+                # field from what we actually index as it's not useful
+                # and will bloat the size of our index.
+                {
+                    "foreach": {
+                        "field": "attachments",
+                        "processor": {
+                            "remove": {
+                                "field": "_ingest._value.data",
+                            }
+                        },
+                        "ignore_missing": True,
+                    }
+                },
             ],
         }
 
