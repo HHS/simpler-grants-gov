d4ef7ecaeaa8aad6ec8b34c952313e038d7af012
bruk <brukty@gmail.com>
2025-04-01 14:12:29 -0400

    update task

--- api/src/search/backend/load_agencies_to_index.py
+++ api/src/search/backend/load_agencies_to_index.py
@@ -1,10 +1,10 @@
-from typing import Iterator, Sequence
+from typing import Sequence
 
 from pydantic import Field
-
 from pydantic_settings import SettingsConfigDict
-from sqlalchemy.orm import noload, selectinload
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from src.adapters import db, search
 from src.api.agencies_v1.agency_schema import AgencyResponseSchema
 from src.db.models.agency_models import Agency
@@ -16,24 +16,29 @@ from src.util.env_config import PydanticBaseEnvConfig
 class LoadAgenciesToIndexConfig(PydanticBaseEnvConfig):
     model_config = SettingsConfigDict(env_prefix="LOAD_AGENCY_SEARCH_")
 
-    shard_count: int = Field(default=1)  # LOAD_OPP_SEARCH_SHARD_COUNT # use same one here ?
+    shard_count: int = Field(
+        default=1
+    )  # LOAD_OPP_SEARCH_SHARD_COUNT # use same one here ?  update tf ?
     replica_count: int = Field(default=1)  # LOAD_OPP_SEARCH_REPLICA_COUNT # use same one here ?
 
-    alias_name: str = Field(default="agency-index-alias")  #  LOAD_AGENCIES_SEARCH_ALIAS_NAME
+    alias_name: str = Field(default="agency-index-alias")  # LOAD_AGENCIES_SEARCH_ALIAS_NAME
     index_prefix: str = Field(default="agency-index")  # LOAD_OPP_INDEX_PREFIX
 
-class LoadAgenciesToIndex(Task): #TBC How often does this need to be updated
+
+class LoadAgenciesToIndex(Task):  # TBC How often does this need to be updated
     def __init__(
-            self,
-            db_session: db.Session,
-            search_client: search.SearchClient,
-            config: LoadAgenciesToIndexConfig | None = None,
+        self,
+        db_session: db.Session,
+        search_client: search.SearchClient,
+        config: LoadAgenciesToIndexConfig | None = None,
     ) -> None:
         super().__init__(db_session)
+
         self.search_client = search_client
         if config is None:
             config = LoadAgenciesToIndexConfig()
-            self.config = config
+
+        self.config = config
 
         current_timestamp = get_now_us_eastern_datetime().strftime("%Y-%m-%d_%H-%M-%S")
         self.index_name = f"{self.config.index_prefix}-{current_timestamp}"
@@ -63,49 +68,16 @@ class LoadAgenciesToIndex(Task): #TBC How often does this need to be updated
         """
         Fetch all agencies.
         """
-        return (
-            self.db_session.execute(
-            select(Agency)
-            .options(selectinload("*"))
-        ).scalars().all()
-        )
+        return self.db_session.execute(select(Agency).options(selectinload("*"))).scalars().all()
 
     def load_agencies(self, agencies: Sequence[Agency]) -> None:
         logger.info("Loading agencies...")
-        import pdb; pdb.set_trace()
 
-        schema = AgencyResponseSchema() # Create new one ? more data ?
+        schema = AgencyResponseSchema()  # Create new one ? more data ?
         agencies_json = [schema.dump(record) for record in agencies]
-
-        import pdb; pdb.set_trace()
 
         self.search_client.bulk_upsert(
             self.index_name,
             agencies_json,
             "agency_id",
-            )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        )
