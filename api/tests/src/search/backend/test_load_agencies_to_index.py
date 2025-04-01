705db0a08b6c2a03906902ccc65bbc090cd33817
bruk <brukty@gmail.com>
2025-04-01 14:12:38 -0400

    add test

---
+++ api/tests/src/search/backend/test_load_agencies_to_index.py
@@ -0,0 +1,47 @@
+import pytest
+
+from src.search.backend.load_agencies_to_index import LoadAgenciesToIndex, LoadAgenciesToIndexConfig
+from tests.conftest import BaseTestClass
+from tests.src.db.models.factories import AgencyFactory
+
+
+class TestLoadAgenciesToIndex(BaseTestClass):
+    @pytest.fixture(scope="class")
+    def load_agencies_to_index(self, db_session, search_client, agency_index_alias):
+        config = LoadAgenciesToIndexConfig(
+            alias_name=agency_index_alias,
+            index_prefix="test-load-agencies",
+        )
+
+        return LoadAgenciesToIndex(db_session, search_client, config)
+
+    def test_load_agencies_to_index(
+        self,
+        db_session,
+        search_client,
+        load_agencies_to_index,
+        agency_index_alias,
+        enable_factory_create,
+    ):
+        # Create Agencies to load into the search index
+        agencies = [AgencyFactory.create(agency_code="DOD")]
+        agencies.extend(AgencyFactory.create_batch(size=5, top_level_agency=agencies[0]))
+
+        load_agencies_to_index.run()
+
+        resp = search_client.search(agency_index_alias, {"size": 50})
+
+        assert resp.total_records == len(agencies)
+
+        # Add more agencies to load and rerun
+        agencies.extend(AgencyFactory.create_batch(size=5))
+
+        load_agencies_to_index.index_name = (
+            load_agencies_to_index.index_name + "-new-sub-agency-data"
+        )
+
+        load_agencies_to_index.run()
+
+        resp = search_client.search(agency_index_alias, {"size": 50})
+
+        assert resp.total_records == len(agencies)
