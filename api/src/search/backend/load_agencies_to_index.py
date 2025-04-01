265447eda6ab49a5cd39a9f635b0fa77e459b5d1
bruk <brukty@gmail.com>
2025-04-01 12:10:55 -0400

    wip

---
+++ api/src/search/backend/load_agencies_to_index.py
@@ -0,0 +1,111 @@
+from typing import Iterator, Sequence
+
+from pydantic import Field
+
+from pydantic_settings import SettingsConfigDict
+from sqlalchemy.orm import noload, selectinload
+from sqlalchemy import select
+from src.adapters import db, search
+from src.api.agencies_v1.agency_schema import AgencyResponseSchema
+from src.db.models.agency_models import Agency
+from src.task.task import Task, logger
+from src.util.datetime_util import get_now_us_eastern_datetime
+from src.util.env_config import PydanticBaseEnvConfig
+
+
+class LoadAgenciesToIndexConfig(PydanticBaseEnvConfig):
+    model_config = SettingsConfigDict(env_prefix="LOAD_AGENCY_SEARCH_")
+
+    shard_count: int = Field(default=1)  # LOAD_OPP_SEARCH_SHARD_COUNT # use same one here ?
+    replica_count: int = Field(default=1)  # LOAD_OPP_SEARCH_REPLICA_COUNT # use same one here ?
+
+    alias_name: str = Field(default="agency-index-alias")  #  LOAD_AGENCIES_SEARCH_ALIAS_NAME
+    index_prefix: str = Field(default="agency-index")  # LOAD_OPP_INDEX_PREFIX
+
+class LoadAgenciesToIndex(Task): #TBC How often does this need to be updated
+    def __init__(
+            self,
+            db_session: db.Session,
+            search_client: search.SearchClient,
+            config: LoadAgenciesToIndexConfig | None = None,
+    ) -> None:
+        super().__init__(db_session)
+        self.search_client = search_client
+        if config is None:
+            config = LoadAgenciesToIndexConfig()
+            self.config = config
+
+        current_timestamp = get_now_us_eastern_datetime().strftime("%Y-%m-%d_%H-%M-%S")
+        self.index_name = f"{self.config.index_prefix}-{current_timestamp}"
+
+    def run_task(self) -> None:
+        logger.info("Creating search index")
+        # create the index
+        self.search_client.create_index(
+            self.index_name,
+            shard_count=self.config.shard_count,
+            replica_count=self.config.replica_count,
+        )
+        # load the records
+        agencies = self.fetch_agencies()
+        self.load_agencies(agencies)
+
+        # handle aliasing of endpoints
+        self.search_client.swap_alias_index(
+            self.index_name,
+            self.config.alias_name,
+        )
+
+        # cleanup old indexes
+        self.search_client.cleanup_old_indices(self.config.index_prefix, [self.index_name])
+
+    def fetch_agencies(self) -> Sequence[Agency]:
+        """
+        Fetch all agencies.
+        """
+        return (
+            self.db_session.execute(
+            select(Agency)
+            .options(selectinload("*"))
+        ).scalars().all()
+        )
+
+    def load_agencies(self, agencies: Sequence[Agency]) -> None:
+        logger.info("Loading agencies...")
+        import pdb; pdb.set_trace()
+
+        schema = AgencyResponseSchema() # Create new one ? more data ?
+        agencies_json = [schema.dump(record) for record in agencies]
+
+        import pdb; pdb.set_trace()
+
+        self.search_client.bulk_upsert(
+            self.index_name,
+            agencies_json,
+            "agency_id",
+            )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- api/src/search/backend/load_search_data.py
+++ api/src/search/backend/load_search_data.py
@@ -4,6 +4,7 @@ import src.adapters.db as db
 import src.adapters.search as search
 from src.adapters.db import flask_db
 from src.adapters.search import flask_opensearch
+from src.search.backend.load_agencies_to_index import  LoadAgenciesToIndex
 from src.search.backend.load_opportunities_to_index import LoadOpportunitiesToIndex
 from src.search.backend.load_search_data_blueprint import load_search_data_blueprint
 from src.task.ecs_background_task import ecs_background_task
@@ -23,4 +24,7 @@ from src.task.ecs_background_task import ecs_background_task
 def load_opportunity_data(
     search_client: search.SearchClient, db_session: db.Session, full_refresh: bool
 ) -> None:
-    LoadOpportunitiesToIndex(db_session, search_client, full_refresh).run()
+    # LoadOpportunitiesToIndex(db_session, search_client, full_refresh).run()
+    LoadAgenciesToIndex(db_session, search_client).run()
+
+
