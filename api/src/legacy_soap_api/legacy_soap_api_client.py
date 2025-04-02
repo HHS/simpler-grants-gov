14f366abf4602dbf3dc371d2ad1b3c681cbe9ea4
Freddie Yebra <freddieyebra@navapbc.com>
2025-04-02 09:40:22 -0500

    Revert client refactor.

--- api/src/legacy_soap_api/legacy_soap_api_client.py
+++ api/src/legacy_soap_api/legacy_soap_api_client.py
@@ -1,30 +1,25 @@
 import os
 
 import requests
-from werkzeug.local import LocalProxy
 
 from src.legacy_soap_api.legacy_soap_api_config import LegacySoapAPIConfig
 from src.legacy_soap_api.legacy_soap_api_schemas import LegacySOAPResponse
 from src.legacy_soap_api.legacy_soap_api_utils import format_local_soap_response
 
 
-class LegacySOAPRequestHandler:
-    def __init__(self, request: LocalProxy, service_name: str, service_port_name: str) -> None:
+class LegacySOAPClient:
+    def __init__(self) -> None:
         self.config = LegacySoapAPIConfig()
-        self.service_name = service_name
-        self.service_name_port = service_port_name
-        self._req = request
 
-    def get_proxy_response(self) -> LegacySOAPResponse:
+    def proxy_request(
+        self, method: str, full_path: str, headers: dict | None = None, body: bytes | None = None
+    ) -> LegacySOAPResponse:
         """Proxy incoming SOAP requests to grants.gov
-
         This method handles proxying requests to grants.gov SOAP API and retrieving
         and returning the xml data as is from the existing SOAP API.
         """
-        url = os.path.join(self.config.grants_gov_uri, self._req.full_path.lstrip("/"))
-        response = requests.request(
-            self._req.method, url, data=self._req.data, headers=self._req.headers
-        )
+        url = os.path.join(self.config.grants_gov_uri, full_path.lstrip("/"))
+        response = requests.request(method, url, data=body, headers=headers)
         return LegacySOAPResponse(
             data=self._process_response_response_content(response.content),
             status_code=response.status_code,
--- api/src/legacy_soap_api/legacy_soap_api_routes.py
+++ api/src/legacy_soap_api/legacy_soap_api_routes.py
@@ -3,7 +3,7 @@ import logging
 from flask import request
 
 from src.legacy_soap_api.legacy_soap_api_blueprint import legacy_soap_api_blueprint
-from src.legacy_soap_api.legacy_soap_api_handler import LegacySOAPRequestHandler
+from src.legacy_soap_api.legacy_soap_api_client import LegacySOAPClient
 from src.logging.flask_logger import add_extra_data_to_current_request_logs
 
 logger = logging.getLogger(__name__)
@@ -20,6 +20,12 @@ def soap_api_operations_handler(service_name: str, service_port_name: str) -> tu
     add_extra_data_to_current_request_logs(
         {"service_name": service_name, "service_port_name": service_port_name}
     )
-    soap_request_handler = LegacySOAPRequestHandler(request, service_name, service_port_name)
+    soap_api_client = LegacySOAPClient()
+    proxy_response = soap_api_client.proxy_request(
+        method="POST",
+        full_path=request.full_path,
+        headers=dict(request.headers),
+        body=request.data,
+    )
     # This format will preserve the response data from SOAP API.
-    return soap_request_handler.get_proxy_response().to_flask_response()
+    return proxy_response.to_flask_response()
