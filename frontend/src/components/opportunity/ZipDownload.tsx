66c3c6f79144285a5aaa9f5cc8b07dfba36039cf
Matt Dragon <mattdragon@navapbc.com>
2025-04-04 17:39:34 -0400

    Add support for a button that sends the user all the attachments as a zip file

---
+++ frontend/src/components/opportunity/ZipDownload.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { Button } from "@trussworks/react-uswds";
+
+import { USWDSIcon } from "src/components/USWDSIcon";
+
+type ZipDownloadProps = {
+  opportunityId: string;
+};
+
+const downloadAttachmentsZip = (opportunityId: string) => {
+  const path = `/api/attachment-download/${opportunityId}`;
+  window.open(path, "_blank");
+};
+
+const ZipDownload = ({ opportunityId }: ZipDownloadProps) => {
+  const t = useTranslations("OpportunityListing.description");
+
+  return (
+    <Button
+      onClick={() => downloadAttachmentsZip(opportunityId)}
+      type="button"
+      id={`opportunity-document-button-${opportunityId}`}
+    >
+      <span>{t("zip_download")} </span>
+      <USWDSIcon name={"file_download"} className="usa-icon--size-4" />
+    </Button>
+  );
+};
+
+export default ZipDownload;
