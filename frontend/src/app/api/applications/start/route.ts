eb34f1e5ff10082a50a7fa1640649407a0ba1b29
Aaron Couch <aaroncouch@navapbc.com>
2025-04-04 14:04:05 -0400

    [Issue #4341] apply start page (#4437)

---
+++ frontend/src/app/api/applications/start/route.ts
@@ -0,0 +1,36 @@
+import { ApiRequestError, readError, UnauthorizedError } from "src/errors";
+import { handleStartApplication } from "src/services/fetch/fetchers/applicationFetcher";
+
+const COMPETITION_ID = "fd7f5921-9585-48a5-ab0f-e726f4d1ef94";
+
+export const POST = async () => {
+  try {
+    // TODO: add session support;
+    const session = { token: true };
+    if (!session || !session.token) {
+      throw new UnauthorizedError("No active session start application");
+    }
+
+    const response = await handleStartApplication(COMPETITION_ID);
+
+    if (!response || response.status_code !== 200) {
+      throw new ApiRequestError(
+        `Error starting application: ${response.message}`,
+        "APIRequestError",
+        response.status_code,
+      );
+    }
+    return Response.json({
+      message: "Application start success",
+      applicationId: response?.data?.application_id,
+    });
+  } catch (e) {
+    const { status, message } = readError(e as Error, 500);
+    return Response.json(
+      {
+        message: `Error attempting to start application: ${message}`,
+      },
+      { status },
+    );
+  }
+};
