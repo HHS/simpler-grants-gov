10407d479df1508b6387a439c629655a799510ee
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-04 10:04:15 -0400

    trying to fix but with missing metadata on unauthorized state, may be a next issue

--- frontend/src/app/[locale]/saved-grants/layout.tsx
+++ frontend/src/app/[locale]/saved-grants/layout.tsx
@@ -1,8 +1,22 @@
+import { Metadata } from "next";
 import { SAVED_GRANTS_CRUMBS } from "src/constants/breadcrumbs";
 import { LayoutProps } from "src/types/generalTypes";
+import { LocalizedPageProps } from "src/types/intl";
+
+import { getTranslations } from "next-intl/server";
 
 import Breadcrumbs from "src/components/Breadcrumbs";
 import { AuthenticationGate } from "src/components/user/AuthenticationGate";
+
+export async function generateMetadata({ params }: LocalizedPageProps) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
+  const meta: Metadata = {
+    title: t("SavedGrants.title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
 
 export default function SavedGrantsLayout({ children }: LayoutProps) {
   return (
