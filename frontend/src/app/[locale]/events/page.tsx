ac8e1a725167c62e881ecf24ec9794ccc8dd1c41
karinamzalez <karinamzalez@gmail.com>
2025-04-07 11:54:24 -0500

    events/page: add metadata

--- frontend/src/app/[locale]/events/page.tsx
+++ frontend/src/app/[locale]/events/page.tsx
@@ -1,9 +1,32 @@
+import { use } from "react";
+
+import { Metadata } from "next";
+import {
+  getTranslations,
+  setRequestLocale,
+} from "next-intl/server";
+import { LocalizedPageProps } from "src/types/intl";
+
 import EventsCoding from "./EventsCoding";
 import EventsDemo from "./EventsDemo";
 import EventsHero from "./EventsHero";
 import EventsUpcoming from "./EventsUpcoming";
 
-export default function Events(_p0: { params: Promise<{ locale: string }> }) {
+export async function generateMetadata({ params }: LocalizedPageProps) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
+  const meta: Metadata = {
+    title: t("Events.page_title"),
+    description: t("Index.meta_description"),
+  };
+  return meta;
+}
+
+
+export default function Events({ params }: LocalizedPageProps) {
+  const { locale } = use(params);
+  setRequestLocale(locale);
+  
   return (
     <>
       <EventsHero />
