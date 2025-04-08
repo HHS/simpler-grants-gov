69a36ee487379d5e1813d226bc5a02bd33d3500e
joshtonava <201659582+joshtonava@users.noreply.github.com>
2025-04-07 19:41:11 -0500

    Updated get competition route and tests. Added support for UUID or int in path params

--- api/src/api/opportunities_v1/opportunity_schemas.py
+++ api/src/api/opportunities_v1/opportunity_schemas.py
@@ -234,8 +234,8 @@ class OpportunityAssistanceListingV1Schema(Schema):
 
 
 class OpportunityV1Schema(Schema):
-    opportunity_id = fields.Integer(
-        metadata={"description": "The internal ID of the opportunity", "example": 12345},
+    opportunity_id = fields.UUID(
+        metadata={"description": "The internal ID of the opportunity"},
     )
 
     opportunity_number = fields.String(
