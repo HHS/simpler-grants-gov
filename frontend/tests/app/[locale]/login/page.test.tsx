bf96f0def4d21eec6f94bc099c86b437a3557f5f
Keith WIlliams <keithwilliams@navapbc.com>
2025-04-08 16:04:29 -0400

    feat: another format

--- frontend/tests/app/[locale]/login/page.test.tsx
+++ frontend/tests/app/[locale]/login/page.test.tsx
@@ -1,8 +1,6 @@
 import { render } from "@testing-library/react";
 import LoginPage from "src/app/[locale]/login/page";
 
-
-
 import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const mockPush = jest.fn();
