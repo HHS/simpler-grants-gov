c563b6abba5beafd7b931f9fb1344069e461add8
karinamzalez <karinamzalez@gmail.com>
2025-04-10 12:22:43 -0500

    update Header About section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
 import clsx from "clsx";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import GrantsLogo from "public/img/grants-logo.svg";
 import { useFeatureFlags } from "src/hooks/useFeatureFlags";
 import { useUser } from "src/services/auth/useUser";
 import { isCurrentPath } from "src/utils/generalUtils";
 
-import { useTranslations } from "next-intl";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useCallback, useEffect, useMemo, useState } from "react";
 import {
   GovBanner,
+  Header as USWDSHeader,
   Menu,
   NavDropDownButton,
   NavMenuButton,
   PrimaryNav,
   Title,
-  Header as USWDSHeader,
 } from "@trussworks/react-uswds";
 
 import { UserControl } from "./user/UserControl";
@@ -86,15 +92,9 @@ const NavLinks = ({
       {
         text: t("about"),
         children: [
-          {
-            text: t("vision"),
-            href: "/vision",
-          },
-          {
-            text: t("roadmap"),
-            href: "/roadmap",
-          },
-        ],
+          { text: t("vision"), href: "/vision" },
+          { text: t("roadmap"), href: "/roadmap" },
+        ]
       },
       { text: t("research"), href: "/research" },
       { text: t("subscribe"), href: "/subscribe" },
