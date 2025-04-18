b6a033c7640890adca5ee30fea86dd4bed9984f5
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-18 15:50:07 -0400

    unit test

---
+++ frontend/tests/components/search/SearchFilterAccordion/AnyOptionCheckbox.test.tsx
@@ -0,0 +1,90 @@
+import "@testing-library/jest-dom";
+
+import { waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { axe } from "jest-axe";
+import { render, screen } from "tests/react-utils";
+
+import React from "react";
+
+import { AnyOptionCheckbox } from "src/components/search/SearchFilterAccordion/AnyOptionCheckbox";
+
+const mockSetQueryParam = jest.fn();
+
+jest.mock("src/hooks/useSearchParamUpdater", () => ({
+  useSearchParamUpdater: () => ({
+    setQueryParam: mockSetQueryParam,
+  }),
+}));
+
+describe("AnyOptionCheckbox", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it("should not have basic accessibility issues", async () => {
+    const { container } = render(
+      <AnyOptionCheckbox title={"any"} checked={false} queryParamKey={"any"} />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it("calls the appropriate handlers when the checkbox is checked", async () => {
+    render(
+      <AnyOptionCheckbox title={"any"} checked={false} queryParamKey={"any"} />,
+    );
+
+    // Simulate user clicking the checkbox
+    const checkbox = await screen.findByRole("checkbox");
+    userEvent.click(checkbox);
+
+    // Wait for the updateCheckedOption function to be called with the checkbox being checked
+    await waitFor(() => {
+      expect(mockSetQueryParam).toHaveBeenCalledWith("any", "");
+    });
+  });
+
+  it("calls the appropriate handlers when the checkbox is checked (default value edition)", async () => {
+    render(
+      <AnyOptionCheckbox
+        title={"any"}
+        checked={false}
+        queryParamKey={"any"}
+        defaultEmptySelection={new Set(["default"])}
+      />,
+    );
+
+    // Simulate user clicking the checkbox
+    const checkbox = await screen.findByRole("checkbox");
+    userEvent.click(checkbox);
+
+    // Wait for the updateCheckedOption function to be called with the checkbox being checked
+    await waitFor(() => {
+      expect(mockSetQueryParam).toHaveBeenCalledWith("any", "default");
+    });
+  });
+
+  it("does nothing when clicked if the checkbox is already checked", async () => {
+    render(
+      <AnyOptionCheckbox title={"any"} checked={true} queryParamKey={"any"} />,
+    );
+
+    // Simulate user clicking the checkbox
+    const checkbox = await screen.findByRole("checkbox");
+    userEvent.click(checkbox);
+
+    expect(mockSetQueryParam).toHaveBeenCalledTimes(0);
+  });
+
+  it("displays the correct label", () => {
+    render(
+      <AnyOptionCheckbox
+        title={"For the title"}
+        checked={false}
+        queryParamKey={"any"}
+      />,
+    );
+    const label = screen.getByText("Any for the title");
+    expect(label).toBeInTheDocument();
+  });
+});
