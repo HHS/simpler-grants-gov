d805cdd2eca99cb2c17052f9099a8d6a4b4bc483
Kai Siren <kaisiren@navapbc.com>
2025-04-18 21:17:05 -0700

    iter

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -16,5 +16,5 @@ locals {
 
   github_actions_role_name                = "${local.project_name}-github-actions"
   aws_services_security_group_name_prefix = "aws-service-vpc-endpoints"
-  latest_fluent_bit_commit                = "a23953c11a8ab0213810c03f3ac5dee07e599528"
+  latest_fluent_bit_commit                = "0f8a37d83fa510fab30108366ad9964385024785"
 }
