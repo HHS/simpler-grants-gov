94930c872dff437d0b52f15eb91c320d9259196c
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-22 13:57:56 -0400

    route change hook test

---
+++ frontend/tests/hooks/useRouteChange.test.ts
@@ -0,0 +1,38 @@
+import { renderHook } from "@testing-library/react";
+import { useRouteChange } from "src/hooks/useRouteChange";
+
+let params = "hi";
+let pathname = "hello";
+
+jest.mock("next/navigation", () => ({
+  useSearchParams: () => params,
+  usePathname: () => pathname,
+}));
+
+describe("useRouteChange", () => {
+  it("calls the passed handler on load", () => {
+    const mockHandler = jest.fn();
+    const { rerender } = renderHook(() => useRouteChange(mockHandler));
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+  });
+  it("calls the passed handler whenenver path changes", () => {
+    const mockHandler = jest.fn();
+    const { rerender } = renderHook(() => useRouteChange(mockHandler));
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+
+    pathname += "hello";
+    rerender();
+
+    expect(mockHandler).toHaveBeenCalledTimes(2);
+  });
+  it("calls the passed handler whenenver query params change", () => {
+    const mockHandler = jest.fn();
+    const { rerender } = renderHook(() => useRouteChange(mockHandler));
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+
+    params += "hi";
+    rerender();
+
+    expect(mockHandler).toHaveBeenCalledTimes(2);
+  });
+});
