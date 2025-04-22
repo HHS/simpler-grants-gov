74d99222d99799ff2992f45555ee0f54c151e521
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-22 15:28:27 -0400

    wip client user fetch tests

---
+++ frontend/tests/services/fetch/fetchers/clientUserFetcher.test.ts
@@ -0,0 +1,5 @@
+describe("debouncedClientUserFetcher", () => {
+  it("calls fetch and resturns data on success as expected", () => {});
+  it("throws on non 200", () => {});
+  it("debounces on multiple calls within the window", () => {});
+});
