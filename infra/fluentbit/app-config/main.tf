8f491b3493df1f6cd57db9251b4112327e07ef19
Kai Siren <kaisiren@navapbc.com>
2025-04-22 22:17:34 -0700

    format

--- infra/fluentbit/app-config/main.tf
+++ infra/fluentbit/app-config/main.tf
@@ -1,11 +1,11 @@
 locals {
-  app_name = regex("/infra/([^/]+)/app-config$", abspath(path.module))[0]
+  app_name     = regex("/infra/([^/]+)/app-config$", abspath(path.module))[0]
   environments = []
   project_name = module.project_config.project_name
 
-  environment_configs = {}
+  environment_configs          = {}
   account_names_by_environment = {}
-  shared_network_name = "dev"
+  shared_network_name          = "dev"
 }
 
 module "project_config" {
