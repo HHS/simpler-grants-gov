ef2597e87e801de1c3f1c2a2561f86e951a094f3
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-23 10:06:00 -0400

    debounced fetcher test

--- frontend/tests/services/fetch/fetchers/clientUserFetcher.test.ts
+++ frontend/tests/services/fetch/fetchers/clientUserFetcher.test.ts
@@ -1,5 +1,46 @@
+import { waitFor } from "@testing-library/dom";
+import { debouncedUserFetcher } from "src/services/fetch/fetchers/clientUserFetcher";
+import { wrapForExpectedError } from "src/utils/testing/commonTestUtils";
+
+const jsonMock = jest.fn();
+
+const mockResponse = {
+  status: 200,
+  ok: true,
+  json: () => jsonMock(),
+} as unknown as Response;
+
+const fetchMock = jest.fn();
+
 describe("debouncedClientUserFetcher", () => {
-  it("calls fetch and resturns data on success as expected", () => {});
-  it("throws on non 200", () => {});
-  it("debounces on multiple calls within the window", () => {});
+  let originalFetch: typeof global.fetch;
+  beforeEach(() => {
+    fetchMock.mockImplementation(() => Promise.resolve(mockResponse));
+    originalFetch = global.fetch;
+    global.fetch = fetchMock;
+  });
+  afterEach(() => {
+    global.fetch = originalFetch;
+    jest.resetAllMocks();
+  });
+  it("calls fetch and returns data on success as expected", async () => {
+    jsonMock.mockResolvedValue({ a: "thing " });
+    const response = await debouncedUserFetcher();
+    expect(response).toMatchObject({ a: "thing " });
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    expect(fetchMock).toHaveBeenCalledWith("/api/auth/session", {
+      cache: "no-store",
+    });
+  });
+  it("throws on non 200", async () => {
+    fetchMock.mockResolvedValue({ status: 500, ok: false });
+    const error = await wrapForExpectedError(() => debouncedUserFetcher());
+    expect(error).toBeInstanceOf(Error);
+  });
+  it("debounces on multiple calls within the window", async () => {
+    jsonMock.mockResolvedValue({ a: "thing " });
+    await debouncedUserFetcher();
+    await debouncedUserFetcher();
+    waitFor(() => expect(fetchMock).toHaveBeenCalledTimes(1));
+  });
 });
