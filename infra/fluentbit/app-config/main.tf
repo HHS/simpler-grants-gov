8bbe8995fd229310a8d7ff0e91d8607b55733481
Kai <coilysiren@gmail.com>
2025-04-30 08:47:03 -0700

    [Issue #4663] Builds an entire custom fluentbit container  (#4792)
    ## Summary
    
    Relates to #4663
    
    ## Motivation
    
    This is a well known way to get logs pushing to multiple locations! In
    particular we need logs in New Relic for observability, and Cloudwatch
    for auditing.
    
    ## Testing
    
    I need whoever approves this to actually run through the testing
    directions! I don't want to be the only person who can run this.

---
+++ infra/fluentbit/app-config/main.tf
@@ -0,0 +1,13 @@
+locals {
+  app_name     = regex("/infra/([^/]+)/app-config$", abspath(path.module))[0]
+  environments = []
+  project_name = module.project_config.project_name
+
+  environment_configs          = {}
+  account_names_by_environment = {}
+  shared_network_name          = "dev"
+}
+
+module "project_config" {
+  source = "../../project-config"
+}
