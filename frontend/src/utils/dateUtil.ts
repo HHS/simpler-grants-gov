93c40af2633d049f2562bb3110e0c42fe93382fe
Doug Schrashun <dougschrashun@navapbc.com>
2025-04-30 20:37:39 -0400

    date util tests

--- frontend/src/utils/dateUtil.ts
+++ frontend/src/utils/dateUtil.ts
@@ -3,6 +3,7 @@ import advancedFormat from "dayjs/plugin/advancedFormat";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import timezone from "dayjs/plugin/timezone";
+import { clientTokenRefreshInterval } from "src/constants/auth";
 
 dayjs.extend(advancedFormat);
 dayjs.extend(customParseFormat);
@@ -28,4 +29,4 @@ export const isExpired = (expiration?: number) =>
 export const isExpiring = (expiration?: number) =>
   !isExpired(expiration) &&
   !!expiration &&
-  expiration < Date.now() + 10 * 60 * 1000;
+  expiration < Date.now() + clientTokenRefreshInterval;
--- frontend/tests/utils/dateUtil.test.ts
+++ frontend/tests/utils/dateUtil.test.ts
@@ -1,6 +1,10 @@
 import { identity } from "lodash";
 import { formatDate, isExpired, isExpiring } from "src/utils/dateUtil";
 
+jest.mock("src/constants/auth", () => ({
+  clientTokenRefreshInterval: 1000,
+}));
+
 describe("formatDate", () => {
   beforeEach(() => {
     jest.spyOn(console, "warn").mockImplementation(identity);
@@ -31,14 +35,14 @@ describe("isExpiring", () => {
   it("returns false if no expiration", () => {
     expect(isExpiring()).toEqual(false);
   });
-  it("returns false if expiration is more than 10 minutes in the future", () => {
-    expect(isExpiring(Date.now() + 11 * 60 * 1000)).toEqual(false);
+  it("returns false if expiration is more than the refresh interval in the future", () => {
+    expect(isExpiring(Date.now() + 1001)).toEqual(false);
   });
   it("returns false if expiration is in the past", () => {
-    expect(isExpiring(Date.now() - 11 * 60 * 1000)).toEqual(false);
+    expect(isExpiring(Date.now() - 1)).toEqual(false);
   });
-  it("returns true if expiration is in the next 10 minutes", () => {
-    expect(isExpiring(Date.now() + 9 * 60 * 1000)).toEqual(true);
+  it("returns true if expiration falls within the refresh interval", () => {
+    expect(isExpiring(Date.now() + 500)).toEqual(true);
   });
 });
 
