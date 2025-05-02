c0c184bb0c03fd1b5165745a484bb804b1f5e36c
Freddie Yebra <freddieyebra@navapbc.com>
2025-05-02 17:05:59 -0500

    Add test cases.

--- api/tests/src/util/test_xml_utils.py
+++ api/tests/src/util/test_xml_utils.py
@@ -10,12 +10,13 @@ def test_invalid_xml_raises():
 
 def test_basic_xml_to_dict():
     given = """
+    <!-- Random comment: -->
     <grant>
         <title>val</title>
     </grant>
     """
     result_without_namespace = xml_to_dict(given)
-    result_with_namespace = xml_to_dict(give, preserve_namespace=True)
+    result_with_namespace = xml_to_dict(given, preserve_namespace=True)
     expected = {"grant": {"title": "val"}}
     assert result_without_namespace == result_with_namespace == expected
 
@@ -82,3 +83,59 @@ def test_non_leaf_element_attribute_does_not_include_attributes():
         }
     }
     assert result_with_namespaces == expected_with_namespaces
+
+
+def test_soap_envelope():
+    given = """
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:app="http://apply.grants.gov/services/ApplicantWebServices-V2.0" xmlns:gran="http://apply.grants.gov/system/GrantsCommonElements-V1.0">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <app:GetApplicationListRequest>
+                <gran:ApplicationFilter>
+                    <gran:Filter>GrantsGovTrackingNumber</gran:Filter>
+                    <gran:FilterValue>1</gran:FilterValue>
+                </gran:ApplicationFilter>
+                <gran:ApplicationFilter>
+                    <gran:Filter>GrantsGovTrackingNumber</gran:Filter>
+                    <gran:FilterValue>2</gran:FilterValue>
+                </gran:ApplicationFilter>
+            </app:GetApplicationListRequest>
+        </soapenv:Body>
+    </soapenv:Envelope>
+    """
+    result_without_namespaces = xml_to_dict(given)
+    expected_without_namespaces = {
+        "Envelope": {
+            "Header": None,
+            "Body": {
+                "GetApplicationListRequest": {
+                    "ApplicationFilter": [
+                        {"Filter": "GrantsGovTrackingNumber", "FilterValue": "1"},
+                        {"Filter": "GrantsGovTrackingNumber", "FilterValue": "2"},
+                    ]
+                }
+            },
+        }
+    }
+    assert result_without_namespaces == expected_without_namespaces
+    result_with_namespace = xml_to_dict(given, preserve_namespace=True)
+    expected_with_namespace = {
+        "{http://schemas.xmlsoap.org/soap/envelope/}Envelope": {
+            "{http://schemas.xmlsoap.org/soap/envelope/}Header": None,
+            "{http://schemas.xmlsoap.org/soap/envelope/}Body": {
+                "{http://apply.grants.gov/services/ApplicantWebServices-V2.0}GetApplicationListRequest": {
+                    "{http://apply.grants.gov/system/GrantsCommonElements-V1.0}ApplicationFilter": [
+                        {
+                            "{http://apply.grants.gov/system/GrantsCommonElements-V1.0}Filter": "GrantsGovTrackingNumber",
+                            "{http://apply.grants.gov/system/GrantsCommonElements-V1.0}FilterValue": "1",
+                        },
+                        {
+                            "{http://apply.grants.gov/system/GrantsCommonElements-V1.0}Filter": "GrantsGovTrackingNumber",
+                            "{http://apply.grants.gov/system/GrantsCommonElements-V1.0}FilterValue": "2",
+                        },
+                    ]
+                }
+            },
+        }
+    }
+    assert result_with_namespace == expected_with_namespace
