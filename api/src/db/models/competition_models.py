755eee2802739f70ba52b22de505eb0b9a7bf1a2
Michael Huneke <mike.huneke@focusconsulting.io>
2025-05-02 13:32:46 -0400

    Format

--- api/src/db/models/competition_models.py
+++ api/src/db/models/competition_models.py
@@ -7,14 +7,13 @@ from sqlalchemy.ext.associationproxy import AssociationProxy, association_proxy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.adapters.db.type_decorators.postgres_type_decorators import LookupColumn
-from src.constants.lookup_constants import CompetitionOpenToApplicant, FormFamily
+from src.constants.lookup_constants import ApplicationStatus, CompetitionOpenToApplicant, FormFamily
 from src.db.models.base import ApiSchemaTable, TimestampMixin
 from src.db.models.lookup_models import (
     LkApplicationStatus,
     LkCompetitionOpenToApplicant,
     LkFormFamily,
 )
-from src.constants.lookup_constants import ApplicationStatus
 from src.db.models.opportunity_models import Opportunity, OpportunityAssistanceListing
 
 
