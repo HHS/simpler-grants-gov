d2b15b759d4339aa1d952ab68b6e672170170b07
Freddie Yebra <freddieyebra@navapbc.com>
2025-05-05 13:36:32 -0500

    Add cert forwarding.

--- api/src/legacy_soap_api/legacy_soap_api_client.py
+++ api/src/legacy_soap_api/legacy_soap_api_client.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import requests
@@ -5,6 +6,10 @@ import requests
 from src.legacy_soap_api.legacy_soap_api_config import LegacySoapAPIConfig
 from src.legacy_soap_api.legacy_soap_api_schemas import LegacySOAPResponse
 from src.legacy_soap_api.legacy_soap_api_utils import format_local_soap_response
+
+logger = logging.getLogger(__name__)
+
+MTLS_CERT_HEADER_KEY = "X-Amzn-Mtls-Clientcert"
 
 
 class LegacySOAPClient:
@@ -18,8 +23,13 @@ class LegacySOAPClient:
         This method handles proxying requests to grants.gov SOAP API and retrieving
         and returning the xml data as is from the existing SOAP API.
         """
+        headers = headers if headers else {}
+        logger.info("soap_header_keys", extra={"request_header_keys": headers.keys()})
         url = os.path.join(self.config.grants_gov_uri, full_path.lstrip("/"))
-        response = requests.request(method, url, data=body, headers=headers)
+        cert = headers.get(MTLS_CERT_HEADER_KEY, None)
+        if cert:
+            logger.info("retrieved and forwarding mtls cert")
+        response = requests.request(method, url, data=body, headers=headers, cert=cert)
         return LegacySOAPResponse(
             data=self._process_response_response_content(response.content),
             status_code=response.status_code,
