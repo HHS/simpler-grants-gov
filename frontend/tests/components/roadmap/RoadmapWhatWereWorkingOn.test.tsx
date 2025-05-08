ae8345af050a5c13682b2c67f12176daad212595
chei <chei@chei.local>
2025-05-08 19:23:25 -0400

    fix CI code format failure

--- frontend/tests/components/roadmap/RoadmapWhatWereWorkingOn.test.tsx
+++ frontend/tests/components/roadmap/RoadmapWhatWereWorkingOn.test.tsx
@@ -7,7 +7,7 @@ describe("RoadmapWhatWereWorkingOn Content", () => {
     render(<RoadmapWhatWereWorkingOn />);
     const RoadmapWhatWereWorkingOnH2 = screen.getByRole("heading", {
       level: 2,
-      name: /What we’re working on?/i,
+      name: /What we're working on?/i,
     });
 
     expect(RoadmapWhatWereWorkingOnH2).toBeInTheDocument();
