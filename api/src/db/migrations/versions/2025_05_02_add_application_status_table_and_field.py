efb0047ee5054adcf3187e0bee2f37ea38740d97
joshtonava <201659582+joshtonava@users.noreply.github.com>
2025-05-15 15:12:24 -0500

    Fixed migration heads

--- api/src/db/migrations/versions/2025_05_02_add_application_status_table_and_field.py
+++ api/src/db/migrations/versions/2025_05_02_add_application_status_table_and_field.py
@@ -1,7 +1,7 @@
 """Add application status table and field
 
 Revision ID: 462b5b67c0ea
-Revises: 3b0705202c54
+Revises: 7962ce400424
 Create Date: 2025-05-02 17:17:28.799500
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "462b5b67c0ea"
-down_revision = "3b0705202c54"
+down_revision = "7962ce400424"
 branch_labels = None
 depends_on = None
 
