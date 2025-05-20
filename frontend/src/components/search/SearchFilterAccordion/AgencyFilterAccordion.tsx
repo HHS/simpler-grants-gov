6028abfd76061ed66f10b97b90d5f07c675b9e97
Doug Schrashun <dougschrashun@navapbc.com>
2025-05-20 11:56:08 -0400

    more wip, lots or reorg to try and get suspense and data fetching to work, not there yet

--- frontend/src/components/search/SearchFilterAccordion/AgencyFilterAccordion.tsx
+++ frontend/src/components/search/SearchFilterAccordion/AgencyFilterAccordion.tsx
@@ -1,58 +1,70 @@
+"use client";
+
 import { FilterOption } from "src/types/search/searchFilterTypes";
 import { SearchAPIResponse } from "src/types/search/searchRequestTypes";
 
 import { useTranslations } from "next-intl";
+import { Suspense, useState } from "react";
+import { TextInput } from "@trussworks/react-uswds";
 
-import { CheckboxFilter } from "src/components/search/Filters/CheckboxFilter";
-import SearchFilterAccordion from "src/components/search/SearchFilterAccordion/SearchFilterAccordion";
+import { BasicSearchFilterAccordion } from "src/components/search/SearchFilterAccordion/SearchFilterAccordion";
+import { AgencyFilterContent } from "../Filters/AgencyFilterContent";
+import { CheckboxFilter } from "../Filters/CheckboxFilter";
 
-export async function AgencyFilterAccordion({
+const searchForAgencies = (searchTerm: string) => Promise.resolve([]);
+
+export function AgencyFilterAccordion({
   query,
-  agencyOptionsPromise,
+  searchResultsPromise,
 }: {
   query: Set<string>;
-  agencyOptionsPromise: Promise<[FilterOption[], SearchAPIResponse]>;
+  searchResultsPromise: Promise<SearchAPIResponse>;
 }) {
   const t = useTranslations("Search");
+  const [agenciesPromise, setAgenciesPromise] = useState<
+    Promise<FilterOption[]>
+  >(Promise.resolve([]));
 
-  let agencies: FilterOption[] = [];
-  let facetCounts: { [key: string]: number } = {};
-  try {
-    let searchResults: SearchAPIResponse;
-    [agencies, searchResults] = await agencyOptionsPromise;
-    facetCounts = searchResults.facet_counts.agency;
-  } catch (e) {
-    // Come back to this to show the user an error
-    console.error("Unable to fetch agencies for filter list", e);
-  }
+  const updateAgencyListOnSearch = (searchTerm: string) => {
+    console.log("$$$ search keyword", searchTerm);
+    setAgenciesPromise(searchForAgencies(searchTerm));
+  };
+
   return (
-    <Suspense
-      fallback={
-        <Accordion
-          bordered={true}
-          items={[
-            {
-              title: t("accordion.titles.agency"),
-              content: [],
-              expanded: false,
-              id: "opportunity-filter-agency-disabled",
-              headingLevel: "h2",
-            },
-          ]}
-          multiselectable={true}
-          className="margin-top-4"
-        />
-      }
+    <BasicSearchFilterAccordion
+      query={query}
+      queryParamKey={"agency"}
+      title={t("accordion.titles.agency")}
     >
-      <SearchFilterAccordion
-        filterOptions={agencies}
-        query={query}
-        queryParamKey={"agency"}
-        title={t("accordion.titles.agency")}
-        wrapForScroll={true}
-        facetCounts={facetCounts}
-      />
-    </Suspense>
+      <>
+        <TextInput
+          type="text"
+          name="LastName"
+          id="LastName"
+          onChange={(e) => updateAgencyListOnSearch(e.target.value)}
+        />
+        <Suspense
+          key=""
+          fallback={
+            <CheckboxFilter
+              filterOptions={[]}
+              query={query}
+              queryParamKey={"agency"}
+              title={t("accordion.titles.agency")}
+              wrapForScroll={true}
+              facetCounts={{}}
+            />
+          }
+        >
+          <AgencyFilterContent
+            query={query}
+            title={t("accordion.titles.agency")}
+            searchResultsPromise={searchResultsPromise}
+            agencySearchPromise={agenciesPromise}
+          />
+        </Suspense>
+      </>
+    </BasicSearchFilterAccordion>
   );
 }
 
