78c98ab5f843c7135f0b6469c6cde968c8d7e6f1
Doug Schrashun <dougschrashun@navapbc.com>
2025-05-21 13:34:21 -0400

    add 'no results' view

---
+++ frontend/src/components/search/Filters/FilterSearchNoResults.tsx
@@ -0,0 +1,23 @@
+import { useTranslations } from "next-intl";
+
+export function FilterSearchNoResults() {
+  const t = useTranslations("Search.filters.searchNoResults");
+  const suggestions = [
+    t("suggestions.0"),
+    t("suggestions.1"),
+    t("suggestions.2"),
+  ];
+  return (
+    <div>
+      <div className="text-bold">{t("title")}</div>
+      <div>
+        {t("heading")}
+        <ul>
+          {suggestions.map((suggestion) => (
+            <li>{suggestion}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
