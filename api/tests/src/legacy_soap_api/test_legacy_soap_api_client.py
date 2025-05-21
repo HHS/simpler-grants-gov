5ee811423b28fae7b491cac354231db885d508c0
Freddie Yebra <freddieyebra@navapbc.com>
2025-05-21 08:55:08 -0500

    Update client test.

--- api/tests/src/legacy_soap_api/test_legacy_soap_api_client.py
+++ api/tests/src/legacy_soap_api/test_legacy_soap_api_client.py
@@ -1,12 +1,20 @@
 import pytest
 
 from src.legacy_soap_api.legacy_soap_api_client import BaseSOAPClient
+from src.legacy_soap_api.legacy_soap_api_schemas import SOAPRequest
 
 
 class TestSOAPClient:
     @pytest.fixture(scope="class")
     def base_soap_client(self):
-        return BaseSOAPClient()
+        return BaseSOAPClient(
+            SOAPRequest(
+                method="POST",
+                headers={},
+                data=b"",
+                full_path="/",
+            )
+        )
 
     def test_can_instantiate(self, base_soap_client) -> None:
         assert isinstance(base_soap_client, BaseSOAPClient)
