3a2294c2304e0aeffec30b4f263a02a79aec3474
Freddie Yebra <freddieyebra@navapbc.com>
2025-05-23 08:44:29 -0500

    Update spacing.

--- frontend/src/components/roadmap/sections/RoadmapMilestones.tsx
+++ frontend/src/components/roadmap/sections/RoadmapMilestones.tsx
@@ -44,7 +44,10 @@ export default function RoadmapMilestones() {
                     Object.keys(archivedRoadmapItems).map((key) => {
                       const title = t(`archivedRoadmapItems.${key}.title`);
                       return (
-                        <div key={`roadmap-archived-item-${key}-section`}>
+                        <div
+                          key={`roadmap-archived-item-${key}-section`}
+                          className={"margin-bottom-3"}
+                        >
                           <h4 key={`roadmap-milestones-late2024-${key}`}>
                             {title}
                           </h4>
