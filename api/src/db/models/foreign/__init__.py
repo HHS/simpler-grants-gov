a3e7b3339c6235bace172883eea17740160b3137
Mike H <mike.huneke@focusconsulting.io>
2025-05-23 12:08:14 -0400

    [Issue #5033] Setup a staging & foreign table for the TINSTRUCTIONS table (#5151)
    ## Summary
    
    Fixes #5033
    
    ## Changes proposed
    
    Add db migration, mixin, and models for `tinstructions`
    
    ## Context for reviewers
    
    This follows established pattern for other transfer/staging tables.
    
    ## Validation steps
    
    See migration / compare with exact columns from ticket.

--- api/src/db/models/foreign/__init__.py
+++ api/src/db/models/foreign/__init__.py
@@ -2,7 +2,17 @@
 # SQLAlchemy models for foreign tables.
 #
 
-from . import attachment, competition, forecast, foreignbase, opportunity, synopsis, tgroups, user
+from . import (
+    attachment,
+    competition,
+    forecast,
+    foreignbase,
+    instructions,
+    opportunity,
+    synopsis,
+    tgroups,
+    user,
+)
 
 metadata = foreignbase.metadata
 
@@ -15,4 +25,5 @@ __all__ = [
     "attachment",
     "user",
     "competition",
+    "instructions",
 ]
