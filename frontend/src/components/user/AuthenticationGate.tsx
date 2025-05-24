a7c1a710ad9980f22cbd799164d819aa6e9a2e9d
GlennTatum <glennetatum@gmail.com>
2025-05-23 20:03:58 -0400

    lint rules

--- frontend/src/components/user/AuthenticationGate.tsx
+++ frontend/src/components/user/AuthenticationGate.tsx
@@ -1,9 +1,6 @@
 import { getSession } from "src/services/auth/session";
-
-import { getTranslations } from "next-intl/server";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-import { Alert, GridContainer } from "@trussworks/react-uswds";
 
 export async function AuthenticationGate({
   children,
@@ -11,7 +8,6 @@ export async function AuthenticationGate({
   children: ReactNode;
 }) {
   const session = await getSession();
-  const t = await getTranslations("Errors");
   if (!session?.token) {
     return redirect("/");
   }
