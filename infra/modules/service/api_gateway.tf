dd480fbb8fe42a17373725135bad92ed481c18da
Kai Siren <kaisiren@navapbc.com>
2025-06-02 14:06:49 -0700

    actually remove stuff

--- infra/modules/service/api_gateway.tf
+++ infra/modules/service/api_gateway.tf
@@ -7,33 +7,6 @@ resource "aws_api_gateway_rest_api" "api" {
   }
 }
 
-# resource "aws_api_gateway_deployment" "api" {
-#   count       = var.enable_api_gateway ? 1 : 0
-#   rest_api_id = aws_api_gateway_rest_api.api[0].id
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# resource "aws_api_gateway_stage" "api" {
-#   count         = var.enable_api_gateway ? 1 : 0
-#   deployment_id = aws_api_gateway_deployment.api[0].id
-#   rest_api_id   = aws_api_gateway_rest_api.api[0].id
-#   stage_name    = var.environment_name
-# }
-
-# resource "aws_api_gateway_method_settings" "api" {
-#   count       = var.enable_api_gateway ? 1 : 0
-#   rest_api_id = aws_api_gateway_rest_api.api[0].id
-#   stage_name  = aws_api_gateway_stage.api[0].stage_name
-#   method_path = "*/*"
-
-#   settings {
-#     metrics_enabled = true
-#   }
-# }
-
 resource "aws_api_gateway_domain_name" "api" {
   count                    = var.enable_api_gateway ? 1 : 0
   domain_name              = var.domain_name
@@ -43,10 +16,3 @@ resource "aws_api_gateway_domain_name" "api" {
     types = ["REGIONAL"]
   }
 }
-
-# resource "aws_api_gateway_base_path_mapping" "api" {
-#   count       = var.enable_api_gateway ? 1 : 0
-#   api_id      = aws_api_gateway_rest_api.api[0].id
-#   domain_name = var.domain_name
-#   stage_name  = aws_api_gateway_stage.api[0].stage_name
-# }
