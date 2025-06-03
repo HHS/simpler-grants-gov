cb2e38c22c052a69df4eb62b081c6799e372cb48
My Duong <myduong@navapbc.com>
2025-06-03 16:14:06 -0500

    chore: reorganize the properties

--- frontend/src/components/application/mapOpportunity.ts
+++ frontend/src/components/application/mapOpportunity.ts
@@ -2,8 +2,8 @@ import { BaseOpportunity, OpportunityOverview } from "src/types/opportunity/oppo
 
 export const mapOpportunityOverview = (opportunity: BaseOpportunity): OpportunityOverview => {
     const {
-        agency_name,
         agency_code,
+        agency_name,
         opportunity_assistance_listings,
         opportunity_id,
         opportunity_number,
@@ -13,27 +13,27 @@ export const mapOpportunityOverview = (opportunity: BaseOpportunity): Opportunit
         agency_contact_description,
         award_ceiling,
         award_floor,
+        close_date,
         estimated_total_program_funding,
         expected_number_of_awards,
         is_cost_sharing,
         post_date,
-        close_date
     } = opportunity.summary
 
     return {
-        opportunity_title,
-        opportunity_id,
-        opportunity_number,
-        post_date,
-        close_date,
-        agency_name,
-        opportunity_assistance_listings,
-        is_cost_sharing,
         agency_code,
         agency_contact_description,
+        agency_name,
+        award_ceiling,
+        award_floor,
+        close_date,
         estimated_total_program_funding,
         expected_number_of_awards,
-        award_ceiling,
-        award_floor
+        is_cost_sharing,
+        opportunity_id,
+        opportunity_title,
+        opportunity_number,
+        opportunity_assistance_listings,
+        post_date,
     };
 }
