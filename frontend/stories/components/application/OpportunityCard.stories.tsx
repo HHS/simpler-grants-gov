d59e3570e2d1566d2bd6c3913a3e462a421e55e8
My Duong <myduong@navapbc.com>
2025-06-04 13:22:22 -0500

    chore: remove extra map function

--- frontend/stories/components/application/OpportunityCard.stories.tsx
+++ frontend/stories/components/application/OpportunityCard.stories.tsx
@@ -1,22 +1,16 @@
 import { Meta } from "@storybook/react";
 import {
-  BaseOpportunity,
   OpportunityOverview,
 } from "src/types/opportunity/opportunityResponseTypes";
 
-import { mapOpportunityOverview } from "src/components/application/mapOpportunity";
 import { OpportunityCard } from "src/components/application/OpportunityCard";
 import opportunityMock from "./opportunity.mock.json";
-
-const mockData = mapOpportunityOverview(
-  opportunityMock as unknown as BaseOpportunity,
-);
 
 const meta: Meta<OpportunityOverview> = {
   title: "Components/Application/OpportunityCard",
   component: OpportunityCard,
   args: {
-    ...mockData,
+    ...opportunityMock,
   },
 };
 export default meta;
