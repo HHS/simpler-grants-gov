fa40f260d677c9b42ed74cc140a75ee1d2978073
Michael Huneke <mike.huneke@focusconsulting.io>
2025-06-04 09:50:30 -0400

    Remove mocks, update to CDN URL download_path

--- api/tests/src/api/extracts_v1/test_extract_schema.py
+++ api/tests/src/api/extracts_v1/test_extract_schema.py
@@ -17,7 +17,7 @@ def sample_extract_metadata():
     return ExtractMetadata(
         extract_type="opportunities_csv",
         file_name="test_extract.csv",
-        file_path="/test/path/test_extract.csv",
+        file_path="s3://local-mock-public-bucket/test/path/test_extract.csv",
         file_size_bytes=2048,
     )
 
@@ -55,20 +55,14 @@ def test_request_schema_validation():
 def test_response_schema_single(sample_extract_metadata):
     schema = ExtractMetadataResponseSchema()
 
-    # Mock the download_path property
-    with mock.patch(
-        "src.db.models.extract_models.ExtractMetadata.download_path",
-        new_callable=mock.PropertyMock,
-        return_value="http://www.example.com",
-    ):
-        extract_metadata = schema.dump(sample_extract_metadata)
+    extract_metadata = schema.dump(sample_extract_metadata)
 
-        assert extract_metadata["download_path"] == "http://www.example.com"
-        assert (
-            extract_metadata["extract_metadata_id"] == sample_extract_metadata.extract_metadata_id
-        )
-        assert extract_metadata["extract_type"] == "opportunities_csv"
-        assert extract_metadata["file_size_bytes"] == 2048
+    assert extract_metadata["download_path"] == "http://localhost:4566/local-mock-public-bucket/test/path/test_extract.csv"
+    assert (
+        extract_metadata["extract_metadata_id"] == sample_extract_metadata.extract_metadata_id
+    )
+    assert extract_metadata["extract_type"] == "opportunities_csv"
+    assert extract_metadata["file_size_bytes"] == 2048
 
 
 def test_response_schema_list(sample_extract_metadata):
@@ -78,31 +72,25 @@ def test_response_schema_list(sample_extract_metadata):
     other_extract_metadata = ExtractMetadata(
         extract_type="opportunities_json",
         file_name="test_extract2.xml",
-        file_path="/test/path/test_extract2.xml",
+        file_path="s3://local-mock-public-bucket/test/path/test_extract2.xml",
         file_size_bytes=1024,
     )
 
     metadata_list = {"data": [sample_extract_metadata, other_extract_metadata]}
 
-    # Mock the download_path property for both objects
-    with mock.patch(
-        "src.db.models.extract_models.ExtractMetadata.download_path",
-        new_callable=mock.PropertyMock,
-        return_value="http://www.example.com",
-    ):
-        result = schema.dump(metadata_list)
+    result = schema.dump(metadata_list)
 
-        assert len(result["data"]) == 2
-        assert (
-            result["data"][0]["extract_metadata_id"] == sample_extract_metadata.extract_metadata_id
-        )
-        assert result["data"][0]["extract_type"] == "opportunities_csv"
-        assert result["data"][0]["download_path"] == "http://www.example.com"
-        assert (
-            result["data"][1]["extract_metadata_id"] == other_extract_metadata.extract_metadata_id
-        )
-        assert result["data"][1]["extract_type"] == "opportunities_json"
-        assert result["data"][1]["download_path"] == "http://www.example.com"
+    assert len(result["data"]) == 2
+    assert (    
+        result["data"][0]["extract_metadata_id"] == sample_extract_metadata.extract_metadata_id
+    )
+    assert result["data"][0]["extract_type"] == "opportunities_csv"
+    assert result["data"][0]["download_path"] == "http://localhost:4566/local-mock-public-bucket/test/path/test_extract.csv"
+    assert (
+        result["data"][1]["extract_metadata_id"] == other_extract_metadata.extract_metadata_id
+    )
+    assert result["data"][1]["extract_type"] == "opportunities_json"
+    assert result["data"][1]["download_path"] == "http://localhost:4566/local-mock-public-bucket/test/path/test_extract2.xml"
 
 
 def test_request_schema_null_values():
