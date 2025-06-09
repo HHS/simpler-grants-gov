3cb4cb1896ec66234dce3af6ad03bd8a9b900369
Paul Craig <paul@pcraig3.ca>
2025-06-09 13:52:35 -0400

    Add missing environment variables to the NOFO Builder

--- infra/nofos/app-config/env-config/environment_variables.tf
+++ infra/nofos/app-config/env-config/environment_variables.tf
@@ -3,14 +3,28 @@ locals {
   # This is a map rather than a list so that variables can be easily
   # overridden per environment using terraform's `merge` function
   default_extra_environment_variables = {
-    DEBUG                = "false"
-    DJANGO_ALLOWED_HOSTS = "*"
+    DEBUG = "false"
   }
 
   secrets = {
+    API_TOKEN = {
+      manage_method     = "manual"
+      secret_store_name = "/nofos/${var.environment}/api-token"
+    }
+
+    DJANGO_ALLOWED_HOSTS = {
+      manage_method     = "manual"
+      secret_store_name = "/nofos/${var.environment}/django-allowed-hosts"
+    }
+
     DOCRAPTOR_API_KEY = {
       manage_method     = "manual"
       secret_store_name = "/nofos/${var.environment}/docraptor-api-key"
+    }
+
+    SECRET_KEY = {
+      manage_method     = "manual"
+      secret_store_name = "/nofos/${var.environment}/secret-key"
     }
   }
 }
