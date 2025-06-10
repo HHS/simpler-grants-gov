986e5129056b54534a70d716b7f1b86a853f3214
Aaron Couch <aaroncouch@navapbc.com>
2025-06-10 12:07:51 -0400

    [Issue #5039] Application page flow (#5222)

--- frontend/src/app/[locale]/workspace/layout.tsx
+++ frontend/src/app/[locale]/workspace/layout.tsx
@@ -2,7 +2,7 @@ import { LayoutProps } from "src/types/generalTypes";
 
 import { AuthenticationGate } from "src/components/user/AuthenticationGate";
 
-export default function FormLayout({ children }: LayoutProps) {
+export default function WorkspaceLayout({ children }: LayoutProps) {
   return (
     <>
       <AuthenticationGate>{children}</AuthenticationGate>
