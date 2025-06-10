30ca15ce3742d61c2c7df2326c75c4a71cf5e8ff
Michael Huneke <mike.huneke@focusconsulting.io>
2025-06-10 14:00:31 -0400

    Modify the GET /application endpoint to return organization info

--- api/src/api/application_alpha/application_schemas.py
+++ api/src/api/application_alpha/application_schemas.py
@@ -78,6 +78,21 @@ class ApplicationUserSchema(Schema):
     email = fields.String()
 
 
+class SamGovEntitySchema(Schema):
+    """Schema for SAM.gov entity information"""
+
+    uei = fields.String()
+    legal_business_name = fields.String()
+    expiration_date = fields.Date()
+
+
+class OrganizationSchema(Schema):
+    """Schema for organization information"""
+
+    organization_id = fields.UUID()
+    sam_gov_entity = fields.Nested(SamGovEntitySchema(), allow_none=True)
+
+
 class ApplicationGetResponseDataSchema(Schema):
     application_id = fields.UUID()
     competition = fields.Nested(CompetitionAlphaSchema())
@@ -85,6 +100,7 @@ class ApplicationGetResponseDataSchema(Schema):
     application_status = fields.String()
     application_name = fields.String()
     users = fields.List(fields.Nested(ApplicationUserSchema()))
+    organization = fields.Nested(OrganizationSchema(), allow_none=True)
 
     form_validation_warnings = fields.Dict(
         metadata={
