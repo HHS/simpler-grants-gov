88be6e8294908f3337f60271dd64ff6b651ee41f
Matt Dragon <mattdragon@navapbc.com>
2025-06-12 16:39:46 -0400

    Remove some extra logging from when things were failing confusingly

--- api/src/adapters/aws/pinpoint_adapter.py
+++ api/src/adapters/aws/pinpoint_adapter.py
@@ -98,10 +98,6 @@ def send_pinpoint_email_raw(
 
     try:
         raw_response = pinpoint_client.send_messages(**request)
-        logger.info(
-            "Pinpoint Request Sent",
-            extra={"trace_id": trace_id},
-        )
     except ClientError:
         logger.exception("Failed to send email", extra={"trace_id": trace_id})
         raise
