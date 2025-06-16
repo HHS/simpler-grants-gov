ae3e0f7d370006e2dc8baa9bb04960eb49a0dd9b
Michael Huneke <mike.huneke@focusconsulting.io>
2025-06-16 13:43:22 -0400

    Refactor to DRY

--- api/src/services/applications/update_application_attachment.py
+++ api/src/services/applications/update_application_attachment.py
@@ -1,16 +1,11 @@
 import logging
 import uuid
-from typing import cast
-
-from werkzeug.datastructures import FileStorage
 
 import src.adapters.db as db
 import src.util.file_util as file_util
 from src.db.models.competition_models import ApplicationAttachment
 from src.db.models.user_models import User
-from src.services.applications.create_application_attachment import (
-    build_s3_application_attachment_path,
-)
+from src.services.applications.create_application_attachment import upsert_application_attachment
 from src.services.applications.get_application_attachment import get_application_attachment
 
 logger = logging.getLogger(__name__)
@@ -29,46 +24,21 @@ def update_application_attachment(
         db_session, application_id, application_attachment_id, user
     )
 
-    # This uses a werkzeug FileStorage object for managing the file operations
-    # Mimetype is set if the user specifies it when uploading the file which
-    # if it's done via a standard HTML file box would include it.
-    file_attachment: FileStorage = cast(FileStorage, form_and_files_data.get("file_attachment"))
-
-    # secure_filename makes the file safe in path operations and removes non-ascii characters
-    secure_file_name = file_util.get_secure_file_name(file_attachment.filename)
-
-    # Build the new s3 path
-    new_s3_file_location = build_s3_application_attachment_path(
-        secure_file_name, application_id, application_attachment_id
-    )
-
     # Store the old file location before updating
     old_s3_file_location = application_attachment.file_location
 
-    # Upload the file to s3
-    logger.info(
-        "Uploading application attachment to s3",
-        extra={"application_attachment_id": application_attachment_id},
+    # Use the upsert function to update the attachment
+    updated_application_attachment = upsert_application_attachment(
+        db_session=db_session,
+        application_id=application_id,
+        user=user,
+        form_and_files_data=form_and_files_data,
+        application=application_attachment.application,
+        application_attachment=application_attachment,
     )
-    with file_util.open_stream(
-        new_s3_file_location, mode="wb", content_type=file_attachment.mimetype
-    ) as writefile:
-        file_attachment.save(writefile)
 
-    # The content length is not set in most cases on the FileStorage object (needs to be done explicitly by user)
-    # so instead get the content-length from the file on s3 after we upload it.
-    file_size_bytes = file_util.get_file_length_bytes(new_s3_file_location)
-
-    # Update the application attachment record
-    application_attachment.file_location = new_s3_file_location
-    # In the file_name column we store the users actual file name unmodified
-    # so when we display it on the UI it matches whatever they uploaded.
-    application_attachment.file_name = file_util.get_file_name(file_attachment.filename)
-    application_attachment.mime_type = file_attachment.mimetype
-    application_attachment.file_size_bytes = file_size_bytes
-
-    # IF the filepath on s3 is different than before, delete the old file from s3
-    if old_s3_file_location != new_s3_file_location:
+    # If the filepath on s3 is different than before, delete the old file from s3
+    if old_s3_file_location != updated_application_attachment.file_location:
         logger.info(
             "Deleting old application attachment from s3",
             extra={
@@ -82,4 +52,4 @@ def update_application_attachment(
         "Updated application attachment",
         extra={"application_attachment_id": application_attachment_id},
     )
-    return application_attachment
+    return updated_application_attachment
