858cc331109873db9948896c200f511936dd775a
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2025-06-16 11:16:13 -0400

    [Issue #5248] Create a task that creates organizations from ebiz POCs (#5300)
    ## Summary
    
    Fixes #5248
    
    ## Changes proposed
    Created a task that sets up organizations when the EBIZ POC email has a
    corresponding user
    
    ## Context for reviewers
    Organizations aren't created by users, instead if the sam.gov entity
    record we imported happens to have an EBIZ POC email that matches one in
    our system we want to setup the user as an owner of that organization.
    
    This implementation handles a few scenarios:
    * Org doesn't exist at all -> Org is created, user is added as an owner
    of the organization
    * Org exists, user isn't a member -> User is added as an owner of the
    organization
    * Org exists, user is a member, but not an owner -> User is made owner
    of the organization
    * Org exists, user is already an owner -> nothing happens
    
    Any existing users in an organization are unaffected, including other
    owners. This only adds someone as an owner, it doesn't kick out the old
    owners (when user management is built, we will let them do that
    potentially).
    
    ## Validation steps
    Just the tests which handle various scenarios / data
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

---
+++ api/tests/src/task/sam_extracts/test_create_orgs_from_sam_entity.py
@@ -0,0 +1,148 @@
+import pytest
+
+from src.db.models.entity_models import SamGovEntity
+from src.db.models.user_models import LinkExternalUser
+from src.task.sam_extracts.create_orgs_from_sam_entity import CreateOrgsFromSamEntityTask
+from tests.lib.db_testing import cascade_delete_from_db_table
+from tests.src.db.models.factories import (
+    LinkExternalUserFactory,
+    OrganizationUserFactory,
+    SamGovEntityFactory,
+)
+
+
+@pytest.fixture(autouse=True)
+def cleanup_existing_entities(db_session):
+    cascade_delete_from_db_table(db_session, SamGovEntity)
+    cascade_delete_from_db_table(db_session, LinkExternalUser)
+
+
+def test_run_task_multiple_entities_and_users_same_email(enable_factory_create, db_session):
+    """Test that the query works as expected - if we setup the following
+    entities, every combination of them will be added to each other.
+
+    That is, if a user is is the EBIZ POC of multiple entities, they
+    should be made the owner of all of those.
+    """
+    # It's not really realistic that multiple users have the same email address
+    # but if they did, verify it works this way.
+    user_a = LinkExternalUserFactory.create(email="mymail@mail.com")
+    user_b = LinkExternalUserFactory.create(email="mymail@mail.com")
+    user_c = LinkExternalUserFactory.create(email="mymail@mail.com")
+
+    entity_a = SamGovEntityFactory.create(ebiz_poc_email="mymail@mail.com")
+    entity_b = SamGovEntityFactory.create(ebiz_poc_email="mymail@mail.com")
+    entity_c = SamGovEntityFactory.create(ebiz_poc_email="mymail@mail.com")
+
+    task = CreateOrgsFromSamEntityTask(db_session)
+    task.run()
+
+    user_ids = [user_a.user_id, user_b.user_id, user_c.user_id]
+
+    for entity in [entity_a, entity_b, entity_c]:
+        assert entity.organization is not None
+        assert [org_user.user_id for org_user in entity.organization.organization_users] == user_ids
+        for org_user in entity.organization.organization_users:
+            assert org_user.is_organization_owner
+
+    metrics = task.metrics
+    assert metrics[task.Metrics.RECORDS_PROCESSED] == 9
+    assert metrics[task.Metrics.NEW_ORGANIZATION_CREATED_COUNT] == 3
+    assert metrics[task.Metrics.NEW_USER_ORGANIZATION_CREATED_COUNT] == 9
+    assert metrics[task.Metrics.NEW_ORGANIZATION_OWNER_COUNT] == 9
+
+
+def test_run_task_multiple_different_email_addresses(enable_factory_create, db_session):
+    user_a = LinkExternalUserFactory.create(email="entity_a@mail.com")
+    user_b = LinkExternalUserFactory.create(email="entity_b@mail.com")
+    user_c = LinkExternalUserFactory.create(email="entity_c@mail.com")
+
+    entity_a = SamGovEntityFactory.create(ebiz_poc_email=user_a.email)
+    entity_b = SamGovEntityFactory.create(ebiz_poc_email=user_b.email)
+    entity_c = SamGovEntityFactory.create(ebiz_poc_email=user_c.email)
+
+    task = CreateOrgsFromSamEntityTask(db_session)
+    task.run()
+
+    assert entity_a.organization is not None
+    assert len(entity_a.organization.organization_users) == 1
+    assert entity_a.organization.organization_users[0].user_id == user_a.user_id
+    assert entity_a.organization.organization_users[0].is_organization_owner is True
+
+    assert entity_b.organization is not None
+    assert len(entity_b.organization.organization_users) == 1
+    assert entity_b.organization.organization_users[0].user_id == user_b.user_id
+    assert entity_b.organization.organization_users[0].is_organization_owner is True
+
+    assert entity_c.organization is not None
+    assert len(entity_c.organization.organization_users) == 1
+    assert entity_c.organization.organization_users[0].user_id == user_c.user_id
+    assert entity_c.organization.organization_users[0].is_organization_owner is True
+
+    metrics = task.metrics
+    assert metrics[task.Metrics.RECORDS_PROCESSED] == 3
+    assert metrics[task.Metrics.NEW_ORGANIZATION_CREATED_COUNT] == 3
+    assert metrics[task.Metrics.NEW_USER_ORGANIZATION_CREATED_COUNT] == 3
+    assert metrics[task.Metrics.NEW_ORGANIZATION_OWNER_COUNT] == 3
+
+
+def test_run_task_varying_scenarios_org_exists(enable_factory_create, db_session):
+    ### Everything already exists, won't be changed
+    user_a = LinkExternalUserFactory.create(email="entity_a@mail.com")
+    entity_a = SamGovEntityFactory.create(ebiz_poc_email=user_a.email, has_organization=True)
+    OrganizationUserFactory.create(
+        organization=entity_a.organization, user=user_a.user, is_organization_owner=True
+    )
+    # Also has other random users with other emails that won't be affected/picked up
+    OrganizationUserFactory.create(organization=entity_a.organization, is_organization_owner=False)
+    OrganizationUserFactory.create(organization=entity_a.organization, is_organization_owner=True)
+
+    ### User already exists, but is not an owner
+    user_b = LinkExternalUserFactory.create(email="entity_b@mail.com")
+    entity_b = SamGovEntityFactory.create(ebiz_poc_email=user_b.email, has_organization=True)
+    OrganizationUserFactory.create(
+        organization=entity_b.organization, user=user_b.user, is_organization_owner=False
+    )
+    # Other random users in org will be unaffected
+    OrganizationUserFactory.create(organization=entity_b.organization, is_organization_owner=False)
+    OrganizationUserFactory.create(organization=entity_b.organization, is_organization_owner=True)
+    OrganizationUserFactory.create(organization=entity_b.organization, is_organization_owner=True)
+
+    ### Org exists, but user is not a member/owner
+    user_c = LinkExternalUserFactory.create(email="entity_c@mail.com")
+    entity_c = SamGovEntityFactory.create(ebiz_poc_email=user_c.email, has_organization=True)
+    # Other random users in org will be unaffected
+    OrganizationUserFactory.create(organization=entity_c.organization, is_organization_owner=True)
+
+    # Various other users/entities that won't get picked up
+    SamGovEntityFactory.create(ebiz_poc_email="random_email123@mail.com")
+    SamGovEntityFactory.create(ebiz_poc_email="random_email456@mail.com")
+    SamGovEntityFactory.create(ebiz_poc_email="random_email789@mail.com")
+    LinkExternalUserFactory.create(email="another_email123@mail.com")
+    LinkExternalUserFactory.create(email="another_email456@mail.com")
+    LinkExternalUserFactory.create(email="another_email789@mail.com")
+
+    task = CreateOrgsFromSamEntityTask(db_session)
+    task.run()
+
+    assert len(user_a.user.organizations) == 1
+    assert user_a.user.organizations[0].organization_id == entity_a.organization.organization_id
+    assert user_a.user.organizations[0].is_organization_owner is True
+    assert len(entity_a.organization.organization_users) == 3
+
+    # TODO - other compares
+    assert len(user_b.user.organizations) == 1
+    assert user_b.user.organizations[0].organization_id == entity_b.organization.organization_id
+    assert user_b.user.organizations[0].is_organization_owner is True
+    assert len(entity_b.organization.organization_users) == 4
+
+    assert len(user_c.user.organizations) == 1
+    assert user_c.user.organizations[0].organization_id == entity_c.organization.organization_id
+    assert user_c.user.organizations[0].is_organization_owner is True
+    assert len(entity_c.organization.organization_users) == 2
+
+    metrics = task.metrics
+    assert metrics[task.Metrics.RECORDS_PROCESSED] == 3
+    assert metrics[task.Metrics.NEW_ORGANIZATION_CREATED_COUNT] == 0
+    assert metrics[task.Metrics.NEW_USER_ORGANIZATION_CREATED_COUNT] == 1
+    assert metrics[task.Metrics.NEW_ORGANIZATION_OWNER_COUNT] == 2
