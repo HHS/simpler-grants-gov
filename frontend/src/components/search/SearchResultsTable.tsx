68050cdadb4591a98d2e3f99531fd57f2a6ff329
Doug Schrashun <dougschrashun@navapbc.com>
2025-06-20 16:29:08 -0400

    part two responsive table - need to fix styling and content overflow

--- frontend/src/components/search/SearchResultsTable.tsx
+++ frontend/src/components/search/SearchResultsTable.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 import { isNil } from "lodash";
-import { OpportunityStatus } from "src/types/opportunity/opportunityResponseTypes";
+import {
+  BaseOpportunity,
+  OpportunityStatus,
+} from "src/types/opportunity/opportunityResponseTypes";
 import { SearchResponseData } from "src/types/search/searchRequestTypes";
 import { toShortMonthDate } from "src/utils/dateUtil";
 import { formatCurrency } from "src/utils/formatCurrencyUtil";
@@ -10,11 +13,21 @@ import { useTranslations } from "next-intl";
 import { ReactNode } from "react";
 import { Table } from "@trussworks/react-uswds";
 
+type TableCellData = {
+  cellData: ReactNode;
+  hideWhenStacked?: boolean;
+  className?: string;
+};
+
 const statusColorClasses = {
   posted: "bg-accent-warm-light",
   forecasted: "bg-accent-warm-lightest",
   closed: "bg-base-lightest",
   archived: "bg-base-lightest",
+};
+
+const DummyTableHeaderCell = () => {
+  return <th scope="col" className="display-none"></th>;
 };
 
 export const SearchTableStatusDisplay = ({
@@ -35,10 +48,94 @@ export const SearchTableStatusDisplay = ({
   );
 };
 
-type TableCellData = {
-  cellData: ReactNode;
-  hideWhenStacked?: boolean;
-  className?: string;
+export const toSearchResultsTableRow = (
+  result: BaseOpportunity,
+): TableCellData[] => {
+  const t = useTranslations("Search.table");
+  return [
+    {
+      cellData: result.summary.close_date
+        ? toShortMonthDate(result.summary.close_date)
+        : t("tbd"),
+    },
+    {
+      cellData: <SearchTableStatusDisplay status={result.opportunity_status} />,
+    },
+    {
+      cellData: (
+        <>
+          <div className="font-sans-lg text-bold">
+            <a href={getOpportunityUrl(result.opportunity_id)}>
+              {result.opportunity_title}
+            </a>
+          </div>
+          <div className="font-sans-xs">
+            <span className="text-bold">{t("number")}:</span>{" "}
+            {result.opportunity_number}
+          </div>
+        </>
+      ),
+    },
+    {
+      cellData: (
+        <>
+          <div className="margin-bottom-1">{result.agency_name}</div>
+          <div className="font-sans-xs">
+            <span className="text-bold">{t("published")}</span>:{" "}
+            {result.summary.post_date}
+          </div>
+          <div className="font-sans-xs">
+            {t("expectedAwards")}:{" "}
+            {isNil(result.summary.expected_number_of_awards)
+              ? "--"
+              : result.summary.expected_number_of_awards}
+          </div>
+        </>
+      ),
+    },
+    {
+      cellData: isNil(result.summary.award_floor)
+        ? "$--"
+        : formatCurrency(result.summary.award_floor),
+    },
+    {
+      cellData: isNil(result.summary.award_ceiling)
+        ? "$--"
+        : formatCurrency(result.summary.award_ceiling),
+    },
+  ];
+};
+
+export const SearchResultsTable = ({
+  searchResults,
+}: {
+  searchResults: SearchResponseData;
+}) => {
+  const t = useTranslations("Search.table");
+  const headerContent: TableCellData[] = [
+    { cellData: t("headings.closeDate") },
+    { cellData: t("headings.status") },
+    { cellData: t("headings.title") },
+    { cellData: t("headings.agency") },
+    { cellData: t("headings.awardMin") },
+    { cellData: t("headings.awardMax") },
+  ];
+  const tableRowData = searchResults.map(toSearchResultsTableRow);
+  // const tableRowDataWithResponsiveHeaderCells = tableRowData.map(
+  //   (resultData, i) => {
+  //     console.log("###", resultData, headerContent[i]);
+  //     resultData.unshift({
+  //       cellData: headerContent[i].cellData,
+  //     });
+  //     return resultData;
+  //   },
+  // );
+  return (
+    <TableWithResponsiveHeader
+      headerContent={headerContent}
+      tableRowData={tableRowData}
+    />
+  );
 };
 
 const TableWithResponsiveHeader = ({
@@ -56,130 +153,62 @@ const TableWithResponsiveHeader = ({
     );
     return;
   }
+
+  const headerNodes = headerContent.map((headerItem, i) => {
+    return (
+      <th
+        key={`responsiveHeaderItem-${i}`}
+        scope="col"
+        className={clsx(
+          "bg-base-lightest padding-y-205 minw-15",
+          headerItem.className,
+        )}
+      >
+        {headerItem.cellData}
+      </th>
+    );
+  });
+
+  const dataRows = tableRowData.map((tableRow, i) => {
+    return (
+      <tr key={`responsiveTableRow-${i}`}>
+        {tableRow.map((tableCell, j) => {
+          return (
+            <td
+              key={`responsiveTableCell-${i}-${j}`}
+              className={clsx(
+                "tablet-lg:display-table-cell",
+                tableCell.className,
+                {
+                  "display-none": tableCell.hideWhenStacked,
+                  "display-block": !tableCell.hideWhenStacked,
+                },
+              )}
+            >
+              <div className="display-flex">
+                <div className="tablet-lg:display-none flex-1">
+                  {headerContent[j].cellData}
+                </div>
+                <div className="flex-3">{tableCell.cellData}</div>
+              </div>
+            </td>
+          );
+        })}
+      </tr>
+    );
+  });
+
   return (
     <Table className="simpler-responsive-table">
       <thead>
         <tr>
-          {headerContent.map((headerItem, i) => {
-            return (
-              <th
-                key={`responsiveHeaderItem-${i}`}
-                scope="col"
-                className={clsx(
-                  "bg-base-lightest padding-y-205 minw-15",
-                  headerItem.className,
-                )}
-              >
-                {headerItem.cellData}
-              </th>
-            );
-          })}
+          {[<DummyTableHeaderCell key="dummyTableHeaderCell" />].concat(
+            headerNodes,
+          )}
         </tr>
       </thead>
-      <tbody>
-        {tableRowData.map((tableRow, i) => {
-          return (
-            <tr key={`responsiveTableRow-${i}`}>
-              {tableRow.map((tableCell, j) => {
-                return (
-                  <td
-                    key={`responsiveTableCell-${i}-${j}`}
-                    className={clsx(
-                      "tablet-lg:display-table-cell",
-                      tableCell.className,
-                      {
-                        "display-none": tableCell.hideWhenStacked,
-                        "display-block": !tableCell.hideWhenStacked,
-                      },
-                    )}
-                  >
-                    {tableCell.cellData}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
+      <tbody>{dataRows}</tbody>
     </Table>
-  );
-};
-
-export const SearchResultsTable = ({
-  searchResults,
-}: {
-  searchResults: SearchResponseData;
-}) => {
-  const t = useTranslations("Search.table");
-  const headerContent = [
-    { cellData: t("headings.closeDate") },
-    { cellData: t("headings.status") },
-    { cellData: t("headings.title") },
-    { cellData: t("headings.agency") },
-    { cellData: t("headings.awardMin") },
-    { cellData: t("headings.awardMax") },
-  ];
-  const tableRowData = searchResults.map((result) => {
-    return [
-      {
-        cellData: result.summary.close_date
-          ? toShortMonthDate(result.summary.close_date)
-          : t("tbd"),
-      },
-      {
-        cellData: (
-          <SearchTableStatusDisplay status={result.opportunity_status} />
-        ),
-      },
-      {
-        cellData: (
-          <>
-            <div className="font-sans-lg text-bold">
-              <a href={getOpportunityUrl(result.opportunity_id)}>
-                {result.opportunity_title}
-              </a>
-            </div>
-            <div className="font-sans-xs">
-              <span className="text-bold">{t("number")}:</span>{" "}
-              {result.opportunity_number}
-            </div>
-          </>
-        ),
-      },
-      {
-        cellData: (
-          <>
-            <div className="margin-bottom-1">{result.agency_name}</div>
-            <div className="font-sans-xs">
-              <span className="text-bold">{t("published")}</span>:{" "}
-              {result.summary.post_date}
-            </div>
-            <div className="font-sans-xs">
-              {t("expectedAwards")}:{" "}
-              {isNil(result.summary.expected_number_of_awards)
-                ? "--"
-                : result.summary.expected_number_of_awards}
-            </div>
-          </>
-        ),
-      },
-      {
-        cellData: isNil(result.summary.award_floor)
-          ? "$--"
-          : formatCurrency(result.summary.award_floor),
-      },
-      {
-        cellData: isNil(result.summary.award_ceiling)
-          ? "$--"
-          : formatCurrency(result.summary.award_ceiling),
-      },
-    ];
-  });
-  return (
-    <TableWithResponsiveHeader
-      headerContent={headerContent}
-      tableRowData={tableRowData}
-    />
   );
 };
 
