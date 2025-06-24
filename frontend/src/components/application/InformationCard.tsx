1d2c19b1712e1acb5a29985b78bbec62ef30be7e
My Duong <myduong@navapbc.com>
2025-06-24 12:56:31 -0500

    chore: linting and formatting

--- frontend/src/components/application/InformationCard.tsx
+++ frontend/src/components/application/InformationCard.tsx
@@ -29,7 +29,10 @@ export const InformationCard = ({
   // TODO: check this after mvp
   // instructions were to use the first available path
   // this may change
-  const instructionsDownloadPath = applicationDetails.competition.competition_instructions.length ? applicationDetails.competition.competition_instructions[0].download_path : undefined;
+  const instructionsDownloadPath = applicationDetails.competition
+    .competition_instructions.length
+    ? applicationDetails.competition.competition_instructions[0].download_path
+    : undefined;
 
   const ApplicantDetails = () => {
     if (hasOrganization) {
@@ -73,16 +76,16 @@ export const InformationCard = ({
         <dd>
           {instructionsDownloadPath && (
             <Link href={instructionsDownloadPath}>
-            <Button
-              type="button"
-              data-testid="application-instructions-download"
-              outline
-            >
-              <USWDSIcon name="file_download" />
-              {t("applicationDownloadInstructions")}
-            </Button>
-          </Link>)
-          }
+              <Button
+                type="button"
+                data-testid="application-instructions-download"
+                outline
+              >
+                <USWDSIcon name="file_download" />
+                {t("applicationDownloadInstructions")}
+              </Button>
+            </Link>
+          )}
         </dd>
       </div>
     );
