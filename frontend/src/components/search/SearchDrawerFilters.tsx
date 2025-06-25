HHS/simpler-grants-gov

commit 25ee6d21a715b92b76a344d75e3eaffdb6e62d1d

[Issue #4717] funding types tool tips (#5311)
Adds tool tips for the "cooperative agreement" and "procurement contract" filter options under "funding types"

frontend/src/components/search/SearchDrawerFilters.tsx
@@ -62,6 +62,7 @@ export async function SearchDrawerFilters({
         title={t("accordion.titles.status")}
         defaultEmptySelection={new Set([SEARCH_NO_STATUS_VALUE])}
         facetCounts={facetCounts?.opportunity_status || {}}
+        contentClassName="maxh-mobile-lg overflow-auto position-relative" // these classes allow the filter contents to scroll
       />
       <CheckboxFilter
         filterOptions={fundingOptions}
@@ -69,6 +70,7 @@ export async function SearchDrawerFilters({
         queryParamKey="fundingInstrument"
         title={t("accordion.titles.funding")}
         facetCounts={facetCounts?.funding_instrument || {}}
+        contentClassName="overflow-visible"
       />
       <CheckboxFilter
         query={eligibility}
@@ -76,6 +78,7 @@ export async function SearchDrawerFilters({
         title={t("accordion.titles.eligibility")}
         filterOptions={eligibilityOptions}
         facetCounts={facetCounts?.applicant_type || {}}
+        contentClassName="maxh-mobile-lg overflow-auto position-relative"
       />
       <Suspense
         fallback={
@@ -103,6 +106,7 @@ export async function SearchDrawerFilters({
         queryParamKey={"category"}
         title={t("accordion.titles.category")}
         facetCounts={facetCounts?.funding_category || {}}
+        contentClassName="maxh-mobile-lg overflow-auto position-relative"
       />
       <RadioButtonFilter
         filterOptions={closeDateOptions}

frontend/src/components/search/SearchFilterAccordion/AgencyFilterAccordion.tsx
@@ -68,6 +68,7 @@ export async function AgencyFilter({
       queryParamKey={"agency"}
       title={t("accordion.titles.agency")}
       facetCounts={facetCounts}
+      contentClassName="maxh-mobile-lg overflow-auto position-relative"
     />
   );
 }

frontend/src/components/search/SearchFilterAccordion/SearchFilterAccordion.tsx
@@ -34,7 +34,7 @@ export interface CommonSearchFilterAccordionProps {
 export interface BasicSearchFilterAccordionProps
   extends CommonSearchFilterAccordionProps {
   className?: string;
-  wrapForScroll?: boolean;
+  contentClassName?: string;
   expanded?: boolean;
   children: React.ReactNode;
 }
@@ -50,6 +50,10 @@ export interface SearchAccordionContentProps
 export interface SearchFilterAccordionProps
   extends SearchAccordionContentProps {
   wrapForScroll?: boolean;
+}
+
+export interface SearchFilterProps extends SearchAccordionContentProps {
+  contentClassName?: string;
 }
 
 const AccordionTitle = ({
@@ -189,8 +193,8 @@ export function BasicSearchFilterAccordion({
   queryParamKey,
   query,
   className,
+  contentClassName,
   expanded = false,
-  wrapForScroll = true,
 }: BasicSearchFilterAccordionProps) {
   const accordionOptions: AccordionItemProps[] = [
     {
@@ -199,9 +203,7 @@ export function BasicSearchFilterAccordion({
       expanded,
       id: `opportunity-filter-${queryParamKey as string}`,
       headingLevel: "h2",
-      className: wrapForScroll
-        ? "maxh-mobile-lg overflow-auto position-relative"
-        : "",
+      className: contentClassName || "",
     },
   ];
 

frontend/tests/components/search/SearchFilterAccordion/SearchFilterAccordion.test.tsx
@@ -200,33 +200,6 @@ describe("BasicSearchFilterAccordion", () => {
     expect(contentDiv).not.toHaveAttribute("hidden");
   });
 
-  it("ensures only the component contents scroll when wrapForScroll is true", () => {
-    render(
-      <BasicSearchFilterAccordion
-        title={title}
-        queryParamKey={queryParamKey}
-        query={new Set("")}
-      >
-        <div>some filter content</div>
-      </BasicSearchFilterAccordion>,
-    );
-
-    const scrollableContainer = screen.getByTestId(
-      `accordionItem_opportunity-filter-${queryParamKey}`,
-    );
-
-    expect(scrollableContainer).toHaveClass("overflow-auto");
-
-    const initialScrollTop = scrollableContainer.scrollTop;
-
-    fireEvent.scroll(scrollableContainer, { target: { scrollTop: 100 } });
-
-    expect(scrollableContainer.scrollTop).toBeGreaterThan(initialScrollTop);
-
-    // Assert that the document body has not scrolled
-    expect(window.scrollY).toBe(0);
-  });
-
   it("renders the correct title", () => {
     render(
       <BasicSearchFilterAccordion

frontend/src/components/InfoTooltip.tsx
@@ -1,18 +1,24 @@
+import clsx from "clsx";
+
 import { forwardRef, ReactNode } from "react";
 
-import { TooltipWrapper } from "./TooltipWrapper";
+import { DynamicTooltipWrapper } from "./TooltipWrapper";
 import { USWDSIcon } from "./USWDSIcon";
 
 export interface InfoTooltipProps {
   text: ReactNode;
   position?: "top" | "bottom" | "left" | "right";
   className?: string;
+  wrapperClasses?: string;
+  title?: string;
 }
 
 const InfoTooltip = ({
   text,
   position = "top",
   className,
+  wrapperClasses,
+  title,
 }: InfoTooltipProps) => {
   const IconWrapper = forwardRef<
     HTMLSpanElement,
@@ -22,7 +28,7 @@ const InfoTooltip = ({
       {...props}
       ref={ref}
       style={{ cursor: "help" }}
-      className="text-secondary"
+      className={clsx("text-secondary", props.className)}
     >
       <USWDSIcon name="info_outline" />
     </span>
@@ -31,15 +37,17 @@ const InfoTooltip = ({
   IconWrapper.displayName = "IconWrapper";
 
   return (
-    <TooltipWrapper
+    <DynamicTooltipWrapper
+      title={title}
       label={text}
       position={position}
       asCustom={IconWrapper}
       className={className}
+      wrapperclasses={wrapperClasses}
       data-testid="tooltipWrapper"
     >
       <></>
-    </TooltipWrapper>
+    </DynamicTooltipWrapper>
   );
 };
 

frontend/src/components/TooltipWrapper.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import dynamic from "next/dynamic";
 import { ForwardRefExoticComponent, ReactNode } from "react";
 import { Tooltip } from "@trussworks/react-uswds";
 
@@ -18,5 +17,12 @@ type TooltipProps = {
 export const TooltipWrapper = (props: TooltipProps) => {
   return <Tooltip {...props} aria-label={props.title} />;
 };
+
+export const DynamicTooltipWrapper = dynamic(
+  () => import("src/components/TooltipWrapper"),
+  {
+    ssr: false, // works around bug with Trussworks assigning different random ids on server and client renders
+  },
+);
 
 export default TooltipWrapper;

frontend/src/components/search/Filters/CheckboxFilter.tsx
@@ -12,12 +12,12 @@ import { useContext, useMemo } from "react";
 import { AnyOptionCheckbox } from "src/components/search/SearchFilterAccordion/AnyOptionCheckbox";
 import {
   BasicSearchFilterAccordion,
-  SearchFilterAccordionProps,
+  SearchFilterProps,
 } from "src/components/search/SearchFilterAccordion/SearchFilterAccordion";
 import SearchFilterCheckbox from "src/components/search/SearchFilterAccordion/SearchFilterCheckbox";
 import SearchFilterSection from "src/components/search/SearchFilterAccordion/SearchFilterSection/SearchFilterSection";
 
-interface CheckboxFilterBodyProps extends SearchFilterAccordionProps {
+interface CheckboxFilterBodyProps extends SearchFilterProps {
   referenceOptions?: FilterOption[];
 }
 
@@ -98,18 +98,18 @@ export function CheckboxFilter({
   query,
   queryParamKey,
   title,
-  wrapForScroll = true,
+  contentClassName,
   defaultEmptySelection,
   includeAnyOption = true,
   filterOptions,
   facetCounts,
-}: SearchFilterAccordionProps) {
+}: SearchFilterProps) {
   return (
     <BasicSearchFilterAccordion
       query={query}
       queryParamKey={queryParamKey}
       title={title}
-      wrapForScroll={wrapForScroll}
+      contentClassName={contentClassName}
       expanded={!!query.size}
       className="width-100 padding-right-5"
     >

frontend/src/components/search/Filters/FilterOptionLabel.tsx
@@ -1,0 +1,35 @@
+import clsx from "clsx";
+import { FilterOption } from "src/types/search/searchFilterTypes";
+
+import InfoTooltip from "src/components/InfoTooltip";
+
+export function FilterOptionLabel({
+  option,
+  facetCounts,
+}: {
+  option: FilterOption;
+  facetCounts?: { [key: string]: number };
+}) {
+  return (
+    <>
+      <span>{option.label}</span>
+      {!!facetCounts && (
+        <span
+          className={clsx("text-base-dark", {
+            "padding-left-05": true,
+          })}
+        >
+          [{facetCounts[option.value] || 0}]
+        </span>
+      )}
+      {!!option.tooltip && (
+        <InfoTooltip
+          text={<div className="text-wrap">{option.tooltip}</div>}
+          title={option.label}
+          position="top"
+          wrapperClasses="margin-left-1 simpler-tooltip"
+        />
+      )}
+    </>
+  );
+}

commit 3e099c0dd2a45f8d6fd2ca8aed31377ee1af1ed1

[unticketed] allow tooltips to display properly in legacy filter rail (#5435)
fixes bug introduced in https://github.com/HHS/simpler-grants-gov/pull/5311 where funding instrument tooltips did not properly overflow their container in legacy filter implementation

also fixes a bug introduced in https://github.com/HHS/simpler-grants-gov/pull/5121 that caused legacy filters not to scroll properly

frontend/src/components/search/SearchDrawerFilters.tsx
@@ -62,7 +62,6 @@ export async function SearchDrawerFilters({
         title={t("accordion.titles.status")}
         defaultEmptySelection={new Set([SEARCH_NO_STATUS_VALUE])}
         facetCounts={facetCounts?.opportunity_status || {}}
-        contentClassName="maxh-mobile-lg overflow-auto position-relative" // these classes allow the filter contents to scroll
       />
       <CheckboxFilter
         filterOptions={fundingOptions}
@@ -78,7 +77,7 @@ export async function SearchDrawerFilters({
         title={t("accordion.titles.eligibility")}
         filterOptions={eligibilityOptions}
         facetCounts={facetCounts?.applicant_type || {}}
-        contentClassName="maxh-mobile-lg overflow-auto position-relative"
+        contentClassName="maxh-mobile-lg overflow-auto position-relative" // these classes allow the filter contents to scroll
       />
       <Suspense
         fallback={

frontend/src/components/search/SearchFilterAccordion/AgencyFilterAccordion.tsx
@@ -31,6 +31,7 @@ export async function AgencyFilterAccordion({
       query={query}
       queryParamKey={"agency"}
       title={t("accordion.titles.agency")}
+      contentClassName="maxh-mobile-lg overflow-auto position-relative"
     >
       <AgencyFilterContent
         query={query}

frontend/src/components/search/SearchFilterAccordion/SearchFilterAccordion.tsx
@@ -150,9 +150,9 @@ export function SearchFilterAccordion({
   query,
   facetCounts,
   defaultEmptySelection,
+  contentClassName,
   includeAnyOption = true,
-  wrapForScroll = false,
-}: SearchFilterAccordionProps) {
+}: SearchFilterProps) {
   const accordionOptions: AccordionItemProps[] = [
     {
       title: <AccordionTitle title={title} totalCheckedCount={query.size} />,
@@ -170,9 +170,10 @@ export function SearchFilterAccordion({
       expanded: !!query.size,
       id: `opportunity-filter-${queryParamKey as string}`,
       headingLevel: "h2",
-      className: wrapForScroll
-        ? "maxh-mobile-lg overflow-auto position-relative"
-        : "",
+      className: clsx(
+        "maxh-mobile-lg overflow-auto position-relative",
+        contentClassName,
+      ),
     },
   ];
 

frontend/src/components/search/SearchFilters.tsx
@@ -61,6 +61,7 @@ export default async function SearchFilters({
         queryParamKey="fundingInstrument"
         title={t("accordion.titles.funding")}
         facetCounts={facetCounts?.funding_instrument || {}}
+        contentClassName="overflow-visible"
       />
       <SearchFilterAccordion
         filterOptions={eligibilityOptions}

frontend/tests/components/search/SearchFilterAccordion/SearchFilterAccordion.test.tsx
@@ -132,7 +132,7 @@ describe("SearchFilterAccordion", () => {
     });
     expect(anyCheckbox).toBeInTheDocument();
   });
-  it("ensures only the component contents scroll when wrapForScroll is true", () => {
+  it("ensures the component contents scroll", () => {
     render(
       <SearchFilterAccordion
         filterOptions={initialFilterOptions}
@@ -140,7 +140,6 @@ describe("SearchFilterAccordion", () => {
         queryParamKey={queryParamKey}
         query={new Set("")}
         facetCounts={fakeFacetCounts}
-        wrapForScroll={true}
       />,
     );
 