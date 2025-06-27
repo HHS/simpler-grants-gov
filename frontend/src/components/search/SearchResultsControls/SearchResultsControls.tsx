ab251c621b433afe4f07bfef595c16dd294d513f
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-06-27 09:43:06 -0400

    [Issue #4721] search results table (#5411)
    Introduces a table view for opportunity search results (currently behind the `searchTableOn` feature flag)
    
    * some updates to existing search related components for reusability
    * creates `TableWithResponsiveHeader` as a reusable component to support headers collapsing into a stackable format at mobile widths
    * adds sort drop down into filter drawer at mobile widths
    * updates "withFeatureFlag" to allow it to work on non-top-level-page components

---
+++ frontend/src/components/search/SearchResultsControls/SearchResultsControls.tsx
@@ -0,0 +1,55 @@
+import { useTranslations } from "next-intl";
+
+import { ExportSearchResultsButton } from "src/components/search/ExportSearchResultsButton";
+import SearchPagination from "src/components/search/SearchPagination";
+import SearchSortBy from "src/components/search/SearchSortBy";
+
+const gridRowClasses = [
+  "tablet-lg:grid-col-fill",
+  "margin-top-5",
+  "tablet-lg:margin-top-2",
+  "tablet-lg:margin-bottom-0",
+];
+
+type SearchResultsControlsProps = {
+  sortby: string | null;
+  page: number;
+  totalResults: string;
+  totalPages: number;
+  query?: string | null;
+};
+
+export const SearchResultsControls = ({
+  sortby,
+  page,
+  totalResults,
+  totalPages,
+  query,
+}: SearchResultsControlsProps) => {
+  const t = useTranslations("Search");
+
+  return (
+    <div className="grid-row padding-top-4">
+      <div className="flex-1">
+        <h3 className={gridRowClasses.join(" ")}>
+          {t("resultsHeader.message", { count: totalResults })}
+        </h3>
+        <div className="tablet:display-block display-none">
+          <ExportSearchResultsButton />
+        </div>
+      </div>
+      <div className="flex-1 text-right">
+        <SearchPagination
+          totalPages={totalPages}
+          page={page}
+          query={query}
+          totalResults={totalResults}
+          paginationClassName="flex-justify-start tablet:flex-justify-end border-top-0"
+        />
+        <div className="tablet:display-block display-none">
+          <SearchSortBy sortby={sortby} queryTerm={query} />
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/tests/components/search/SearchResults.test.tsx
+++ frontend/tests/components/search/SearchResults.test.tsx
@@ -45,6 +45,11 @@ jest.mock("src/services/fetch/fetchers/searchFetcher", () => ({
   searchForOpportunities: jest.fn(() => Promise.resolve()),
 }));
 
+jest.mock("src/services/featureFlags/withFeatureFlag", () => ({
+  __esModule: true,
+  default: (component: unknown) => component,
+}));
+
 describe("SearchResults", () => {
   it("Renders without errors", () => {
     render(
