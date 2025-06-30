807e52957065afe46367c5384ed4ae8537916548
babebe <brukty@gmail.com>
2025-06-30 16:43:28 -0400

    [Issue #5003] Eligibility notification content (#5505)
    ## Summary
    
    Fixes https://github.com/HHS/simpler-grants-gov/issues/5003
    
    Update content generation for Eligibility section per
    [DOC](https://docs.google.com/document/d/1ypejg8hL7uOXdrI7auwf9SWQ1Ag07o7rCjnrVCLfA-s/edit?tab=t.0#heading=h.sl8i9r9yex9j)
    Add tests

--- api/src/task/notifications/opportunity_notifcation.py
+++ api/src/task/notifications/opportunity_notifcation.py
@@ -332,6 +332,31 @@ class OpportunityNotificationTask(BaseNotificationTask):
             )
         return category_section
 
+    def _build_eligibility_content(self, eligibility_change: dict) -> str:
+        eligibility_section = SECTION_STYLING.format("Eligibility")
+        for field, change in eligibility_change.items():
+            before = change["before"]
+            after = change["after"]
+
+            if field == "applicant_types":
+                added = sorted(set(after) - set(before), key=lambda x: x.value)
+                removed = sorted(set(before) - set(after), key=lambda x: x.value)
+                stmt = ELIGIBILITY_FIELDS["applicant_types"]
+                if added:
+                    eligibility_section += f"{BULLET_POINTS_STYLING} Additional {stmt} {[self._format_slug(e_type.value) for e_type in added]}.<br>"
+                if removed:
+                    eligibility_section += f"{BULLET_POINTS_STYLING} Removed {stmt} {[self._format_slug(e_type.value) for e_type in removed]}.<br>"
+
+            if field == "applicant_eligibility_description":
+                stmt = f"{BULLET_POINTS_STYLING} {ELIGIBILITY_FIELDS["applicant_eligibility_description"]}"
+                if not before and after:
+                    eligibility_section += f"{stmt} added.<br>"
+                elif before and not after:
+                    eligibility_section += f"{stmt} deleted.<br>"
+                else:
+                    eligibility_section += f"{stmt} changed.<br>"
+        return eligibility_section
+
     def _format_currency(self, value: int | str) -> str:
         if isinstance(value, int):
             return f"${value:,}"
@@ -421,6 +446,8 @@ class OpportunityNotificationTask(BaseNotificationTask):
             sections.append(
                 self._build_grantor_contact_fields_content(grantor_contact_fields_diffs)
             )
+        if eligibility_fields_diffs := {k: changes[k] for k in ELIGIBILITY_FIELDS if k in changes}:
+            sections.append(self._build_eligibility_content(eligibility_fields_diffs))
         if not sections:
             logger.info(
                 "Opportunity has changes, but none are in fields that trigger user notifications",
