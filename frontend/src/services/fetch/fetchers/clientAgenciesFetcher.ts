58a5f399fa0696f00361644e3f75d2b95b22f41e
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-07-02 12:48:03 -0400

    [unticketed] convert agency search results to filter options (#5578)
    fixes bug where searching for agencies results in unlabeled agency filter options

--- frontend/src/services/fetch/fetchers/clientAgenciesFetcher.ts
+++ frontend/src/services/fetch/fetchers/clientAgenciesFetcher.ts
@@ -1,4 +1,4 @@
-import { FilterOption } from "src/types/search/searchFilterTypes";
+import { RelevantAgencyRecord } from "src/types/search/searchFilterTypes";
 
 export const agencySearch = async (searchKeyword: string) => {
   const response = await fetch("/api/agencies", {
@@ -8,7 +8,7 @@ export const agencySearch = async (searchKeyword: string) => {
     }),
   });
   if (response.ok && response.status === 200) {
-    const data = (await response.json()) as FilterOption[];
+    const data = (await response.json()) as RelevantAgencyRecord[];
     return data;
   }
   throw new Error(`Unable to fetch agencies search: ${response.status}`);
