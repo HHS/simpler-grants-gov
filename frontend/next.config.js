4cad1b2f4891769bc60e6a4f57a2b75f2c780a02
My Duong <myduong@navapbc.com>
2025-07-02 10:16:08 -0500

    chore: turns strict mode back on

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -144,7 +144,7 @@ const nextConfig = {
     return headers;
   },
   basePath,
-  reactStrictMode: false,
+  reactStrictMode: true,
   // Output only the necessary files for a deployment, excluding irrelevant node_modules
   // https://nextjs.org/docs/app/api-reference/next-config-js/output
   output: "standalone",
