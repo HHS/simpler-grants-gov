da209666fe8bdaab2a52092d639ab099ca1281be
Doug Schrashun <dougschrashun@navapbc.com>
2025-07-11 16:27:12 -0400

    wip, fails to load staging assets locally...

--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -6,6 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+// const LOCAL_BASE_URL = "http://127.0.0.1:3000";
+const LOCAL_BASE_URL = "https://staging.simpler.grants.gov";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: process.env.E2E_BASE_URL || LOCAL_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -73,9 +76,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.E2E_BASE_URL
+    ? undefined
+    : {
+        command: "npm run start",
+        url: LOCAL_BASE_URL,
+        reuseExistingServer: !process.env.CI,
+      },
 });
