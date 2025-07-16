dbdfd111415bd7245863e899a43add37c74061f8
Andy Cochran <andycochran@navapbc.com>
2025-07-16 17:12:38 -0400

    roadmap page: swap img, add timeline section

--- frontend/src/components/roadmap/RoadmapSections.tsx
+++ frontend/src/components/roadmap/RoadmapSections.tsx
@@ -1,4 +1,5 @@
 import RoadmapHeader from "./sections/RoadmapHeader";
+import RoadmapTimeline from "./sections/RoadmapTimeline";
 import RoadmapMilestones from "./sections/RoadmapMilestones";
 import RoadmapProcess from "./sections/RoadmapProcess";
 import RoadmapWhatWereWorkingOn from "./sections/RoadmapWhatWereWorkingOn";
@@ -7,6 +8,7 @@ export default function RoadmapPageSections() {
   return (
     <>
       <RoadmapHeader />
+      <RoadmapTimeline />
       <RoadmapWhatWereWorkingOn />
       <RoadmapMilestones />
       <RoadmapProcess />
