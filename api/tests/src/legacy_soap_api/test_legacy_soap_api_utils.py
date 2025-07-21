c37be630b24ed1471e51940f9ca025e259f93243
Freddie Yebra <freddieyebra@navapbc.com>
2025-07-21 15:29:46 -0500

    Add key value diff.

--- api/tests/src/legacy_soap_api/test_legacy_soap_api_utils.py
+++ api/tests/src/legacy_soap_api/test_legacy_soap_api_utils.py
@@ -76,3 +76,162 @@ def test_bool_to_string() -> None:
     assert bool_to_string(True) == "true"
     assert bool_to_string(False) == "false"
     assert bool_to_string(None) is None
+
+
+def is_list_of_dicts(lst: list) -> bool:
+    return isinstance(lst, list) and all(isinstance(item, dict) for item in lst)
+
+
+def dict_diff(
+    sgg_dict: dict, gg_dict: dict, key_indexes: dict | None = None, keys_only: bool = False
+):
+    # if a dict key, value pair are not equal and are instances of a list of dicts, use list_of_dict_key_indexes
+    # to determine how to match up entries based on the key name and which key name to use to find and compare
+    # the dicts with the specified matching key name.
+    key_indexes = key_indexes if key_indexes else {}
+    sgg_keys = set(sgg_dict.keys())
+    gg_keys = set(gg_dict.keys())
+
+    key_diffs = {}
+    if keys_only_in_sgg := sgg_keys - gg_keys:
+        key_diffs["keys_only_in_sgg"] = {k: sgg_dict[k] for k in keys_only_in_sgg}
+    if keys_only_in_gg := gg_keys - sgg_keys:
+        key_diffs["keys_only_in_gg"] = {k: gg_dict[k] for k in keys_only_in_gg}
+
+    differing = {}
+    for k in sgg_keys & gg_keys:
+        sgg_value, gg_value = sgg_dict[k], gg_dict[k]
+        if isinstance(sgg_value, dict) and isinstance(gg_value, dict):
+            nested_diff = dict_diff(sgg_value, gg_value, key_indexes, keys_only)
+            if nested_diff:
+                differing[k] = nested_diff
+        elif sgg_value != gg_value:
+            # Only support diffing list of dicts if key_indexes is specified
+            if key_indexes and is_list_of_dicts(sgg_value) and is_list_of_dicts(gg_value):
+                key_index = key_indexes.get(k)
+                if key_index:
+                    differing[k] = diff_list_of_dicts(sgg_value, gg_value, key_index)
+            else:
+                differing[k] = {"sgg_dict": sgg_value, "gg_dict": gg_value}
+
+    return {**key_diffs, **differing}
+
+
+def diff_list_of_dicts(sgg_list: list[dict], gg_list: list[dict], index_key: str):
+    """
+    Get the differences from dicts within a list of dicts.
+
+    The index_key param indicates how to find the corresponding dict in the list.
+    """
+    sgg_dict = {item[index_key]: item for item in sgg_list if index_key in item}
+    sgg_dict_keys = sgg_dict.keys()
+    gg_dict = {item[index_key]: item for item in gg_list if index_key in item}
+    gg_dict_keys = gg_dict.keys()
+    only_in_sgg = {k: sgg_dict[k] for k in sgg_dict_keys - gg_dict_keys}
+    only_in_gg = {k: gg_dict[k] for k in gg_dict_keys - sgg_dict_keys}
+    return {
+        "index_key": index_key,
+        "found_only_in_sgg": list(only_in_sgg.values()),
+        "found_only_in_gg": list(only_in_gg.values()),
+        "different_values": {
+            k: {"sgg_dict": sgg_dict[k], "gg_dict": gg_dict[k]}
+            for k in sgg_dict_keys & gg_dict_keys
+            if sgg_dict[k] != gg_dict[k]
+        },
+    }
+
+
+def test_is_list_of_dicts_true():
+    assert is_list_of_dicts([{"a": 1}, {"b": 2}]) is True
+
+
+def test_is_list_of_dicts_false_not_list():
+    assert is_list_of_dicts({"a": 1}) is False
+
+
+def test_is_list_of_dicts_false_list_not_all_dicts():
+    assert is_list_of_dicts([{"a": 1}, 2, "x"]) is False
+
+
+def test_dict_diff_simple_difference():
+    sgg = {"a": 1, "b": 2}
+    gg = {"a": 1, "b": 3}
+    result = dict_diff(sgg, gg)
+    expected = {"b": {"sgg_dict": 2, "gg_dict": 3}}
+    assert result == expected
+
+
+def test_dict_diff_missing_keys():
+    sgg = {"a": 1, "b": 2}
+    gg = {"a": 1, "c": 3}
+    result = dict_diff(sgg, gg)
+    expected = {"keys_only_in_sgg": {"b": 2}, "keys_only_in_gg": {"c": 3}}
+    assert result == expected
+
+
+def test_dict_diff_nested_dict():
+    sgg = {"a": {"x": 1, "y": 2}}
+    gg = {"a": {"x": 1, "y": 3}}
+    result = dict_diff(sgg, gg)
+    expected = {"a": {"y": {"sgg_dict": 2, "gg_dict": 3}}}
+    assert result == expected
+
+
+def test_dict_diff_list_of_dicts():
+    sgg = {"a": [{"id": 1, "val": 10}, {"id": 2, "val": 20}]}
+    gg = {"a": [{"id": 2, "val": 20}, {"id": 3, "val": 30}]}
+    result = dict_diff(sgg, gg, key_indexes={"a": "id"})
+    expected = {
+        "a": {
+            "index_key": "id",
+            "found_only_in_sgg": [{"id": 1, "val": 10}],
+            "found_only_in_gg": [{"id": 3, "val": 30}],
+            "different_values": {},
+        }
+    }
+    assert result == expected
+
+
+def test_dict_diff_list_of_dicts_with_value_diff():
+    sgg = {"a": [{"id": 1, "val": 10}]}
+    gg = {"a": [{"id": 1, "val": 99}]}
+    result = dict_diff(sgg, gg, key_indexes={"a": "id"})
+    expected = {
+        "a": {
+            "index_key": "id",
+            "found_only_in_sgg": [],
+            "found_only_in_gg": [],
+            "different_values": {
+                1: {"sgg_dict": {"id": 1, "val": 10}, "gg_dict": {"id": 1, "val": 99}}
+            },
+        }
+    }
+    assert result == expected
+
+
+def test_diff_list_of_dicts_basic():
+    sgg_list = [{"id": 1, "val": 100}, {"id": 2, "val": 200}]
+    gg_list = [{"id": 2, "val": 200}, {"id": 3, "val": 300}]
+    result = diff_list_of_dicts(sgg_list, gg_list, index_key="id")
+    expected = {
+        "index_key": "id",
+        "found_only_in_sgg": [{"id": 1, "val": 100}],
+        "found_only_in_gg": [{"id": 3, "val": 300}],
+        "different_values": {},
+    }
+    assert result == expected
+
+
+def test_diff_list_of_dicts_value_mismatch():
+    sgg_list = [{"id": 1, "val": 100}]
+    gg_list = [{"id": 1, "val": 101}]
+    result = diff_list_of_dicts(sgg_list, gg_list, index_key="id")
+    expected = {
+        "index_key": "id",
+        "found_only_in_sgg": [],
+        "found_only_in_gg": [],
+        "different_values": {
+            1: {"sgg_dict": {"id": 1, "val": 100}, "gg_dict": {"id": 1, "val": 101}}
+        },
+    }
+    assert result == expected
