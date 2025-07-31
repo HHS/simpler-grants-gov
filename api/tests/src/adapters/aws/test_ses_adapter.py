e8eb23e014a2bfa04921fa94be781b5c4046951b
bruk <brukty@gmail.com>
2025-07-31 11:11:31 -0400

    add time

--- api/tests/src/adapters/aws/test_ses_adapter.py
+++ api/tests/src/adapters/aws/test_ses_adapter.py
@@ -10,14 +10,14 @@ def test_ses_adapter():
         SuppressedDestination(
             EmailAddress="bounce@simulator.amazonses.com",
             Reason="BOUNCE",
-            LastUpdateTime=datetime(2020, 1, 1),
+            LastUpdateTime=datetime(2020, 1, 1, 12, 0, 0),
         )
     )
     client.add_mock_responses(
         SuppressedDestination(
             EmailAddress="bounce2@simulator.amazonses.com",
             Reason="COMPLAINT",
-            LastUpdateTime=datetime(2025, 1, 1),
+            LastUpdateTime=datetime(2025, 1, 1, 9, 30, 0),
         )
     )
 
@@ -27,15 +27,15 @@ def test_ses_adapter():
 
     assert client.mock_responses[0].email_address == "bounce@simulator.amazonses.com"
     assert client.mock_responses[0].reason == "BOUNCE"
-    assert client.mock_responses[0].last_update_time == datetime(2020, 1, 1)
+    assert client.mock_responses[0].last_update_time == datetime(2020, 1, 1, 12, 0, 0)
 
     assert client.mock_responses[1].email_address == "bounce2@simulator.amazonses.com"
     assert client.mock_responses[1].reason == "COMPLAINT"
-    assert client.mock_responses[1].last_update_time == datetime(2025, 1, 1)
+    assert client.mock_responses[1].last_update_time == datetime(2025, 1, 1, 9, 30, 0)
 
     resp = client.list_suppressed_destinations(start_date=datetime(2022, 1, 1))
     data = resp["SuppressedDestinationSummaries"]
 
     assert data[0]["EmailAddress"] == "bounce2@simulator.amazonses.com"
     assert data[0]["Reason"] == "COMPLAINT"
-    assert data[0]["LastUpdateTime"] == datetime(2025, 1, 1)
+    assert data[0]["LastUpdateTime"] == datetime(2025, 1, 1, 9, 30, 0)
