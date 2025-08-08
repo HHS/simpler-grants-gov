308730592626b8c7e7cad71cea79da47f86356d8
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-08-08 08:13:58 -0400

    [Issue #4517] clean up display and spacing on opportunity detail (#5888)
    Adjust some spacing and typography on opportunity detail to be more in line with designs

--- frontend/src/components/opportunity/OpportunityAwardInfo.tsx
+++ frontend/src/components/opportunity/OpportunityAwardInfo.tsx
@@ -73,7 +73,7 @@ const OpportunityAwardInfo = ({ opportunityData }: Props) => {
   };
 
   return (
-    <div className="margin-top-2">
+    <div className="margin-top-4">
       <h2>Award</h2>
       <Grid row className="margin-top-2 grid-gap-2">
         {Object.entries(awardGridInfo).map(([title, content], index) => (
