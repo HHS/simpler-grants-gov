80433bc181abfebcb8b785b146868da90bcfee73
Doug Schrashun <dougschrashun@navapbc.com>
2025-08-11 09:50:50 -0400

    working, with lots of logs. should add some automated tests if possible

--- frontend/src/components/ActivityMonitor.tsx
+++ frontend/src/components/ActivityMonitor.tsx
@@ -2,24 +2,15 @@
 
 import { useUser } from "src/services/auth/useUser";
 
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function ActivityMonitor() {
   const { user, refreshIfExpiring, refreshIfExpired, logoutLocalUser } =
     useUser();
-  // const [clickListener, setClickListener] = useState<EventListener | null>(
-  //   null,
-  // );
-  // const [keyboardListener, setKeyboardListener] =
-  //   useState<EventListener | null>(null);
 
   const [listening, setListening] = useState(false);
+  const handlerRef = useRef<EventListener>(null);
 
-  // an issue is that the refresh that should happen to log the user out during testing is not doing that
-  // because on the API side the login is still valid
-  // Maybe we move the logout to somewhere else? but this shouldn't be an issue as long as expiration times match up with the API
-  // can we adjust the API's expiration time for testing?
-  // even with adjusting the API expiration we're still trying to log out after log out. I think the handlers are not being properly removed, we may need to go back to holding them in state?
   const refreshTokenIfExpiringOrLogout = useCallback(() => {
     console.log("~~~ doing it");
     refreshIfExpired()
@@ -40,23 +31,25 @@ export function ActivityMonitor() {
   const addHandlers = useCallback(() => {
     console.log("~~~ maybe add handlers");
     if (!listening) {
-      console.log("~~~ YES add handlers");
       document.addEventListener("click", refreshTokenIfExpiringOrLogout);
       document.addEventListener("keydown", refreshTokenIfExpiringOrLogout);
+      // setHandler(refreshTokenIfExpiringOrLogout);
+      console.log("~~~ YES add handlers", !!refreshTokenIfExpiringOrLogout);
       setListening(true);
     }
   }, [refreshTokenIfExpiringOrLogout, listening]);
 
   const removeHandlers = useCallback(() => {
+    console.log("~~~ REMOVING it???", listening, handlerRef.current);
     if (!listening) {
       console.log("~~~ not REMOVING it");
       return;
     }
-    console.log("~~~ REMOVING it");
-    document.removeEventListener("click", refreshTokenIfExpiringOrLogout);
-    document.removeEventListener("keydown", refreshTokenIfExpiringOrLogout);
+    document.removeEventListener("click", handlerRef.current);
+    document.removeEventListener("keydown", handlerRef.current);
     setListening(false);
-  }, [refreshTokenIfExpiringOrLogout, listening]);
+    handlerRef.current = null;
+  }, [listening]);
 
   useEffect(() => {
     if (!user || !user.token) {
@@ -66,6 +59,13 @@ export function ActivityMonitor() {
     }
     addHandlers();
   }, [user, addHandlers, removeHandlers]);
+
+  useEffect(() => {
+    if (refreshTokenIfExpiringOrLogout && !listening) {
+      console.log("~~~ refreshing refresh function", listening);
+      handlerRef.current = refreshTokenIfExpiringOrLogout;
+    }
+  }, [refreshTokenIfExpiringOrLogout, listening]);
 
   return null;
 }
