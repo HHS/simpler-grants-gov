20426785cc3510fad52d311b4e7cbc909877493f
Doug Schrashun <dougschrashun@navapbc.com>
2025-08-15 09:41:41 -0400

    revert testing related code

--- frontend/src/constants/auth.ts
+++ frontend/src/constants/auth.ts
@@ -1,7 +1,7 @@
 // 10 minutes
-export const clientTokenRefreshInterval = 30 * 1000;
+export const clientTokenRefreshInterval = 10 * 60 * 1000;
 
 // 15 minutes
-export const clientTokenExpirationInterval = 60 * 1000;
+export const clientTokenExpirationInterval = 15 * 60 * 1000;
 
 export const LOGIN_URL = "/api/auth/login";
