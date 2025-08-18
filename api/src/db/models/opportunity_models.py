01a8ef2ff141325a37512e383df78679ca189fed
bruk <brukty@gmail.com>
2025-08-18 11:28:06 -0400

    new mig update model

--- api/src/db/models/opportunity_models.py
+++ api/src/db/models/opportunity_models.py
@@ -498,10 +498,7 @@ class OpportunityVersion(ApiSchemaTable, TimestampMixin):
 class ExcludedOpportunityReview(ApiSchemaTable, TimestampMixin):
     __tablename__ = "excluded_opportunity_review"
 
-    opportunity_id: Mapped[uuid.UUID] = mapped_column(
-        UUID, ForeignKey(Opportunity.opportunity_id), primary_key=True, index=True
-    )
-    opportunity: Mapped[Opportunity] = relationship(Opportunity)
+    opportunity_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
     omb_review_status_display: Mapped[str]
     omb_review_status_date: Mapped[date | None]
     last_update_date: Mapped[datetime | None]
--- api/tests/src/db/models/factories.py
+++ api/tests/src/db/models/factories.py
@@ -2664,8 +2664,7 @@ class ExcludedOpportunityReviewFactory(BaseFactory):
     class Meta:
         model = opportunity_models.ExcludedOpportunityReview
 
-    opportunity = factory.SubFactory(OpportunityFactory)
-    opportunity_id = factory.LazyAttribute(lambda s: s.opportunity.opportunity_id)
+    opportunity_id = factory.Sequence(lambda n: n)
     omb_review_status_display = factory.Faker("random_element", elements=["RETURNED", "REVIEWABLE"])
     omb_review_status_date = factory.Faker("date_between", start_date="-5y", end_date="-3y")
     last_update_date = factory.Faker("date_time_between", start_date="-5y", end_date="-3y")
