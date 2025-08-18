f070de81ae9592633e88861fda798a4adaf04d24
babebe <brukty@gmail.com>
2025-08-18 14:53:12 -0400

    [Issue #5913] add  excluded opportunity review table (#5949)
    ## Summary
    Fixes for https://github.com/HHS/simpler-grants-gov/issues/5913
    
    ## Changes proposed
    
    Created table for ExcludedOpportunityReview table and factory.
    
    ---------
    
    Co-authored-by: nava-platform-bot <platform-admins@navapbc.com>

--- api/tests/src/db/models/factories.py
+++ api/tests/src/db/models/factories.py
@@ -2658,3 +2658,13 @@ class SuppressedEmailFactory(BaseFactory):
     last_update_time = factory.Faker(
         "date_time_between", start_date="-1y", end_date="now", tzinfo=timezone.utc
     )
+
+
+class ExcludedOpportunityReviewFactory(BaseFactory):
+    class Meta:
+        model = opportunity_models.ExcludedOpportunityReview
+
+    opportunity_id = factory.Sequence(lambda n: n)
+    omb_review_status_display = factory.Faker("random_element", elements=["RETURNED", "REVIEWABLE"])
+    omb_review_status_date = factory.Faker("date_between", start_date="-5y", end_date="-3y")
+    last_update_date = factory.Faker("date_time_between", start_date="-5y", end_date="-3y")
