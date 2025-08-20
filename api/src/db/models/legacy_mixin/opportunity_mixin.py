04a0cb8af97bf824f6b0acfc5923a3492c43030d
bruk <brukty@gmail.com>
2025-08-20 17:25:03 -0400

    add prim key

--- api/src/db/models/legacy_mixin/opportunity_mixin.py
+++ api/src/db/models/legacy_mixin/opportunity_mixin.py
@@ -49,7 +49,7 @@ class TopportunityCfdaMixin:
 
 @declarative_mixin
 class VopportunitySummaryMixin:
-    opportunity_id: Mapped[int]
+    opportunity_id: Mapped[int] = mapped_column(primary_key=True)
     omb_review_status_display: Mapped[str | None]
     omb_review_status_date: Mapped[datetime.date | None]
     fo_last_upd_dt: Mapped[datetime.datetime | None]
