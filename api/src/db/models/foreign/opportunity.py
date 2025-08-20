d967f06f0de4f290161e3531f476a30a13b28c92
bruk <brukty@gmail.com>
2025-08-20 16:56:38 -0400

    remove relation

--- api/src/db/models/foreign/opportunity.py
+++ api/src/db/models/foreign/opportunity.py
@@ -31,8 +31,3 @@ class TopportunityCfda(foreignbase.ForeignBase, opportunity_mixin.TopportunityCf
 
 class VopportunitySummary(foreignbase.ForeignBase, opportunity_mixin.VopportunitySummaryMixin):
     __tablename__ = "vopportunity_summary"
-
-    opportunity: Mapped[Topportunity | None] = relationship(
-        primaryjoin=lambda: TopportunityCfda.opportunity_id == foreign(Topportunity.opportunity_id),
-        uselist=False,
-    )
