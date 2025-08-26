1de18eba5e3316f5d72219fc89507378965aaa07
Doug Schrashun <dougschrashun@navapbc.com>
2025-08-26 13:53:23 -0400

    replace search page with v2

--- frontend/src/app/[locale]/(base)/search/page.tsx
+++ frontend/src/app/[locale]/(base)/search/page.tsx
@@ -1,45 +1,37 @@
-import { Metadata } from "next";
 import { SEARCH_CRUMBS } from "src/constants/breadcrumbs";
 import { environment } from "src/constants/environments";
 import withFeatureFlag from "src/services/featureFlags/withFeatureFlag";
+import { performAgencySearch } from "src/services/fetch/fetchers/agenciesFetcher";
 import { searchForOpportunities } from "src/services/fetch/fetchers/searchFetcher";
 import QueryProvider from "src/services/search/QueryProvider";
 import { OptionalStringDict } from "src/types/generalTypes";
-import { LocalizedPageProps } from "src/types/intl";
-import { Breakpoints } from "src/types/uiTypes";
 import { convertSearchParamsToProperTypes } from "src/utils/search/searchUtils";
 
 import { useTranslations } from "next-intl";
-import { getTranslations, setRequestLocale } from "next-intl/server";
+import { setRequestLocale } from "next-intl/server";
 import { redirect } from "next/navigation";
-import { use } from "react";
+import { Suspense, use } from "react";
 
 import BetaAlert from "src/components/BetaAlert";
 import Breadcrumbs from "src/components/Breadcrumbs";
-import ContentDisplayToggle from "src/components/ContentDisplayToggle";
+import { DrawerUnit } from "src/components/drawer/DrawerUnit";
+import { AndOrPanel } from "src/components/search/AndOrPanel";
+import { FilterPillPanel } from "src/components/search/FilterPillPanel";
+import { PillListSkeleton } from "src/components/search/PillList";
 import { SaveSearchPanel } from "src/components/search/SaveSearchPanel";
 import SearchAnalytics from "src/components/search/SearchAnalytics";
-import { LegacySearchBar } from "src/components/search/SearchBarWithLabel";
+import { NewSearchBar } from "src/components/search/SearchBarWithLabel";
 import SearchCallToAction from "src/components/search/SearchCallToAction";
-import SearchFilters from "src/components/search/SearchFilters";
+import { SearchDrawerFilters } from "src/components/search/SearchDrawerFilters";
+import { SearchDrawerHeading } from "src/components/search/SearchDrawerHeading";
 import SearchResults from "src/components/search/SearchResults";
-import { SearchVersionTwo } from "src/components/search/SearchVersionTwo";
 
-export async function generateMetadata({ params }: LocalizedPageProps) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale });
-  const meta: Metadata = {
-    title: t("Search.title"),
-    description: t("Search.metaDescription"),
-  };
-  return meta;
-}
 type SearchPageProps = {
   searchParams: Promise<OptionalStringDict>;
   params: Promise<{ locale: string }>;
 };
 
-function Search({ searchParams, params }: SearchPageProps) {
+export function Search({ searchParams, params }: SearchPageProps) {
   const { locale } = use(params);
   const resolvedSearchParams = use(searchParams);
   setRequestLocale(locale);
@@ -47,15 +39,6 @@ function Search({ searchParams, params }: SearchPageProps) {
 
   const convertedSearchParams =
     convertSearchParamsToProperTypes(resolvedSearchParams);
-  const {
-    agency,
-    category,
-    eligibility,
-    fundingInstrument,
-    query,
-    status,
-    topLevelAgency,
-  } = convertedSearchParams;
 
   if (!("page" in resolvedSearchParams)) {
     resolvedSearchParams.page = "1";
@@ -63,71 +46,98 @@ function Search({ searchParams, params }: SearchPageProps) {
 
   const searchResultsPromise = searchForOpportunities(convertedSearchParams);
 
+  // this represents the list of agencies as filtered based on the user's selected opportunity status
+  // to be used to form the list of agencies available from the agency filter
+  const filteredAgencyListPromise = performAgencySearch({
+    selectedStatuses: Array.from(convertedSearchParams.status),
+  });
+
+  // this represents the list of ALL agencies regardless of the user's selected opportunity status
+  // since a user may have agencies selected in a filter that only have archived / close opportunities
+  // and may then alter the list of agency options by deselecting archived / closed status
+  // this full list ensures that filter pills will always have labels regardless of opportunity status selection
+  const agencyListPromise = performAgencySearch();
+
   return (
     <QueryProvider>
       <SearchAnalytics
         params={resolvedSearchParams}
         newRelicEnabled={environment.NEW_RELIC_ENABLED === "true"}
       />
-      <BetaAlert
-        containerClasses="margin-top-5"
-        heading={t("betaAlert.alertTitle")}
-        alertMessage={t.rich("betaAlert.alert", {
-          ethnioSurveyLink: (chunks) => (
-            <a
-              href="https://ethn.io/16188"
-              target="_blank"
-              className="usa-link--external"
-            >
-              {chunks}
-            </a>
-          ),
-        })}
-      />
-      <div className="grid-container">
-        <Breadcrumbs breadcrumbList={SEARCH_CRUMBS} />
-        <SearchCallToAction />
-        <div className="search-bar">
-          <LegacySearchBar queryTermFromParent={query} />
-        </div>
-        <div className="grid-row grid-gap">
-          <div className="tablet:grid-col-4">
-            <ContentDisplayToggle
-              showCallToAction={t("filterDisplayToggle.showFilters")}
-              hideCallToAction={t("filterDisplayToggle.hideFilters")}
-              breakpoint={Breakpoints.TABLET}
-              type="centered"
-            >
-              <SaveSearchPanel />
-              <SearchFilters
-                opportunityStatus={status}
-                eligibility={eligibility}
-                category={category}
-                fundingInstrument={fundingInstrument}
-                agency={agency}
-                topLevelAgency={topLevelAgency}
-                searchResultsPromise={searchResultsPromise}
+      <div className="bg-base-lightest">
+        <BetaAlert
+          containerClasses="padding-top-5"
+          heading={t("betaAlert.alertTitle")}
+          alertMessage={t.rich("betaAlert.alert", {
+            ethnioSurveyLink: (chunks) => (
+              <a
+                href="https://ethn.io/16188"
+                target="_blank"
+                className="usa-link--external"
+              >
+                {chunks}
+              </a>
+            ),
+          })}
+        />
+        <div className="grid-container">
+          <Breadcrumbs
+            breadcrumbList={SEARCH_CRUMBS}
+            className="bg-base-lightest"
+          />
+          <SearchCallToAction />
+          <div className="tablet:display-flex tablet:margin-bottom-2 margin-top-0">
+            <div className="flex-6 flex-align-self-end">
+              <NewSearchBar
+                tableView={true}
+                queryTermFromParent={convertedSearchParams.query}
               />
-            </ContentDisplayToggle>
+            </div>
+            <div className="display-flex tablet:flex-2 desktop:flex-5 margin-y-2 tablet:margin-y-0">
+              <div className="flex-2 flex-align-self-end">
+                <DrawerUnit
+                  drawerId="search-filter-drawer"
+                  closeText={t("drawer.submit")}
+                  openText={t("filterDisplayToggle.drawer")}
+                  headingText={<SearchDrawerHeading />}
+                  iconName="filter_list"
+                  buttonClass="tablet:margin-x-auto"
+                >
+                  <SearchDrawerFilters
+                    searchParams={convertedSearchParams}
+                    searchResultsPromise={searchResultsPromise}
+                    agencyListPromise={filteredAgencyListPromise}
+                  />
+                </DrawerUnit>
+              </div>
+              <div className="flex-3 flex-align-self-end display-none desktop:display-block">
+                <SaveSearchPanel />
+              </div>
+            </div>
           </div>
-          <div className="tablet:grid-col-8">
-            <SearchResults
+          <AndOrPanel hasSearchTerm={!!convertedSearchParams.query} />
+          <Suspense fallback={<PillListSkeleton />}>
+            <FilterPillPanel
               searchParams={convertedSearchParams}
-              loadingMessage={t("loading")}
-              searchResultsPromise={searchResultsPromise}
-            ></SearchResults>
-          </div>
+              agencyListPromise={agencyListPromise}
+            />
+          </Suspense>
         </div>
+      </div>
+      <div className="grid-container">
+        <SearchResults
+          searchParams={convertedSearchParams}
+          loadingMessage={t("loading")}
+          searchResultsPromise={searchResultsPromise}
+        ></SearchResults>
       </div>
     </QueryProvider>
   );
 }
 
 // Exports page behind both feature flags
-export default withFeatureFlag<SearchPageProps, React.ReactNode>(
-  withFeatureFlag<SearchPageProps, never>(Search, "searchOff", () =>
-    redirect("/maintenance"),
-  ),
-  "searchDrawerOn",
-  (props) => <SearchVersionTwo {...props} />,
+export default withFeatureFlag<SearchPageProps, never>(
+  Search,
+  "searchOff",
+  () => redirect("/maintenance"),
 );
