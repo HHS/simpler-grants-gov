997b61225e6db535bd16cbbaf609caaedd2503e0
Michael Chouinard <46358556+chouinar@users.noreply.github.com>
2025-09-11 09:36:44 -0400

    [Issue #6209] Add support for math summation and arrays in pre/post population logic (#6300)
    ## Summary
    
    Fixes #6209
    
    ## Changes proposed
    * Add support for math summation of fields
    * Add support for pre/post populating fields with arrays
    * Add support for pre/post populating fields while fetching values from
    arrays
    * Allow a way to specify an order to JSON rules
    
    ## Context for reviewers
    This started as a prototype / tech spec and evolved a bit into the
    actual implementation. This is a fairly big refactor of how we do
    pre/post population, but doesn't require reconfiguring any of our
    existing configurations as the config itself just has some new features.
    
    Summing several fields and storing it in another field requires two
    things:
    * A way to fetch fields including supporting arrays and relative paths
    * A way to handle processing fields within arrays
    
    Arrays were by far the most complex part of this (fetching values is
    mostly done with the jsonpath_ng library). When building the rules we
    need to handle running rules a variable number of times depending on the
    data, and populating data required a complete rewrite of the field
    population logic (note: jsonpath_ng's populate functions couldn't be
    used as the way they work with arrays would cause issues - if an array
    doesn't exist it makes it an object which doesn't make sense).
    
    See the updated readme for more info on using this, as well as the
    comments throughout giving plenty of examples.
    
    ## Validation steps
    A considerable number of new tests were added to test every piece of
    this change, this was largely built by first setting up the tests and
    adjusting the code until they worked.

--- api/src/form_schema/rule_processing/json_rule_context.py
+++ api/src/form_schema/rule_processing/json_rule_context.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from src.api.response import ValidationErrorDetail
 from src.db.models.competition_models import ApplicationForm
 from src.db.models.opportunity_models import Opportunity
+from src.form_schema.rule_processing.json_rule_util import build_path_str
 
 
 @dataclass
@@ -13,6 +14,24 @@ class JsonRuleConfig:
     do_pre_population: bool = True
     do_post_population: bool = True
     do_field_validation: bool = True
+
+
+@dataclass
+class JsonRule:
+    """Container for holding a JSON rule that we're going to process"""
+
+    handler: str
+    rule: dict
+    path: list[str]
+
+    order: int = 1
+
+    def get_log_context(self) -> dict:
+        return {
+            "path": build_path_str(self.path),
+            "handler": self.handler,
+            "rule": self.rule.get("rule", "unknown"),
+        }
 
 
 class JsonRuleContext:
@@ -26,6 +45,8 @@ class JsonRuleContext:
         self.json_data = copy.deepcopy(self.application_form.application_response)
 
         self.validation_issues: list[ValidationErrorDetail] = []
+
+        self.rules: list[JsonRule] = []
 
     @property
     def opportunity(self) -> Opportunity:
