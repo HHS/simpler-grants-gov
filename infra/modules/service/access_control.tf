57a39e1f3346af794434216d0d196650c2b53303
Matt Dragon <mattdragon@navapbc.com>
2025-09-12 09:09:11 -0400

    Add IAM action for pulling bouncing emails

--- infra/modules/service/access_control.tf
+++ infra/modules/service/access_control.tf
@@ -134,7 +134,7 @@ data "aws_iam_policy_document" "email_access" {
   statement {
     sid       = "ListSuppressedDestinations"
     actions   = ["ses:ListSuppressedDestinations"]
-    resources = ["arn:aws:ses:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}"]
+    resources = ["*"]
   }
 }
 
