dda720105f2411d38608461cc9f3c69bcfa9834d
bruk <brukty@gmail.com>
2025-09-17 12:54:32 -0400

    update tests

--- api/tests/src/auth/test_endpoint_access_util.py
+++ api/tests/src/auth/test_endpoint_access_util.py
@@ -19,145 +19,190 @@ from tests.src.db.models.factories import (
     OrganizationFactory,
     OrganizationUserFactory,
     OrganizationUserRoleFactory,
+    RoleFactory,
+    UserFactory,
 )
 
+ORG_PRIVILEGES = [
+    Privilege.VIEW_APPLICATION,
+    Privilege.MODIFY_APPLICATION,
+    Privilege.SUBMIT_APPLICATION,
+    Privilege.START_APPLICATION,
+    Privilege.MANAGE_ORG_MEMBERS,
+    Privilege.MANAGE_ORG_ADMIN_MEMBERS,
+    Privilege.VIEW_ORG_MEMBERSHIP,
+]
 
-@pytest.fixture
-def app():
-    return ApplicationFactory()
-
-
-@pytest.fixture
-def org():
-    return OrganizationFactory.create()
-
-
-@pytest.fixture
-def app_owned_by_org(org):
-    return ApplicationFactory(with_organization=org)
-
-
-@pytest.fixture
-def org_user(app_owned_by_org):
-    return OrganizationUserFactory(organization=app_owned_by_org.organization)
+APP_PRIVILEGES = [
+    Privilege.VIEW_APPLICATION,
+    Privilege.MODIFY_APPLICATION,
+    Privilege.SUBMIT_APPLICATION,
+]
 
 
 @pytest.fixture
-def user_org_role(org_user):
-    return OrganizationUserRoleFactory(organization_user=org_user)
+def app(enable_factory_create):
+    return ApplicationFactory.create()
 
 
 @pytest.fixture
-def app_user(app):
-    return ApplicationUserFactory(application=app)
+def org_a(enable_factory_create):
+    return OrganizationFactory()
 
 
 @pytest.fixture
-def user_app_role(app_user):
-    return ApplicationUserRoleFactory(application_user=app_user)
+def org_b(enable_factory_create):
+    return OrganizationFactory()
+
+
+@pytest.fixture
+def app_owned_by_org_a(org_a):
+    return ApplicationFactory(organization=org_a)
+
+
+@pytest.fixture
+def app_owned_by_org_b(org_b):
+    return ApplicationFactory(organization=org_b)
+
+
+@pytest.fixture
+def org_role_privileges():
+    return RoleFactory(privileges=ORG_PRIVILEGES)
+
+
+@pytest.fixture
+def app_role_privileges():
+    return RoleFactory(privileges=APP_PRIVILEGES)
+
+
+@pytest.fixture
+def user_a_org_a(org_a, org_role_privileges):
+    return OrganizationUserRoleFactory(
+        organization_user=OrganizationUserFactory(organization=org_a), role=org_role_privileges
+    )
+
+
+@pytest.fixture
+def user_b_org_b(app_owned_by_org_b, org_role_privileges):
+    return OrganizationUserRoleFactory(
+        organization_user=OrganizationUserFactory(organization=app_owned_by_org_b.organization),
+        role=org_role_privileges,
+    )
+
+
+@pytest.fixture
+def user_a_app_a(app_owned_by_org_a, app_role_privileges):
+    return ApplicationUserRoleFactory(
+        application_user=ApplicationUserFactory(application=app_owned_by_org_a),
+        role=app_role_privileges,
+    )
+
+
+@pytest.fixture
+def user_b_app_b(app_owned_by_org_b, app_role_privileges):
+    return ApplicationUserRoleFactory(
+        application_user=ApplicationUserFactory(application=app_owned_by_org_b),
+        role=app_role_privileges,
+    )
+
+
+# test for multiple users with diff priv/role
+# create fixture for
+# 2 orgs 2 apps multiple users
+# user update-form privilige for internal role
+# add manage-agency-members , get-submitted-applications for agency
 
 
 class TestEndpointAccessUtil(BaseTestClass):
 
     # Organization
-    def test_get_roles_for_org_no_role(self, user, org):
+    def test_get_roles_for_org_no_role(self, user, org_a):
         """User has no organization role; should return an empty list"""
-        res = get_roles_for_org(user, org)
+        res = get_roles_for_org(user, org_a)
         assert not res
 
-    def test_get_roles_for_org_has_role(self, app_owned_by_org, user_org_role):
-        """User has a role in the organization  should return a non-empty list"""
-        res = get_roles_for_org(user_org_role.organization_user.user, app_owned_by_org.organization)
-        assert res
-
-    def test_get_roles_for_org_multiple_orgs(self, org_user, user_org_role, enable_factory_create):
-        """Set up user with two org links; only one matches the target org"""
-        user = org_user.user
-        OrganizationUserRoleFactory(organization_user=OrganizationUserFactory(user=user))
-
-        result = get_roles_for_org(user, org_user.organization)
-        assert result == [user_org_role.role]
-
-    @pytest.mark.parametrize(
-        "allowed_privilege",
-        [
-            # org role privileges
-            ({Privilege.MODIFY_APPLICATION}),
-            ({Privilege.SUBMIT_APPLICATION}),
-            ({Privilege.MANAGE_ORG_MEMBERS}),
-            ({Privilege.MANAGE_ORG_ADMIN_MEMBERS}),
-            ({Privilege.VIEW_ORG_MEMBERSHIP}),
-        ],
-    )
-    def test_can_access_organization_granted(
-        self, user_org_role, app_owned_by_org, allowed_privilege
-    ):
-        assert can_access(
-            user_org_role.organization_user.user, allowed_privilege, app_owned_by_org.organization
+    def test_get_roles_for_correct_org(self, user, org_a, org_b, org_role_privileges):
+        """User has organization roles across multiple organizations; should return only the target org roles"""
+        org_a_user_role = OrganizationUserRoleFactory(
+            organization_user=OrganizationUserFactory(organization=org_a, user=user),
+            role=org_role_privileges,
         )
 
-    def test_can_access_organization_denied(
-        self,
-        user_app_role,
-        app_owned_by_org,
-    ):
-        assert not can_access(
-            user_app_role.application_user.user,
-            {Privilege.VIEW_ORG_MEMBERSHIP},
-            app_owned_by_org.organization,
+        OrganizationUserRoleFactory(
+            organization_user=OrganizationUserFactory(organization=org_b, user=user),
+            role=org_role_privileges,
         )
+
+        res = get_roles_for_org(user, org_a)
+
+        user_orgs = user.organizations
+        assert len(user_orgs) == 2
+
+        assert len(res) == 1
+        assert res[0].role_id == org_a_user_role.role_id
+
+    @pytest.mark.parametrize("privilege", [{p} for p in ORG_PRIVILEGES])
+    def test_user_a_org_a_accessing_org_a(self, user_a_org_a, org_a, privilege):
+        """User with Org role accessing their own organization"""
+        assert can_access(user_a_org_a.organization_user.user, privilege, org_a)
+
+
+    @pytest.mark.parametrize("privilege", [{p} for p in ORG_PRIVILEGES])
+    def test_user_a_org_a_accessing_org_b(self, user_a_org_a, org_b, privilege):
+        """User with Org role accessing a different organization"""
+        assert not can_access(user_a_org_a.organization_user.user, privilege, org_b)
+
+    @pytest.mark.parametrize("privilege", [{p} for p in ORG_PRIVILEGES])
+    def test_user_a_app_a_accessing_org_a(self, user_a_app_a, org_a, privilege):
+        """User with App role trying to access organization-level privileges"""
+        assert not can_access(user_a_app_a.application_user.user, privilege, org_a)
 
     # Application
-
     def test_get_roles_for_app_no_role(self, user, app):
         """User has no application role; should return an empty list"""
         res = get_roles_for_app(user, app)
         assert not res
 
-    def test_get_roles_for_app_has_role(self, app_user, user_app_role):
-        """User has a role on the application; should return non-empty list"""
-        res = get_roles_for_app(app_user.user, app_user.application)
-        assert res
-
-    def test_get_roles_for_app_multiple_users(self, app_user, user_app_role, enable_factory_create):
-        # Set up an application with two users
-        application = app_user.application
-        # Add a second ApplicationUser link for the same application but different user
-        ApplicationUserFactory(application=application)
-        result = get_roles_for_app(app_user.user, application)
-        assert result == [user_app_role.role]
-
-    def test_get_roles_for_app_with_org_role_only(self, app_owned_by_org, user_org_role):
-        """
-        User has a role in the organization that owns the application,
-        but is not directly assigned to the application. get_roles_for_app should return the org-based roles.
-        """
-        user = user_org_role.organization_user.user
-        app = app_owned_by_org
-
-        # Make sure the user is NOT linked to the application directly
-        assert all(app_user.user != user for app_user in app.application_users)
-
-        result = get_roles_for_app(user, app)
-
-        # Should return the user's org roles since the app is owned by the org
-        expected_roles = user_org_role.organization_user.roles
-        assert result == expected_roles
-
-    @pytest.mark.parametrize(
-        "allowed_privilege",
-        [
-            # application role privileges
-            ({Privilege.VIEW_APPLICATION}),
-            ({Privilege.SUBMIT_APPLICATION}),
-            ({Privilege.MODIFY_APPLICATION}),
-        ],
-    )
-    def test_can_access_application_granted(self, user_app_role, app_user, allowed_privilege):
-        assert can_access(
-            user_app_role.application_user.user, allowed_privilege, app_user.application
+    def test_get_app_roles(self, user_a_org_a, user_a_app_a, org_a, org_role_privileges):
+        """Application is owned by an org for which a user has organization and application role. Should return both roles"""
+        user = user_a_app_a.application_user.user
+        OrganizationUserRoleFactory(
+            organization_user=OrganizationUserFactory(organization=org_a, user=user),
+            role=org_role_privileges,
         )
 
-    def test_can_access_application_denied(self, app_user):
-        # User has no role, access to app should be denied
-        assert not can_access(app_user.user, {Privilege.MODIFY_APPLICATION}, app_user.application)
+        result = get_roles_for_app(user, user_a_app_a.application_user.application)
+
+        assert len(result) == 2
+        assert set([role.role_id for role in result]) == set(
+            [user_a_org_a.role_id, user_a_app_a.role_id]
+        )
+
+    @pytest.mark.parametrize("privilege", [{p} for p in APP_PRIVILEGES])
+    def test_user_a_org_a_accessing_app_a(self, user_a_org_a, app_owned_by_org_a, privilege):
+        """User with Org role accessing application owned by their org"""
+        user = user_a_org_a.organization_user.user
+        assert can_access(user, privilege, app_owned_by_org_a)
+
+    @pytest.mark.parametrize("privilege", [{p} for p in APP_PRIVILEGES])
+    def test_user_a_org_a_accessing_app_b(self, user_a_org_a, app_owned_by_org_b, privilege):
+        """User with Org role accessing application owned by another org"""
+        assert not can_access(user_a_org_a.organization_user.user, privilege, app_owned_by_org_b)
+
+    @pytest.mark.parametrize("privilege", [{p} for p in APP_PRIVILEGES])
+    def test_user_a_app_a_accessing_app_b(self, user_a_app_a, app_owned_by_org_b, privilege):
+        """User with App role trying to access a different app"""
+        assert not can_access(user_a_app_a.application_user.user, privilege, app_owned_by_org_b)
+
+    @pytest.mark.parametrize("privilege", [{p} for p in APP_PRIVILEGES])
+    def test_user_a_app_b_accessing_app_b(self, user_b_app_b, app_owned_by_org_b, privilege):
+        """User with App role accessing their assigned app"""
+        assert can_access(user_b_app_b.application_user.user, privilege, app_owned_by_org_b)
+
+    @pytest.mark.parametrize("privilege", [{p} for p in APP_PRIVILEGES])
+    def test_app_user_accessing_standalone_app(self, app, app_role_privileges, privilege):
+        """User with App role accessing their app not owned by any org"""
+        app_user_role = ApplicationUserRoleFactory(
+            application_user=ApplicationUserFactory(application=app), role=app_role_privileges
+        )
+        assert can_access(app_user_role.application_user.user, privilege, app)
