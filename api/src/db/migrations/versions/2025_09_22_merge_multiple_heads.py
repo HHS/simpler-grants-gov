8a7811b509aaa9637010c696fd04e043ba9cb2b1
Michael Chouinard <chouinar6@gmail.com>
2025-09-22 12:46:05 -0400

    Lint

--- api/src/db/migrations/versions/2025_09_22_merge_multiple_heads.py
+++ api/src/db/migrations/versions/2025_09_22_merge_multiple_heads.py
@@ -6,9 +6,6 @@ Create Date: 2025-09-22 12:39:56.691320
 
 """
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "e4ac8da61902"
 down_revision = ("430b57cd6d23", "5de110bf1389")
