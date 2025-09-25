ac53d9fadc656222ad979ab42435243aebb9cd4b
bruk <brukty@gmail.com>
2025-09-25 14:09:29 -0400

    update test

--- api/tests/src/api/users/test_user_roles_and_privileges_post.py
+++ api/tests/src/api/users/test_user_roles_and_privileges_post.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from src.constants.lookup_constants import Privilege
 from tests.src.db.models.factories import (
     AgencyUserFactory,
@@ -39,3 +41,32 @@ def test_get_roles_and_privileges(enable_factory_create, client, user, db_sessio
     assert len(data["application_users"]) == 2
     assert len(data["user_agencies"]) == 1
     assert len(data["internal_user_roles"]) == 1
+
+
+def test_get_roles_and_privileges_none(
+    enable_factory_create, client, user, db_session, user_auth_token
+):
+    resp = client.post(
+        f"/v1/users/{user.user_id}/privileges", headers={"X-SGG-Token": user_auth_token}
+    )
+    data = resp.get_json()["data"]
+
+    assert resp.status_code == 200
+    assert data["user_id"] == str(user.user_id)
+
+    assert not data["organizations"]
+    assert not data["application_users"]
+    assert not data["user_agencies"]
+    assert not data["internal_user_roles"]
+
+
+def test_get_roles_and_privileges_unauthorized(
+    enable_factory_create, client, user, db_session, user_auth_token
+):
+    response = client.post(
+        f"/v1/users/{uuid4()}/privileges",
+        headers={"X-SGG-Token": user_auth_token},
+    )
+
+    assert response.status_code == 403
+    assert response.json["message"] == "Forbidden"
