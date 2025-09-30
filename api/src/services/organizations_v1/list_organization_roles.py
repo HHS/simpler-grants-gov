d6df83856c2d566ff5e078051902c8b5b3845c71
bruk <brukty@gmail.com>
2025-09-30 16:46:51 -0400

    query for static roels

--- api/src/services/organizations_v1/list_organization_roles.py
+++ api/src/services/organizations_v1/list_organization_roles.py
@@ -1,25 +1,33 @@
+from typing import Sequence
 from uuid import UUID
 
+from sqlalchemy import select
+
 from src.adapters import db
-from src.constants.static_role_values import ORG_ADMIN, ORG_MEMBER
-from src.db.models.user_models import Role, User
+from src.constants.lookup_constants import RoleType
+from src.db.models.user_models import LinkRoleRoleType, Role, User
 from src.services.organizations_v1.get_organization import get_organization_and_verify_access
 
 
 def get_organization_roles_and_verify_access(
     db_session: db.Session, user: User, organization_id: UUID
-) -> list[Role]:
+) -> Sequence[Role]:
     """Verifies that the user has access to the specified organization,
-    then returns the core roles associated with that organization.
+    then returns the roles associated with that organization.
 
     Note: Currently only core (default) roles are returned, not custom roles."""
-    # We are currently only grabbing the core roles
     get_organization_and_verify_access(db_session, user, organization_id)
-    return get_organization_roles()
+    return get_organization_roles(db_session)
 
 
-def get_organization_roles() -> list[Role]:
+def get_organization_roles(db_session: db.Session) -> Sequence[Role]:
     """Returns the organization core roles."""
-    core_org_roles = [ORG_ADMIN, ORG_MEMBER]
+    stmt = (
+        select(Role)
+        .join(LinkRoleRoleType)
+        .where(LinkRoleRoleType.role_type == RoleType.ORGANIZATION)
+    )
 
-    return core_org_roles
+    roles = db_session.execute(stmt).scalars().all()
+
+    return roles
