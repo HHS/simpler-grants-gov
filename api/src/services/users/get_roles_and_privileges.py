1ec167b4062601704e2d053ea4e385f10ea4d6e9
bruk <brukty@gmail.com>
2025-09-30 11:08:02 -0400

    simplify query and add raise 404

--- api/src/services/users/get_roles_and_privileges.py
+++ api/src/services/users/get_roles_and_privileges.py
@@ -5,6 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
 from src.adapters import db
+from src.api.route_utils import raise_flask_error
 from src.db.models.user_models import AgencyUser, ApplicationUser, OrganizationUser, User
 
 logger = logging.getLogger(__name__)
@@ -19,29 +20,23 @@ def get_roles_and_privileges(
         .where(User.user_id == user_id)
         .options(
             # Organization-related relationships
-            selectinload(User.organizations),
             selectinload(User.organizations).selectinload(
                 OrganizationUser.organization
-            ),  # Load Organization inside each OrganizationUser
-            selectinload(User.organizations).selectinload(OrganizationUser.organization_user_roles),
+            ),
             # Application-related relationships
-            selectinload(User.application_users),  # Load list of ApplicationUser
             selectinload(User.application_users).selectinload(
                 ApplicationUser.application
-            ),  # Load Application inside each ApplicationUser
-            selectinload(User.application_users).selectinload(
-                ApplicationUser.application_user_roles
             ),
             # Agency-related relationships
-            selectinload(User.user_agencies),  # Load list of AgencyUser
             selectinload(User.user_agencies).selectinload(
                 AgencyUser.agency
-            ),  # Load Agency inside each AgencyUser
-            selectinload(User.user_agencies).selectinload(AgencyUser.agency_user_roles),
+            ),
             # Internal roles
             selectinload(User.internal_user_roles),
         )
     )
     user = db_session.execute(stmt).scalar_one_or_none()
+    if not user:
+        raise_flask_error(404, "User not found")
 
     return user
