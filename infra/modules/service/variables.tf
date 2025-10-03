7b3e96be7f568252d45628d8e14067cfd8d1fb95
prasnava <parikatla@Prasanthreddys-MacBook-Pro-2.local>
2025-10-03 12:37:00 -0400

    update defaults

--- infra/modules/service/variables.tf
+++ infra/modules/service/variables.tf
@@ -281,8 +281,8 @@ variable "scheduled_jobs" {
     task_command        = list(string)
     schedule_expression = string
     state               = string
-    cpu                 = optional(number, 256)
-    mem                 = optional(number, 512)
+    cpu                 = optional(number, 1024)
+    mem                 = optional(number, 2048)
   }))
   default = {}
 }
