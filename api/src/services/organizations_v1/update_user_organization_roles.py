81979b5a8e4f75d3b043370ca8d429953da4b9b0
bruk <brukty@gmail.com>
2025-10-03 18:02:42 -0400

    update query , fix error , reorder logic

--- api/src/services/organizations_v1/update_user_organization_roles.py
+++ api/src/services/organizations_v1/update_user_organization_roles.py
@@ -7,10 +7,10 @@ from sqlalchemy import select
 from src.adapters import db
 from src.api.route_utils import raise_flask_error
 from src.auth.endpoint_access_util import can_access
-from src.constants.lookup_constants import Privilege
+from src.constants.lookup_constants import Privilege, RoleType
 from src.constants.static_role_values import ORG_ADMIN_ID
 from src.db.models.entity_models import Organization
-from src.db.models.user_models import OrganizationUser, OrganizationUserRole, Role, User
+from src.db.models.user_models import OrganizationUser, OrganizationUserRole, Role, User, LinkRoleRoleType
 from src.services.organizations_v1.get_organization import get_organization
 
 logger = logging.getLogger(__name__)
@@ -21,7 +21,10 @@ ADMIN_ROLES = [str(ORG_ADMIN_ID)]
 
 def get_role(db_session: db.Session, role_ids: list[UUID]) -> Sequence[Role]:
     """Retrieve Role objects matching the given role_ids"""
-    return db_session.execute(select(Role).where(Role.role_id.in_(role_ids))).scalars().all()
+    # TODO: In the future, extend this query to check if the role is either:
+    #         - a core role (shared across all orgs), OR
+    #         - owned by the organization making the request
+    return db_session.execute(select(Role).where(Role.role_id.in_(role_ids)).where(LinkRoleRoleType.role_type == RoleType.ORGANIZATION).where(Role.is_core.is_(True))).scalars().all()
 
 
 def validate_organization_user(
@@ -34,7 +37,7 @@ def validate_organization_user(
         .where(OrganizationUser.user_id == user_id)
     ).scalar_one_or_none()
     if not org_user:
-        raise_flask_error(403, "Forbidden")
+        raise_flask_error(404, message=f"Could not find User with ID {user_id}")
     return org_user
 
 
@@ -44,9 +47,8 @@ def update_user_organization_roles(
     """Update roles of a user in an organization, after validating permissions and membership."""
     logger.info("Attempting to update roles for user")
 
-    # Lookup organization and validate target user exists
+    # Lookup organization
     organization = get_organization(db_session, organization_id)
-    org_user = validate_organization_user(db_session, target_user_id, organization)
     # Permission checks
     role_ids = data["role_ids"]
     if not can_access(user, {Privilege.MANAGE_ORG_MEMBERS}, organization):
@@ -56,6 +58,9 @@ def update_user_organization_roles(
         logger.info("Admin role assignment requested")
         if not can_access(user, {Privilege.MANAGE_ORG_ADMIN_MEMBERS}, organization):
             raise_flask_error(403, "Forbidden")
+    # Validate target user exists
+    org_user = validate_organization_user(db_session, target_user_id, organization)
+
 
     # Fetch and assign new roles
     roles = get_role(db_session, role_ids)
