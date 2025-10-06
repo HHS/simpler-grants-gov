10796451d8086cfc017e368a6b690da75a6761d4
Doug Schrashun <dougschrashun@navapbc.com>
2025-10-06 09:58:33 -0400

    test display component

--- frontend/src/app/[locale]/(base)/organization/[id]/page.tsx
+++ frontend/src/app/[locale]/(base)/organization/[id]/page.tsx
@@ -3,12 +3,12 @@ import { ApiRequestError, parseErrorStatus } from "src/errors";
 import { getSession } from "src/services/auth/session";
 import withFeatureFlag from "src/services/featureFlags/withFeatureFlag";
 import { getOrganizationDetails } from "src/services/fetch/fetchers/organizationsFetcher";
-import { SamGovEntity } from "src/types/applicationResponseTypes";
 
-import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
 import { notFound, redirect } from "next/navigation";
-import { ErrorMessage, Grid, GridContainer } from "@trussworks/react-uswds";
+import { ErrorMessage, GridContainer } from "@trussworks/react-uswds";
+
+import { OrganizationInfo } from "src/components/organization/OrganizationInfo";
 
 type OrganizationDetailPageProps = {
   params: Promise<{ id: string }>;
@@ -44,54 +44,6 @@ export async function generateMetadata({
     description: t("Index.metaDescription"),
   };
 }
-
-const OrganizationInfo = ({
-  organizationDetails,
-}: {
-  organizationDetails: SamGovEntity;
-}) => {
-  const t = useTranslations("OrganizationDetail");
-  const {
-    ebiz_poc_email,
-    ebiz_poc_first_name,
-    ebiz_poc_last_name,
-    expiration_date,
-    uei,
-  } = organizationDetails;
-  return (
-    <>
-      <Grid row>
-        <Grid tablet={{ col: 3 }}>
-          <span className="text-bold padding-right-2">{t("ebizPoc")}:</span>
-          <span>
-            {ebiz_poc_first_name} {ebiz_poc_last_name}
-          </span>
-        </Grid>
-        <Grid tablet={{ col: 3 }}>
-          <span className="text-bold padding-right-2">{t("contact")}:</span>
-          <span>{ebiz_poc_email}</span>
-        </Grid>
-        <Grid tablet={{ col: 3 }}>
-          <span className="text-bold padding-right-2">{t("uei")}:</span>
-          <span>{uei}</span>
-        </Grid>
-        <Grid tablet={{ col: 3 }}>
-          <span className="text-bold padding-right-2">{t("expiration")}:</span>
-          <span>{expiration_date}</span>
-        </Grid>
-      </Grid>
-      <div className="margin-top-2">
-        {t.rich("visitSam", {
-          link: (chunk) => (
-            <a href="https://sam.gov" target="_blank">
-              {chunk}
-            </a>
-          ),
-        })}
-      </div>
-    </>
-  );
-};
 
 async function OrganizationDetail({ params }: OrganizationDetailPageProps) {
   const t = await getTranslations("OrganizationDetail");
