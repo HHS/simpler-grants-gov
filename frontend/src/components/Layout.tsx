7f0a7e0f73bb0951719fab6fe1442bb963f39995
Andy Cochran <andycochran@navapbc.com>
2025-10-09 17:05:34 -0400

    improve layout of Oct 1 message

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -40,22 +40,24 @@ export default function Layout({ children, locale }: Props) {
             {/* temp Oct 1, 2025 banner */}
             <div className="bg-warning-lighter padding-y-3">
               <div className="grid-container">
-                <p className="measure-none">
-                  There has been a lapse in appropriated federal funds as of
-                  October 1, 2025. <a href="https://grants.gov/">Grants.gov</a>{" "}
-                  and{" "}
-                  <a href="https://simpler.grants.gov/">Simpler.Grants.gov</a>{" "}
-                  will still be available, but service may be delayed with
-                  reduced Federal support staff presence.
-                </p>
-                <p className="measure-none">
-                  For those programs affected by the funding lapse, the{" "}
-                  <a href="https://grants.gov/">Grants.gov</a> and{" "}
-                  <a href="https://simpler.grants.gov/">Simpler.Grants.gov</a>{" "}
-                  systems will accept and store applications until such time as
-                  the responsible awarding agency has the authority and funding
-                  to return to normal business operations.
-                </p>
+                <div className="grid-row grid-gap-4 font-sans-2xs">
+                  <div className="tablet-lg:grid-col-5">
+                    There has been a lapse in appropriated federal funds as of
+                    October 1, 2025. <a href="https://grants.gov/">Grants.gov</a>{" "}
+                    and{" "}
+                    <a href="https://simpler.grants.gov/">Simpler.Grants.gov</a>{" "}
+                    will still be available, but service may be delayed with
+                    reduced Federal support staff presence.
+                  </div>
+                  <div className="tablet-lg:grid-col-7 margin-top-2 tablet-lg:margin-top-0">
+                    For those programs affected by the funding lapse, the{" "}
+                    <a href="https://grants.gov/">Grants.gov</a> and{" "}
+                    <a href="https://simpler.grants.gov/">Simpler.Grants.gov</a>{" "}
+                    systems will accept and store applications until such time as
+                    the responsible awarding agency has the authority and funding
+                    to return to normal business operations.
+                  </div>
+                </div>
               </div>
             </div>
             {/* end temp banner */}
