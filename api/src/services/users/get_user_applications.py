32e35a6bd6863611d316dc1d583a442afc3134fc
bruk <brukty@gmail.com>
2025-10-16 11:45:57 -0400

    add for priv through org and test

--- api/src/services/users/get_user_applications.py
+++ api/src/services/users/get_user_applications.py
@@ -1,18 +1,22 @@
 import logging
 from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import or_, select
 from sqlalchemy.orm import selectinload
 
 import src.adapters.db as db
 from src.constants.lookup_constants import Privilege
 from src.db.models.competition_models import Application
 from src.db.models.entity_models import Organization
-from src.db.models.user_models import ApplicationUser, ApplicationUserRole, Role, LinkRolePrivilege, OrganizationUser, \
-    OrganizationUserRole
+from src.db.models.user_models import (
+    ApplicationUser,
+    ApplicationUserRole,
+    LinkRolePrivilege,
+    OrganizationUser,
+    OrganizationUserRole,
+)
 
 logger = logging.getLogger(__name__)
-
 
 
 def get_user_applications(db_session: db.Session, user_id: UUID) -> list[Application]:
@@ -24,31 +28,37 @@ def get_user_applications(db_session: db.Session, user_id: UUID) -> list[Applica
     app_access_sq = (
         select(Application.application_id)
         .join(ApplicationUser, ApplicationUser.application_id == Application.application_id)
-        .join(ApplicationUserRole, ApplicationUserRole.application_user_id == ApplicationUser.application_user_id)
+        .join(
+            ApplicationUserRole,
+            ApplicationUserRole.application_user_id == ApplicationUser.application_user_id,
+        )
         .join(LinkRolePrivilege, LinkRolePrivilege.role_id == ApplicationUserRole.role_id)
         .where(
-            (ApplicationUser.user_id == user_id) &
-            (LinkRolePrivilege.privilege == Privilege.VIEW_APPLICATION)
+            (ApplicationUser.user_id == user_id)
+            & (LinkRolePrivilege.privilege == Privilege.VIEW_APPLICATION)
         )
     )
     # Applications user can view through organization roles
     org_access_sq = (
         select(Application.application_id)
         .join(OrganizationUser, OrganizationUser.organization_id == Application.organization_id)
-        .join(OrganizationUserRole, OrganizationUserRole.organization_user_id == OrganizationUser.organization_user_id)
+        .join(
+            OrganizationUserRole,
+            OrganizationUserRole.organization_user_id == OrganizationUser.organization_user_id,
+        )
         .join(LinkRolePrivilege, LinkRolePrivilege.role_id == OrganizationUserRole.role_id)
         .where(
-            (OrganizationUser.user_id == user_id) &
-            (LinkRolePrivilege.privilege == Privilege.VIEW_APPLICATION)
+            (OrganizationUser.user_id == user_id)
+            & (LinkRolePrivilege.privilege == Privilege.VIEW_APPLICATION)
         )
     )
     result = db_session.execute(
         select(Application)
         .where(
-            # or_(
+            or_(
                 Application.application_id.in_(app_access_sq),
-                # Application.application_id.in_(org_access_sq)
-            # )
+                Application.application_id.in_(org_access_sq),
+            )
         )
         .options(
             # Load the competition data
@@ -62,5 +72,3 @@ def get_user_applications(db_session: db.Session, user_id: UUID) -> list[Applica
     logger.info(f"Retrieved {len(applications)} applications for user {user_id}")
 
     return applications
-
-    db_session.execute(select(ApplicationUserRole)).scalars().all()
