14652a6edb03fa48e1d8a4188786cf8f54af6c80
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2025-10-20 08:48:23 -0400

    [Issue #6674] authorization gate refactors (#6765)
    Updates the AuthorizationGate component in a number of ways to make it more flexible to handle real world auth situations, particularly around application permissions.
    
    * Instead of checking for permissions based access on the frontend, hit new API endpoint to check access (note that this endpoint is actively being built out in ticket #6686. This change uses a dummy response, wiring up will happen in #6763)
    * make onUnauthorized optional
    * unless there is an error or an unauthorized response handled with the onUnauthorized handler, always pass BOTH fetched resource data and fetched permission check results as child prop and via context
    * passes children and resource call results to onUnauthorized to allow for more customized behavior
    * passes non 403 errors encountered in API requests down to children rather than handling them within the gate

--- frontend/src/types/userTypes.ts
+++ frontend/src/types/userTypes.ts
@@ -6,16 +6,16 @@ export interface UserOrganization extends Organization {
 
 export type GatedResourceTypes = "application" | "organization" | "agency";
 
-export type UserPrivilegeDefinition = {
+export interface UserPrivilegeDefinition {
   resourceId?: string;
   resourceType: GatedResourceTypes;
   privilege: string; // we can narrow this later
-};
+}
 
-export type UserPrivilegesDefinition = {
-  resourceId?: string;
-  privileges: string[]; // we can narrow this later
-};
+export interface UserPrivilegeResult extends UserPrivilegeDefinition {
+  authorized: boolean;
+  error?: string;
+}
 
 export type UserRole = {
   role_id: string;
