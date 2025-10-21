844dfa09fe4a95e1ce90ea6be648944c9b6630f1
bruk <brukty@gmail.com>
2025-10-21 18:11:04 -0400

    no app user if owned by org

--- api/tests/src/api/applications/test_application_routes.py
+++ api/tests/src/api/applications/test_application_routes.py
@@ -2590,9 +2590,8 @@ def test_application_start_with_organization_success(
     assert str(application.organization_id) == organization_id
     assert application.application_status == ApplicationStatus.IN_PROGRESS
 
-    # Assert it does not assign application user role
-    assert len(application.application_users) == 1
-    assert not application.application_users[0].roles
+    # Assert it does create an application user
+    assert not application.application_users
 
 
 def test_application_start_with_organization_and_custom_name(
