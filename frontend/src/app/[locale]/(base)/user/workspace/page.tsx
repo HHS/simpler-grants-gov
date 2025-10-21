44c5446c68a47c5a103c0632eced7afc0e0399d2
Doug Schrashun <dougschrashun@navapbc.com>
2025-10-21 16:40:34 -0400

    reorganize files

--- frontend/src/app/[locale]/(base)/user/workspace/page.tsx
+++ frontend/src/app/[locale]/(base)/user/workspace/page.tsx
@@ -3,20 +3,13 @@ import { getSession } from "src/services/auth/session";
 import withFeatureFlag from "src/services/featureFlags/withFeatureFlag";
 import { getUserOrganizations } from "src/services/fetch/fetchers/organizationsFetcher";
 import { getUserPrivileges } from "src/services/fetch/fetchers/userFetcher";
-import { Organization } from "src/types/applicationResponseTypes";
 import { LocalizedPageProps } from "src/types/intl";
-import { UserPrivilegesResponse } from "src/types/userTypes";
 
-import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import {
-  Button,
-  ErrorMessage,
-  Grid,
-  GridContainer,
-} from "@trussworks/react-uswds";
+import { ErrorMessage, GridContainer } from "@trussworks/react-uswds";
+
+import { UserOrganizationsList } from "src/components/workspace/UserOrganizationsList";
 
 import { UserOrganizationInvite } from "src/components/workspace/UserOrganizationInvite";
 
@@ -31,93 +24,6 @@ export async function generateMetadata({
   };
   return meta;
 }
-
-// find the user's role within the passed organization
-const userRoleForOrganization = (
-  organization: Organization,
-  user: UserPrivilegesResponse,
-): string => {
-  const organizationPrivilegeSet = user.organization_users.find(
-    (role) =>
-      role.organization.organization_id === organization.organization_id,
-  );
-  if (!organizationPrivilegeSet) {
-    console.error("no user roles for organization");
-    return "";
-  }
-  return organizationPrivilegeSet.organization_user_roles.length > 1
-    ? organizationPrivilegeSet.organization_user_roles
-        .map((role) => role.role_name)
-        .join(", ")
-    : organizationPrivilegeSet.organization_user_roles[0].role_name;
-};
-
-const OrganizationItem = ({
-  organization,
-  role,
-}: {
-  organization: Organization;
-  role: string;
-}) => {
-  const t = useTranslations("UserWorkspace");
-  return (
-    <li className="border-base-lighter border-1px padding-2 margin-top-2">
-      <Grid row>
-        <Grid tablet={{ col: 6 }}>
-          <div className="font-sans-2xs text-base-dark">{role}</div>
-          <h3 className="margin-top-0">
-            {organization.sam_gov_entity.legal_business_name}
-          </h3>
-        </Grid>
-        <Grid tablet={{ col: 6 }} className="flex-align-self-end text-right">
-          <Link href={`/organization/${organization.organization_id}`}>
-            <Button type="button">{t("organizationButtons.view")}</Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </li>
-  );
-};
-
-const NoOrganizations = () => {
-  const t = useTranslations("UserWorkspace.noOrganizations");
-  return (
-    <li className="border-primary border-1px padding-2 margin-top-2">
-      <h3>{t("title")}</h3>
-      <div>{t("description")}</div>
-    </li>
-  );
-};
-
-const UserOrganizationsList = ({
-  userOrganizations,
-  userRoles,
-}: {
-  userOrganizations: Organization[];
-  userRoles: UserPrivilegesResponse;
-}) => {
-  const t = useTranslations("UserWorkspace");
-  return (
-    <>
-      <h2>{t("organizations")}</h2>
-      <ul className="usa-list--unstyled">
-        {!userOrganizations.length ? (
-          <NoOrganizations />
-        ) : (
-          <>
-            {userOrganizations.map((userOrganization) => (
-              <OrganizationItem
-                organization={userOrganization}
-                role={userRoleForOrganization(userOrganization, userRoles)}
-                key={userOrganization.organization_id}
-              />
-            ))}
-          </>
-        )}
-      </ul>
-    </>
-  );
-};
 
 // add breadcrumb
 async function UserWorkspace() {
@@ -142,7 +48,7 @@ async function UserWorkspace() {
       userOrganizationsPromise,
     ]);
   } catch (e) {
-    console.error("Unable to fetch user details", e);
+    console.error("Unable to fetch user details or organizations", e);
   }
 
   return (
@@ -150,7 +56,7 @@ async function UserWorkspace() {
       <h1>
         {t.rich("title", {
           color: (chunks) => (
-            <span className="text-action-primary">{chunks}</span>
+            <span className="text-primary-dark">{chunks}</span>
           ),
         })}
       </h1>
