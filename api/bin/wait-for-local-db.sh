4e8e7e3e36c434efa9afd0a9a1848da7e6772cc8
Prasanthreddy Arikatla <prasanthreddyarikatla@navapbc.com>
2025-10-24 14:31:18 -0400

    wip

--- api/bin/wait-for-local-db.sh
+++ api/bin/wait-for-local-db.sh
@@ -5,15 +5,16 @@ set -e
 
 # Color formatting
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 
 MAX_WAIT_TIME=30 # seconds
 WAIT_TIME=0
 
-# Use pg_isready to wait for the DB to be ready to accept connections
+# Use pg_isready inside the docker container to wait for the DB to be ready
 # We check every 3 seconds and consider it failed if it gets to 30+
 # https://www.postgresql.org/docs/current/app-pg-isready.html
-until pg_isready -h localhost -d grants-db -q;
+until docker exec grants-db pg_isready -h localhost -d grants-db -q > /dev/null 2>&1;
 do
   echo "waiting on Postgres DB to initialize..."
   sleep 3
@@ -27,6 +28,6 @@ do
   fi
 done
 
-echo "Postgres DB is ready after ~${WAIT_TIME} seconds"
+echo -e "${GREEN}Postgres DB is ready after ~${WAIT_TIME} seconds${NO_COLOR}"
 
 
