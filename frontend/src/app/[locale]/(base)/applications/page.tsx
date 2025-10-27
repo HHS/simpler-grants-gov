e791fb06a9eae0aaae967566d136a212a581931c
Erin Pattison <erinpattison@navapbc.com>
2025-10-27 13:55:12 -0700

    feat: adding the skeleton of the Applications page

---
+++ frontend/src/app/[locale]/(base)/applications/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+import { LocalizedPageProps } from "src/types/intl";
+
+import { useTranslations } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import { use } from "react";
+import { GridContainer } from "@trussworks/react-uswds";
+
+export async function generateMetadata({ params }: LocalizedPageProps) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
+  const meta: Metadata = {
+    title: t("Applications.pageTitle"),
+    description: t("Applications.metaDescription"),
+  };
+  return meta;
+}
+
+export default function Applications({ params }: LocalizedPageProps) {
+  const { locale } = use(params);
+  setRequestLocale(locale);
+  const t = useTranslations("Applications");
+
+  return (
+    <>
+      <GridContainer>
+        <h1 className="margin-top-9 margin-bottom-7">{t("pageTitle")}</h1>
+        <div className="margin-bottom-15">
+          <div className="font-sans-xl text-bold margin-bottom-3">
+            {t("noApplicationsMessage.primary")}
+          </div>
+          <div>{t("noApplicationsMessage.secondary")}</div>
+        </div>
+      </GridContainer>
+    </>
+  );
+}
