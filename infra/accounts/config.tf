131d4af4d6016f488558aa65dae9c142cace9840
Sean Thomas <sean.thomas@navapbc.com>
2025-11-10 16:41:22 -0500

    ISSUE #6993 Simplify Config recorder management - remove moved block

--- infra/accounts/config.tf
+++ infra/accounts/config.tf
@@ -1,29 +1,8 @@
 # AWS Config recorder for Security Hub compliance (Config.1)
 #
-# This resource was originally managed by the NewRelic module but has been moved here
-# to maintain Security Hub compliance. The recorder is configured with:
-# - Service-linked role (AWSServiceRoleForConfig)
+# The Config recorder has been manually configured in AWS to meet Security Hub requirements:
+# - Service-linked role (AWSServiceRoleForConfig) instead of custom role
 # - Global resource recording enabled (includeGlobalResourceTypes: true)
 #
-# We use ignore_changes to prevent Terraform from modifying the manually-configured
-# compliant settings.
-
-resource "aws_config_configuration_recorder" "main" {
-  name     = "newrelic_configuration_recorder-simpler-grants-gov"
-  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
-
-  recording_group {
-    all_supported                 = true
-    include_global_resource_types = true
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
-# Move the resource from NewRelic module management to local management
-moved {
-  from = module.newrelic-aws-cloud-integrations.aws_config_configuration_recorder.newrelic_recorder
-  to   = aws_config_configuration_recorder.main
-}
+# The recorder is kept out of Terraform state because the NewRelic module creates
+# a non-compliant version. Terraform plans will show drift (1 to add) - this is expected.
