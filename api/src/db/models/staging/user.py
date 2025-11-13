3045cb6ec487d3d3bbeb2888a855cdff9a2faa8c
bruk <brukty@gmail.com>
2025-11-13 09:36:20 -0500

    rm changes

--- api/src/db/models/staging/user.py
+++ api/src/db/models/staging/user.py
@@ -6,7 +6,6 @@
 from sqlalchemy import Index
 
 from src.db.models.legacy_mixin import user_mixin
-from src.db.models.legacy_mixin.user_mixin import TuserProfileMixin, VuserAccountMixin
 from src.db.models.staging.staging_base import StagingBase, StagingParamMixin
 
 
@@ -30,31 +29,3 @@ class TsubscriptionOpportunityMixin(
     StagingBase, user_mixin.TsubscriptionOpportunityMixin, StagingParamMixin
 ):
     __tablename__ = "tsubscription_opportunity"
-
-
-class VuserAccount(StagingBase, VuserAccountMixin, StagingParamMixin):
-    __tablename__ = "v_user_account"
-
-    __table_args__ = (
-        # Indexes for query performance
-        Index("ix_v_user_account_email", "email"),
-        Index("ix_v_user_account_is_active", "is_active"),
-        Index("ix_v_user_account_is_deleted_legacy", "is_deleted_legacy"),
-        # Must include parent __table_args__ to inherit schema setting
-        # Without this, Alembic will recreate the entire table
-        StagingBase.__table_args__,
-    )  # type: ignore[assignment]
-
-
-class TuserProfile(StagingBase, TuserProfileMixin, StagingParamMixin):
-    __tablename__ = "tuser_profile"
-
-    __table_args__ = (
-        # Indexes for query performance
-        Index("ix_tuser_profile_user_account_id", "user_account_id"),
-        Index("ix_tuser_profile_profile_duns", "profile_duns"),
-        Index("ix_tuser_profile_profile_type_id", "profile_type_id"),
-        Index("ix_tuser_profile_is_deleted_legacy", "is_deleted_legacy"),
-        # Must include parent __table_args__ to inherit schema setting
-        StagingBase.__table_args__,
-    )  # type: ignore[assignment]
