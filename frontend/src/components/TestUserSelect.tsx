327d2f8625d3638b8d66129cb85d3bf56b32fc15
Doug Schrashun <dougschrashun@navapbc.com>
2025-11-14 10:57:42 -0500

    sign in works

--- frontend/src/components/TestUserSelect.tsx
+++ frontend/src/components/TestUserSelect.tsx
@@ -1,23 +1,41 @@
 import { useClientFetch } from "src/hooks/useClientFetch";
+import { useUser } from "src/services/auth/useUser";
 import { storeCurrentPage } from "src/services/sessionStorage/sessionStorage";
 import { TestUser } from "src/types/userTypes";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Select } from "@trussworks/react-uswds";
 
+// should only ever appear or be used in local / non-deployed environments
 export const TestUserSelect = ({ testUsers }: { testUsers: TestUser[] }) => {
-  const { clientFetch } = useClientFetch("unable to perform fake login");
+  const [loggingIn, setLoggingIn] = useState(false);
+  const router = useRouter();
+  const { refreshUser } = useUser();
+  const { clientFetch } = useClientFetch("unable to perform fake login", {
+    jsonResponse: false,
+  });
 
   const logInTestUser = (jwt: string) => {
     storeCurrentPage();
+    setLoggingIn(true);
     // will redirect to /login on success
-    clientFetch("/api/localQuickLogin", {
+    clientFetch("/api/user/local-quick-login", {
       method: "POST",
       body: JSON.stringify({
         jwt,
       }),
-    }).catch((e) => {
-      console.error("unable to log in local test user", e);
-    });
+    })
+      .then(() => {
+        return refreshUser();
+      })
+      .then(() => {
+        setLoggingIn(false);
+        router.refresh();
+      })
+      .catch((e) => {
+        console.error("unable to log in local test user", e);
+      });
   };
   const validTestUsers = testUsers.filter((testUser) => testUser.user_jwt);
   return (
@@ -26,6 +44,7 @@ export const TestUserSelect = ({ testUsers }: { testUsers: TestUser[] }) => {
       id="test-users-select"
       name="test-users=select"
       onChange={(e) => logInTestUser(e.target.value)}
+      disabled={loggingIn}
     >
       {validTestUsers.map((testUser) => (
         <option key={testUser.user_id} value={testUser.user_jwt}>
