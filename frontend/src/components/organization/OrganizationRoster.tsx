1b0a4a1ef9dc1003e46cf2d6516861aa2608b507
My Duong <myduong@navapbc.com>
2025-11-18 20:50:22 -0600

    chore: formatting

--- frontend/src/components/organization/OrganizationRoster.tsx
+++ frontend/src/components/organization/OrganizationRoster.tsx
@@ -84,9 +84,7 @@ export const OrganizationRoster = async ({
 
   let organizationUsers;
   try {
-    organizationUsers = await getOrganizationUsers(
-      organizationId,
-    );
+    organizationUsers = await getOrganizationUsers(organizationId);
   } catch (e) {
     console.error(e);
     return <ServerErrorAlert />;
