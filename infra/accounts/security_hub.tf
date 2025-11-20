cdfd957f19d51bae3e500be9ece3dc54cbd564e9
Sean Thomas <sean.thomas@navapbc.com>
2025-11-20 17:06:56 -0500

    [Issue #914] Update Security Hub config to match existing AWS settings and import resources

--- infra/accounts/security_hub.tf
+++ infra/accounts/security_hub.tf
@@ -5,7 +5,7 @@
 # Enable Security Hub
 resource "aws_securityhub_account" "main" {
   enable_default_standards  = false
-  control_finding_generator = "SECURITY_CONTROL"
+  control_finding_generator = "STANDARD_CONTROL"
   auto_enable_controls      = true
 }
 
