fefe17b19aecbcbd054335d0f53c17cf3e78381c
Sean Thomas <sean.thomas@navapbc.com>
2025-11-26 14:48:26 -0500

    Add Checkov skip for organization-level GuardDuty check
    CKV2_AWS_3 expects organization-level GuardDuty configuration, but this
    is a member account in an AWS Organization where the organization
    administrator (account 215331682793) manages GuardDuty settings.

--- infra/accounts/guardduty.tf
+++ infra/accounts/guardduty.tf
@@ -4,6 +4,7 @@
 
 # Import existing GuardDuty detector
 # To import: terraform import aws_guardduty_detector.main 94c62cc0d4fe7b2eb627a33e8273238c
+#checkov:skip=CKV2_AWS_3:This is a member account in an AWS Organization. Organization-level GuardDuty configuration is managed by the organization administrator account (215331682793).
 resource "aws_guardduty_detector" "main" {
   enable                       = true
   finding_publishing_frequency = "SIX_HOURS"
