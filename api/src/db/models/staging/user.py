a6446376fdad2fe1baadf2b51e28e3572c1da108
bruk <brukty@gmail.com>
2025-12-17 16:43:38 -0500

    rm models/factories

--- api/src/db/models/staging/user.py
+++ api/src/db/models/staging/user.py
@@ -5,19 +5,8 @@
 # match by oracle_fdw, but we are matching them for maintainability.
 from sqlalchemy import Index
 
-from src.db.models.legacy_mixin import user_mixin
 from src.db.models.legacy_mixin.user_mixin import TuserProfileMixin, VuserAccountMixin
 from src.db.models.staging.staging_base import StagingBase, StagingParamMixin
-
-
-# TODO(#7340): Evaluate removing TuserAccount/TuserAccountMapper - no longer synced from Oracle
-class TuserAccountMapper(StagingBase, user_mixin.TuserAccountMapperMixin, StagingParamMixin):
-    __tablename__ = "tuser_account_mapper"
-
-
-# TODO(#7340): Evaluate removing TuserAccount/TuserAccountMapper - no longer synced from Oracle
-class TuserAccount(StagingBase, user_mixin.TuserAccountMixin, StagingParamMixin):
-    __tablename__ = "tuser_account"
 
 
 class VuserAccount(StagingBase, VuserAccountMixin, StagingParamMixin):
