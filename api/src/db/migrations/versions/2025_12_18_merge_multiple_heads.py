bff50db1f616ead239d747cc385da20316a1f9c5
bruk <brukty@gmail.com>
2025-12-18 16:30:39 -0500

    merge migration heads

---
+++ api/src/db/migrations/versions/2025_12_18_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: 20cbfe4c4fed
+Revises: 22bf34acf454, 627e63fb1eae
+Create Date: 2025-12-18 21:29:53.652394
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20cbfe4c4fed"
+down_revision = ("22bf34acf454", "627e63fb1eae")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
