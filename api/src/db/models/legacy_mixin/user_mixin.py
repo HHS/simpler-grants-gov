4ec7eb172aa628a7ccd2a0f869979299805e8b1e
babebe <brukty@gmail.com>
2025-12-18 15:15:05 -0500

    [Issue #7340] user account and tuser account mapper cleanup (#7553)
    ## Summary
    
    <!-- Use "Fixes" to automatically close issue upon PR merge. Use "Work
    for" when UAT is required. -->
    Fixes / Work for #7340  
    
    ## Changes proposed
    - Removed legacy models for `tuser_account` and `tuser_account_mapper`
    - Removed util func `get_legacy_user_for_login_gov_id ` that is no
    longer needed
    - Migration file for dropping tables
    - Removed related tests and factories

--- api/src/db/models/legacy_mixin/user_mixin.py
+++ api/src/db/models/legacy_mixin/user_mixin.py
@@ -4,42 +4,6 @@ from sqlalchemy.orm import Mapped, declarative_mixin, mapped_column
 
 
 @declarative_mixin
-class TuserAccountMapperMixin:
-    user_account_id: Mapped[int] = mapped_column(primary_key=True)
-    ext_user_id: Mapped[str]
-    ext_issuer: Mapped[str]
-    ext_subject: Mapped[str | None]
-    last_auth_date: Mapped[datetime]
-    created_date: Mapped[datetime | None]
-    creator_id: Mapped[str | None]
-    last_upd_date: Mapped[datetime | None]
-    last_upd_id: Mapped[str | None]
-    source_type: Mapped[str] = mapped_column(primary_key=True)
-
-
-@declarative_mixin
-class TuserAccountMixin:
-    user_account_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str]
-    full_name: Mapped[str | None]
-    email_address: Mapped[str | None]
-    phone_number: Mapped[str | None]
-    first_name: Mapped[str | None]
-    middle_name: Mapped[str | None]
-    last_name: Mapped[str | None]
-    is_deleted_legacy: Mapped[str]
-    is_duplicate: Mapped[str]
-    is_active: Mapped[str]
-    created_date: Mapped[datetime | None]
-    creator_id: Mapped[str | None]
-    last_upd_date: Mapped[datetime | None]
-    last_upd_id: Mapped[str | None]
-    is_email_confirm_pending: Mapped[str | None]
-    deactivated_date: Mapped[datetime | None]
-    mobile_number: Mapped[str | None]
-
-
-@declarative_mixin
 class VuserAccountMixin:
     user_account_id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[str]
