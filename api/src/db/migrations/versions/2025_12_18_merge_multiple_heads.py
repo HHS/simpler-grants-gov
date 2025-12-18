bc5fe824016a431b23aa52b05cf23330f1366261
bruk <brukty@gmail.com>
2025-12-18 16:44:09 -0500

    lint

--- api/src/db/migrations/versions/2025_12_18_merge_multiple_heads.py
+++ api/src/db/migrations/versions/2025_12_18_merge_multiple_heads.py
@@ -6,9 +6,6 @@ Create Date: 2025-12-18 21:29:53.652394
 
 """
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "20cbfe4c4fed"
 down_revision = ("22bf34acf454", "627e63fb1eae")
