1f8aec40b96be2b87da67c4cebfd2022322459ac
Erin Pattison <erinpattison@navapbc.com>
2026-01-09 11:44:25 -0800

    Revert "[Issue #7621] Update Opportunity endpoints for two types of auth (#7775)"
    This reverts commit 2828653aa69bfbd82b97bd4b696011e69783ee35.

--- api/src/api/opportunities_v1/opportunity_routes.py
+++ api/src/api/opportunities_v1/opportunity_routes.py
@@ -12,12 +12,7 @@ import src.api.opportunities_v1.opportunity_schemas as opportunity_schemas
 import src.api.response as response
 import src.util.datetime_util as datetime_util
 from src.api.opportunities_v1.opportunity_blueprint import opportunity_blueprint
-from src.auth.multi_auth import (
-    api_key_multi_auth,
-    api_key_multi_auth_security_schemes,
-    jwt_or_api_user_key_multi_auth,
-    jwt_or_api_user_key_security_schemes,
-)
+from src.auth.multi_auth import api_key_multi_auth, api_key_multi_auth_security_schemes
 from src.logging.flask_logger import add_extra_data_to_current_request_logs
 from src.services.opportunities_v1.get_opportunity import (
     get_opportunity,
@@ -234,9 +229,9 @@ def opportunity_search(
 
 @opportunity_blueprint.get("/opportunities/<int:legacy_opportunity_id>")
 @opportunity_blueprint.output(opportunity_schemas.OpportunityGetResponseV1Schema())
-@jwt_or_api_user_key_multi_auth.login_required
+@api_key_multi_auth.login_required
 @opportunity_blueprint.doc(
-    description=SHARED_ALPHA_DESCRIPTION, security=jwt_or_api_user_key_security_schemes
+    description=SHARED_ALPHA_DESCRIPTION, security=api_key_multi_auth_security_schemes
 )
 @flask_db.with_db_session()
 def opportunity_get_legacy(
@@ -252,9 +247,9 @@ def opportunity_get_legacy(
 
 @opportunity_blueprint.get("/opportunities/<uuid:opportunity_id>")
 @opportunity_blueprint.output(opportunity_schemas.OpportunityGetResponseV1Schema())
-@jwt_or_api_user_key_multi_auth.login_required
+@api_key_multi_auth.login_required
 @opportunity_blueprint.doc(
-    description=SHARED_ALPHA_DESCRIPTION, security=jwt_or_api_user_key_security_schemes
+    description=SHARED_ALPHA_DESCRIPTION, security=api_key_multi_auth_security_schemes
 )
 @flask_db.with_db_session()
 def opportunity_get(db_session: db.Session, opportunity_id: UUID) -> response.ApiResponse:
