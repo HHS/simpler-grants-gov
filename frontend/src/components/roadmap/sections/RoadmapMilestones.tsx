c6440513a419d9de645b1cf80df5a77b412fcdf3
Andy Cochran <andycochran@navapbc.com>
2026-01-26 16:09:15 -0500

    update roadmap page content & layout

--- frontend/src/components/roadmap/sections/RoadmapMilestones.tsx
+++ frontend/src/components/roadmap/sections/RoadmapMilestones.tsx
@@ -9,11 +9,11 @@ import RoadmapPageSection from "src/components/roadmap/RoadmapPageSection";
 export default function RoadmapMilestones() {
   const t = useTranslations("Roadmap.sections.milestones");
   const messages = useMessages() as unknown as IntlMessages;
-  const { contentItems, archivedRoadmapItems = {} } =
+  const { contentItems = {}, archivedRoadmapSections } =
     messages.Roadmap.sections.milestones;
 
   return (
-    <RoadmapPageSection className="bg-base-lightest" title={t("title")}>
+    <RoadmapPageSection title={t("title")}>
       <p>
         <Link
           target="_blank"
@@ -37,43 +37,61 @@ export default function RoadmapMilestones() {
               key={`roadmap-milestones-${title}-key`}
             >
               <h4>{title}</h4>
-              <p>{t(`contentItems.${key}.content`)}</p>
+              {t.rich(
+                `contentItems.${key}.content`,
+                {
+                  p: (chunks) => <p className="font-sans-xs">{chunks}</p>,
+                  releaseNotesLink: (chunks) => <a href="https://wiki.simpler.grants.gov/product/release-notes" target="_blank" className="usa-link--external">{chunks}</a>,
+                  commonGrantsProtocolLink: (chunks) => <a href="https://commongrants.org/" target="_blank" className="usa-link--external">{chunks}</a>,
+                },
+              )}
             </div>
           );
         })}
-      {/* Late 2024 Section with Accordion */}
-      <div className="margin-top-5">
-        <Accordion
-          items={[
-            {
-              title: t("archivedRoadmapTitle"),
-              headingLevel: "h3",
-              className: "",
-              expanded: false,
-              id: "archived-roadmap-content",
-              content: (
-                <div className="margin-top-2">
-                  {archivedRoadmapItems &&
-                    Object.keys(archivedRoadmapItems).map((key) => {
-                      const title = t(`archivedRoadmapItems.${key}.title`);
-                      return (
-                        <div
-                          key={`roadmap-archived-item-${key}-section`}
-                          className={"margin-bottom-3"}
-                        >
-                          <h4 key={`roadmap-milestones-late2024-${key}`}>
-                            {title}
-                          </h4>
-                          <p>{t(`archivedRoadmapItems.${key}.content`)}</p>
-                        </div>
-                      );
-                    })}
-                </div>
-              ),
-            },
-          ]}
-        />
-      </div>
+      {/* Accordion sections */}
+      {archivedRoadmapSections &&
+        Object.keys(archivedRoadmapSections).map((sectionKey:string) => {
+          const sectionTitle = t(`archivedRoadmapSections.${sectionKey}.sectionTitle`);
+          return (
+            <div
+              className="margin-bottom-4"
+              key={`archived-roadmap-sections-${sectionTitle}-key`}
+            >
+              <Accordion
+                bordered
+                items={[
+                  {
+                    title: sectionTitle,
+                    headingLevel: "h3",
+                    className: "",
+                    expanded: false,
+                    id: `archived-roadmap-accordion--${sectionKey}`,
+                    content: (
+                      <div className="margin-top-2">
+                        {Object.keys(archivedRoadmapSections[sectionKey].sectionItems).map((sectionItemKey) => {
+                          return (
+                            <div 
+                              key={`archived-roadmap-sections-${sectionTitle}-sections-${sectionItemKey}-key`}
+                              className="margin-bottom-4"
+                            >
+                              <h4>{t(`archivedRoadmapSections.${sectionKey}.sectionItems.${sectionItemKey}.title`)}</h4>
+                              {t.rich(
+                                `archivedRoadmapSections.${sectionKey}.sectionItems.${sectionItemKey}.content`,
+                                {
+                                  p: (chunks) => <p className="font-sans-xs">{chunks}</p>,
+                                },
+                              )}
+                            </div>
+                          )
+                        })}
+                      </div>
+                    ),
+                  },
+                ]}
+              />
+            </div>
+          );
+        })}
     </RoadmapPageSection>
   );
 }
