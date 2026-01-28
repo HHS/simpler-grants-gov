941bb7c144275381f7885a3fd795797b7e37018e
Freddie Yebra <freddieyebra@navapbc.com>
2026-01-28 17:19:04 -0600

    Update sf424b form schema

--- api/src/form_schema/forms/sf424b.py
+++ api/src/form_schema/forms/sf424b.py
@@ -4,10 +4,9 @@ from src.constants.lookup_constants import FormType
 from src.db.models.competition_models import Form
 from src.form_schema.shared import COMMON_SHARED_V1
 
-DIRECTIONS = """Public reporting burden for this collection of information is estimated to average 15 minutes per response, including time for reviewing instructions, searching existing data sources, gathering and maintaining the data needed, and completing and reviewing the collection of information. Send comments regarding the burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden, to the Office of Management and Budget, Paperwork Reduction Project (0348-0040), Washington, DC 20503.
+DIRECTIONS = "Public reporting burden for this collection of information is estimated to average 15 minutes per response, including time for reviewing instructions, searching existing data sources, gathering and maintaining the data needed, and completing and reviewing the collection of information. Send comments regarding the burden estimate or any other aspect of this collection of information, including suggestions for reducing this burden, to the Office of Management and Budget, Paperwork Reduction Project (0348-0040), Washington, DC 20503."
 
-PLEASE DO NOT RETURN YOUR COMPLETED FORM TO THE OFFICE OF MANAGEMENT AND BUDGET. SEND IT TO THE ADDRESS PROVIDED BY THE SPONSORING AGENCY.
-
+ACKNOWLEDGEMENT_STATEMENT = """
 NOTE: Certain of these assurances may not be applicable to your project or program. If you have questions, please contact the awarding agency. Further, certain Federal awarding agencies may require applicants to certify to additional assurances. If such is the case, you will be notified.
 
 As the duly authorized representative of the applicant, I certify that the applicant:
@@ -71,7 +70,7 @@ FORM_JSON_SCHEMA = {
         },
         "date_signed": {
             "allOf": [{"$ref": COMMON_SHARED_V1.field_ref("submitted_date")}],
-            "title": "Date Signed",
+            "title": "Date Submitted",
         },
     },
 }
@@ -79,14 +78,21 @@ FORM_JSON_SCHEMA = {
 FORM_UI_SCHEMA = [
     {
         "type": "section",
-        "label": "1. Directions",
+        "label": "Burden Statement",
         "name": "directions",
         "description": DIRECTIONS,
         "children": [],
     },
     {
         "type": "section",
-        "label": "2. Signature",
+        "label": "Acknowledgement and assurances",
+        "name": "acknowledgement",
+        "description": ACKNOWLEDGEMENT_STATEMENT,
+        "children": [],
+    },
+    {
+        "type": "section",
+        "label": "Signature",
         "name": "signature",
         "children": [
             {"type": "null", "definition": "/properties/signature"},
