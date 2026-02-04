8b7343c31b0b1fac9c702a91fc5f09d1b6456901
Doug Schrashun <dougschrashun@navapbc.com>
2026-02-04 17:26:55 -0500

    upgrade sass, revert future type fix

--- frontend/stories/components/application/ApplicationFormsTable.stories.tsx
+++ frontend/stories/components/application/ApplicationFormsTable.stories.tsx
@@ -1,14 +1,11 @@
-import {
-  ApplicationDetail,
-  ApplicationStatus,
-} from "src/types/applicationResponseTypes";
+import { ApplicationDetail } from "src/types/applicationResponseTypes";
 
 import { ApplicationFormsTable } from "src/components/application/ApplicationFormsTable";
 import competitionMock from "./competition.mock.json";
 
 const applicationDetailsObject: ApplicationDetail = {
   ...(competitionMock as unknown as ApplicationDetail),
-  application_status: ApplicationStatus.IN_PROGRESS,
+  application_status: "in_progress",
   application_id: "12345",
   competition: {
     ...(competitionMock.competition as unknown as ApplicationDetail["competition"]),
