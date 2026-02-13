7618c42be1aface4728abaf839967fe6e1155e27
jakobpederson <jakob.pederson+github@gmail.com>
2026-02-13 06:27:54 -0800

    [Issue #8530] Fix SimplerSoapAPI.get_api_name to handle jwt service name

--- api/tests/src/legacy_soap_api/test_legacy_soap_api_config.py
+++ api/tests/src/legacy_soap_api/test_legacy_soap_api_config.py
@@ -1,5 +1,16 @@
-from src.legacy_soap_api.legacy_soap_api_config import LegacySoapAPIConfig, get_soap_config
+from src.legacy_soap_api.legacy_soap_api_config import (
+    LegacySoapAPIConfig,
+    SimplerSoapAPI,
+    get_soap_config,
+)
 
 
 def test_get_soap_config():
     assert isinstance(get_soap_config(), LegacySoapAPIConfig)
+
+
+def test_simpler_soap_api_get_api_name_with_jwt_auth_url():
+    api_name = SimplerSoapAPI.get_soap_api(
+        service_name="grantsws-agency-partner", service_port_name="AgencyWebServicesSoapPort"
+    )
+    assert api_name == SimplerSoapAPI.GRANTORS
