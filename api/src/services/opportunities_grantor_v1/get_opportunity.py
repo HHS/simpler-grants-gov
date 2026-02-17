38a55cb5dbcce753af97b02fa00d1625e142d8cf
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-02-17 15:56:59 -0600

    8005 Opportunity Table Create Endpoint (#8498)
    ## Summary
    
    Fixes for #8005 
    
    ## Changes proposed
    
    Adding Create Endpoint for Creating new opportunities.
    
    ## Context for reviewers
    
    [Tech
    Spec](https://navasage.atlassian.net/wiki/spaces/Grantsgov/pages/2612789251/Phase+1+Core+Foundations)
    
    Opportunity Publishing in Simpler Grants will enable the creation of
    funding opportunities directly into Simpler Grants. With this
    capability, key opportunity data can be entered into Simpler Grants and
    displayed to applicants in a consistent, unified experience. This PR is
    focused on making the Create endpoint for creating a new opportunity.
    
    Endpoint information:
    
    `POST /v1/grantors/opportunities/`
    
    Expected responses:
    
    - 200 - Successful response (opportunity is created)
    - 401 - Authentication error
    - 403 - User does not have the `create_opportunity` for their assigned
    agency
    - 404 - Agency ID doesn't exist
    - 422 - User attempts to create opportunity with number that already
    exists/Missing Data
    
    ## Validation steps
    
    - [x] 5 new Unit Tests pass
    - [x] Confirmed all expected request+responses on local
    - [x] Confirmed all information populates in local DB

---
+++ api/src/services/opportunities_grantor_v1/get_opportunity.py
@@ -0,0 +1,15 @@
+from sqlalchemy import select
+
+import src.adapters.db as db
+from src.api.route_utils import raise_flask_error
+from src.db.models.opportunity_models import Opportunity
+
+
+def check_opportunity_number_exists(db_session: db.Session, opportunity_number: str) -> None:
+    stmt = select(Opportunity).where(Opportunity.opportunity_number == opportunity_number)
+    existing_opportunity = db_session.execute(stmt).scalar_one_or_none()
+
+    if existing_opportunity is not None:
+        raise_flask_error(
+            422, message=f"Opportunity with number '{opportunity_number}' already exists"
+        )
