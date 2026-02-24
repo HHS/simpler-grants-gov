ba81b8abb77fab3be1eb097369fb955ce20e5e1c
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-02-24 09:39:18 -0500

    Fix import

--- api/src/services/opportunities_grantor_v1/get_opportunity.py
+++ api/src/services/opportunities_grantor_v1/get_opportunity.py
@@ -1,8 +1,7 @@
 import uuid
 
-from sqlalchemy import select
+from sqlalchemy import ColumnExpressionArgument, select
 from sqlalchemy.orm import selectinload
-from sqlalchemy.sql.elements import ColumnExpressionArgument
 
 import src.adapters.db as db
 from src.api.route_utils import raise_flask_error
