46f0466b1efd51321e870ae153e16ff92330c6e2
Aoy Simmons <aoy.simmons@reisystems.com>
2026-02-24 12:22:59 -0500

    Add session cookie creation to E2E test suite

--- frontend/tests/e2e/apply/specs/Suite-8037-e2e-SF424B-SFLLv20.spec.ts
+++ frontend/tests/e2e/apply/specs/Suite-8037-e2e-SF424B-SFLLv20.spec.ts
@@ -2,10 +2,10 @@
 // This test suite verifies the creation of a new application and the completion of the SFLL v2.0 form.
 // ============================================================================
 import { test } from "@playwright/test";
-import { Help_completeSFLLv20Form } from "tests/e2e/helpers/Help-complete-SFLLv20-form";
+// import { Help_completeSFLLv20Form } from "tests/e2e/helpers/Help-complete-SFLLv20-form";
 import { Help_createNewApplication } from "tests/e2e/helpers/Help-create-new-application";
 import { ensurePageClosedAfterEach } from "tests/e2e/helpers/safeHelp";
-
+import { createSpoofedSessionCookie } from "tests/e2e/loginUtils";
 // ============================================================================
 // Main Test
 // ============================================================================
@@ -15,7 +15,9 @@ test.describe("Test Suite 8037: Create New Application Flow and Complete SFLL v2
 
   test("Test Suite 8037: Create New Application Flow and Complete SFLL v2.0 Formn", async ({
     page,
+    context
   }, testInfo) => {
+    await createSpoofedSessionCookie(context);
     await Help_createNewApplication(testInfo, page);
     // await Help_completeSFLLv20Form(testInfo, page);
   });
