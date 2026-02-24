178367898cfa310a42ef5c2d6c318ab21a7314ee
Aoy Simmons <aoy.simmons@reisystems.com>
2026-02-23 23:47:55 -0500

    Add e2e test for SFLL v2.0 application flow
    This test suite verifies the creation of a new application and the completion of the SFLL v2.0 form.

---
+++ frontend/tests/e2e/apply/specs/Suite-8037-e2e-SF424B-SFLLv20.spec.ts
@@ -0,0 +1,22 @@
+// ============================================================================
+// This test suite verifies the creation of a new application and the completion of the SFLL v2.0 form.
+// ============================================================================
+import { expect, test } from "@playwright/test";
+import { Help_completeSFLLv20Form } from "tests/e2e/helpers/Help-complete-SFLLv20-form";
+import { Help_createNewApplication } from "tests/e2e/helpers/Help-create-new-application";
+import { ensurePageClosedAfterEach } from "tests/e2e/helpers/safeHelp";
+
+// ============================================================================
+// Main Test
+// ============================================================================
+
+test.describe("Test Suite 8037: Create New Application Flow and Complete SFLL v2.0 Form", () => {
+  ensurePageClosedAfterEach(test);
+
+  test("Test Suite 8037: Create New Application Flow and Complete SFLL v2.0 Formn", async ({
+    page,
+  }, testInfo) => {
+    await Help_createNewApplication(testInfo, page);
+    await Help_completeSFLLv20Form(testInfo, page);
+  });
+});
