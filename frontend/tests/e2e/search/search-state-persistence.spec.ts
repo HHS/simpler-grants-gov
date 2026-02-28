44aae7a43ad3b5023e54df3f442251943d61a597
Bhavna Ramachandran <bhavna.ramachandran@bpsconsulting.com>
2026-02-28 00:37:58 +0000

    Resolve lint issues

--- frontend/tests/e2e/search/search-state-persistence.spec.ts
+++ frontend/tests/e2e/search/search-state-persistence.spec.ts
@@ -11,7 +11,6 @@ import {
   ensureAccordionExpanded,
   ensureFilterDrawerOpen,
   expectCheckboxesChecked,
-  expectCheckboxIDIsChecked,
   expectSortBy,
   fillSearchInputAndSubmit,
   getFirstNonNumericAgencyCheckboxId,
