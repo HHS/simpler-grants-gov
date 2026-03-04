6ec6bc37df4bcc1cbaba5e006151c405444f4cc8
Nathan Stricker <nathan.stricker@focusconsulting.io>
2026-03-04 09:59:11 -0500

    fix AR hero content spacing

--- frontend/src/components/award-recommendation/AwardRecommendationHero.tsx
+++ frontend/src/components/award-recommendation/AwardRecommendationHero.tsx
@@ -40,8 +40,8 @@ export default async function AwardRecommendationHero({
               },
             ]}
           />
-          <Grid className="padding-y-2 tablet:padding-y-3">
-            <h1>
+          <Grid className="padding-y-4 tablet:padding-y-3">
+            <h1 className="font-sans-xl tablet:font-sans-2xl">
               {t("heroTitle", { defaultValue: "Award Rec #" })}: {recordNumber}
             </h1>
           </Grid>
@@ -55,20 +55,20 @@ export default async function AwardRecommendationHero({
                   {datePrepared}
                 </span>
               </Grid>
-              <Grid className="padding-top-1 tablet:padding-top-2 display-flex flex-align-center">
+              <Grid className="padding-top-2 tablet:padding-top-2 display-flex flex-align-center">
                 <strong>{t("status", { defaultValue: "Status" })}:</strong>{" "}
                 <span className="margin-left-1 display-inline-flex flex-align-center">
                   <AwardRecommendationStatusTag status={status} />
                 </span>
               </Grid>
             </Grid>
-            <Grid tablet={{ col: "auto" }} className="flex-align-self-end">
+            <Grid className="flex-align-self-end margin-top-4 tablet:margin-top-2 display-flex flex-justify-start">
               {/* TODO: add save functionality when endpoint is available */}
-              <Button type="button" outline className="margin-top-2">
+              <Button type="button" outline style={{ width: "auto" }}>
                 {t("heroButtons.save")}
               </Button>
               {/* TODO: add create functionality when endpoint is available */}
-              <Button type="button" className="margin-top-1">
+              <Button type="button" style={{ width: "auto" }}>
                 {t("heroButtons.create")}
               </Button>
             </Grid>
