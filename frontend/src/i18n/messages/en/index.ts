71292bf322ebc2a2ccb3525d36dfb1b27a1078a9
Amit Desai <adesai@horus.net>
2026-03-06 14:50:41 -0500

    Add Create Opportunity button to opportunities list

--- frontend/src/i18n/messages/en/index.ts
+++ frontend/src/i18n/messages/en/index.ts
@@ -1796,6 +1796,7 @@ export const messages = {
     },
   },
   Opportunities: {
+    createOpportunityButton: "Create Opportunity",
     numOpportunities:
       "{num, plural, =1 {1 opportunity} other {# opportunities}}",
     errorMessage:
