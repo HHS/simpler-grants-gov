e47083b24c530650c629ac4c5aa60adf18e0d868
Andy Cochran <andycochran@navapbc.com>
2026-03-10 16:12:30 -0400

    remove breadcrumbs from newsletter subpages

--- frontend/src/constants/breadcrumbs.ts
+++ frontend/src/constants/breadcrumbs.ts
@@ -2,18 +2,9 @@ import { Breadcrumb } from "src/components/Breadcrumbs";
 
 // individual breadcrumbs
 const HOME: Breadcrumb = { title: "Home", path: "/" };
-const SUBSCRIBE: Breadcrumb = { title: "Newsletter", path: "/newsletter/" };
 const SEARCH: Breadcrumb = {
   title: "Search",
   path: "/search",
-};
-export const SUBSCRIBE_CONFIRMATION: Breadcrumb = {
-  title: "Confirmation",
-  path: "/newsletter/confirmation/",
-};
-const UNSUBSCRIBE: Breadcrumb = {
-  title: "Unsubscribe",
-  path: "/newsletter/unsubscribe/",
 };
 const SAVED_OPPORTUNITIES: Breadcrumb = {
   title: "Saved opportunities",
@@ -29,12 +20,6 @@ const ACTIVITY_DASHBOARD: Breadcrumb = {
 };
 
 // page breadcrumbs
-export const SUBSCRIBE_CONFIRMATION_CRUMBS: Breadcrumb[] = [
-  HOME,
-  SUBSCRIBE,
-  SUBSCRIBE_CONFIRMATION,
-];
-export const UNSUBSCRIBE_CRUMBS: Breadcrumb[] = [HOME, SUBSCRIBE, UNSUBSCRIBE];
 export const SEARCH_CRUMBS: Breadcrumb[] = [HOME, SEARCH];
 export const OPPORTUNITY_CRUMBS: Breadcrumb[] = [HOME, SEARCH];
 export const SAVED_OPPORTUNITIES_CRUMBS: Breadcrumb[] = [
