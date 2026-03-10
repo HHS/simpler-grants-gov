7a491724d659da33e1d4b762c086d8af3da3f784
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-03-10 12:46:00 -0400

    Remove unneeded OpportunitySummaryCreateRequest

--- api/src/services/opportunities_grantor_v1/opportunity_summaries.py
+++ api/src/services/opportunities_grantor_v1/opportunity_summaries.py
@@ -1,16 +1,11 @@
 import logging
-from datetime import date
 from uuid import UUID
 
-from pydantic import BaseModel
 
 from src.adapters import db
 from src.api.route_utils import raise_flask_error
 from src.auth.endpoint_access_util import verify_access
 from src.constants.lookup_constants import (
-    ApplicantType,
-    FundingCategory,
-    FundingInstrument,
     Privilege,
 )
 from src.db.models.opportunity_models import Opportunity, OpportunitySummary
@@ -18,37 +13,6 @@ from src.db.models.user_models import User
 from src.services.opportunities_grantor_v1.get_opportunity import get_opportunity_for_grantors
 
 logger = logging.getLogger(__name__)
-
-
-class OpportunitySummaryCreateRequest(BaseModel):
-    legacy_opportunity_id: int | None = None
-    is_forecast: bool
-    summary_description: str | None = None
-    is_cost_sharing: bool | None = None
-    post_date: date | None = None
-    close_date: date | None = None
-    close_date_description: str | None = None
-    archive_date: date | None = None
-    expected_number_of_awards: int | None = None
-    estimated_total_program_funding: int | None = None
-    award_floor: int | None = None
-    award_ceiling: int | None = None
-    additional_info_url: str | None = None
-    additional_info_url_description: str | None = None
-    funding_categories: list[FundingCategory] | None = None
-    funding_category_description: str | None = None
-    funding_instruments: list[FundingInstrument] | None = None
-    applicant_types: list[ApplicantType] | None = None
-    applicant_eligibility_description: str | None = None
-    agency_contact_description: str | None = None
-    agency_email_address: str | None = None
-    agency_email_address_description: str | None = None
-    forecasted_post_date: date | None = None
-    forecasted_close_date: date | None = None
-    forecasted_close_date_description: str | None = None
-    forecasted_award_date: date | None = None
-    forecasted_project_start_date: date | None = None
-    fiscal_year: int | None = None
 
 
 def _check_existing_summary(opportunity: Opportunity, is_forecast: bool) -> None:
