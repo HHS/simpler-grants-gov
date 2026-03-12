76963b104d8b6bf12a1406397b7b7467776c3f4e
Matt Dragon <mattdragon@navapbc.com>
2026-03-12 15:52:56 -0400

    Add retries if Login.gov fails when we ask for token details

--- api/src/adapters/oauth/login_gov/mock_login_gov_oauth_client.py
+++ api/src/adapters/oauth/login_gov/mock_login_gov_oauth_client.py
@@ -7,15 +7,29 @@ class MockLoginGovOauthClient(BaseOauthClient):
     def __init__(self) -> None:
         self.responses: dict[str, OauthTokenResponse] = {}
 
+        # Used to control testing of retry behavior for Login.gov token lookup calls
+        self.retries: dict[str, OauthTokenResponse] = {}
+
     def add_token_response(self, code: str, response: OauthTokenResponse) -> None:
         self.responses[code] = response
 
     def get_token(self, request: OauthTokenRequest) -> OauthTokenResponse:
-        response = self.responses.get(request.code, None)
+        retries = self.retries.get(request.code)
+        # if we don't have retries enabled on the mock, behave as usual
+        if retries is not None:
+            self.retries[request.code] = retries - 1
+        # retries would be one the last time through, as we've reduced it to zero but retries accounts for the data before that
+        if retries is None or retries == 1:
+            response = self.responses.get(request.code, None)
 
-        if response is None:
-            response = OauthTokenResponse(
-                error="error", error_description="default mock error description"
-            )
+            if response is None:
+                response = OauthTokenResponse(
+                    error="error", error_description="default mock error description"
+                )
 
-        return response
+            return response
+
+        # if we did turn on retries on the mock, do retry stuff
+        return OauthTokenResponse(
+                    error="error", error_description="mock oauth token error description"
+                )
