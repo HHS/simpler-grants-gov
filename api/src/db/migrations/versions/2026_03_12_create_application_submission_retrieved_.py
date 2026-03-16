0b430e3eafe6888f3258f6865f269f4b48b78022
Michael Huneke <mike.huneke@focusconsulting.io>
2026-03-16 11:43:56 -0400

    Fix head

--- api/src/db/migrations/versions/2026_03_12_create_application_submission_retrieved_.py
+++ api/src/db/migrations/versions/2026_03_12_create_application_submission_retrieved_.py
@@ -1,7 +1,7 @@
 """create application_submission_retrieved table
 
 Revision ID: f8eaafc955ec
-Revises: 3bbb6f5e037e
+Revises: f8eaafc955ec
 Create Date: 2026-03-12 14:17:02.346821
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "f8eaafc955ec"
-down_revision = "3bbb6f5e037e"
+down_revision = "f8eaafc955ec"
 branch_labels = None
 depends_on = None
 
