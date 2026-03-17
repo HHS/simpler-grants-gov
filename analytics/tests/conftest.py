622b91db72d9acb4bf4c440e580907a711be032a
Matt Dragon <mattdragon@navapbc.com>
2026-03-17 12:42:40 -0400

    Upgrade black and format with it

--- analytics/tests/conftest.py
+++ analytics/tests/conftest.py
@@ -5,6 +5,7 @@ Configure pytest settings and create reusable fixtures and functions.
 Visit pytest docs for more info:
 https://docs.pytest.org/en/7.1.x/reference/fixtures.html
 """
+
 import json
 import logging
 import uuid
