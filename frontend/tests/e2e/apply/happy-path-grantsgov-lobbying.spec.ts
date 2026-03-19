66f8d6e35b7f96b81d5ba9391163ec45cd7a47a2
Aoy Simmons <aoy.simmons@reisystems.com>
2026-03-19 17:25:40 -0400

    Refactor import statements and format code

--- frontend/tests/e2e/apply/happy-path-grantsgov-lobbying.spec.ts
+++ frontend/tests/e2e/apply/happy-path-grantsgov-lobbying.spec.ts
@@ -1,9 +1,8 @@
-
 import {
-	test,
-	type BrowserContext,
-	type Page,
-	type TestInfo,
+  test,
+  type BrowserContext,
+  type Page,
+  type TestInfo,
 } from "@playwright/test";
 import playwrightEnv from "tests/e2e/playwright-env";
 import { authenticateE2eUser } from "tests/e2e/utils/authenticate-e2e-user-utils";
@@ -19,28 +18,28 @@ const OPPORTUNITY_ID = "c3c59562-a54f-4203-b0f6-98f2f0383481";
 const OPPORTUNITY_URL = `/opportunity/${OPPORTUNITY_ID}`;
 
 test("Application form completion happy path - Grants.gov Lobbying", async ({
-	page,
-	context,
+  page,
+  context,
 }: { page: Page; context: BrowserContext }, testInfo: TestInfo) => {
-	test.setTimeout(300_000); // 5 min timeout
+  test.setTimeout(300_000); // 5 min timeout
 
-	const isMobile = testInfo.project.name.match(/[Mm]obile/);
+  const isMobile = testInfo.project.name.match(/[Mm]obile/);
 
-	await authenticateE2eUser(page, context, !!isMobile);
+  await authenticateE2eUser(page, context, !!isMobile);
 
-	// Call reusable create application function from utils
-	await createApplication(page, OPPORTUNITY_URL, testOrgLabel);
+  // Call reusable create application function from utils
+  await createApplication(page, OPPORTUNITY_URL, testOrgLabel);
 
-	await fillForm(
-		testInfo,
-		page,
-		GRANTSGOV_LOBBYING_FORM_CONFIG,
-		grantsGovLobbyingHappyPathTestData,
-		false,
-	);
+  await fillForm(
+    testInfo,
+    page,
+    GRANTSGOV_LOBBYING_FORM_CONFIG,
+    grantsGovLobbyingHappyPathTestData,
+    false,
+  );
 
-	await page.waitForTimeout(2000);
+  await page.waitForTimeout(2000);
 
-	// Verify form status after save
-	await verifyFormStatusAfterSave(page, "complete");
+  // Verify form status after save
+  await verifyFormStatusAfterSave(page, "complete");
 });
