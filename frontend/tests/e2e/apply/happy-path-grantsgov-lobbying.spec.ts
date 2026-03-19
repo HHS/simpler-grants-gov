60bd2dbe903f424162cb84ce802e848f4c2ae243
Aoy Simmons <aoy.simmons@gmail.com>
2026-03-19 16:58:56 -0400

    Aoy/-8892-Application Form Happy Path E2E Test - Grants.gov Lobbying Form

---
+++ frontend/tests/e2e/apply/happy-path-grantsgov-lobbying.spec.ts
@@ -0,0 +1,46 @@
+
+import {
+	test,
+	type BrowserContext,
+	type Page,
+	type TestInfo,
+} from "@playwright/test";
+import playwrightEnv from "tests/e2e/playwright-env";
+import { authenticateE2eUser } from "tests/e2e/utils/authenticate-e2e-user-utils";
+import { createApplication } from "tests/e2e/utils/create-application-utils";
+import { fillForm } from "tests/e2e/utils/forms/general-forms-filling";
+import { verifyFormStatusAfterSave } from "tests/e2e/utils/forms/verify-form-status-utils";
+
+import { GRANTSGOV_LOBBYING_FORM_CONFIG } from "./fixtures/grantsgov-lobbying-field-definitions";
+import { grantsGovLobbyingHappyPathTestData } from "./fixtures/grantsgov-lobbying-fill-data";
+
+const { testOrgLabel } = playwrightEnv;
+const OPPORTUNITY_ID = "c3c59562-a54f-4203-b0f6-98f2f0383481";
+const OPPORTUNITY_URL = `/opportunity/${OPPORTUNITY_ID}`;
+
+test("Application form completion happy path - Grants.gov Lobbying", async ({
+	page,
+	context,
+}: { page: Page; context: BrowserContext }, testInfo: TestInfo) => {
+	test.setTimeout(300_000); // 5 min timeout
+
+	const isMobile = testInfo.project.name.match(/[Mm]obile/);
+
+	await authenticateE2eUser(page, context, !!isMobile);
+
+	// Call reusable create application function from utils
+	await createApplication(page, OPPORTUNITY_URL, testOrgLabel);
+
+	await fillForm(
+		testInfo,
+		page,
+		GRANTSGOV_LOBBYING_FORM_CONFIG,
+		grantsGovLobbyingHappyPathTestData,
+		false,
+	);
+
+	await page.waitForTimeout(2000);
+
+	// Verify form status after save
+	await verifyFormStatusAfterSave(page, "complete");
+});
