3a46b4ea4b08b0bb076dcf0b1ed52c8ad4f3c30a
Aoy Simmons <aoy.simmons@reisystems.com>
2026-03-19 16:23:47 -0400

    Refactor test code for project abstract summary

--- frontend/tests/e2e/apply/happy-path-project-abstract-summary.spec.ts
+++ frontend/tests/e2e/apply/happy-path-project-abstract-summary.spec.ts
@@ -1,8 +1,8 @@
 import {
-	test,
-	type BrowserContext,
-	type Page,
-	type TestInfo,
+  test,
+  type BrowserContext,
+  type Page,
+  type TestInfo,
 } from "@playwright/test";
 import playwrightEnv from "tests/e2e/playwright-env";
 import { authenticateE2eUser } from "tests/e2e/utils/authenticate-e2e-user-utils";
@@ -14,32 +14,32 @@ import { PROJECT_ABSTRACT_SUMMARY_FORM_CONFIG } from "./fixtures/project-abstrac
 import { projectAbstractSummaryHappyPathTestData } from "./fixtures/project-abstract-summary-fill-data";
 
 const { testOrgLabel } = playwrightEnv;
-const OPPORTUNITY_ID = "c3c59562-a54f-4203-b0f6-98f2f0383481";
+const OPPORTUNITY_ID = "39cf0a5c-5fed-40b4-8f46-5374101ae419";
 const OPPORTUNITY_URL = `/opportunity/${OPPORTUNITY_ID}`;
 
 test("Application form completion happy path - Project Abstract Summary", async ({
-	page,
-	context,
+  page,
+  context,
 }: { page: Page; context: BrowserContext }, testInfo: TestInfo) => {
-	test.setTimeout(300_000); // 5 min timeout
+  test.setTimeout(300_000); // 5 min timeout
 
-	const isMobile = testInfo.project.name.match(/[Mm]obile/);
+  const isMobile = testInfo.project.name.match(/[Mm]obile/);
 
-	await authenticateE2eUser(page, context, !!isMobile);
+  await authenticateE2eUser(page, context, !!isMobile);
 
-	// Call reusable create application function from utils
-	await createApplication(page, OPPORTUNITY_URL, testOrgLabel);
+  // Call reusable create application function from utils
+  await createApplication(page, OPPORTUNITY_URL, testOrgLabel);
 
-	await fillForm(
-		testInfo,
-		page,
-		PROJECT_ABSTRACT_SUMMARY_FORM_CONFIG,
-		projectAbstractSummaryHappyPathTestData,
-		false,
-	);
+  await fillForm(
+    testInfo,
+    page,
+    PROJECT_ABSTRACT_SUMMARY_FORM_CONFIG,
+    projectAbstractSummaryHappyPathTestData,
+    false,
+  );
 
-	await page.waitForTimeout(2000);
+  await page.waitForTimeout(2000);
 
-	// Verify form status after save
-	await verifyFormStatusAfterSave(page, "complete");
+  // Verify form status after save
+  await verifyFormStatusAfterSave(page, "complete");
 });
