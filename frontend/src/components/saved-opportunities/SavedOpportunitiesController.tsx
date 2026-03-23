6ab7f03b9d371c5577f8d7d86af730730359b2de
hayeshorus <hmurdock@horus.net>
2026-03-23 15:31:18 -0600

    hmurdock/8440-Add-Delete-Opportunities commenting out share button
    hmurdock/8440-Add-Delete-Opportunities commenting out share button

--- frontend/src/components/saved-opportunities/SavedOpportunitiesController.tsx
+++ frontend/src/components/saved-opportunities/SavedOpportunitiesController.tsx
@@ -24,7 +24,7 @@ export function SavedOpportunitiesController({
   const [opportunitiesState, setOpportunitiesState] =
     useState<BaseOpportunity[]>(opportunities);
 
-  const [selectedOpportunityId, setSelectedOpportunityId] = useState<
+  const [selectedOpportunityId] = useState<
     string | null
   >(null);
 
