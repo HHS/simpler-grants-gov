c56e3d9f4d28d1d49cdf6b06c5b7e1c7d287200c
Bhavna Ramachandran <bhavna.ramachandran@bpsconsulting.com>
2026-03-23 17:51:23 +0000

    Add test-upload-files for E2E tests

--- frontend/tests/e2e/apply/fixtures/project-abstract-fill-data.ts
+++ frontend/tests/e2e/apply/fixtures/project-abstract-fill-data.ts
@@ -1,3 +1,3 @@
 export const projectAbstractHappyPathTestData = (): Record<string, string> => ({
-  attachment: "tests/e2e/apply/fixtures/files/sample-upload-kb.pdf",
+  attachment: "tests/e2e/test-upload-files/sample-upload-kb.pdf",
 });
