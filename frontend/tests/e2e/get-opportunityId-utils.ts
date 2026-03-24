5bc1775aeb3fa5dc92de4d89a74cb118442763bf
Aoy Simmons <aoy.simmons@gmail.com>
2026-03-23 20:04:27 -0400

    Refactor opportunity ID imports and formatting in E2E test utilities

--- frontend/tests/e2e/get-opportunityId-utils.ts
+++ frontend/tests/e2e/get-opportunityId-utils.ts
@@ -1,12 +1,16 @@
+import {
+  testdata_local_environment,
+  testdata_staging_environment,
+} from "./opportunity-id-data";
 import playwrightEnv from "./playwright-env";
-import { testdata_local_environment, testdata_staging_environment } from "./opportunity-id-data";
 
 const { targetEnv } = playwrightEnv;
 
 // Select opportunityID based on targetEnv
-const opportunityId = targetEnv === "local"
-	? testdata_local_environment.opportunityID
-	: testdata_staging_environment.opportunityID;
+const opportunityId =
+  targetEnv === "local"
+    ? testdata_local_environment.opportunityID
+    : testdata_staging_environment.opportunityID;
 
 export { opportunityId };
 
--- frontend/tests/e2e/opportunity-id-data.ts
+++ frontend/tests/e2e/opportunity-id-data.ts
@@ -4,4 +4,3 @@ export const testdata_local_environment = {
 export const testdata_staging_environment = {
   opportunityID: "39cf0a5c-5fed-40b4-8f46-5374101ae419",
 } as const;
-
