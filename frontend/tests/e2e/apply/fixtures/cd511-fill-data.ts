31e3e66aafaceb52af5284f1f4296b474f9e974e
Aoy Simmons <aoy.simmons@reisystems.com>
2026-03-26 10:21:14 -0400

    Update applicant and contact person data in test fixture

--- frontend/tests/e2e/apply/fixtures/cd511-fill-data.ts
+++ frontend/tests/e2e/apply/fixtures/cd511-fill-data.ts
@@ -1,10 +1,10 @@
 export const cd511HappyPathTestData = {
-  applicant_name: "Application Name test",
+  applicant_name: "Applicant Name test",
   project_name: "Project Name test",
-  contact_person_prefix: "Prefix tes",
+  contact_person_prefix: "Prefixtest",
   contact_person_first_name: "First Name test",
   contact_person_middle_name: "Middle Name test",
   contact_person_last_name: "Last Name test",
-  contact_person_suffix: "Suffix tes",
+  contact_person_suffix: "Suffixtest",
   contact_person_title: "Title test",
 };
