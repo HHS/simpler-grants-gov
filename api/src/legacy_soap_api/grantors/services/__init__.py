05539bcdee0fdd7513d209a8281856d30ec8456a
Kenny Krug <kennykrug@navapbc.com>
2026-04-09 10:27:08 -0400

    [Issue #9407] SOAP/Proxy: Refactor services/get_submission_list_expanded.py

--- api/src/legacy_soap_api/grantors/services/__init__.py
+++ api/src/legacy_soap_api/grantors/services/__init__.py
@@ -6,6 +6,7 @@ from src.legacy_soap_api.grantors.services.get_application_zip_response import (
     get_application_zip_response,
 )
 from src.legacy_soap_api.grantors.services.get_submission_list_expanded_response import (
+    get_submission_list_expanded,
     get_submission_list_expanded_response,
 )
 from src.legacy_soap_api.grantors.services.update_application_info_response import (
@@ -16,6 +17,7 @@ __all__ = [
     "confirm_application_delivery",
     "get_confirm_application_delivery_response",
     "get_application_zip_response",
+    "get_submission_list_expanded",
     "get_submission_list_expanded_response",
     "update_application_info_response",
 ]
