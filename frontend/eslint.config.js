c9cb0c490be6dcfc9f19e786329183477c838f1f
Matt Dragon <mattdragon@navapbc.com>
2026-04-15 11:28:55 -0400

    [Issue #9587] Revert python version bump (#9620)
    ## Summary
    
    <!-- Use "Fixes" to automatically close issue upon PR merge. Use "Work
    for" when UAT is required. -->
    Work for #9587  
    
    ## Changes proposed
    
    Merged this too soon thinking it was Thursday. Revert for today's
    release.
    
    ## Context for reviewers
    
    <!-- Technical or background context, more in-depth details of the
    implementation, and anything else you'd like reviewers to know about
    that will help them understand the changes in the PR. -->
    
    ## Validation steps
    
    <!-- Manual testing instructions, as well as any helpful references
    (screenshots, GIF demos, code examples or output). -->

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig([
       "nava",
       "plugin:storybook/recommended",
       "prettier",
-      "next/core-web-vitals",
+      // "next/core-web-vitals",
     ),
 
     rules: {
@@ -48,6 +48,7 @@ module.exports = defineConfig([
           ],
         },
       ],
+      "react/react-in-jsx-scope": "off",
     },
 
     settings: {
