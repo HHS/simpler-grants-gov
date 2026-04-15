46f577261b18202b7a11986f62104c9960580c9d
Dao <anhdao.nguyen@reisystems.com>
2026-04-15 09:18:44 -0400

    [Issue #9397] Add enhancements to the Opportunity List page (#9447)
    ## Summary
    
    <!-- Use "Fixes" to automatically close issue upon PR merge. Use "Work
    for" when UAT is required. -->
    Work for #9397 
    
    ## Changes proposed
    
    <!-- What was added, updated, or removed in this PR. -->
    - Enhancements were made to the Opportunity List page per Issue #9397. 
    
    ## Context for reviewers
    
    <!-- Technical or background context, more in-depth details of the
    implementation, and anything else you'd like reviewers to know about
    that will help them understand the changes in the PR. -->
    To support the enhancements to the Opportunity List page, these
    additional changes were made:
    - createOpportunityFetcher was renamed to grantorOpportunitiesFetcher
    and the searchOpportunitiesByAgency method was added to retrieve draft
    as well as published opportunities. (NOTE: Some backend work is needed
    to pull the published opportunities with that same endpoint. This will
    be in a separate ticket.)
    - The component UserPrivileges.tsx was created to copy the
    checkRequiredPrivileges() function from AuthorizationGate.tsx to be used
    as a standalone component without the AuthorizationGate (per
    @doug-s-nava's instructions).
    
    ## Validation steps
    
    <!-- Manual testing instructions, as well as any helpful references
    (screenshots, GIF demos, code examples or output). -->
    - Code review
    - UAT: validate the AC's in the ticket #9397

---
+++ frontend/src/utils/userPrivileges.ts
@@ -0,0 +1,43 @@
+import { parseErrorStatus } from "src/errors";
+import { checkUserPrivilege } from "src/services/fetch/fetchers/userFetcher";
+
+export type GatedResourceTypes = "application" | "organization" | "agency";
+
+export type Privileges =
+  | "view_opportunity"
+  | "update_opportunity"
+  | "create_opportunity";
+
+export interface UserPrivilegeRequest {
+  resourceId?: string;
+  resourceType: GatedResourceTypes;
+  privilege: Privileges;
+}
+
+export interface UserPrivilegeResult extends UserPrivilegeRequest {
+  authorized: boolean;
+  error?: string;
+}
+
+// Check the user's privileges
+export const checkRequiredPrivileges = async (
+  token: string,
+  userId: string,
+  privileges: UserPrivilegeRequest[],
+): Promise<UserPrivilegeResult[]> => {
+  const privilegeCheckResults = await Promise.all(
+    privileges.map((privilege) => {
+      return checkUserPrivilege(token, userId, privilege)
+        .then(() => {
+          return { ...privilege, authorized: true };
+        })
+        .catch((e: Error) => {
+          if (parseErrorStatus(e) === 403) {
+            return { ...privilege, authorized: false };
+          }
+          throw e;
+        });
+    }),
+  );
+  return privilegeCheckResults;
+};
