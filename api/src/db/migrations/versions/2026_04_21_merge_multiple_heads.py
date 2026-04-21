6b9df2dc72dbcc75b8dcd3876b099e274c6cd53a
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-04-21 10:44:04 -0400

    Resolve alembic head merge

---
+++ api/src/db/migrations/versions/2026_04_21_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: dcfb9efbbecf
+Revises: 2af4d2b051f6, 31c3125a932e
+Create Date: 2026-04-21 14:43:36.928841
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "dcfb9efbbecf"
+down_revision = ("2af4d2b051f6", "31c3125a932e")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
