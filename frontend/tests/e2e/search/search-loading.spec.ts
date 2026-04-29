a947dcdcb6cea171479acd2d39f9db64e82f4f50
Aoy Simmons <aoy.simmons@reisystems.com>
2026-04-29 12:45:28 -0400

    Update feature file path in search loading test

--- frontend/tests/e2e/search/search-loading.spec.ts
+++ frontend/tests/e2e/search/search-loading.spec.ts
@@ -1,6 +1,6 @@
 /**
  * @feature Search - Loading State
- * @featureFile frontend/tests/e2e/search/features/search-core/search-loading.feature
+ * @featureFile e2e/search/features/search-core/search-loading.feature
  * @scenario Show and hide loading indicator on search
  */
 import { expect, test } from "@playwright/test";
