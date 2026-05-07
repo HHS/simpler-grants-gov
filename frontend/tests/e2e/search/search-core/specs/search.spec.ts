cc44a90ded8eb10d61e4e8e173de8a057f0acf27
Aoy Simmons <aoy.simmons@gmail.com>
2026-05-07 12:26:04 -0400

    test(e2e): update search core spec

--- frontend/tests/e2e/search/search-core/specs/search.spec.ts
+++ frontend/tests/e2e/search/search-core/specs/search.spec.ts
@@ -1,25 +1,12 @@
-/**
- * @feature Search - Core Search Behaviors
- * @featureFile e2e/search/search-core/features/search.feature
- * @scenario Validate search pagination, sorting, filter behavior, and page-boundary handling
- */
-
 import { expect, test } from "@playwright/test";
-import {
-  refreshPageWithCurrentURL,
-  waitForAnyURLChange,
-} from "tests/e2e/playwrightUtils";
+import { waitForAnyURLChange } from "tests/e2e/playwrightUtils";
 import {
   clickAccordionWithTitle,
   clickLastPaginationPage,
   clickPaginationPageNumber,
-  expectCheckboxIDIsChecked,
-  expectSortBy,
-  fillSearchInputAndSubmit,
   getFirstSearchResultTitle,
   getLastSearchResultTitle,
   getNumberOfOpportunitySearchResults,
-  getSearchInput,
   selectSortBy,
   toggleCheckboxes,
   toggleFilterDrawer,
@@ -30,181 +17,22 @@ import { VALID_TAGS } from "tests/e2e/tags";
 
 const { GRANTEE, OPPORTUNITY_SEARCH, FULL_REGRESSION, CORE_REGRESSION } =
   VALID_TAGS;
-const searchTerm = "education";
-const statusCheckboxes = {
-  "status-closed": "closed",
-};
-const fundingInstrumentCheckboxes = {
-  "funding-instrument-other": "other",
-  "funding-instrument-grant": "grant",
-};
-
-const eligibilityCheckboxes = {
-  "eligibility-state_governments": "state_governments",
-  "eligibility-county_governments": "county_governments",
-};
-const categoryCheckboxes = {
-  "category-recovery_act": "recovery_act",
-  "category-agriculture": "agriculture",
-};
 
 test.describe("Search page tests", () => {
-  // This test has been split out into multiple individual tests in the search-state-persistence file
-  // Scenario: Refresh and retain filters and sort order with search term and multiple filters
-  test.skip("should refresh and retain filters in a new tab", async ({ page }, {
-    project,
-  }) => {
-    const isMobile = !!project.name.match(/[Mm]obile/);
-    const agencyCheckboxes: { [key: string]: string } = {};
-    // Given I am on the "Search funding opportunities" page
-    await page.goto("/search");
-
-    // And I wait for the search results to load
-    await waitForSearchResultsInitialLoad(page);
-
-    // When I enter "education" in the search input and submit
-    await fillSearchInputAndSubmit(searchTerm, page, project.name);
-
-    // And I open the filters
-    if (isMobile) {
-      await toggleFilterDrawer(page);
-    }
-
-    // And I select "Award maximum (Highest)" from the sort by dropdown
-    await selectSortBy(page, "awardCeilingDesc", isMobile, project.name);
-
-    // Then the search results should be sorted by "Award maximum (Highest)"
-    await expectSortBy(page, "awardCeilingDesc", isMobile);
-
-    // And I open the filters
-    if (!isMobile) {
-      await toggleFilterDrawer(page);
-    }
-
-    // And I wait for the filters to load
-    await waitForFilterOptions(page, "agency");
-
-    // And I select the "Opportunity status" filter category
-    await toggleCheckboxes(
-      page,
-      statusCheckboxes,
-      "status",
-      "forecasted,posted",
-    );
-
-    // And I select the "Funding instrument" filter category
-    await clickAccordionWithTitle(page, "Funding instrument");
-
-    // And I check the "Other" and "Grant" funding instrument filters
-    await toggleCheckboxes(
-      page,
-      fundingInstrumentCheckboxes,
-      "fundingInstrument",
-    );
-
-    // And I select the "Eligibility" filter category
-    await clickAccordionWithTitle(page, "Eligibility");
-
-    // And I check the "State governments" and "County governments" eligibility filters
-    await toggleCheckboxes(page, eligibilityCheckboxes, "eligibility");
-
-    // And I select the "Agency" filter category
-    await clickAccordionWithTitle(page, "Agency");
-    let agencyId;
-
-    // need to find the first subagency box with an ID that doesn't start with a number
-    // targeting checkboxes with ids that start with a number raises issues related
-    // to querySelector functionality and what it considers a valid id
-    // And I check the first available agency filter with a valid ID
-    for (let i = 1; !agencyId || !isNaN(parseInt(agencyId[0])); i++) {
-      const subAgency = page
-        .locator(
-          `div[data-testid='Agency-filter'] > ul > li:nth-child(${i}) ul input`,
-        )
-        .first();
-
-      const exists = await subAgency.count();
-
-      if (!exists) {
-        continue;
-      }
-
-      agencyId = await subAgency.getAttribute("id");
-    }
-    expect(agencyId).toBeTruthy();
-    if (!agencyId) {
-      test.fail();
-      return;
-    }
-    agencyCheckboxes[agencyId] = agencyId;
-    await toggleCheckboxes(page, agencyCheckboxes, "agency");
-
-    // And I select the "Category" filter category
-    await clickAccordionWithTitle(page, "Category");
-
-    // And I check the "Recovery Act" and "Agriculture" category filters
-    await toggleCheckboxes(page, categoryCheckboxes, "category");
-
-    /***********************************************************/
-    /* Page refreshed should have all the same inputs selected
-    /***********************************************************/
-
-    // When I refresh the page
-    await refreshPageWithCurrentURL(page);
-
-    // And I wait for the search results to load after the refresh
-    await waitForSearchResultsInitialLoad(page);
-
-    // Then the sort order should still be "Award maximum (Highest)"
-    await expectSortBy(page, "awardCeilingDesc", isMobile);
-
-    // And all the same filters I selected before the refresh should still be selected after the refresh
-    const searchInput = getSearchInput(page);
-
-    // And the search input should still have the value "education"
-    await expect(searchInput).toHaveValue(searchTerm);
-
-    // When I open the filters
-    await toggleFilterDrawer(page);
-    for (const [checkboxID] of Object.entries(statusCheckboxes)) {
-      // And the "Closed" opportunity status filter should still be selected after the refresh
-      await expectCheckboxIDIsChecked(page, `#${checkboxID}`);
-    }
-
-    for (const [checkboxID] of Object.entries(fundingInstrumentCheckboxes)) {
-      // And the "Other" and "Grant" funding instrument filters should still be selected after the refresh
-      await expectCheckboxIDIsChecked(page, `#${checkboxID}`);
-    }
-    for (const [checkboxID] of Object.entries(eligibilityCheckboxes)) {
-      // And the "State governments" and "County governments" eligibility filters should still be selected after the refresh
-      await expectCheckboxIDIsChecked(page, `#${checkboxID}`);
-    }
-    for (const [checkboxID] of Object.entries(agencyCheckboxes)) {
-      // And the first available agency filter with a valid ID should still be selected after the refresh
-      await expectCheckboxIDIsChecked(page, `#${checkboxID}`);
-    }
-    for (const [checkboxID] of Object.entries(categoryCheckboxes)) {
-      // And the "Recovery Act" and "Agriculture" category filters should still be selected after the refresh
-      await expectCheckboxIDIsChecked(page, `#${checkboxID}`);
-    }
-  });
-
   test(
     "resets page back to 1 when choosing a filter",
     { tag: [GRANTEE, OPPORTUNITY_SEARCH, CORE_REGRESSION] },
     async ({ page }) => {
-      // Scenario: Page resets to 1 after filter change
       await page.goto("/search?status=none");
-      // When I click to go to page 2 of the search results
       await clickPaginationPageNumber(page, 2);
 
-      // Then I should be on page 2
+      // Verify that page 1 is highlighted
       let currentPageButton = page
         .locator(".usa-pagination__button.usa-current")
         .first();
       await expect(currentPageButton).toHaveAttribute("aria-label", "Page 2");
 
-      // And I check the "Closed" opportunity status filter
+      // Select the 'Closed' checkbox under 'Opportunity status'
       const statusCheckboxes = {
         "status-closed": "closed",
       };
@@ -214,19 +42,19 @@ test.describe("Search page tests", () => {
       await clickAccordionWithTitle(page, "Opportunity status");
       await toggleCheckboxes(page, statusCheckboxes, "status");
 
-      // And I wait for the search results to load with the new filter
+      // Wait for the page to reload
       await Promise.all([
         waitForSearchResultsInitialLoad(page),
         waitForFilterOptions(page, "agency"),
       ]);
 
-      // Then the current page should reset to page 1
+      // Verify that page 1 is highlighted
       currentPageButton = page
         .locator(".usa-pagination__button.usa-current")
         .first();
       await expect(currentPageButton).toHaveAttribute("aria-label", "Page 1");
 
-      // And the URL should include "status=closed"
+      // It should not have a page query param set
       await page.waitForURL("search?status=closed");
     },
   );
@@ -235,64 +63,74 @@ test.describe("Search page tests", () => {
     "last result becomes first result when flipping sort order",
     { tag: [GRANTEE, OPPORTUNITY_SEARCH, FULL_REGRESSION] },
     async ({ page }, { project }) => {
-      // Scenario: Flipping sort order reverses first and last result positions
-      // When I open the sorting
       const isMobile = !!project.name.match(/[Mm]obile/);
       await page.goto("/search");
 
-      // And I wait for the search results to load
       await waitForSearchResultsInitialLoad(page);
 
       if (isMobile) {
-        // When I open the filters
         await toggleFilterDrawer(page);
       }
-      // And I select "Opportunity title (A to Z)" from the sort by dropdown
-      await selectSortBy(page, "opportunityTitleAsc", isMobile, project.name);
+      await selectSortBy(page, "opportunityTitleAsc", isMobile);
 
-      // And I wait for the search results to load with the sort applied
-      await waitForSearchResultsInitialLoad(page);
+      if (isMobile) {
+        await toggleFilterDrawer(page);
+      }
+      const firstSearchResultTitle = await getFirstSearchResultTitle(page);
 
-      // And I note the title of the first and last results
-      const firstAscendingResultTitle = await getFirstSearchResultTitle(page);
-      const lastAscendingResultTitle = await getLastSearchResultTitle(page);
+      if (isMobile) {
+        await toggleFilterDrawer(page);
+      }
+      await selectSortBy(page, "opportunityTitleDesc", isMobile);
 
-      // When I change the sort to "Opportunity title (Z to A)"
-      await selectSortBy(page, "opportunityTitleDesc", isMobile, project.name);
-      await waitForSearchResultsInitialLoad(page);
+      if (isMobile) {
+        await toggleFilterDrawer(page);
+      }
 
-      // Then the first descending result should match the prior last ascending result
-      expect(await getFirstSearchResultTitle(page)).toBe(
-        lastAscendingResultTitle,
-      );
+      await clickLastPaginationPage(page);
 
-      // And the last descending result should match the prior first ascending result
-      expect(await getLastSearchResultTitle(page)).toBe(
-        firstAscendingResultTitle,
-      );
+      const lastSearchResultTitle = await getLastSearchResultTitle(page);
+
+      expect(firstSearchResultTitle).toBe(lastSearchResultTitle);
     },
   );
 
   test(
-    "keeps correct results count and boundaries when paging",
-    { tag: [GRANTEE, OPPORTUNITY_SEARCH, CORE_REGRESSION] },
+    "number of results is the same with none or all opportunity status checked",
+    { tag: [GRANTEE, OPPORTUNITY_SEARCH, FULL_REGRESSION] },
     async ({ page }) => {
-      // Scenario: Pagination shows valid counts and respects last page boundaries
-      await page.goto("/search");
-      await waitForSearchResultsInitialLoad(page);
-
-      const initialResultsCount =
+      await page.goto("/search?status=none");
+      const initialSearchResultsCount =
         await getNumberOfOpportunitySearchResults(page);
-      expect(initialResultsCount).toBeGreaterThan(0);
 
-      await clickLastPaginationPage(page);
-      await waitForAnyURLChange(page, "/search");
-      await waitForSearchResultsInitialLoad(page);
+      // check all 4 boxes
+      const statusCheckboxes = {
+        "status-forecasted": "forecasted",
+        "status-open": "posted",
+        "status-closed": "closed",
+        "status-archived": "archived",
+      };
 
-      const finalPageResultsCount =
+      await toggleFilterDrawer(page);
+      await clickAccordionWithTitle(page, "Opportunity status");
+      await toggleCheckboxes(page, statusCheckboxes, "status");
+
+      const updatedSearchResultsCount =
         await getNumberOfOpportunitySearchResults(page);
-      expect(finalPageResultsCount).toBeGreaterThan(0);
-      expect(finalPageResultsCount).toBeLessThanOrEqual(initialResultsCount);
+
+      expect(initialSearchResultsCount).toBe(updatedSearchResultsCount);
+    },
+  );
+
+  test(
+    "should redirect to the last page of results when page param is too high",
+    { tag: [GRANTEE, OPPORTUNITY_SEARCH, FULL_REGRESSION] },
+    async ({ page }) => {
+      await page.goto("/search?page=1000000");
+
+      await waitForAnyURLChange(page, "/search?page=1000000");
+
+      expect(page.url()).toMatch(/search\?page=\d{1,3}/);
     },
   );
 });
