fc7701b573c7de3afa4d9f551535f3a0ff7cd052
Hao Wang <hao.wang@reisystems.com>
2026-05-07 10:44:01 -0400

    Update the wait script for trouble shooting

--- api/bin/wait-for-local-dynamodb.sh
+++ api/bin/wait-for-local-dynamodb.sh
@@ -28,23 +28,3 @@ done
 
 echo "DynamoDB Local HTTP endpoint is up after ~${WAIT_TIME} seconds"
 
-# Now verify DynamoDB API is actually functional
-echo "Verifying DynamoDB Local API is functional..."
-API_WAIT_TIME=0
-MAX_API_WAIT_TIME=60
-
-until AWS_ACCESS_KEY_ID=local AWS_SECRET_ACCESS_KEY=local aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-east-1 --output json > /dev/null 2>&1;
-do
-  echo "waiting on DynamoDB Local API to become functional..."
-  sleep 3
-
-  API_WAIT_TIME=$(($API_WAIT_TIME+3))
-  if [ $API_WAIT_TIME -gt $MAX_API_WAIT_TIME ]
-  then
-    echo -e "${RED}ERROR: DynamoDB Local API not responding to requests, running \"docker logs dynamodb-local\" to troubleshoot.${NO_COLOR}"
-    docker logs dynamodb-local
-    exit 1
-  fi
-done
-
-echo "DynamoDB Local is fully ready after ~$(($WAIT_TIME+$API_WAIT_TIME)) seconds"
