007eba36fab3afc106ec0d77753f1c3edbec21d1
Hao Wang <hao.wang@reisystems.com>
2026-05-11 14:59:23 -0400

    Update as review feedback changes

--- api/src/db/setup_local_dynamodb.py
+++ api/src/db/setup_local_dynamodb.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import boto3
 
@@ -8,7 +9,7 @@ from src.util.local import error_if_not_local
 logger = logging.getLogger(__name__)
 
 # DynamoDB table configuration
-LOCAL_VIRUS_SCAN_TABLE_NAME = "local-virus-scan"
+file_scan_cache_table_name = os.getenv("FILE_SCAN_CACHE_TABLE_NAME", "local-virus-scan")
 
 
 def setup_local_dynamodb() -> None:
@@ -16,9 +17,11 @@ def setup_local_dynamodb() -> None:
     with src.logging.init(__package__):
         error_if_not_local()
 
+        dynamodb_endpoint_url = os.getenv("DYNAMODB_ENDPOINT_URL", "http://dynamodb-local:8000")
+
         dynamodb = boto3.resource(
             "dynamodb",
-            endpoint_url="http://dynamodb-local:8000",
+            endpoint_url=dynamodb_endpoint_url,
             region_name="us-east-1",
             aws_access_key_id="local",
             aws_secret_access_key="local",
@@ -30,12 +33,12 @@ def setup_local_dynamodb() -> None:
 def _create_virus_scan_table(dynamodb: boto3.resource) -> None:
     """Create the local-virus-scan table if it doesn't already exist"""
     logger.info(
-        "Creating DynamoDB table '%s' if it does not already exist", LOCAL_VIRUS_SCAN_TABLE_NAME
+        "Creating DynamoDB table '%s' if it does not already exist", file_scan_cache_table_name
     )
 
     try:
         table = dynamodb.create_table(
-            TableName=LOCAL_VIRUS_SCAN_TABLE_NAME,
+            TableName=file_scan_cache_table_name,
             KeySchema=[
                 {"AttributeName": "attachment_id", "KeyType": "HASH"},  # Partition key
             ],
@@ -45,9 +48,9 @@ def _create_virus_scan_table(dynamodb: boto3.resource) -> None:
             BillingMode="PAY_PER_REQUEST",  # On-demand billing for local development
         )
         table.wait_until_exists()
-        logger.info("Successfully created table '%s'", LOCAL_VIRUS_SCAN_TABLE_NAME)
+        logger.info("Successfully created table '%s'", file_scan_cache_table_name)
     except dynamodb.meta.client.exceptions.ResourceInUseException:
-        logger.info("Table '%s' already exists", LOCAL_VIRUS_SCAN_TABLE_NAME)
+        logger.info("Table '%s' already exists", file_scan_cache_table_name)
     except Exception as e:
-        logger.error("Error creating table '%s': %s", LOCAL_VIRUS_SCAN_TABLE_NAME, {e})
+        logger.exception("Error creating table '%s': %s", file_scan_cache_table_name, e)
         raise
