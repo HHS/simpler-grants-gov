118aafb03e8fdde4f85171a8693b99ac9c3dfa5b
bruk <brukty@gmail.com>
2026-05-13 18:18:01 -0400

    update task

--- api/src/task/opportunities/build_automatic_opportunities.py
+++ api/src/task/opportunities/build_automatic_opportunities.py
@@ -507,13 +507,13 @@ class BuildAutomaticOpportunitiesTask(Task):
         # Opportunity with static OpportunityID open to both orgs and ind
         self.create_opportunity(
             OpportunityContainer(
-                opportunity_title="TEST-APPLY-ORG-IND-OT02",
-                opportunity_number="TEST-APPLY-ORG-IND-ON02",
+                opportunity_title="TEST-PRINT-ORG-IND-OT01",
+                opportunity_number="TEST-PRINT-ORG-IND-ON01",
                 opportunity_id=uuid.UUID("f21dc67e-84d8-4e2b-ae3e-2d68f83957db"),
             ),
             competitions=[
                 CompetitionContainer(
-                    competition_title="TEST-APPLY-ORG-IND-CT02",
+                    competition_title="TEST-PRINT-ORG-IND-CT01",
                     required_form_ids=[ProjectAbstractSummary_v2_0.form_id],
                     open_to_applicants=[
                         CompetitionOpenToApplicant.INDIVIDUAL,
