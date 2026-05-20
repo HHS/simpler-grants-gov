0787db244008426ffa395efd90e53ffa1a67d9a2
Nathan Stricker <nathan.stricker@focusconsulting.io>
2026-05-20 16:01:31 -0400

    Moved `get_award_recommendation_attachment` to shared location

--- api/src/services/award_recommendations/delete_award_recommendation_attachment.py
+++ api/src/services/award_recommendations/delete_award_recommendation_attachment.py
@@ -1,47 +1,19 @@
 import logging
 import uuid
 
-from sqlalchemy import select
-
 import src.adapters.db as db
-from src.api.route_utils import raise_flask_error
 from src.constants.lookup_constants import AwardRecommendationAuditEvent
-from src.db.models.award_recommendation_models import (
-    AwardRecommendationAttachment,
-    AwardRecommendationAudit,
-)
+from src.db.models.award_recommendation_models import AwardRecommendationAudit
 from src.db.models.user_models import User
 from src.services.award_recommendations.get_award_recommendation import (
     get_award_recommendation_for_update,
 )
+from src.services.award_recommendations.get_award_recommendation_attachment import (
+    get_award_recommendation_attachment,
+)
 from src.util import file_util
 
 logger = logging.getLogger(__name__)
-
-
-def get_attachment_for_update(
-    db_session: db.Session,
-    award_recommendation_id: uuid.UUID,
-    award_recommendation_attachment_id: uuid.UUID,
-) -> AwardRecommendationAttachment:
-    stmt = select(AwardRecommendationAttachment).where(
-        AwardRecommendationAttachment.award_recommendation_attachment_id
-        == award_recommendation_attachment_id,
-        AwardRecommendationAttachment.award_recommendation_id == award_recommendation_id,
-        AwardRecommendationAttachment.is_deleted.isnot(True),
-    )
-
-    attachment = db_session.execute(stmt).scalar_one_or_none()
-    if attachment is None:
-        raise_flask_error(
-            404,
-            message=(
-                f"Could not find Award Recommendation Attachment with ID "
-                f"{award_recommendation_attachment_id}"
-            ),
-        )
-
-    return attachment
 
 
 def delete_award_recommendation_attachment(
@@ -54,7 +26,7 @@ def delete_award_recommendation_attachment(
         db_session, user, award_recommendation_id
     )
 
-    attachment = get_attachment_for_update(
+    attachment = get_award_recommendation_attachment(
         db_session, award_recommendation_id, award_recommendation_attachment_id
     )
 
