c0345ea589a7a18d6c9d7b1c1e6d34cf06d44939
Michael Chouinard <chouinar6@gmail.com>
2026-05-22 10:06:06 -0400

    Fix workflow, adjust smoke test comments, more makefile utility

--- backend/grants_shared/tests/grants_shared/smoke_test.py
+++ backend/grants_shared/tests/grants_shared/smoke_test.py
@@ -1,21 +1,37 @@
 """
 This is a smoke test we run as part of the release process.
+This DOES NOT run as a unit test.
 
 We run it to verify that we're able to successfully import
 the grants_shared code and didn't mess up the build process
 before we push it to PyPi.
+
+
 """
 
-# we want to print before imports in case they fail
-# as the imports are all we're testing here.
-print("Running smoke test.")  # noqa: T201
-from datetime import datetime  # noqa: E402
 
-import grants_shared.util.datetime_util as datetime_util  # noqa: E402
+def main():
+    """
+    Run the smoke test.
 
-now = datetime_util.utcnow()
+    We don't do imports until this function begins running
+    as we're testing the imports work and want to have
+    print output to verify where it is if it fails.
+    """
+    print("Running smoke test.")  # noqa: T201
+    from datetime import datetime
 
-if not isinstance(now, datetime):
-    raise Exception("utcnow from our datetime util was an unexpected type %s" % type(now))
+    import grants_shared.util.datetime_util as datetime_util
 
-print("Smoke test successful.")  # noqa: T201
+    now = datetime_util.utcnow()
+
+    if not isinstance(now, datetime):
+        raise Exception("utcnow from our datetime util was an unexpected type %s" % type(now))
+
+    print("Smoke test successful.")  # noqa: T201
+
+
+# Run inside of a main function so that importing the file
+# doesn't cause it to run, only if it's directly invoked.
+if __name__ == "__main__":
+    main()
