d3881cc684c0f71f88815d255161fcbbca40ecc5
Aoy Simmons <aoy.simmons@gmail.com>
2026-05-27 20:21:14 -0400

    refactor: update import paths for FormFillFieldDefinitions to use absolute paths across multiple fixture files

--- frontend/tests/e2e/apply/fixtures/sf424-field-definitions.ts
+++ frontend/tests/e2e/apply/fixtures/sf424-field-definitions.ts
@@ -1,5 +1,5 @@
 import { FORM_DEFAULTS } from "tests/e2e/utils/forms/form-defaults";
-import { FormFillFieldDefinitions } from "../../utils/types";
+import { FormFillFieldDefinitions } from "tests/e2e/utils/types";
 import { FieldError } from "tests/e2e/utils/forms/verify-form-errors-utils";
 
 export const SF424_FORM_MATCHER =
