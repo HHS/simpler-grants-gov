c3113d4dd77e3e3f72cee76d5d8b3a095308d78e
Doug Schrashun <dougschrashun@navapbc.com>
2026-05-29 15:54:56 -0400

    research guide

--- frontend/src/app/[locale]/(base)/research-participant-guide/_components/ResearchParticipantGuide.test.tsx
+++ frontend/src/app/[locale]/(base)/research-participant-guide/_components/ResearchParticipantGuide.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
-
-import ResearchParticipantGuide from "src/components/research-participant-guide/ResearchParticipantGuide";
+import ResearchParticipantGuide from "src/app/[locale]/(base)/research-participant-guide/_components/ResearchParticipantGuide";
 
 describe("ResearchParticipantGuide Content", () => {
   it("Renders with expected header", () => {
