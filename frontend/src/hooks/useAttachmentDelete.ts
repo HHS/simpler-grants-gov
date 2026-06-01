105af3b549958bf5e5064a0a8b98307796c7451f
Doug Schrashun <dougschrashun@navapbc.com>
2026-06-01 13:53:49 -0400

    path fix

--- frontend/src/hooks/useAttachmentDelete.ts
+++ frontend/src/hooks/useAttachmentDelete.ts
@@ -1,12 +1,13 @@
 "use client";
 
-import {
-  deleteAttachmentAction,
-  DeleteAttachmentActionState,
-} from "src/app/[locale]/(base)/workspace/applications/[applicationId]/form/[appFormId]/actions";
 import { deleteUploadActionsInitialState } from "src/constants/attachment/deleteUploadActionsInitialState";
 
 import { startTransition, useActionState } from "react";
+
+import {
+  deleteAttachmentAction,
+  DeleteAttachmentActionState,
+} from "src/components/application/attachments/actions";
 
 export const useAttachmentDelete = () => {
   const [deleteState, deleteActionFormAction, deletePending] = useActionState(
