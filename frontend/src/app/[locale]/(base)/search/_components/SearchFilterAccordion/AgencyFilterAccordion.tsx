f784804013f8adfdd9ec4eb4feeaef2c0062906a
Doug Schrashun <dougschrashun@navapbc.com>
2026-06-01 13:32:17 -0400

    local reorg search

---
+++ frontend/src/app/[locale]/(base)/search/_components/SearchFilterAccordion/AgencyFilterAccordion.tsx
@@ -0,0 +1,55 @@
+import { AgencyFilterContent } from "src/app/[locale]/(base)/search/_components/Filters/AgencyFilterContent";
+import { BasicSearchFilterAccordion } from "src/app/[locale]/(base)/search/_components/SearchFilterAccordion/SearchFilterAccordion";
+import { RelevantAgencyRecord } from "src/types/search/searchFilterTypes";
+import { SearchAPIResponse } from "src/types/search/searchRequestTypes";
+import { agenciesToSortedAndNestedFilterOptions } from "src/utils/search/filterUtils";
+
+import { getTranslations } from "next-intl/server";
+
+export async function AgencyFilterAccordion({
+  query,
+  agencyOptionsPromise,
+  topLevelQuery,
+  className,
+  selectedStatuses,
+}: {
+  query: Set<string>;
+  agencyOptionsPromise: Promise<[RelevantAgencyRecord[], SearchAPIResponse]>;
+  topLevelQuery: Set<string>;
+  className?: string;
+  selectedStatuses?: string[];
+}) {
+  const t = await getTranslations("Search");
+
+  let allAgencies: RelevantAgencyRecord[] = [];
+  let facetCounts: { [key: string]: number } = {};
+  try {
+    let searchResults: SearchAPIResponse;
+    [allAgencies, searchResults] = await agencyOptionsPromise;
+    facetCounts = searchResults.facet_counts.agency;
+  } catch (e) {
+    // Come back to this to show the user an error
+    console.error("Unable to fetch agencies for filter list", e);
+  }
+
+  const agencyOptions = agenciesToSortedAndNestedFilterOptions(allAgencies);
+
+  return (
+    <BasicSearchFilterAccordion
+      query={query}
+      queryParamKey={"agency"}
+      title={t("accordion.titles.agency")}
+      className={className}
+      contentClassName="force-scrolling"
+    >
+      <AgencyFilterContent
+        query={query}
+        title={t("accordion.titles.agency")}
+        allAgencies={agencyOptions}
+        facetCounts={facetCounts}
+        topLevelQuery={topLevelQuery}
+        selectedStatuses={selectedStatuses}
+      />
+    </BasicSearchFilterAccordion>
+  );
+}
