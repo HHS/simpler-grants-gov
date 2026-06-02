d7a2b7e6aeaa7d4c81136efb97dc3515e1b4d8f6
My Duong <myduong@navapbc.com>
2026-06-02 14:04:57 -0500

    chore: remove the sandbox files

--- api/src/form_schema/forms/__init__.py
+++ api/src/form_schema/forms/__init__.py
@@ -14,9 +14,6 @@ from .project_abstract import ProjectAbstract_v1_2
 from .project_abstract_summary import ProjectAbstractSummary_v2_0
 from .project_narrative_attachment import ProjectNarrativeAttachment_v1_2
 from .project_performance_site_location import ProjectPerformanceSiteLocation_v4_0
-from .sandbox import SANDBOX
-from .sandbox_budget_items import SANDBOX_BUDGET_ITEMS
-from .sandbox_min_max import SANDBOX_FIELDLIST_MIN_MAX
 from .sf424 import SF424_v4_0
 from .sf424a import SF424a_v1_0
 from .sf424b import SF424b_v1_1
@@ -39,9 +36,6 @@ _ALL_FORMS: list[Form] = [
     OtherNarrativeAttachment_v1_2,
     AttachmentForm_v1_2,
     CD511_v1_1,
-    SANDBOX,
-    SANDBOX_BUDGET_ITEMS,
-    SANDBOX_FIELDLIST_MIN_MAX,
     SupplementaryNEHCoverSheet_v3_0,
     GG_LobbyingForm_v1_1,
     EPA_FORM_4700_4_v5_0,
