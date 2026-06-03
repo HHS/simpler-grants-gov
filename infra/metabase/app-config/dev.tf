08c14b11d0aec3fe9f36f9aaf274e4dc3b337073
prasnava <prasanthreddyarikatla@navapbc.com>
2026-06-03 11:36:34 -0400

    increase metabase memory config in dev and staging

--- infra/metabase/app-config/dev.tf
+++ infra/metabase/app-config/dev.tf
@@ -12,9 +12,10 @@ module "dev_config" {
   domain_name  = "data.dev.simpler.grants.gov" # Match naming convention (cert expired, will need renewal)
   enable_https = true                          # Certificate expired - needs to be renewed before enabling HTTPS
 
-  # Scaled down resources for dev environment (Issue #10239)
-  service_cpu    = 512  # Down from 2048
-  service_memory = 1024 # Down from 4096
+  # When we set memory at 1024 metabase is intermittently running into oom.
+  # The recommend min config for memory is at 2GB
+  service_cpu    = 1024
+  service_memory = 2048
 
   service_desired_instance_count = 1
   instance_scaling_min_capacity  = 1
