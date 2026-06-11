c23d9fe8bd58d676a9d3b92d52395f3a31b48d5f
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-06-11 12:16:21 -0400

    Remove unneeded opportunity.is_draft check

--- api/src/services/opportunities_grantor_v1/opportunity_utils.py
+++ api/src/services/opportunities_grantor_v1/opportunity_utils.py
@@ -27,12 +27,8 @@ def validate_opportunity_can_be_updated(opportunity: Opportunity) -> None:
     """Raise a 422 error if the opportunity cannot be updated.
 
     Opportunities can be updated if:
-    - They are in draft state, OR
     - They are published but were created in Simpler Grants
     """
-    if opportunity.is_draft:
-        return
-
     if opportunity.is_simpler_grants_opportunity:
         return
 
