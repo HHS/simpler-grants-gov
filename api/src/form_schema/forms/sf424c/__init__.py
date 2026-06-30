1719934bc7eac43e34f2ca203297793f5c198e73
bruk <brukty@gmail.com>
2026-06-30 14:32:08 -0400

    add SF-424C v2.0 form schema, rules, and XML transform

---
+++ api/src/form_schema/forms/sf424c/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from src.form_schema.forms._loader import load_versioned_form
+
+_mod = load_versioned_form(Path(__file__).parent, "1.0")
+FORM_JSON_SCHEMA = _mod.FORM_JSON_SCHEMA
+FORM_UI_SCHEMA = _mod.FORM_UI_SCHEMA
+FORM_RULE_SCHEMA = _mod.FORM_RULE_SCHEMA
+FORM_XML_TRANSFORM_RULES = _mod.FORM_XML_TRANSFORM_RULES
+SF424c_v2_0 = _mod.SF424c_v2_0
+del _mod
