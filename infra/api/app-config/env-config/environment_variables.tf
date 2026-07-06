4841aae8deaf0b9094ad770586b9600cbb3de340
Kenny Krug <kennykrug@navapbc.com>
2026-07-06 12:50:00 -0400

    [Issue #10836] Define maintenance-mode SSM parameter and wire to ECS task definitions

--- infra/api/app-config/env-config/environment_variables.tf
+++ infra/api/app-config/env-config/environment_variables.tf
@@ -126,6 +126,11 @@ locals {
       secret_store_name = "/api/${var.environment}/enable-simpler-route"
     }
 
+    MAINTENANCE_MODE = {
+      manage_method     = "manual"
+      secret_store_name = "/api/${var.environment}/maintenance-mode"
+    }
+
     SAVE_SOAP_MESSAGES_TO_S3 = {
       manage_method     = "manual"
       secret_store_name = "/api/${var.environment}/save-soap-messages-to-s3"
