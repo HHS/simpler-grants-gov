afe44e532c504e70e6052941400a4973127d545d
nkhazi <nkhazi@horus.net>
2026-07-16 17:13:33 -0400

    chore: fix linting issue

--- frontend/src/types/applyForm/types.ts
+++ frontend/src/types/applyForm/types.ts
@@ -12,7 +12,12 @@ import { HTMLAttributes } from "react";
 export type GeneralRecord = Record<string, unknown>;
 
 export type BroadlyDefinedWidgetValue =
-  string | GeneralRecord | GeneralRecord[] | string[] | number | boolean;
+  | string
+  | GeneralRecord
+  | GeneralRecord[]
+  | string[]
+  | number
+  | boolean;
 
 export type SchemaField = {
   type?: string;
@@ -165,7 +170,8 @@ export type FieldListWidgetProps = {
     rootFormData?: unknown;
     widgetSupport?: {
       validationWarnings?:
-        FormattedFormValidationWarning[] | FormValidationWarning[];
+        | FormattedFormValidationWarning[]
+        | FormValidationWarning[];
       deletedEntryIndexesByFieldListPath?: Record<string, number[]>;
       onFieldListEntryDelete?: (
         fieldListPath: string,
@@ -318,7 +324,9 @@ export type UiSchemaTableMultiField = {
 };
 
 export type UiSchemaField =
-  UiSchemaBasicField | UiSchemaMultiField | UiSchemaTableMultiField;
+  | UiSchemaBasicField
+  | UiSchemaMultiField
+  | UiSchemaTableMultiField;
 
 export interface UiSchemaSection {
   type: "section";
@@ -351,7 +359,13 @@ export type UiSchemaNode = UiSchemaField | UiSchemaSection | UiSchemaFieldList;
 export type UiSchema = UiSchemaNode[];
 
 export type TextTypes =
-  "text" | "email" | "number" | "password" | "search" | "tel" | "url";
+  | "text"
+  | "email"
+  | "number"
+  | "password"
+  | "search"
+  | "tel"
+  | "url";
 
 // extends the WidgetProps type from rjsf for USWDS and this project implementation
 // see https://github.com/rjsf-team/react-jsonschema-form/blob/7395afcdee6aaea128d943dd17e126c4ed301e58/packages/utils/src/types.ts#L898
@@ -359,9 +373,7 @@ export interface UswdsWidgetProps<
   T = unknown,
   S extends StrictRJSFSchema = RJSFSchema,
   F extends FormContextType = never,
->
-  extends
-    GenericObjectType,
+> extends GenericObjectType,
     Pick<
       HTMLAttributes<HTMLElement>,
       Exclude<keyof HTMLAttributes<HTMLElement>, "onBlur" | "onFocus">
@@ -406,7 +418,8 @@ export interface UswdsWidgetProps<
     rootFormData?: unknown;
     widgetSupport?: {
       validationWarnings?:
-        FormattedFormValidationWarning[] | FormValidationWarning[];
+        | FormattedFormValidationWarning[]
+        | FormValidationWarning[];
       deletedEntryIndexesByFieldListPath?: Record<string, number[]>;
       onFieldListEntryDelete?: (
         fieldListPath: string,
