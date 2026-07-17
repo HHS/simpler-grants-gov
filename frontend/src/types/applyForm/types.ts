650b3d0300536b80bdeab237840057b606a7fa4e
nkhazi <nkhazi@horus.net>
2026-07-17 12:13:57 -0400

    chore: fix linting issue

--- frontend/src/types/applyForm/types.ts
+++ frontend/src/types/applyForm/types.ts
@@ -12,12 +12,7 @@ import { HTMLAttributes } from "react";
 export type GeneralRecord = Record<string, unknown>;
 
 export type BroadlyDefinedWidgetValue =
-  | string
-  | GeneralRecord
-  | GeneralRecord[]
-  | string[]
-  | number
-  | boolean;
+  string | GeneralRecord | GeneralRecord[] | string[] | number | boolean;
 
 export type SchemaField = {
   type?: string;
@@ -170,8 +165,7 @@ export type FieldListWidgetProps = {
     rootFormData?: unknown;
     widgetSupport?: {
       validationWarnings?:
-        | FormattedFormValidationWarning[]
-        | FormValidationWarning[];
+        FormattedFormValidationWarning[] | FormValidationWarning[];
       deletedEntryIndexesByFieldListPath?: Record<string, number[]>;
       onFieldListEntryDelete?: (
         fieldListPath: string,
@@ -214,11 +208,7 @@ export type FieldListGroupItem = {
 export type UiSchemaTableCellType = "input" | "readOnly" | "plainText";
 
 export type UiSchemaTableNumberFormat =
-  | "integer"
-  | "decimal"
-  | "currency"
-  | "dollar"
-  | "percentage";
+  "integer" | "decimal" | "currency" | "dollar" | "percentage";
 
 export type UiSchemaTableColumn = {
   columnHeader: string;
@@ -324,9 +314,7 @@ export type UiSchemaTableMultiField = {
 };
 
 export type UiSchemaField =
-  | UiSchemaBasicField
-  | UiSchemaMultiField
-  | UiSchemaTableMultiField;
+  UiSchemaBasicField | UiSchemaMultiField | UiSchemaTableMultiField;
 
 export interface UiSchemaSection {
   type: "section";
@@ -359,13 +347,7 @@ export type UiSchemaNode = UiSchemaField | UiSchemaSection | UiSchemaFieldList;
 export type UiSchema = UiSchemaNode[];
 
 export type TextTypes =
-  | "text"
-  | "email"
-  | "number"
-  | "password"
-  | "search"
-  | "tel"
-  | "url";
+  "text" | "email" | "number" | "password" | "search" | "tel" | "url";
 
 // extends the WidgetProps type from rjsf for USWDS and this project implementation
 // see https://github.com/rjsf-team/react-jsonschema-form/blob/7395afcdee6aaea128d943dd17e126c4ed301e58/packages/utils/src/types.ts#L898
@@ -373,7 +355,9 @@ export interface UswdsWidgetProps<
   T = unknown,
   S extends StrictRJSFSchema = RJSFSchema,
   F extends FormContextType = never,
-> extends GenericObjectType,
+>
+  extends
+    GenericObjectType,
     Pick<
       HTMLAttributes<HTMLElement>,
       Exclude<keyof HTMLAttributes<HTMLElement>, "onBlur" | "onFocus">
@@ -418,8 +402,7 @@ export interface UswdsWidgetProps<
     rootFormData?: unknown;
     widgetSupport?: {
       validationWarnings?:
-        | FormattedFormValidationWarning[]
-        | FormValidationWarning[];
+        FormattedFormValidationWarning[] | FormValidationWarning[];
       deletedEntryIndexesByFieldListPath?: Record<string, number[]>;
       onFieldListEntryDelete?: (
         fieldListPath: string,
