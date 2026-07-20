89b72b4e0091bf0a6f95131b4cb8d77c18768bd6
doug-s-nava <92806979+doug-s-nava@users.noreply.github.com>
2026-07-20 10:26:07 -0400

    [Issue #9602] make formDataToJson flexible to be used outside of apply form context (#11490)
    In the interest of allowing all forms in the application to benefit from the FormData -> JSON functionality built to support the apply form system, this introduces some changes to make the functionality more universal.
    
    Note that with this change the functionality is not yet being used outside of the apply form system, but will be introduced elsewhere in a subsequent PR.
    
    Changes made here include:
    
    * making the nesting delimiter fully customizable
    * moving non apply form specific functionality to a non apply form specific utils folder
    * allowing customization of default "empty field" value
    * note that this is necessary due to API implementation details that result in all non-apply related forms accepting `null` for required fields, but apply forms requiring fields to be left out of a payload (or set as `undefined` which is effectively the same thing) in order to properly trigger required validations
    
    Also some more `quality of life` changes
    * adding some code comments where I ran into questions in the existing apply form flow
    * making path delimiters more explicit with constants
    * in order to support this I had to build out a `RegExp.escape` polyfill

--- frontend/src/utils/applyForm/getFieldConfig.ts
+++ frontend/src/utils/applyForm/getFieldConfig.ts
@@ -14,13 +14,12 @@ import {
   UswdsWidgetProps,
   WidgetTypes,
 } from "src/types/applyForm/types";
-
 import {
   getByPointer,
-  getFieldNameForHtml,
   getFieldPathFromHtml,
-  getFieldSchema,
-} from "./applyFormUtils";
+} from "src/utils/formData/formDataUtils";
+
+import { getFieldNameForHtml, getFieldSchema } from "./applyFormUtils";
 
 type WidgetOptions = NonNullable<UswdsWidgetProps["options"]>;
 
@@ -225,6 +224,9 @@ export const getWarningsForField = ({
   });
 };
 
+// returns either the final slash delimited path segment from definition
+// or the "title" attribute from the field schema (with dashes for whitespace)
+// or "untitled"
 export const getNameFromDef = ({
   definition,
   schema,
@@ -251,7 +253,7 @@ const getBasicFieldInfo = ({
 }: {
   uiFieldObject: UiSchemaField;
   formSchema: RJSFSchema;
-  formData: object;
+  formData: object; // this will be either the saved form JSON, or an empty FormData object (?s)
   errors: FormattedFormValidationWarning[] | null;
 }): FieldInfo<string> => {
   const { schema } = uiFieldObject;
@@ -266,6 +268,19 @@ const getBasicFieldInfo = ({
     schema,
     formSchema,
   }) as RJSFSchema;
+
+  /*
+    this section of code formats the field's name and path for various use cases
+    by way of example, where `definition` = `/properties/applicant/properties/state`
+      * `fieldName` = `state`
+        * used as a fallback ID in case htmlFieldName can't be computed
+        * seems like it'd be used for something else, like a label, but it's not
+      * `htmlFieldName` = `applicant--state`
+        * used for referencing data values within HTML form's FormData
+        * used for defining ID on each widget
+      * `formDataPath` =  `/applicant/state`
+        * used for retrieving data from saved form data JSON
+  */
   const fieldName = getNameFromDef({ definition, schema });
   const htmlFieldName = getFieldNameForHtml({ definition, schema });
   const formDataPath = getFieldPathFromHtml(htmlFieldName);
