6716bfd974b8921c170719eda9324295f8924f1f
David Huang <dhuang316@gatech.edu>
2026-07-27 23:56:09 -0400

    Cleanup foreign table definitions we no longer use

--- api/src/data_migration/transformation/transform_constants.py
+++ api/src/data_migration/transformation/transform_constants.py
@@ -4,21 +4,15 @@ from typing import TypeVar
 from src.db.models.api_schema_table import ApiSchemaTable
 from src.db.models.staging.forecast import (
     TapplicanttypesForecast,
-    TapplicanttypesForecastHist,
     Tforecast,
     TfundactcatForecast,
-    TfundactcatForecastHist,
     TfundinstrForecast,
-    TfundinstrForecastHist,
 )
 from src.db.models.staging.staging_base import StagingParamMixin
 from src.db.models.staging.synopsis import (
     TapplicanttypesSynopsis,
-    TapplicanttypesSynopsisHist,
     TfundactcatSynopsis,
-    TfundactcatSynopsisHist,
     TfundinstrSynopsis,
-    TfundinstrSynopsisHist,
     Tsynopsis,
 )
 
@@ -61,17 +55,15 @@ type SourceSummary = Tforecast | Tsynopsis
 
 type SourceApplicantType = (
     TapplicanttypesForecast
-    | TapplicanttypesForecastHist
     | TapplicanttypesSynopsis
-    | TapplicanttypesSynopsisHist
 )
 
 type SourceFundingCategory = (
-    TfundactcatForecast | TfundactcatForecastHist | TfundactcatSynopsis | TfundactcatSynopsisHist
+    TfundactcatForecast | TfundactcatSynopsis
 )
 
 type SourceFundingInstrument = (
-    TfundinstrForecastHist | TfundinstrForecast | TfundinstrSynopsisHist | TfundinstrSynopsis
+    TfundinstrForecast | TfundinstrSynopsis
 )
 
 type SourceAny = SourceApplicantType | SourceFundingCategory | SourceFundingInstrument
--- api/src/db/models/foreign/synopsis.py
+++ api/src/db/models/foreign/synopsis.py
@@ -24,17 +24,6 @@ class Tsynopsis(foreignbase.ForeignBase, synopsis_mixin.TsynopsisMixin):
     )
 
 
-class TsynopsisHist(foreignbase.ForeignBase, synopsis_mixin.TsynopsisHistMixin):
-    __tablename__ = "tsynopsis_hist"
-
-    opportunity: Mapped[Topportunity | None] = relationship(
-        Topportunity,
-        primaryjoin=lambda: TsynopsisHist.opportunity_id == foreign(Topportunity.opportunity_id),
-        uselist=False,
-        overlaps="opportunity",
-    )
-
-
 class TapplicanttypesSynopsis(foreignbase.ForeignBase, synopsis_mixin.TapplicanttypesSynopsisMixin):
     __tablename__ = "tapplicanttypes_synopsis"
 
@@ -42,22 +31,6 @@ class TapplicanttypesSynopsis(foreignbase.ForeignBase, synopsis_mixin.Tapplicant
         Tsynopsis,
         primaryjoin=lambda: TapplicanttypesSynopsis.opportunity_id
         == foreign(Tsynopsis.opportunity_id),
-        uselist=False,
-        overlaps="synopsis",
-    )
-
-
-class TapplicanttypesSynopsisHist(
-    foreignbase.ForeignBase, synopsis_mixin.TapplicanttypesSynopsisHistMixin
-):
-    __tablename__ = "tapplicanttypes_synopsis_hist"
-
-    synopsis: Mapped[TsynopsisHist | None] = relationship(
-        TsynopsisHist,
-        primaryjoin=lambda: and_(
-            TapplicanttypesSynopsisHist.opportunity_id == foreign(TsynopsisHist.opportunity_id),
-            TapplicanttypesSynopsisHist.revision_number == foreign(TsynopsisHist.revision_number),
-        ),
         uselist=False,
         overlaps="synopsis",
     )
@@ -74,40 +47,12 @@ class TfundactcatSynopsis(foreignbase.ForeignBase, synopsis_mixin.TfundactcatSyn
     )
 
 
-class TfundactcatSynopsisHist(foreignbase.ForeignBase, synopsis_mixin.TfundactcatSynopsisHistMixin):
-    __tablename__ = "tfundactcat_synopsis_hist"
-
-    synopsis: Mapped[TsynopsisHist | None] = relationship(
-        TsynopsisHist,
-        primaryjoin=lambda: and_(
-            TfundactcatSynopsisHist.opportunity_id == foreign(TsynopsisHist.opportunity_id),
-            TfundactcatSynopsisHist.revision_number == foreign(TsynopsisHist.revision_number),
-        ),
-        uselist=False,
-        overlaps="synopsis",
-    )
-
-
 class TfundinstrSynopsis(foreignbase.ForeignBase, synopsis_mixin.TfundinstrSynopsisMixin):
     __tablename__ = "tfundinstr_synopsis"
 
     synopsis: Mapped[Tsynopsis | None] = relationship(
         Tsynopsis,
         primaryjoin=lambda: TfundinstrSynopsis.opportunity_id == foreign(Tsynopsis.opportunity_id),
-        uselist=False,
-        overlaps="synopsis",
-    )
-
-
-class TfundinstrSynopsisHist(foreignbase.ForeignBase, synopsis_mixin.TfundinstrSynopsisHistMixin):
-    __tablename__ = "tfundinstr_synopsis_hist"
-
-    synopsis: Mapped[TsynopsisHist | None] = relationship(
-        TsynopsisHist,
-        primaryjoin=lambda: and_(
-            TfundinstrSynopsisHist.opportunity_id == foreign(TsynopsisHist.opportunity_id),
-            TfundinstrSynopsisHist.revision_number == foreign(TsynopsisHist.revision_number),
-        ),
         uselist=False,
         overlaps="synopsis",
     )
