a3d61a290dea286c304c73650c3c57bf4de99b3c
Glen Blosser <glenblosser@navapbc.com>
2026-07-30 11:34:02 -0500

    Cleanup

--- frontend/src/app/[locale]/(base)/grantor/opportunity/[id]/competition/_components/FormSelectModal.tsx
+++ frontend/src/app/[locale]/(base)/grantor/opportunity/[id]/competition/_components/FormSelectModal.tsx
@@ -111,174 +111,168 @@ export const FormSelectModal = ({
     formModalRef.current?.toggleModal();
   };
   return (
-    <>
-      <SimplerModal
-        modalId={"piv-required-modal"}
-        modalRef={formModalRef}
-        titleText={t("title")}
-        onClose={handleCleanup}
-        className="text-wrap maxw-tablet-lg"
-      >
-        <div className="form-modal-header">{t("heading")}</div>
-        <div style={{ padding: 0, overflowY: "scroll", height: "60vh" }}>
-          <GridContainer>
-            <Grid row className="form-modal-select-all">
-              <Grid col={1} className="centered-content">
-                <input
-                  type="checkbox"
-                  checked={
-                    Object.keys(selectedForms).length >= forms.length - 1
-                  }
-                  style={checkboxStyle}
-                  onChange={() => {
-                    toggleSelectAll();
-                  }}
-                />
-              </Grid>
-              <Grid col={11}>{t("selectAll")}</Grid>
+    <SimplerModal
+      modalId={"piv-required-modal"}
+      modalRef={formModalRef}
+      titleText={t("title")}
+      onClose={handleCleanup}
+      className="text-wrap maxw-tablet-lg"
+    >
+      <div className="form-modal-header">{t("heading")}</div>
+      <div style={{ padding: 0, overflowY: "scroll", height: "60vh" }}>
+        <GridContainer>
+          <Grid row className="form-modal-select-all">
+            <Grid col={1} className="centered-content">
+              <input
+                type="checkbox"
+                checked={Object.keys(selectedForms).length >= forms.length - 1}
+                style={checkboxStyle}
+                onChange={() => {
+                  toggleSelectAll();
+                }}
+              />
             </Grid>
-            {tableForms.map((form, index) => {
-              const alwaysRequired = !!alwaysRequiredForms[form.form_id];
-              return (
-                <Grid
-                  row
-                  className={`form-modal-rows ${alwaysRequired ? "form-modal-always-required" : "form-modal-optional"}`}
-                  key={`forms-table-row-${index}`}
-                >
-                  <Grid col={1} className="centered-content">
-                    {!alwaysRequired ? (
-                      <input
-                        type="checkbox"
-                        checked={
-                          typeof selectedForms[form.form_id] !== "undefined"
+            <Grid col={11}>{t("selectAll")}</Grid>
+          </Grid>
+          {tableForms.map((form, index) => {
+            const alwaysRequired = !!alwaysRequiredForms[form.form_id];
+            return (
+              <Grid
+                row
+                className={`form-modal-rows ${alwaysRequired ? "form-modal-always-required" : "form-modal-optional"}`}
+                key={`forms-table-row-${index}`}
+              >
+                <Grid col={1} className="centered-content">
+                  {!alwaysRequired ? (
+                    <input
+                      type="checkbox"
+                      checked={
+                        typeof selectedForms[form.form_id] !== "undefined"
+                      }
+                      style={checkboxStyle}
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          form.isSelected = true;
+                          setTableForms(tableForms);
+                          setSelectedForms({
+                            ...selectedForms,
+                            [form.form_id]: form.isRequired,
+                          });
+                        } else {
+                          form.isSelected = false;
+                          setTableForms(tableForms);
+                          setSelectedForms((current) => {
+                            const { [form.form_id]: _id, ...rest } = current;
+                            return rest;
+                          });
                         }
-                        style={checkboxStyle}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            form.isSelected = true;
-                            setTableForms(tableForms);
-                            setSelectedForms({
-                              ...selectedForms,
-                              [form.form_id]: form.isRequired,
-                            });
-                          } else {
-                            form.isSelected = false;
-                            setTableForms(tableForms);
-                            setSelectedForms((current) => {
-                              const { [form.form_id]: _id, ...rest } = current;
-                              return rest;
-                            });
-                          }
-                        }}
-                      />
+                      }}
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </Grid>
+                <Grid col={5} style={{ fontWeight: "bold" }}>
+                  <div
+                    style={{
+                      fontSize: 14,
+                    }}
+                  >
+                    {form.short_name.split("_")[0].substring(0, 25)}{" "}
+                    <span
+                      style={{
+                        color: "#76766A",
+                        fontWeight: "normal",
+                      }}
+                    >
+                      v{form.current_version.major_version}.
+                      {form.current_version.minor_version}
+                    </span>
+                    {alwaysRequired ? (
+                      <span className="always-required-label">
+                        Always Required
+                      </span>
                     ) : (
                       <></>
                     )}
-                  </Grid>
-                  <Grid col={5} style={{ fontWeight: "bold" }}>
-                    <div
-                      style={{
-                        fontSize: 14,
-                      }}
-                    >
-                      {form.short_name.split("_")[0].substring(0, 25)}{" "}
-                      <span
-                        style={{
-                          color: "#76766A",
-                          fontWeight: "normal",
-                        }}
-                      >
-                        v{form.current_version.major_version}.
-                        {form.current_version.minor_version}
-                      </span>
-                      {alwaysRequired ? (
-                        <span className="always-required-label">
-                          Always Required
-                        </span>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                    <div style={{ fontSize: 16 }}>
-                      {form.name.split(" (")[0]}
-                    </div>
-                  </Grid>
-                  <Grid col={6} className="centered-content">
-                    {alwaysRequired ? (
-                      <span style={{ color: "#286846" }}>
-                        <span style={{ position: "relative", top: 5 }}>
-                          <USWDSIcon
-                            name="check_circle_outline"
-                            className="usa-icon usa-icon--size-3"
-                          />
-                        </span>
-                        <span>Auto added</span>
-                      </span>
-                    ) : (
-                      <span>
-                        <Radio
-                          id={`form-required-yes-${index}`}
-                          name={`is_form_required_${index}`}
-                          label={t("requiredStates.required")}
-                          value="true"
-                          checked={form.isRequired}
-                          disabled={
-                            typeof selectedForms[form.form_id] === "undefined"
-                          }
-                          onChange={(e) => {
-                            form.isRequired = e.target.checked;
-                            setTableForms(tableForms);
-                            setSelectedForms({
-                              ...selectedForms,
-                              [form.form_id]: e.target.checked,
-                            });
-                          }}
-                        />
-                        <Radio
-                          id={`form-required-no-${index}`}
-                          name={`is_form_required_${index}`}
-                          label={t("requiredStates.conditional")}
-                          value="false"
-                          checked={!form.isRequired}
-                          disabled={
-                            typeof selectedForms[form.form_id] === "undefined"
-                          }
-                          onChange={(e) => {
-                            form.isRequired = !e.target.checked;
-                            setTableForms(tableForms);
-                            setSelectedForms({
-                              ...selectedForms,
-                              [form.form_id]: !e.target.checked,
-                            });
-                          }}
-                        />
-                      </span>
-                    )}
-                  </Grid>
+                  </div>
+                  <div style={{ fontSize: 16 }}>{form.name.split(" (")[0]}</div>
                 </Grid>
-              );
-            })}
-          </GridContainer>
-        </div>
-        <ModalFooter>
-          <Button
-            type="button"
-            onClick={handleSubmit}
-            data-testid="save-search-button"
-          >
-            {t("buttons.save")}
-          </Button>
-          <ModalToggleButton
-            modalRef={formModalRef}
-            closer
-            unstyled
-            className="padding-105 text-center"
-            onClick={onClose}
-          >
-            {t("buttons.cancel")}
-          </ModalToggleButton>
-        </ModalFooter>
-      </SimplerModal>
-    </>
+                <Grid col={6} className="centered-content">
+                  {alwaysRequired ? (
+                    <span style={{ color: "#286846" }}>
+                      <span style={{ position: "relative", top: 5 }}>
+                        <USWDSIcon
+                          name="check_circle_outline"
+                          className="usa-icon usa-icon--size-3"
+                        />
+                      </span>
+                      <span>Auto added</span>
+                    </span>
+                  ) : (
+                    <span>
+                      <Radio
+                        id={`form-required-yes-${index}`}
+                        name={`is_form_required_${index}`}
+                        label={t("requiredStates.required")}
+                        value="true"
+                        checked={form.isRequired}
+                        disabled={
+                          typeof selectedForms[form.form_id] === "undefined"
+                        }
+                        onChange={(e) => {
+                          form.isRequired = e.target.checked;
+                          setTableForms(tableForms);
+                          setSelectedForms({
+                            ...selectedForms,
+                            [form.form_id]: e.target.checked,
+                          });
+                        }}
+                      />
+                      <Radio
+                        id={`form-required-no-${index}`}
+                        name={`is_form_required_${index}`}
+                        label={t("requiredStates.conditional")}
+                        value="false"
+                        checked={!form.isRequired}
+                        disabled={
+                          typeof selectedForms[form.form_id] === "undefined"
+                        }
+                        onChange={(e) => {
+                          form.isRequired = !e.target.checked;
+                          setTableForms(tableForms);
+                          setSelectedForms({
+                            ...selectedForms,
+                            [form.form_id]: !e.target.checked,
+                          });
+                        }}
+                      />
+                    </span>
+                  )}
+                </Grid>
+              </Grid>
+            );
+          })}
+        </GridContainer>
+      </div>
+      <ModalFooter>
+        <Button
+          type="button"
+          onClick={handleSubmit}
+          data-testid="save-search-button"
+        >
+          {t("buttons.save")}
+        </Button>
+        <ModalToggleButton
+          modalRef={formModalRef}
+          closer
+          unstyled
+          className="padding-105 text-center"
+          onClick={onClose}
+        >
+          {t("buttons.cancel")}
+        </ModalToggleButton>
+      </ModalFooter>
+    </SimplerModal>
   );
 };
