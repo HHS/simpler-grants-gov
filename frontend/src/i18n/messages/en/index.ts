f367e95423809f061d237509b98bc9f6f2f349fc
glenblosser-nava <glenblosser@navapbc.com>
2026-08-03 09:02:19 -0500

    Gblosser/11014 create forms modal (#11726)
    ## Summary
    Fixes for #11014 
    
    ## Changes proposed
    
    Creates a modal for adding/removing forms, and toggling between required
    and conditionally required for those forms.
    
    ## Context for reviewers
    
    This is a precursor to #11013
    The "Open Modal Test" button is included only for review purposes, and
    will be removed prior to merge. The actual modal button will be on the
    page after 11013 is completed.
    Furthermore, we are not yet saving permanent changes. This ticket will
    only save the forms locally, as we are not creating a competition to
    save to yet.
    
    ## Validation steps
    
    - [ ] Navigate to the Application Package page for an opportunity, and
    click "Open Modal Test"
    - [ ] You can select and unselect forms. Required/Conditionally required
    is only editable while the form is selected
    - [ ] Closing the modal clears any changes
    - [ ] Clicking "Save" will close the modal and save the changes
    - [ ] Changes that have been saved persist when re-opening the modal

--- frontend/src/i18n/messages/en/index.ts
+++ frontend/src/i18n/messages/en/index.ts
@@ -901,6 +901,21 @@ export const messages = {
       "You must sign in with your government employee ID. Make sure you've set up your Personal Identity Verification (PIV) or Common Access Card (CAC) as a two-factor authentication method.",
     button: "Sign in using PIV/CAC",
   },
+  FormSelectModal: {
+    title: "Form Library",
+    heading: "Select Forms",
+    selectAll: "Select all",
+    buttons: {
+      cancel: "Cancel",
+      save: "Save",
+    },
+    requiredStates: {
+      required: "Required",
+      conditional: "Conditionally Required",
+      auto: "Auto added",
+      always: "Always required",
+    },
+  },
   Footer: {
     agencyName: "Grants.gov",
     agencyContactCenter: "Grants.gov Program Management Office",
