a024dcf95d29e062f26a1fd7249c9c6cb60be9c1
Aaron Brown <aaronbrown@navapbc.com>
2026-08-07 08:49:19 -0700

    Revert "insert my migration script into proper place"
    This reverts commit a68ac537f3ad577f6d6f2c417a3e82ad715a61bf.

--- api/src/db/migrations/versions/2026_08_05_add_tagline_and_purpose_statement_to_.py
+++ api/src/db/migrations/versions/2026_08_05_add_tagline_and_purpose_statement_to_.py
@@ -1,7 +1,7 @@
 """add tagline and purpose_statement to opportunity
 
 Revision ID: 15bfdec59153
-Revises: 956a32b92993
+Revises: 0b3e125c6bac
 Create Date: 2026-08-05 16:35:51.131047
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "15bfdec59153"
-down_revision = "956a32b92993"
+down_revision = "0b3e125c6bac"
 branch_labels = None
 depends_on = None
 
