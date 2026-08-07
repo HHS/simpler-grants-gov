4919d9cf8b035094dde6bf08fe188ed72f6dead8
prasnava <prasanthreddyarikatla@navapbc.com>
2026-08-07 13:34:47 -0400

    wip

--- infra/project-config/networks.tf
+++ infra/project-config/networks.tf
@@ -130,6 +130,8 @@ locals {
       second_octet                 = 4               # The second octet of the VPC CIDR block (10.4.0.0/20)
       grants_gov_oracle_cidr_block = "10.220.0.0/16" # MicroHealth managed CIDR block where the dev origin Oracle database for Grants.gov is located
 
+      dms_transit_gateway_id = "tgw-03ae893b3141f907a" # reach the Oracle CIDR via the transit gateway instead of the VPC peering
+
       domain_config = {
         manage_dns  = false
         hosted_zone = null # DNS is managed externally; set once a Route53 hosted zone is created
