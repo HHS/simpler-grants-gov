e00f90916c3ddb4c91d7f0eadbeba0a784fa1edf
Daniel Ghazvini <daniel.ghazvini@reisystems.com>
2026-08-10 12:59:14 -0400

    Fixed albemic reference order

--- api/src/db/migrations/versions/2026_08_10_added_is_delete_flag_to_opportunity_.py
+++ api/src/db/migrations/versions/2026_08_10_added_is_delete_flag_to_opportunity_.py
@@ -1,7 +1,7 @@
 """Added is_delete flag to Opportunity table
 
 Revision ID: 65431e8d567a
-Revises: 15bfdec59153
+Revises: 956a32b92993
 Create Date: 2026-08-10 14:55:03.468115
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "65431e8d567a"
-down_revision = "15bfdec59153"
+down_revision = "956a32b92993"
 branch_labels = None
 depends_on = None
 
