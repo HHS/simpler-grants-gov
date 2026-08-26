0550c304643ecc38878d7d45fa9835e546f75f43
David Dudas <david.dudas@intuitial.com>
2026-08-25 17:21:59 -0700

    add v2 dataset and rm v1 dataset

--- analytics/src/analytics/integrations/metabase/sql/133-ETL_Metrics/852-ETL_Data_Quality_Deliverable_History_Age.sql
+++
@@ -1,5 +0,0 @@
-SELECT NOW() - MAX(t_created) AS deliverable_time_since_last_update
-FROM gh_deliverable_history
-WHERE d_effective =
-    (SELECT MAX(d_effective)
-     FROM gh_deliverable_history)
