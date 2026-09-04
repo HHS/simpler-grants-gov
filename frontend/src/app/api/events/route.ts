4c5feb50a145da5bae64c45cfb47a8165c0d7e73
Freddie Yebra <freddieyebra@navapbc.com>
2026-09-04 13:29:37 -0500

    Add comment to route

--- frontend/src/app/api/events/route.ts
+++ frontend/src/app/api/events/route.ts
@@ -1,6 +1,14 @@
 import { logUserEvent } from "src/services/event/logUserEvent";
 import { UserEvent } from "src/types/userEventTypes";
 
+/**
+ * This is an endpoint for taking in user events and logging to
+ * new relic with a correlation id.
+ *
+ * Since this is only acting as a fire and forget route, this route is
+ * intentionally not wrapped with the respondWithTraceAndLogs similar
+ * to other routes.
+ */
 export const POST = async (request: Request) => {
   try {
     const requestData = (await request.json()) as UserEvent;
