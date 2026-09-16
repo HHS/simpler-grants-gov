e110401a5ea48965178d790ea90504e899a5f491
Kenny Krug <kennykrug@navapbc.com>
2026-09-16 13:58:25 -0400

    [Issue #12424] Route changes cause very slight page scroll

--- frontend/src/components/core/RouteFocusManager.tsx
+++ frontend/src/components/core/RouteFocusManager.tsx
@@ -22,7 +22,8 @@ export default function RouteFocusManager({
     const mainContentElement = document.getElementById("main-content");
 
     if (mainContentElement instanceof HTMLElement) {
-      mainContentElement.focus();
+      // mainContentElement.focus();
+      mainContentElement.focus({ preventScroll: true });
     }
   }, [pathname]);
 
