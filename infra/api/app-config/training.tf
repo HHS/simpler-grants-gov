7bdf6cd95b9fe6ffda671900bfda28f8999ccdeb
prasnava <prasanthreddyarikatla@navapbc.com>
2026-09-19 11:49:51 -0400

    [unticketed]: enable ssl and api gateway domain in infra-training (#12436)
    ## Summary
    
    Enabled ssl for infra-training. 
    
    ## Validation steps
    
    Deployed from local.

--- infra/api/app-config/training.tf
+++ infra/api/app-config/training.tf
@@ -8,6 +8,7 @@ module "training_config" {
   domain_name                       = "api.training.simpler.grants.gov"
   s3_cdn_domain_name                = "files.training.simpler.grants.gov"
   secondary_domain_names            = ["alb.training.simpler.grants.gov"]
+  enable_api_gateway_domain_name    = false
   scanner_callback_domain_name      = "alb.training.simpler.grants.gov"
   enable_secure_alb                 = true
   mtls_domain_name                  = "soap.training.simpler.grants.gov"
